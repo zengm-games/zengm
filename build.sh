50756fcc21eff0e0d97ca7f1bdb6468f731175b6
jdscheff <jdscheff@gmail.com>
2008-06-27 20:07:21 +0000

    git-svn-id: http://basketball-gm.googlecode.com/svn/trunk@22 a33a9aad-4c50-0410-a2a9-357a5b962ba0

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 gtk-builder-convert basketball_gm.glade basketball_gm.xml
 rm -rf database.sqlite
-rm -f box_scores/*
-#sqlite3 -init schema.sql database.sqlite
+sqlite3 -init data/tables.sql database.sqlite
 sqlite3 -init data/players.sql database.sqlite
 sqlite3 -init data/teams.sql database.sqlite
-sqlite3 -init data/game.sql database.sqlite

965e8bcbb244b432096be98c821dcffad7525d96
Jeremy Scheff <jdscheff@gmail.com>
2011-07-10 13:11:01 -0400

    Removed unneeded import

--- bbgm/common.py
+++ bbgm/common.py
@@ -1,6 +1,5 @@
 import gtk
 import os
-import shutil
 import sqlite3
 
 # When DEBUG is True:
@@ -13,7 +12,7 @@ DEBUG = True
 # If True, will attempt to use numpy for random numbers, which is about 10%
 # faster. If numpy is not available, it will automatically fall back on
 # Python's random module. So, this should only be set to False for debugging.
-NUMPY = True
+NUMPY = False
 
 TEAMS = range(30)  # If team id's aren't consecutive integers starting with 0, then some things will break
 SEASON_LENGTH = 82  # If this isn't 82 the scheduling gets fucked up.  Only set it lower for debugging (faster seasons)

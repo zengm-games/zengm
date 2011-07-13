2c7c99ebdfec04ee70fb31e86649e77daff81763
Jeremy Scheff <jdscheff@gmail.com>
2011-07-13 00:10:04 -0400

    Beginnings of setup.py... but it doesn't work yet.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from glob import glob
+
+setup(name='Basketball GM',
+      version='0.1+',
+      description='A basketball simulation game. Make trades, set rosters, draft players, and try to build the next dynasty.',
+      license='AGPLv3',
+      url='https://github.com/jdscheff/basketball-gm',
+      maintainer='Jeremy Scheff',
+      maintainer_email='jdscheff@gmail.com',
+      packages=['bbgm', 'bbgm.core', 'bbgm.views'],
+      data_files=[
+    ('share/basketball-gm/data', glob("data/*.*")),
+    ('share/basketball-gm/ui', glob("ui/*.*")),
+    # Icon stuff
+    ],
+      scripts=['bin/basketball-gm']
+     )
+

9a7a6eecb7eb41dea157dcaf723ed0d676fb17af
Jeremy Scheff <jdscheff@gmail.com>
2012-04-13 18:50:59 -0400

    More contract negotiation work

--- bbgm/core/negotiation.py
+++ bbgm/core/negotiation.py
@@ -1,5 +1,6 @@
 from flask import g
 
+from bbgm.core import play_menu
 from bbgm.util import lock, logger
 
 def new(player_id):
@@ -12,21 +13,31 @@ def new(player_id):
     """
     logger.debug('Trying to start new contract negotiation with player %d' % (player_id,))
 
-    # Check if over roster limit
     g.db.execute('SELECT COUNT(*) FROM %s_player_attributes WHERE team_id = %s', (g.league_id, g.user_team_id))
     num_players_on_roster, = g.db.fetchone()
     if num_players_on_roster >= 15:
         return "Your roster is full. Before you can sign a free agent, you'll have to buy out or release one of your current players.";
-
-    # Check if state is locked
     if not lock.can_start_negotiation():
         return "You cannot initiate a new negotiaion while game simulation is in progress, a previous contract negotiation is in process, or a trade is in progress.";
-
-    # Check if player_id is a free agent
     g.db.execute('SELECT team_id FROM %s_player_attributes WHERE player_id = %s', (g.league_id, player_id))
-    team_id, = g.db.fetchone()
-    if team_id != -1:
-        return "Player %d is not a free agent." % (player_id,);
+    if g.db.rowcount:
+        team_id, = g.db.fetchone()
+        if team_id != -1:
+            return "Player %d is not a free agent." % (player_id,)
+    else:
+        return "Player %d does not exist." % (player_id,)
+
+    # Initial player proposal
+    g.db.execute('SELECT contract_amount*(1+free_agent_times_asked/10), contract_expiration FROM %s_player_attributes WHERE player_id = %s', (g.league_id, player_id))
+    player_amount, expiration = g.db.fetchone()
+    player_years = expiration - g.season
+    # Adjust to account for in-season signings
+    if g.phase <= 2:
+        player_years += 1
+
+    g.db.execute('INSERT INTO %s_negotiation (player_id, team_amount, team_years, player_amount, player_years, num_offers_made) VALUES (%s, %s, %s, %s, %s, 0)', (g.league_id, player_id, player_amount, player_years, player_amount, player_years))
+    lock.set_negotiation_in_progress(True)
+    play_menu.set_status('Contract negotiation in progress')
 
 def get_status(player_id):
     g.db.execute('SELECT team_amount, team_years, player_amount, player_years FROM %s_negotiation WHERE player_id = %s', (g.league_id, player_id))

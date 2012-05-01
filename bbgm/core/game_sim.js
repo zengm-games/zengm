3a9a6584e57fae467b59b81ddfae7ab9914dc3db
Jeremy Scheff <jdscheff@gmail.com>
2012-05-01 18:02:30 -0400

    More JavaScript work

--- bbgm/core/game_sim.js
+++ bbgm/core/game_sim.js
@@ -55,7 +55,7 @@ function GameSim(team1, team2) {
     this.team = [];
     this.team.append(team1);
     this.team.append(team2);
-    this.num_possessions = int(round((this.team[0]['pace'] + this.team[1]['pace']) / 2 * fast_random.gauss(1, 0.03)));
+    this.num_possessions = int(round((this.team[0]['pace'] + this.team[1]['pace']) / 2 * gauss_random(1, 0.03)));
 
     // Starting lineups, which works because players are ordered by their roster_position
     this.players_on_court = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4]];
@@ -136,47 +136,55 @@ GameSim.prototype.update_players_on_court = function() {
 
     for (var t = 0; t < 2; t++) {
         // Overall ratings scaled by fatigue
-        var players = []
-        for (var i = 0; i < this.team[t]['player'].length; i++){
-            players.push(i);
+    	var overalls = [];
+        for (var i = 0; i < this.team[t]['player'].length; i++) {
+            overalls.push(this.team[t]['player'][i]['overall_rating'] * this.team[t]['player'][i]['stat']['energy'] * gauss_random(1, .04));
         }
-        var overalls = [this.team[t]['player'][i]['overall_rating'] * this.team[t]['player'][i]['stat']['energy'] * fast_random.gauss(1, .04) for each (i in players)]
 
         // Loop through players on court (in inverse order of current roster position)
         i = 0;
-        for p in this.players_on_court[t] {
-            this.players_on_court[t][i] = p
+        for (var pp = 0; pp < this.players_on_court[t].length; pp++) {
+            p = this.players_on_court[t][pp];
+            this.players_on_court[t][i] = p;
             // Loop through bench players (in order of current roster position) to see if any should be subbed in)
             for (var b = 0; b < this.team[t]['player'].length; b++) {
-                if b !in this.players_on_court[t] and this.team[t]['player'][p]['stat']['court_time'] > 3 and this.team[t]['player'][b]['stat']['bench_time'] > 3 and overalls[b] > overalls[p] {
+                if (this.players_on_court[t].indexOf(b) == -1 && this.team[t]['player'][p]['stat']['court_time'] > 3 && this.team[t]['player'][b]['stat']['bench_time'] > 3 && overalls[b] > overalls[p]) {
                     // Substitute player
-                    this.players_on_court[t][i] = b
-                    this.team[t]['player'][b]['stat']['court_time'] = fast_random.gauss(0, 2)
-                    this.team[t]['player'][b]['stat']['bench_time'] = fast_random.gauss(0, 2)
-                    this.team[t]['player'][p]['stat']['court_time'] = fast_random.gauss(0, 2)
-                    this.team[t]['player'][p]['stat']['bench_time'] = fast_random.gauss(0, 2)
-            i += 1
+                    this.players_on_court[t][i] = b;
+                    this.team[t]['player'][b]['stat']['court_time'] = gauss_random(0, 2);
+                    this.team[t]['player'][b]['stat']['bench_time'] = gauss_random(0, 2);
+                    this.team[t]['player'][p]['stat']['court_time'] = gauss_random(0, 2);
+                    this.team[t]['player'][p]['stat']['bench_time'] = gauss_random(0, 2);
+                }
+            }
+            i += 1;
+        }
 
         // Update minutes (overall, court, and bench)
         for (var p = 0; p < this.team[t]['player'].length; p++) {
-            if p in this.players_on_court[t] {
-                this.record_stat(t, p, 'minutes', dt)
-                this.record_stat(t, p, 'court_time', dt)
-                this.record_stat(t, p, 'energy', -dt * 0.01)
-                if this.team[t]['player'][p]['stat']['energy'] < 0 {
-                    this.team[t]['player'][p]['stat']['energy'] = 0
+            if (this.players_on_court[t].indexOf(p) >= 0) {
+                this.record_stat(t, p, 'minutes', dt);
+                this.record_stat(t, p, 'court_time', dt);
+                this.record_stat(t, p, 'energy', -dt * 0.01);
+                if (this.team[t]['player'][p]['stat']['energy'] < 0) {
+                    this.team[t]['player'][p]['stat']['energy'] = 0;
+                }
+            }
             else {
-                this.record_stat(t, p, 'bench_time', dt)
-                this.record_stat(t, p, 'energy', dt * 0.2)
-                if this.team[t]['player'][p]['stat']['energy'] > 1 {
-                    this.team[t]['player'][p]['stat']['energy'] = 1
+                this.record_stat(t, p, 'bench_time', dt);
+                this.record_stat(t, p, 'energy', dt * 0.2);
+                if (this.team[t]['player'][p]['stat']['energy'] > 1) {
+                    this.team[t]['player'][p]['stat']['energy'] = 1;
+                }
+            }
+        }
     }
 }
 
 
 
 GameSim.prototype.is_turnover = function() {
-    if (random.random() < 0.1 + this.team[this.d]['defense']) {
+    if (Math.random() < 0.1 + this.team[this.d]['defense']) {
         this.do_turnover();
         return true;
     }
@@ -188,7 +196,7 @@ GameSim.prototype.is_turnover = function() {
 
 
 GameSim.prototype.is_steal = function() {
-    if (random.random() < 0.55) {
+    if (Math.random() < 0.55) {
         this.do_steal();
         return true;
     }
@@ -200,7 +208,7 @@ GameSim.prototype.is_steal = function() {
 
 
 GameSim.prototype.is_block = function() {
-    if (random.random() < (0.02 + this.team[this.d]['defense'])) {
+    if (Math.random() < (0.02 + this.team[this.d]['defense'])) {
         this.do_block();
         return true;
     }
@@ -212,7 +220,7 @@ GameSim.prototype.is_block = function() {
 
 
 GameSim.prototype.is_free_throw = function(shooter) {
-    if (random.random() < 0.15) {
+    if (Math.random() < 0.15) {
         this.do_free_throw(shooter, 2);
         return true;
     }
@@ -227,7 +235,7 @@ GameSim.prototype.is_made_shot = function(shooter) {
     var p = this.players_on_court[this.o][shooter];
     this.record_stat(this.o, p, 'field_goals_attempted');
     // Three pointer or two pointer
-    if (this.team[this.o]['player'][p]['composite_rating']['three_pointer_percentage'] > 0.25 and random.random() < (0.5 * this.team[this.o]['player'][p]['composite_rating']['three_pointer_percentage'])) {
+    if (this.team[this.o]['player'][p]['composite_rating']['three_pointer_percentage'] > 0.25 && Math.random() < (0.5 * this.team[this.o]['player'][p]['composite_rating']['three_pointer_percentage'])) {
         this.record_stat(this.o, p, 'three_pointers_attempted');
         var type = 3;
         var stat = 'three_pointer_percentage';
@@ -237,10 +245,10 @@ GameSim.prototype.is_made_shot = function(shooter) {
         var stat = 'field_goal_percentage';
     }
     // Make or miss
-    if (random.random() < (this.team[this.o]['player'][p]['composite_rating'][stat] - this.team[this.d]['defense'])) {
+    if (Math.random() < (this.team[this.o]['player'][p]['composite_rating'][stat] - this.team[this.d]['defense'])) {
         this.do_made_shot(shooter, type);
         // And 1
-        if (random.random() < 0.1) {
+        if (Math.random() < 0.1) {
             this.do_free_throw(shooter, 1);
         }
         return true;
@@ -253,7 +261,7 @@ GameSim.prototype.is_made_shot = function(shooter) {
 
 
 GameSim.prototype.is_assist = function() {
-    if (random.random() < 0.6) {
+    if (Math.random() < 0.6) {
         return true;
     }
     else {
@@ -293,7 +301,7 @@ GameSim.prototype.do_free_throw = function(shooter, amount) {
     var p = this.players_on_court[this.o][shooter];
     for (var i = 0; i < amount; i++) {
         this.record_stat(this.o, p, 'free_throws_attempted');
-        if (random.random() < this.team[this.o]['player'][p]['composite_rating']['free_throw_percentage']) {
+        if (Math.random() < this.team[this.o]['player'][p]['composite_rating']['free_throw_percentage']) {
             this.record_stat(this.o, p, 'free_throws_made');
             this.record_stat(this.o, p, 'points');
         }
@@ -332,7 +340,7 @@ GameSim.prototype.do_made_shot = function(shooter, type) {
 
 
 GameSim.prototype.do_rebound = function() {
-    if (random.random() < 0.8) {
+    if (Math.random() < 0.8) {
         var ratios = this.rating_array('rebound_ratio', this.d);
         var p = this.players_on_court[this.d][this.pick_player(ratios)];
         this.record_stat(this.d, p, 'defensive_rebounds');
@@ -369,11 +377,12 @@ GameSim.prototype.rating_array = function(rating, t) {
  *         same value as the player ID *or* the index of the
  *         this.team[t]['player'] list. Yes, that's confusing.
  */
-GameSim.prototype.pick_player = function(ratios, exempt=false) {
+GameSim.prototype.pick_player = function(ratios, exempt) {
+    exempt = typeof exempt !== 'undefined' ? exempt : false;
     if (exempt != false) {
         ratios[exempt] = 0;
     }
-    rand = random.random() * (ratios[0] + ratios[1] + ratios[2] + ratios[3] + ratios[4]);
+    rand = Math.random() * (ratios[0] + ratios[1] + ratios[2] + ratios[3] + ratios[4]);
     if (rand < ratios[0]) {
         pick = 0;
     }
@@ -398,9 +407,22 @@ GameSim.prototype.pick_player = function(ratios, exempt=false) {
  * Increments a stat (s) for a player (p) on a team (t) by amount
  * (default is 1).
  */
-GameSim.prototype.record_stat = function(t, p, s, amount=1) {
+GameSim.prototype.record_stat = function(t, p, s, amount) {
+    amount = typeof amount !== 'undefined' ? amount : 1;
     this.team[t]['player'][p]['stat'][s] = this.team[t]['player'][p]['stat'][s] + amount;
-    if (s != 'starter' and s != 'court_time' and s != 'bench_time' and s != 'energy') {
+    if (s != 'starter' && s != 'court_time' && s != 'bench_time' && s != 'energy') {
         this.team[t]['stat'][s] = this.team[t]['stat'][s] + amount;
     }
+}
+
+
+/**
+ * Returns a random number from an approximately Gaussian distribution with
+ * mean mu (default: 0) and standard deviation sigma (default: 1).
+ * See: http://www.protonfish.com/random.shtml
+ */
+function gauss_random(mu, sigma) {
+    mu = typeof mu !== 'undefined' ? mu : 0;
+    sigma = typeof sigma !== 'undefined' ? sigma : 1;
+    return ((Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1))*sigma + mu;
 }

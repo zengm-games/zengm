b4863ac4816bfa0861e6bed9aa51a25ec60945c8
Jeremy Scheff <jdscheff@gmail.com>
2012-06-06 13:53:36 -0400

    Preliminary support for creating/deleting leagues (currently just deals with the metadata)

--- js/views.js
+++ js/views.js
@@ -1,28 +1,33 @@
 var views = {
-    init_db: function() {
+    init_db: function () {
         var data = {'title': 'Initialize Database'};
         var url = '/init_db';
 
         data['content'] = 'Resetting databases...'
 
-        // Delete any current bbgm databases
-        db_bbgm.close();
-        indexedDB.deleteDatabase("bbgm");
+        // Delete any current meta databases
+        db_meta.close();
+        indexedDB.deleteDatabase("meta");
 
-        // Create new bbgm database
-
-        // Create new tables in bbgm
-//        var transaction = db_bbgm.transaction(["customers"], IDBTransaction.READ_WRITE);
+        // Create new meta database
+        request = db.connect_meta();
+        request.onsuccess = function(event) {
+            db_meta = request.result;
+            db_meta.onerror = function(event) {
+                console.log("Meta database error: " + event.target.errorCode);
+            };
+        };
+//        var transaction = db_meta.transaction(["customers"], IDBTransaction.READ_WRITE);
 
         ajax_update(data, url);
     },
 
-    dashboard: function() {
+    dashboard: function () {
         var data = {'title': 'Dashboard'};
         var url = '/';
 
         var leagues = [];
-        var leaguesStore = db_bbgm.transaction("leagues").objectStore("leagues");
+        var leaguesStore = db_meta.transaction("leagues").objectStore("leagues");
         leaguesStore.openCursor().onsuccess = function(event) {
             var cursor = event.target.result;
             if (cursor) {
@@ -39,13 +44,13 @@ var views = {
         };
     },
 
-    new_league: function(req) {
+    new_league: function (req) {
         var data = {'title': 'Create New League'};
         var url = '/new_league';
 
         if (req.method === "get") {
             var teams = [];
-            var teamsStore = db_bbgm.transaction("teams").objectStore("teams");
+            var teamsStore = db_meta.transaction(["teams"]).objectStore("teams");
             teamsStore.openCursor().onsuccess = function(event) {
                 var cursor = event.target.result;
                 if (cursor) {
@@ -63,11 +68,16 @@ var views = {
         }
         else if (req.method === "post") {
             tid = parseInt(req.params['tid'], 10);
-console.log(tid);
+console.log('New tid: ' + tid);
             if (tid >= 0 && tid <= 29) {
-//                lid = league.new(tid);
-                req.redirect('/');
+                league.new(tid);
             }
         }
+    },
+
+    delete_league: function (req) {
+        lid = parseInt(req.params['lid'], 10);
+        league.delete(lid)
+        req.redirect('/');
     }
 };

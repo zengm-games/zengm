63e732df964205be33ec1b90e4e101aff0859b93
Jeremy Scheff <jdscheff@gmail.com>
2012-06-10 12:17:56 -0400

    Dummy util.lock

---
+++ js/util/lock.js
@@ -0,0 +1,64 @@
+/*
+bbgm.util.lock
+
+These functions all deal with locking game state when there is some blocking
+action in progress, currently one of these things:
+
+* Game simulation is in progress
+* User is negotiating a contract
+
+There are also functions to check if it is permissible to start one of those
+actions.
+*/
+
+var lock = {
+    set_games_in_progress: function (status) {
+//        g.dbex('UPDATE game_attributes SET games_in_progress = :games_in_progress', games_in_progress=status)
+    },
+
+    games_in_progress: function () {
+//        r = g.dbex('SELECT games_in_progress FROM game_attributes')
+//        in_progress, = r.fetchone()
+//        return in_progress
+return false;
+    },
+
+    /*Returns true or false depending on whether the negotiations table is
+    empty or not.*/
+    negotiation_in_progress: function () {
+//        r = g.dbex('SELECT 1 FROM negotiations')
+//        if r.rowcount:
+//            return true;
+//        else {
+            return false;
+//        }
+    },
+
+    /*Returns a boolean. Games can be started only when there is no contract
+    negotiation in progress and there is no other game simulation in progress.
+    */
+    can_start_games: function () {
+/*        r = g.dbex('SELECT games_in_progress FROM game_attributes')
+        games_in_progress, = r.fetchone()
+
+        if games_in_progress or negotiation_in_progress():
+            return false;
+        else {
+            return true;
+        }
+
+        can_start_negotiation: function () {
+        r = g.dbex('SELECT games_in_progress FROM game_attributes')
+        games_in_progress, = r.fetchone()
+
+        if games_in_progress:
+            return false;
+
+        // Allow multiple parallel negotiations (ignore negotiation_in_progress) only for resigning players
+        r = g.dbex('SELECT 1 FROM negotiations WHERE resigning = false')
+        if r.rowcount:
+            return false;*/
+
+        return true
+    }
+}

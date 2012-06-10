1fe6d370784c7c3449bf90ed4268d6ce76e623c7
Jeremy Scheff <jdscheff@gmail.com>
2012-06-09 22:26:15 -0400

    Error handling not worth the trouble

--- js/app.js
+++ js/app.js
@@ -18,11 +18,6 @@ request.onsuccess = function(event) {
             this.generateRequestOnPageLoad = true;
         });
 
-        // Error handling
-        this.bind('routeNotFound', function (req) {
-            alert('Route Not Found');
-        });
-
         // Non-league views
         this.get('/init_db', views.init_db);
         this.get('/', views.dashboard);

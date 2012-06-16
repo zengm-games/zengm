e4c4799aab460d17bec665cf5424ff943de1908a
Jeremy Scheff <jdscheff@gmail.com>
2012-06-15 22:06:14 -0400

    validateAbbrevs works, but it does not dynamically read abbreviations from the database

---
+++ js/util/helpers.js
@@ -0,0 +1,51 @@
+define([], function() {
+    /*Validate that the given abbreviation corresponds to a valid team.
+
+    If an invalid abbreviation is passed, the user's team will be used.
+
+    Args:
+        abbrev: Three-letter all-caps string containing a team's
+            abbreviation.
+    Returns:
+        A two element list of the validated team ID and abbreviation.
+    */
+    function validateAbbrev(abbrev) {
+        var abbrevs = ["ATL", "BOS", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NJN", "NOR", "NYK", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "SEA", "TOR", "UTA", "WAS"];
+        var tid = abbrevs.indexOf(abbrev);
+
+        if (tid < 0) {
+            tid = g.userTid;
+            abbrev = abbrevs[tid];
+        }
+
+        return [tid, abbrev];
+    }
+
+    /*Validate that the given season is valid.
+
+    A valid season is the current season or one of the past seasons. If an
+    invalid season is passed, the current will be used.
+
+    Args:
+        season: An integer containing the year of the season.
+    Returns:
+        An integer containing the argument, if valid, or the year of the current
+        season.
+    */
+    function validateSeason(season) {
+        if (!season) {
+            season = g.season
+        }
+        else {
+            // Make sure there is an entry for the supplied season in the DB somewhere
+            season = parseInt(season, 10);
+        }
+
+        return season;
+    }
+
+    return {
+        validateAbbrev: validateAbbrev,
+        validateSeason: validateSeason
+    }
+});

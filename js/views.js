ff4cd7e119ea93e4e3c2b33da1437610b64386bf
Jeremy Scheff <jdscheff@gmail.com>
2012-06-16 15:45:26 -0400

    Reorganize views

--- js/views.js
+++ js/views.js
@@ -31,6 +31,97 @@ define(["bbgm", "db", "core/game", "core/league", "core/season", "util/helpers",
         }
     }
 
+    function init_db(req) {
+        var data = {"title": "Initialize Database"};
+        var url = "/init_db";
+
+        data["content"] = "Resetting databases..."
+
+        // Delete any current league databases
+        console.log("Delete any current league databases...");
+        if (g.dbl !== undefined) {
+            g.dbl.close();
+        }
+        db.getAll(g.dbm, "leagues", function (leagues) {
+            for (var i=0; i<leagues.length; i++) {
+                g.indexedDB.deleteDatabase("league" + leagues[i]["lid"]);
+            }
+        });
+
+        // Delete any current meta database
+        console.log("Delete any current meta database...");
+        g.dbm.close();
+        g.indexedDB.deleteDatabase("meta");
+
+        // Create new meta database
+        console.log("Create new meta database...");
+        request = db.connect_meta();
+        request.onsuccess = function(event) {
+            g.dbm = request.result;
+            g.dbm.onerror = function(event) {
+                console.log("Meta database error: " + event.target.errorCode);
+            };
+        };
+
+        console.log("Done!");
+
+        bbgm.ajaxUpdate(data, url);
+    }
+
+    function dashboard(req) {
+        var data = {"title": "Dashboard"};
+        var url = "/";
+
+        db.getAll(g.dbm, "leagues", function (leagues) {
+            var template = Handlebars.templates['dashboard'];
+            data["content"] = template({leagues: leagues});
+
+            bbgm.ajaxUpdate(data, url);
+        });
+    }
+
+    function new_league(req) {
+        var data = {"title": "Create New League"};
+        var url = "/new_league";
+
+        if (req.method === "get") {
+            db.getAll(g.dbm, "teams", function (teams) {
+                var template = Handlebars.templates['new_league'];
+                data["content"] = template({teams: teams});
+
+                bbgm.ajaxUpdate(data, url);
+            });
+        }
+        else if (req.method === "post") {
+            tid = parseInt(req.params["tid"], 10);
+            if (tid >= 0 && tid <= 29) {
+                league.new(tid);
+            }
+        }
+    }
+
+    function delete_league(req) {
+        lid = parseInt(req.params['lid'], 10);
+        league.delete(lid)
+        req.redirect('/');
+    }
+
+    function league_dashboard(req) {
+        beforeLeague(req, function() {
+            var data = {"title": "Dashboard - League " + g.lid};
+            var url = "/l/" + g.lid;
+
+            var template = Handlebars.templates['league_dashboard'];
+            data["league_content"] = template({g: g});
+
+            bbgm.ajaxUpdate(data, url);
+        });
+    }
+
+    function standings(req) {
+
+    }
+
     function schedule(req) {
         beforeLeague(req, function() {
             var data = {"title": "Schedule - League " + g.lid};
@@ -62,129 +153,51 @@ define(["bbgm", "db", "core/game", "core/league", "core/season", "util/helpers",
         });
     }
 
-    return {
-        init_db: function (req) {
-            var data = {"title": "Initialize Database"};
-            var url = "/init_db";
+    function game_log(req) {
+        beforeLeague(req, function() {
+            var viewAbbrev = typeof req.params.viewAbbrev !== "undefined" ? req.params.viewAbbrev : undefined;
+            [viewTid, viewAbbrev] = helpers.validateAbbrev(viewAbbrev);
+            var viewSeason = typeof req.params.viewSeason !== "undefined" ? req.params.viewSeason : undefined;
+            viewSeason = helpers.validateSeason(viewSeason);
 
-            data["content"] = "Resetting databases..."
+            var data = {"title": "Game Log - League " + g.lid};
+            var url = "/l/" + g.lid + "/game_log";
 
-            // Delete any current league databases
-            console.log("Delete any current league databases...");
-            if (g.dbl !== undefined) {
-                g.dbl.close();
-            }
-            db.getAll(g.dbm, "leagues", function (leagues) {
-                for (var i=0; i<leagues.length; i++) {
-                    g.indexedDB.deleteDatabase("league" + leagues[i]["lid"]);
+            var seasons = [{season: 2012, selected: true}, {season: 2013, selected: false}, {season: 2014, selected: false}];
+            g.dbl.transaction(["teams"]).objectStore("teams").index("season").getAll(viewSeason).onsuccess = function(event) {
+                var teamsAll = event.target.result;
+                var teams = [];
+                for (var i=0; i<teamsAll.length; i++) {
+                    var team = teamsAll[i];
+
+                    if (team.tid == viewTid) {
+                        var selected = true;
+                    }
+                    else {
+                        var selected = false;
+                    }
+
+                    teams.push({tid: team.tid, abbrev: team.abbrev, region: team.region, name: team.name, selected: selected});
                 }
-            });
+                var template = Handlebars.templates['game_log'];
+                data["league_content"] = template({g: g, teams: teams, seasons: seasons});
 
-            // Delete any current meta database
-            console.log("Delete any current meta database...");
-            g.dbm.close();
-            g.indexedDB.deleteDatabase("meta");
-
-            // Create new meta database
-            console.log("Create new meta database...");
-            request = db.connect_meta();
-            request.onsuccess = function(event) {
-                g.dbm = request.result;
-                g.dbm.onerror = function(event) {
-                    console.log("Meta database error: " + event.target.errorCode);
-                };
+                bbgm.ajaxUpdate(data, url);
             };
 
-            console.log("Done!");
+        });
+    }
 
-            bbgm.ajaxUpdate(data, url);
-        },
+    return {
+        init_db: init_db,
 
-        dashboard: function (req) {
-            var data = {"title": "Dashboard"};
-            var url = "/";
+        dashboard: dashboard,
+        new_league: new_league,
+        delete_league: delete_league,
 
-            db.getAll(g.dbm, "leagues", function (leagues) {
-                var template = Handlebars.templates['dashboard'];
-                data["content"] = template({leagues: leagues});
-
-                bbgm.ajaxUpdate(data, url);
-            });
-        },
-
-        new_league: function (req) {
-            var data = {"title": "Create New League"};
-            var url = "/new_league";
-
-            if (req.method === "get") {
-                db.getAll(g.dbm, "teams", function (teams) {
-                    var template = Handlebars.templates['new_league'];
-                    data["content"] = template({teams: teams});
-
-                    bbgm.ajaxUpdate(data, url);
-                });
-            }
-            else if (req.method === "post") {
-                tid = parseInt(req.params["tid"], 10);
-                if (tid >= 0 && tid <= 29) {
-                    league.new(tid);
-                }
-            }
-        },
-
-        delete_league: function (req) {
-            lid = parseInt(req.params['lid'], 10);
-            league.delete(lid)
-            req.redirect('/');
-        },
-
-        league_dashboard: function (req) {
-            beforeLeague(req, function() {
-                var data = {"title": "Dashboard - League " + g.lid};
-                var url = "/l/" + g.lid;
-
-                var template = Handlebars.templates['league_dashboard'];
-                data["league_content"] = template({g: g});
-
-                bbgm.ajaxUpdate(data, url);
-            });
-        },
-
-        game_log: function(req) {
-            beforeLeague(req, function() {
-                var viewAbbrev = typeof req.params.viewAbbrev !== "undefined" ? req.params.viewAbbrev : undefined;
-                [viewTid, viewAbbrev] = helpers.validateAbbrev(viewAbbrev);
-                var viewSeason = typeof req.params.viewSeason !== "undefined" ? req.params.viewSeason : undefined;
-                viewSeason = helpers.validateSeason(viewSeason);
-
-                var data = {"title": "Game Log - League " + g.lid};
-                var url = "/l/" + g.lid + "/game_log";
-
-                var seasons = [{season: 2012, selected: true}, {season: 2013, selected: false}, {season: 2014, selected: false}];
-                g.dbl.transaction(["teams"]).objectStore("teams").index("season").getAll(viewSeason).onsuccess = function(event) {
-                    var teamsAll = event.target.result;
-                    var teams = [];
-                    for (var i=0; i<teamsAll.length; i++) {
-                        var team = teamsAll[i];
-
-                        if (team.tid == viewTid) {
-                            var selected = true;
-                        }
-                        else {
-                            var selected = false;
-                        }
-
-                        teams.push({tid: team.tid, abbrev: team.abbrev, region: team.region, name: team.name, selected: selected});
-                    }
-                    var template = Handlebars.templates['game_log'];
-                    data["league_content"] = template({g: g, teams: teams, seasons: seasons});
-
-                    bbgm.ajaxUpdate(data, url);
-                };
-
-            });
-        },
-
-        schedule: schedule
+        league_dashboard: league_dashboard,
+        standings: standings,
+        schedule: schedule,
+        game_log: game_log
     };
 });

ee20ea8cc4d413187f7125299a896f9a4faf8c3a
Jeremy Scheff <jdscheff@gmail.com>
2012-07-02 23:55:34 -0400

    Deleted old crap

--- tests/test_game_sim.js
+++
@@ -1,8 +0,0 @@
-fs = require('fs');
-teams = JSON.parse(fs.readFileSync('teams.json').toString());
-//console.log(teams);
-game_sim = require('../bbgm/static/js/game_sim');
-//console.log(game_sim);
-gs = new game_sim.GameSim(teams[0], teams[1]);
-//console.log(gs);
-console.log(JSON.stringify(gs.run()));

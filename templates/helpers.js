4101173a8a92b8306868271b40148a033c756e93
Jeremy Scheff <jdscheff@gmail.com>
2012-07-29 13:29:29 -0400

    jslint

--- templates/helpers.js
+++ templates/helpers.js
@@ -1,39 +1,45 @@
-Handlebars.registerHelper("round", function(value, precision) {
+Handlebars.registerHelper("round", function (value, precision) {
+    "use strict";
+
     precision = typeof precision !== "undefined" ? parseInt(precision, 10) : 0;
 
     return parseFloat(value).toFixed(precision);
 });
 
-Handlebars.registerHelper("roundWinp", function(value) {
+Handlebars.registerHelper("roundWinp", function (value) {
+    "use strict";
+    var output;
+
     output = parseFloat(value).toFixed(3);
 
-    // Delete leading 0
-    if (output[0] == "0") {
+    if (output[0] === "0") {
+        // Delete leading 0
         output = output.slice(1, output.length);
-    }
-    // Delete trailing digit if no leading 0
-    else {
-        output = output.slice(0, output.length-1);
+    } else {
+        // Delete trailing digit if no leading 0
+        output = output.slice(0, output.length - 1);
     }
 
     return output;
 });
 
-Handlebars.registerHelper("matchup", function(i, j) {
+Handlebars.registerHelper("matchup", function (i, j) {
+    "use strict";
+    var series, source, template;
+
     series = this.series[i][j];
 
     source = "{{#if series.home.name}}" +
              "  {{#if series.home.wonSeries}}<strong>{{/if}}{{series.home.seed}}. {{series.home.name}} {{series.home.won}}{{#if series.home.wonSeries}}</strong>{{/if}}<br>" +
              "  {{#if series.away.wonSeries}}<strong>{{/if}}{{series.away.seed}}. {{series.away.name}} {{series.away.won}}{{#if series.away.wonSeries}}</strong>{{/if}}" +
              "{{/if}}";
-    var template = Handlebars.compile(source);
+    template = Handlebars.compile(source);
     return new Handlebars.SafeString(template({series: series}));
 });
 
 
-Handlebars.registerHelper("face", function(face) {
-    console.log(face);
-    //faces.display("myDivId", face);
+Handlebars.registerHelper("face", function (face) {
+    "use strict";
 
     return new Handlebars.SafeString('<script>'
          + '$(document).ready(function() {'

856b2e25ad06cfaf2cff3b1537d98b116ecfd7b6
Jeremy Scheff <jdscheff@gmail.com>
2012-08-08 16:31:40 -0400

    Setting a really really high timeout in Mocha seems to eliminate the mysterios errors in the trade tests

--- js/test/app.js
+++ js/test/app.js
@@ -5,7 +5,7 @@ require.config({
 requirejs(["lib/chai"], function (chai) {
     "use strict";
 
-    mocha.setup({ui: "bdd", globals: ["qqq"], timeout: 40000});
+    mocha.setup({ui: "bdd", globals: ["console"], timeout: 2000000000});
     chai.should();
 
     require(["test/core", "test/views"], function (testCore, testViews) {

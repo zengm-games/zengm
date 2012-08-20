b44e984b6fbc48972fbf182194d14ef094de1e68
Jeremy Scheff <jdscheff@gmail.com>
2012-08-19 21:24:00 -0400

    Minor cleanup

--- js/app.js
+++ js/app.js
@@ -9,55 +9,55 @@ requirejs(["db", "views", "bbgm", "api", "util/helpers"], function (db, views, b
             this.configure(function () {
                 this.generateRequestOnPageLoad = true;
                 this.handleRouteNotFound = true;
-                this.linkSelector = 'a:not([data-no-davis=true])';
+                this.linkSelector = "a:not([data-no-davis=true])";
             });
 
-            this.bind('routeNotFound', function (req) {
+            this.bind("routeNotFound", function (req) {
                 helpers.error("Page not found.");
             });
 
 
             // Non-league views
-            this.get('/init_db', views.init_db);
-            this.get('/', views.dashboard);
-            this.get('/new_league', views.new_league);
-            this.post('/new_league', views.new_league);
-            this.post('/delete_league', views.delete_league);
+            this.get("/init_db", views.init_db);
+            this.get("/", views.dashboard);
+            this.get("/new_league", views.new_league);
+            this.post("/new_league", views.new_league);
+            this.post("/delete_league", views.delete_league);
 
             // League views
-            this.get('/l/:lid', views.league_dashboard);
-            this.get('/l/:lid/standings', views.standings);
-            this.get('/l/:lid/standings/:season', views.standings);
-            this.get('/l/:lid/playoffs', views.playoffs);
-            this.get('/l/:lid/playoffs/:season', views.playoffs);
-            this.get('/l/:lid/finances', views.finances);
-            this.get('/l/:lid/history', views.history);
-            this.get('/l/:lid/history/:season', views.history);
-            this.get('/l/:lid/roster', views.roster);
-            this.get('/l/:lid/roster/:abbrev', views.roster);
-            this.get('/l/:lid/roster/:abbrev/:season', views.roster);
-            this.get('/l/:lid/schedule', views.schedule);
-            this.get('/l/:lid/free_agents', views.freeAgents);
-            this.get('/l/:lid/trade', views.trade);
-            this.post('/l/:lid/trade', views.trade);
-            this.get('/l/:lid/draft', views.draft);
-            this.get('/l/:lid/draft/:season', views.draft);
-            this.get('/l/:lid/game_log', views.gameLog);
-            this.get('/l/:lid/game_log/:abbrev', views.gameLog);
-            this.get('/l/:lid/game_log/:abbrev/:season', views.gameLog);
-            this.get('/l/:lid/leaders', views.leaders);
-            this.get('/l/:lid/leaders/:season', views.leaders);
-            this.get('/l/:lid/player_ratings', views.playerRatings);
-            this.get('/l/:lid/player_ratings/:season', views.playerRatings);
-            this.get('/l/:lid/player_stats', views.playerStats);
-            this.get('/l/:lid/player_stats/:season', views.playerStats);
-            this.get('/l/:lid/team_stats', views.teamStats);
-            this.get('/l/:lid/team_stats/:season', views.teamStats);
-            this.get('/l/:lid/player/:pid', views.player);
-            this.get('/l/:lid/negotiation', views.negotiationList);
-            this.get('/l/:lid/negotiation/:pid', views.negotiation);
-            this.post('/l/:lid/negotiation/:pid', views.negotiation);
-            this.get('/l/:lid/test/schedule', views.testSchedule);
+            this.get("/l/:lid", views.league_dashboard);
+            this.get("/l/:lid/standings", views.standings);
+            this.get("/l/:lid/standings/:season", views.standings);
+            this.get("/l/:lid/playoffs", views.playoffs);
+            this.get("/l/:lid/playoffs/:season", views.playoffs);
+            this.get("/l/:lid/finances", views.finances);
+            this.get("/l/:lid/history", views.history);
+            this.get("/l/:lid/history/:season", views.history);
+            this.get("/l/:lid/roster", views.roster);
+            this.get("/l/:lid/roster/:abbrev", views.roster);
+            this.get("/l/:lid/roster/:abbrev/:season", views.roster);
+            this.get("/l/:lid/schedule", views.schedule);
+            this.get("/l/:lid/free_agents", views.freeAgents);
+            this.get("/l/:lid/trade", views.trade);
+            this.post("/l/:lid/trade", views.trade);
+            this.get("/l/:lid/draft", views.draft);
+            this.get("/l/:lid/draft/:season", views.draft);
+            this.get("/l/:lid/game_log", views.gameLog);
+            this.get("/l/:lid/game_log/:abbrev", views.gameLog);
+            this.get("/l/:lid/game_log/:abbrev/:season", views.gameLog);
+            this.get("/l/:lid/leaders", views.leaders);
+            this.get("/l/:lid/leaders/:season", views.leaders);
+            this.get("/l/:lid/player_ratings", views.playerRatings);
+            this.get("/l/:lid/player_ratings/:season", views.playerRatings);
+            this.get("/l/:lid/player_stats", views.playerStats);
+            this.get("/l/:lid/player_stats/:season", views.playerStats);
+            this.get("/l/:lid/team_stats", views.teamStats);
+            this.get("/l/:lid/team_stats/:season", views.teamStats);
+            this.get("/l/:lid/player/:pid", views.player);
+            this.get("/l/:lid/negotiation", views.negotiationList);
+            this.get("/l/:lid/negotiation/:pid", views.negotiation);
+            this.post("/l/:lid/negotiation/:pid", views.negotiation);
+            this.get("/l/:lid/test/schedule", views.testSchedule);
         });
 
         $(document).ready(function () {
--- js/bbgm.js
+++ js/bbgm.js
@@ -1,70 +1,62 @@
 define([], function () {
     "use strict";
 
-    // Play button
-/*    function play_button(url) {
-        $.post(url, function (data) {
-            var url = data['url'];
+    function highlightNav(leaguePage) {
+        if (leaguePage === "") {
+            leaguePage = "league_dashboard";
+        }
+        $("#league_sidebar li").removeClass("active");
+        $("#nav_" + leaguePage).addClass("active");
+    }
 
-            if (url && url != document.URL) {
-                $.getJSON(data['url'], {'json': 1}, function (data) {
-                    ajaxUpdate(data, url);
-                    var result = parse_league_url(document.URL);
-                    var league_page = result[2];
-                    highlight_nav(league_page);
-                });
-            }
-            if ((schedule && schedule.length > 0) || playoffs_continue) {
-                // Stop game simulation when user leaves the page
-                window.onunload = function() {
-                    var result = parse_league_url(document.URL);
-                    var league_root_url = result[1];
-                    $.ajax({
-                        type: 'POST',
-                        url: league_root_url + '/play/stop',
-                        async: false
-                    });
-                };
+    function parseLeagueUrl(url) {
+        var league_id, league_page, league_root_url, split_url;
+        // Returns a list containing the integer league ID (0 if none), the
+        // league root URL up to the league ID (empty string if none), and the
+        // league page (first URL folder after the ID) (empty string if none).
 
-                var results = [];
-                for (var i=0; i<schedule.length; i++) {
-                    gs = new GameSim(schedule[i]['gid'], teams[schedule[i]['home_tid']], teams[schedule[i]['away_tid']]);
-                    results.push(gs.run());
-                }
-                var result = parse_league_url(document.URL);
-                var league_root_url = result[1];
-                $.post(league_root_url + '/save_results', {'results': JSON.stringify(results)});
-                num_days = num_days - 1;
-                if (num_days >= 0) {
-                    play_button(league_root_url + '/play/' + num_days);
-                }
-                if (num_days == 0) {
-                    window.onunload = function() {};
-                }
-            }
-        }, 'json');
-    }*/
+        league_id = 0;
+        league_root_url = "";
+        league_page = "";
+
+        split_url = url.split("/", 6);
+
+        // If there's a URL that starts http://domain.com/l/<int:league_id>,
+        // split_url will have length 4 or 5, depending on if there is a page after
+        // the league ID.
+
+        if (split_url.length >= 5) {
+            league_id = parseInt(split_url[4], 10);
+            league_root_url = split_url.slice(0, 5).join("/");
+        }
+        if (split_url.length === 6) {
+            // Get rid of any trailing #
+            league_page = split_url[5].split("#")[0];
+        }
+
+        return [league_id, league_root_url, league_page];
+    }
 
     // For AJAX updating pages
     function ajaxUpdate(data) {
         var league_page, result;
 
-        if (data.hasOwnProperty('title')) {
-            $('title').text(data.title + ' - Basketball GM');
+        if (data.hasOwnProperty("title")) {
+            $("title").text(data.title + " - Basketball GM");
         }
-        if (data.hasOwnProperty('league_content')) {
-            $('#league_content').html(data.league_content);
+        if (data.hasOwnProperty("league_content")) {
+            $("#league_content").html(data.league_content);
         }
-        if (data.hasOwnProperty('content')) {
-            $('#content').html(data.content);
+        if (data.hasOwnProperty("content")) {
+            $("#content").html(data.content);
         }
-        if (data.hasOwnProperty('message')) {
+        if (data.hasOwnProperty("message")) {
             alert(data.message);
         }
 
-        result = parse_league_url(document.URL);
+        result = parseLeagueUrl(document.URL);
         league_page = result[2];
-        highlight_nav(league_page);
+        highlightNav(league_page);
     }
 
     // Data tables
@@ -92,42 +84,6 @@ define([], function () {
         });
     }
 
-    function parse_league_url(url) {
-        var league_id, league_page, league_root_url, split_url;
-        // Returns a list containing the integer league ID (0 if none), the
-        // league root URL up to the league ID (empty string if none), and the
-        // league page (first URL folder after the ID) (empty string if none).
-
-        league_id = 0;
-        league_root_url = '';
-        league_page = '';
-
-        split_url = url.split('/', 6);
-
-        // If there's a URL that starts http://domain.com/l/<int:league_id>,
-        // split_url will have length 4 or 5, depending on if there is a page after
-        // the league ID.
-
-        if (split_url.length >= 5) {
-            league_id = parseInt(split_url[4], 10);
-            league_root_url = split_url.slice(0, 5).join('/');
-        }
-        if (split_url.length === 6) {
-            // Get rid of any trailing #
-            league_page = split_url[5].split('#')[0];
-        }
-
-        return [league_id, league_root_url, league_page];
-    }
-
-    function highlight_nav(league_page) {
-        if (league_page === '') {
-            league_page = 'league_dashboard';
-        }
-        $('#league_sidebar li').removeClass('active');
-        $('#nav_' + league_page).addClass('active');
-    }
-
     // For dropdown menus to change team/season/whatever
     // This should be cleaned up, but it works for now.
     function dropdown(select1, select2) {
@@ -135,81 +91,46 @@ define([], function () {
             select1.change(function (event) {
                 var league_page, league_root_url, result, url;
 
-                result = parse_league_url(document.URL);
+                result = parseLeagueUrl(document.URL);
                 league_root_url = result[1];
                 league_page = result[2];
-                url = '/l/' + g.lid + '/' + league_page + '/' + select1.val();
+                url = "/l/" + g.lid + "/" + league_page + "/" + select1.val();
                 Davis.location.assign(new Davis.Request(url));
             });
         } else if (arguments.length === 2) {
             select1.change(function (event) {
                 var league_page, league_root_url, result, url;
 
-                result = parse_league_url(document.URL);
+                result = parseLeagueUrl(document.URL);
                 league_root_url = result[1];
                 league_page = result[2];
-                url = '/l/' + g.lid + '/' + league_page + '/' + select1.val() + '/' + select2.val();
+                url = "/l/" + g.lid + "/" + league_page + "/" + select1.val() + "/" + select2.val();
                 Davis.location.assign(new Davis.Request(url));
             });
             select2.change(function (event) {
                 var league_page, league_root_url, result, url;
 
-                result = parse_league_url(document.URL);
+                result = parseLeagueUrl(document.URL);
                 league_root_url = result[1];
                 league_page = result[2];
-                url = '/l/' + g.lid + '/' + league_page + '/' + select1.val() + '/' + select2.val();
+                url = "/l/" + g.lid + "/" + league_page + "/" + select1.val() + "/" + select2.val();
                 Davis.location.assign(new Davis.Request(url));
             });
         }
     }
 
     $(document).ready(function () {
-        var league_id, league_page, league_root_url, play_button, play_phase, play_status, result;
+        var league_id, league_page, league_root_url, result;
 
-        result = parse_league_url(document.URL);
+        result = parseLeagueUrl(document.URL);
         league_id = result[0];
         league_root_url = result[1];
         league_page = result[2];
-        highlight_nav(league_page);
+        highlightNav(league_page);
 
         window.onpopstate = function (event) {
             ajaxUpdate(event.state);
         };
-
-        // Play menu
-        if (league_id > 0) {
-            play_status = $('#play_status');
-            play_phase = $('#play_phase');
-            play_button = $('#play_button');
-    /*
-            var jug = new Juggernaut;
-
-            // Load cached play menu - this only runs once, which is good, but I'm not sure why.
-            jug.on('connect', function(){
-                $.get(league_root_url + '/push_play_menu');
-            });
-
-            // Listen for updates to play menu
-            jug.subscribe(league_id + '_status', function(data){
-                play_status.html(data);
-
-                // Refresh page, as appropriate - maybe this isn't the best place for this
-    //            var refresh_pages = ['standings', 'playoffs', 'schedule']
-    //            var result = parse_league_url(document.URL);
-    //            var league_page = result[2];
-    //            if (jQuery.inArray(league_page, refresh_pages) > -1) {
-    //                $.getJSON(document.URL, {'json': 1}, function (data) {
-    //                    ajaxUpdate(data);
-    //                });
-    //            }
-            });
-            jug.subscribe(league_id + '_phase', function(data){
-                play_phase.html(data);
-            });
-            jug.subscribe(league_id + '_button', function(data){
-                play_button.html(data);
-            });*/
-        }
     });
 
     return {

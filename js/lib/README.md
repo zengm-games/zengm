ae809b80d7f24c51600fdc3c28a75a250f7fc53a
Jeremy Scheff <jdscheff@gmail.com>
2012-09-06 20:58:11 -0400

    Switch back to bootstrap-dropdown.js 2.0.2 because 2.1.0 seemingly interferes with Davis.js.

--- js/lib/README.md
+++ js/lib/README.md
@@ -1,3 +1,6 @@
 These are various third-party libraries used in Basketball GM. They are all
 standard, except jQuery UI is a custom download including only "sortable"
 support as the rest isn't used.
+
+I'm using bootstrap-dropdown.js 2.0.2 because 2.1.0 interferes with Davis.js
+handling normal links in the play menu.
--- js/lib/bootstrap-dropdown.js
+++ js/lib/bootstrap-dropdown.js
@@ -1,5 +1,5 @@
 /* ============================================================
- * bootstrap-dropdown.js v2.1.0
+ * bootstrap-dropdown.js v2.0.2
  * http://twitter.github.com/bootstrap/javascript.html#dropdowns
  * ============================================================
  * Copyright 2012 Twitter, Inc.
@@ -18,16 +18,15 @@
  * ============================================================ */
 
 
-!function ($) {
+!function( $ ){
 
-  "use strict"; // jshint ;_;
-
+  "use strict"
 
  /* DROPDOWN CLASS DEFINITION
   * ========================= */
 
-  var toggle = '[data-toggle=dropdown]'
-    , Dropdown = function (element) {
+  var toggle = '[data-toggle="dropdown"]'
+    , Dropdown = function ( element ) {
         var $el = $(element).on('click.dropdown.data-api', this.toggle)
         $('html').on('click.dropdown.data-api', function () {
           $el.parent().removeClass('open')
@@ -38,92 +37,39 @@
 
     constructor: Dropdown
 
-  , toggle: function (e) {
+  , toggle: function ( e ) {
       var $this = $(this)
+        , selector = $this.attr('data-target')
         , $parent
         , isActive
 
-      if ($this.is('.disabled, :disabled')) return
+      if (!selector) {
+        selector = $this.attr('href')
+        selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+      }
 
-      $parent = getParent($this)
+      $parent = $(selector)
+      $parent.length || ($parent = $this.parent())
 
       isActive = $parent.hasClass('open')
 
       clearMenus()
-
-      if (!isActive) {
-        $parent.toggleClass('open')
-        $this.focus()
-      }
+      !isActive && $parent.toggleClass('open')
 
       return false
-    }
-
-  , keydown: function (e) {
-      var $this
-        , $items
-        , $active
-        , $parent
-        , isActive
-        , index
-
-      if (!/(38|40|27)/.test(e.keyCode)) return
-
-      $this = $(this)
-
-      e.preventDefault()
-      e.stopPropagation()
-
-      if ($this.is('.disabled, :disabled')) return
-
-      $parent = getParent($this)
-
-      isActive = $parent.hasClass('open')
-
-      if (!isActive || (isActive && e.keyCode == 27)) return $this.click()
-
-      $items = $('[role=menu] li:not(.divider) a', $parent)
-
-      if (!$items.length) return
-
-      index = $items.index($items.filter(':focus'))
-
-      if (e.keyCode == 38 && index > 0) index--                                        // up
-      if (e.keyCode == 40 && index < $items.length - 1) index++                        // down
-      if (!~index) index = 0
-
-      $items
-        .eq(index)
-        .focus()
     }
 
   }
 
   function clearMenus() {
-    getParent($(toggle))
-      .removeClass('open')
-  }
-
-  function getParent($this) {
-    var selector = $this.attr('data-target')
-      , $parent
-
-    if (!selector) {
-      selector = $this.attr('href')
-      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
-    }
-
-    $parent = $(selector)
-    $parent.length || ($parent = $this.parent())
-
-    return $parent
+    $(toggle).parent().removeClass('open')
   }
 
 
   /* DROPDOWN PLUGIN DEFINITION
    * ========================== */
 
-  $.fn.dropdown = function (option) {
+  $.fn.dropdown = function ( option ) {
     return this.each(function () {
       var $this = $(this)
         , data = $this.data('dropdown')
@@ -139,12 +85,8 @@
    * =================================== */
 
   $(function () {
-    $('html')
-      .on('click.dropdown.data-api touchstart.dropdown.data-api', clearMenus)
-    $('body')
-      .on('click.dropdown touchstart.dropdown.data-api', '.dropdown', function (e) { e.stopPropagation() })
-      .on('click.dropdown.data-api touchstart.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
-      .on('keydown.dropdown.data-api touchstart.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
+    $('html').on('click.dropdown.data-api', clearMenus)
+    $('body').on('click.dropdown.data-api', toggle, Dropdown.prototype.toggle)
   })
 
-}(window.jQuery);
+}( window.jQuery );

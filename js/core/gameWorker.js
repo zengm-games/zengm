609f87d948360b835b5579784dc7b7d74b8169d3
Jeremy Scheff <jdscheff@gmail.com>
2013-03-06 13:02:54 -0500

    core.gameWorker, but it is not yet used.

---
+++ js/core/gameWorker.js
@@ -0,0 +1,20 @@
+importScripts("../lib/require.js");
+
+require({baseUrl: "../"}, ["core/gameSim"], function (gameSim) {
+    "use strict";
+
+    self.addEventListener("message", function (e) {
+        var gs, i, results, schedule, teams;
+
+        schedule = e.data.schedule;
+        teams = e.data.teams;
+        results = [];
+
+        // Should this be a loop, or should we assume only one game was passed? Depends on structure of what calls this.
+        for (i = 0; i < schedule.length; i++) {
+            gs = new gameSim.GameSim(schedule[i].gid, teams[schedule[i].homeTid], teams[schedule[i].awayTid]);
+            results.push(gs.run());
+        }
+        self.postMessage(results);
+    }, false);
+});

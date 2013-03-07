7327a90ba7e1abb787d46756e8211646f3535dc8
Jeremy Scheff <jdscheff@gmail.com>
2013-03-06 22:44:36 -0500

    Fixed "new window" links

--- js/templates/helpers.js
+++ js/templates/helpers.js
@@ -64,7 +64,7 @@ Handlebars.registerHelper("face", function (face) {
 Handlebars.registerHelper("new_window", function () {
     "use strict";
 
-    return new Handlebars.SafeString('<a href="javascript:api.moveToNewWindow()" class="new_window" title="Move To New Window" data-no-davis="true"><img src="/ico/new_window.png" height="16" width="16"></a>');
+    return new Handlebars.SafeString('<a href="javascript:(function () { api = require(\'api\'); api.moveToNewWindow(); }())" class="new_window" title="Move To New Window" data-no-davis="true"><img src="/ico/new_window.png" height="16" width="16"></a>');
 });
 
 

34e84846c088792c05b594672920ef6a8134aa17
Jeremy Scheff <jdscheff@gmail.com>
2013-03-08 13:11:30 -0500

    Added region populations to util.helpers.getTeams

--- js/util/helpers.js
+++ js/util/helpers.js
@@ -94,41 +94,59 @@ define(["globals"], function (g) {
     }
 
     /*Returns a list of all the teams. If selectedTid is passed, then the "selected" property will be added to each team, true only for selectedTid and false otherwise. If cb is passed, it is run with the teams array as an argument. Otherwise, the teams array is returned.*/
+    /**
+     * Get list of teams, along with some metadata
+     *
+     * Returns an array of 30 teams. Each array is an object with the following properties:
+     *     tid: Integer team ID (0 to 29).
+     *     cid: Integer conference ID (0=East, 1=West).
+     *     did: Integer division ID.
+     *     region: String region name.
+     *     name: String team name.
+     *     abbrev: String 3-letter team abbreviation.
+     *     pop: From http://www.forbes.com/nba-valuations/ number of people in the region, in millions of people.
+     *     selected: If selectedTid is defined, this is a boolean representing whether this team is "selected" or not (see below).
+     * 
+     * @memberOf util.helpers
+     * @param {number} selectedTid A team ID for a team that should be "selected" (as in, from a drop down menu).
+     * @param  {Function} cb          [description]
+     * @return {Array.Object} All teams.
+     */
     function getTeams(selectedTid, cb) {
         var i, teams;
 
-        selectedTid = selectedTid !== undefined ? parseInt(selectedTid, 10) : -1;
+        selectedTid = selectedTid !== undefined ? selectedTid : -1;
         teams = [
-            {tid: 0, cid: 0, did: 2, region: "Atlanta", name: "Herons", abbrev: "ATL"},
-            {tid: 1, cid: 0, did: 0, region: "Boston", name: "Clovers", abbrev: "BOS"},
-            {tid: 2, cid: 0, did: 0, region: "Brooklyn", name: "Nests", abbrev: "BK"},
-            {tid: 3, cid: 0, did: 2, region: "Charlotte", name: "Bay Cats", abbrev: "CHA"},
-            {tid: 4, cid: 0, did: 1, region: "Chicago", name: "Bullies", abbrev: "CHI"},
-            {tid: 5, cid: 0, did: 1, region: "Cleveland", name: "Cobras", abbrev: "CLE"},
-            {tid: 6, cid: 1, did: 3, region: "Dallas", name: "Mares", abbrev: "DAL"},
-            {tid: 7, cid: 1, did: 4, region: "Denver", name: "Ninjas", abbrev: "DEN"},
-            {tid: 8, cid: 0, did: 1, region: "Detroit", name: "Pumps", abbrev: "DET"},
-            {tid: 9, cid: 1, did: 5, region: "Golden State", name: "War Machine", abbrev: "GSW"},
-            {tid: 10, cid: 1, did: 3, region: "Houston", name: "Rock Throwers", abbrev: "HOU"},
-            {tid: 11, cid: 0, did: 1, region: "Indiana", name: "Passers", abbrev: "IND"},
-            {tid: 12, cid: 1, did: 5, region: "Los Angeles", name: "Cutters", abbrev: "LAC"},
-            {tid: 13, cid: 1, did: 5, region: "Los Angeles", name: "Lagoons", abbrev: "LAL"},
-            {tid: 14, cid: 1, did: 3, region: "Memphis", name: "Growls", abbrev: "MEM"},
-            {tid: 15, cid: 0, did: 2, region: "Miami", name: "Heatwave", abbrev: "MIA"},
-            {tid: 16, cid: 0, did: 1, region: "Milwaukee", name: "Buccaneers", abbrev: "MIL"},
-            {tid: 17, cid: 1, did: 4, region: "Minnesota", name: "Trees", abbrev: "MIN"},
-            {tid: 18, cid: 1, did: 3, region: "New Orleans", name: "Honey Bees", abbrev: "NOR"},
-            {tid: 19, cid: 0, did: 0, region: "New York", name: "Knights", abbrev: "NYK"},
-            {tid: 20, cid: 1, did: 4, region: "Oklahoma City", name: "Tornados", abbrev: "OKC"},
-            {tid: 21, cid: 0, did: 2, region: "Orlando", name: "Mystery", abbrev: "ORL"},
-            {tid: 22, cid: 0, did: 0, region: "Philadelphia", name: "Steaks", abbrev: "PHI"},
-            {tid: 23, cid: 1, did: 5, region: "Phoenix", name: "Stars", abbrev: "PHO"},
-            {tid: 24, cid: 1, did: 4, region: "Portland", name: "Trailer Park", abbrev: "POR"},
-            {tid: 25, cid: 1, did: 5, region: "Sacramento", name: "Killers", abbrev: "SAC"},
-            {tid: 26, cid: 1, did: 3, region: "San Antonio", name: "Spurts", abbrev: "SAS"},
-            {tid: 27, cid: 0, did: 0, region: "Toronto", name: "Ravens", abbrev: "TOR"},
-            {tid: 28, cid: 1, did: 4, region: "Utah", name: "Jugglers", abbrev: "UTA"},
-            {tid: 29, cid: 0, did: 2, region: "Washington", name: "Witches", abbrev: "WAS"}
+            {tid: 0, cid: 0, did: 2, region: "Atlanta", name: "Herons", abbrev: "ATL", pop: 5.4},
+            {tid: 1, cid: 0, did: 0, region: "Boston", name: "Clovers", abbrev: "BOS", pop: 5.0},
+            {tid: 2, cid: 0, did: 0, region: "Brooklyn", name: "Nests", abbrev: "BK", pop: 19.1},
+            {tid: 3, cid: 0, did: 2, region: "Charlotte", name: "Bay Cats", abbrev: "CHA", pop: 1.8},
+            {tid: 4, cid: 0, did: 1, region: "Chicago", name: "Bullies", abbrev: "CHI", pop: 9.6},
+            {tid: 5, cid: 0, did: 1, region: "Cleveland", name: "Cobras", abbrev: "CLE", pop: 2.1},
+            {tid: 6, cid: 1, did: 3, region: "Dallas", name: "Mares", abbrev: "DAL", pop: 6.4},
+            {tid: 7, cid: 1, did: 4, region: "Denver", name: "Ninjas", abbrev: "DEN", pop: 2.6},
+            {tid: 8, cid: 0, did: 1, region: "Detroit", name: "Pumps", abbrev: "DET", pop: 4.4},
+            {tid: 9, cid: 1, did: 5, region: "Golden State", name: "War Machine", abbrev: "GSW", pop: 4.3},
+            {tid: 10, cid: 1, did: 3, region: "Houston", name: "Rock Throwers", abbrev: "HOU", pop: 5.9},
+            {tid: 11, cid: 0, did: 1, region: "Indiana", name: "Passers", abbrev: "IND", pop: 1.7},
+            {tid: 12, cid: 1, did: 5, region: "Los Angeles", name: "Cutters", abbrev: "LAC", pop: 12.9},
+            {tid: 13, cid: 1, did: 5, region: "Los Angeles", name: "Lagoons", abbrev: "LAL", pop: 12.9},
+            {tid: 14, cid: 1, did: 3, region: "Memphis", name: "Growls", abbrev: "MEM", pop: 1.3},
+            {tid: 15, cid: 0, did: 2, region: "Miami", name: "Heatwave", abbrev: "MIA", pop: 5.6},
+            {tid: 16, cid: 0, did: 1, region: "Milwaukee", name: "Buccaneers", abbrev: "MIL", pop: 1.6},
+            {tid: 17, cid: 1, did: 4, region: "Minnesota", name: "Trees", abbrev: "MIN", pop: 3.3},
+            {tid: 18, cid: 1, did: 3, region: "New Orleans", name: "Honey Bees", abbrev: "NOR", pop: 1.2},
+            {tid: 19, cid: 0, did: 0, region: "New York", name: "Knights", abbrev: "NYK", pop: 19.1},
+            {tid: 20, cid: 1, did: 4, region: "Oklahoma City", name: "Tornados", abbrev: "OKC", pop: 1.2},
+            {tid: 21, cid: 0, did: 2, region: "Orlando", name: "Mystery", abbrev: "ORL", pop: 2.1},
+            {tid: 22, cid: 0, did: 0, region: "Philadelphia", name: "Steaks", abbrev: "PHI", pop: 6.0},
+            {tid: 23, cid: 1, did: 5, region: "Phoenix", name: "Stars", abbrev: "PHO", pop: 4.4},
+            {tid: 24, cid: 1, did: 4, region: "Portland", name: "Trailer Park", abbrev: "POR", pop: 2.2},
+            {tid: 25, cid: 1, did: 5, region: "Sacramento", name: "Killers", abbrev: "SAC", pop: 2.1},
+            {tid: 26, cid: 1, did: 3, region: "San Antonio", name: "Spurts", abbrev: "SAS", pop: 2.1},
+            {tid: 27, cid: 0, did: 0, region: "Toronto", name: "Ravens", abbrev: "TOR", pop: 6.0},
+            {tid: 28, cid: 1, did: 4, region: "Utah", name: "Jugglers", abbrev: "UTA", pop: 1.0},
+            {tid: 29, cid: 0, did: 2, region: "Washington", name: "Witches", abbrev: "WAS", pop: 5.7}
         ];
 
         if (selectedTid >= 0) {

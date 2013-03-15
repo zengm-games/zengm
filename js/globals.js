410bb4cab4069a825591cf29f6a2ba403a6e86d0
Jeremy Scheff <jdscheff@gmail.com>
2013-03-15 16:36:46 -0400

    Remove g.ticketPrice, as it is was not actually used anymore.

--- js/globals.js
+++ js/globals.js
@@ -12,7 +12,6 @@ define([], function () {
     g = {};
 
     // If any of these things are supposed to change at any point, they should be stored in gameAttributes rather than here.
-    g.ticketPrice = 25;  // [dollars]
     g.numTeams = 30;  // [dollars]
     g.confs = [{cid: 0, name: "Eastern Conference"}, {cid: 1, name: "Western Conference"}];
     g.divs = [{did: 0, cid: 0, name: "Atlantic"}, {did: 1, cid: 0, name: "Central"}, {did: 2, cid: 0, name: "Southeast"}, {did: 3, cid: 1, name: "Southwest"}, {did: 4, cid: 1, name: "Northwest"}, {did: 5, cid: 1, name: "Pacific"}];
@@ -50,7 +49,7 @@ define([], function () {
     }*/
 
     // THIS MUST BE ACCURATE OR BAD STUFF WILL HAPPEN
-    g.notInDb = ["dbm", "dbl", "lid", "ticketPrice", "numTeams", "confs", "divs", "salaryCap", "minPayroll", "luxuryPayroll", "luxuryTax", "minContract", "realtimeUpdate", "notInDb", "PHASE", "PLAYER", "gameSimWorkers"];
+    g.notInDb = ["dbm", "dbl", "lid", "numTeams", "confs", "divs", "salaryCap", "minPayroll", "luxuryPayroll", "luxuryTax", "minContract", "realtimeUpdate", "notInDb", "PHASE", "PLAYER", "gameSimWorkers"];
 
     return g;
 });

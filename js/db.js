b99e3dce531dc136f1f14d3e4fa1e0a12c148e5a
Jeremy Scheff <jdscheff@gmail.com>
2013-03-26 21:02:15 -0400

    Updated TODO

--- js/db.js
+++ js/db.js
@@ -99,7 +99,7 @@ define(["globals", "lib/jquery", "lib/underscore", "util/helpers"], function (g,
         playerStore.createIndex("draft.year", "draft.year", {unique: false});
         playerStore.createIndex("retiredYear", "retiredYear", {unique: false});
         playerStore.createIndex("statsTids", "statsTids", {unique: false, multiEntry: true});
-//            gameStore.createIndex("tid", "tid", {unique: false}); // Not used because it's useless without oppTid checking too
+//        gameStore.createIndex("tids", "tids", {unique: false, multiEntry: true}); // Not used because currently the season index is used. If multiple indexes are eventually supported, then use this too.
         gameStore.createIndex("season", "season", {unique: false});
         releasedPlayersStore.createIndex("tid", "tid", {unique: false});
         releasedPlayersStore.createIndex("contract.exp", "contract.exp", {unique: false});

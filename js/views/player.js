cea523d71a72e4aee6bcdfd829b1b9aeb58fe3eb
Jeremy Scheff <jdscheff@gmail.com>
2013-05-05 23:25:19 -0400

    Rename player variable to p so that core.player can be conveniently imported.

--- js/views/player.js
+++ js/views/player.js
@@ -2,7 +2,7 @@
  * @name views.player
  * @namespace View a single message.
  */
-define(["db", "globals", "ui", "core/freeAgents", "lib/faces", "lib/jquery", "lib/knockout", "lib/knockout.mapping", "util/bbgmView", "util/viewHelpers"], function (db, g, ui, freeAgents, faces, $, ko, komapping, bbgmView, viewHelpers) {
+define(["db", "globals", "ui", "core/freeAgents", "core/player", "lib/faces", "lib/jquery", "lib/knockout", "lib/knockout.mapping", "util/bbgmView", "util/viewHelpers"], function (db, g, ui, freeAgents, player, faces, $, ko, komapping, bbgmView, viewHelpers) {
     "use strict";
 
     var mapping;
@@ -20,8 +20,8 @@ define(["db", "globals", "ui", "core/freeAgents", "lib/faces", "lib/jquery", "li
                     komapping.fromJS(options.data, {
                         face: {
                             create: function (options) {
-console.log('mapping');
-console.log(options.data);
+//console.log('mapping');
+//console.log(options.data);
                                 return ko.observable(options.data);
                             }
                         }
@@ -39,29 +39,29 @@ console.log(options.data);
 
         if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || !vm.retired()) {
             g.dbl.transaction("players").objectStore("players").get(inputs.pid).onsuccess = function (event) {
-                var attributes, currentRatings, data, player, ratings, stats;
+                var attributes, currentRatings, data, p, ratings, stats;
 
                 attributes = ["pid", "name", "tid", "abbrev", "teamRegion", "teamName", "pos", "age", "hgtFt", "hgtIn", "weight", "born", "contract", "draft", "face", "mood", "injury", "salaries", "salariesTotal", "awards", "freeAgentMood"];
                 ratings = ["season", "abbrev", "age", "ovr", "pot", "hgt", "stre", "spd", "jmp", "endu", "ins", "dnk", "ft", "fg", "tp", "blk", "stl", "drb", "pss", "reb", "skills"];
                 stats = ["season", "abbrev", "age", "gp", "gs", "min", "fg", "fga", "fgp", "fgAtRim", "fgaAtRim", "fgpAtRim", "fgLowPost", "fgaLowPost", "fgpLowPost", "fgMidRange", "fgaMidRange", "fgpMidRange", "tp", "tpa", "tpp", "ft", "fta", "ftp", "orb", "drb", "trb", "ast", "tov", "stl", "blk", "pf", "pts", "per"];
 
-                player = db.getPlayer(event.target.result, null, null, attributes, stats, ratings, {playoffs: true, showNoStats: true, fuzz: true});
+                p = db.getPlayer(event.target.result, null, null, attributes, stats, ratings, {playoffs: true, showNoStats: true, fuzz: true});
 
                 // Account for extra free agent demands
-                if (player.tid === g.PLAYER.FREE_AGENT) {
-                    player.contract.amount = freeAgents.amountWithMood(player.contract.amount, player.freeAgentMood[g.userTid]);
+                if (p.tid === g.PLAYER.FREE_AGENT) {
+                    p.contract.amount = freeAgents.amountWithMood(p.contract.amount, p.freeAgentMood[g.userTid]);
                 }
 
-                currentRatings = player.ratings[player.ratings.length - 1];
+                currentRatings = p.ratings[p.ratings.length - 1];
 
                 vars = {
-                    player: player,
+                    player: p,
                     currentRatings: currentRatings,
-                    showTradeFor: player.tid !== g.userTid && player.tid >= 0,
-                    freeAgent: player.tid === g.PLAYER.FREE_AGENT,
-                    retired: player.tid === g.PLAYER.RETIRED,
-                    showContract: player.tid !== g.PLAYER.UNDRAFTED && player.tid !== g.PLAYER.RETIRED,
-                    injured: player.injury.type !== "Healthy"
+                    showTradeFor: p.tid !== g.userTid && p.tid >= 0,
+                    freeAgent: p.tid === g.PLAYER.FREE_AGENT,
+                    retired: p.tid === g.PLAYER.RETIRED,
+                    showContract: p.tid !== g.PLAYER.UNDRAFTED && p.tid !== g.PLAYER.RETIRED,
+                    injured: p.injury.type !== "Healthy"
                 };
 
                 deferred.resolve(vars);
@@ -77,7 +77,7 @@ console.log(options.data);
         }).extend({throttle: 1});
 
         ko.computed(function () {
-console.log(vm.player.face())
+//console.log(vm.player.face())
             faces.display("picture", vm.player.face());
         }).extend({throttle: 1});
     }

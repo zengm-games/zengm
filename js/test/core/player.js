c492df83e6714779ca9e7ddc68b960375f91a45e
Jeremy Scheff <jdscheff@gmail.com>
2013-05-06 00:28:31 -0400

    Initial tests for core.player.filter. More to come.

--- js/test/core/player.js
+++ js/test/core/player.js
@@ -2,7 +2,7 @@
  * @name test.core.player
  * @namespace Tests for core.player.
  */
-define(["db", "core/player"], function (db, player) {
+define(["db", "globals", "core/player"], function (db, g, player) {
     "use strict";
 
     describe("core/player", function () {
@@ -21,6 +21,65 @@ define(["db", "core/player"], function (db, player) {
 
                 p = player.generate(15, 19, "", 25, 55, 2012, false, 15.5);
                 p.stats.length.should.equal(1);
+            });
+        });
+        describe("#filter()", function () {
+            var p;
+
+            before(function () {
+                g.season = 2012;
+                p = player.generate(4, 19, "", 50, 60, 2012, false, 28);
+            });
+
+            it("should return requested info if tid/season match", function () {
+                var pf;
+
+                pf = player.filter(p, {
+                    attrs: ["tid", "awards"],
+                    ratings: ["season", "ovr"],
+                    stats: ["season", "abbrev", "fg", "fgp", "per"],
+                    tid: 4,
+                    season: 2012
+                });
+
+                pf.tid.should.equal(4);
+                pf.awards.should.have.length(0);
+                pf.ratings.season.should.equal(2012);
+                pf.ratings.ovr.should.be.a("number");
+                Object.keys(pf.ratings).should.have.length(2);
+                pf.stats.season.should.equal(2012);
+                pf.stats.abbrev.should.equal("CHI");
+                pf.stats.fg.should.be.a("number");
+                pf.stats.fgp.should.be.a("number");
+                pf.stats.per.should.be.a("number");
+                Object.keys(pf.stats).should.have.length(5);
+            });
+
+            it("should return undefined if tid does not match any on record", function () {
+                var pf;
+
+                pf = player.filter(p, {
+                    attrs: ["tid", "awards"],
+                    ratings: ["season", "ovr"],
+                    stats: ["season", "abbrev", "fg", "fgp", "per"],
+                    tid: 5,
+                    season: 2012
+                });
+
+                (typeof pf).should.equal("undefined");
+            });
+            it("should return undefined if season does not match any on record", function () {
+                var pf;
+
+                pf = player.filter(p, {
+                    attrs: ["tid", "awards"],
+                    ratings: ["season", "ovr"],
+                    stats: ["season", "abbrev", "fg", "fgp", "per"],
+                    tid: 4,
+                    season: 2013
+                });
+
+                (typeof pf).should.equal("undefined");
             });
         });
     });

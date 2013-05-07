015a7adee121ed8235c8a2d3eaa801b29c754a0c
Jeremy Scheff <jdscheff@gmail.com>
2013-05-07 15:24:47 -0400

    Some more tests for core.team.filter.

--- js/test/core/team.js
+++ js/test/core/team.js
@@ -33,8 +33,6 @@ define(["db", "globals", "core/league", "core/team"], function (db, g, league, t
             });
 
             it("should return requested info if tid/season match", function (done) {
-                var tf;
-
                 team.filter({
                     attrs: ["tid", "abbrev"],
                     seasonAttrs: ["season", "won"],
@@ -45,6 +43,7 @@ define(["db", "globals", "core/league", "core/team"], function (db, g, league, t
                     t.tid.should.equal(4);
                     t.abbrev.should.equal("CHI");
                     t.season.should.equal(g.season);
+                    t.won.should.equal(0);
                     t.gp.should.equal(10);
                     t.fg.should.equal(5);
                     t.fgp.should.equal(50);
@@ -54,106 +53,78 @@ define(["db", "globals", "core/league", "core/team"], function (db, g, league, t
                     done();
                 });
             });
-/*            it("should return requested info if tid/season match for an array of team objects", function () {
-                var i, pf;
+            it("should return an array if no team ID is specified", function (done) {
+                team.filter({
+                    attrs: ["tid", "abbrev"],
+                    seasonAttrs: ["season", "won"],
+                    stats: ["gp", "fg", "fgp"],
+                    season: g.season
+                }, function (teams) {
+                    teams.should.have.length(g.numTeams);
+                    teams[4].tid.should.equal(4);
+                    teams[4].abbrev.should.equal("CHI");
+                    teams[4].season.should.equal(g.season);
+                    teams[4].won.should.equal(0);
+                    teams[4].gp.should.equal(10);
+                    teams[4].fg.should.equal(5);
+                    teams[4].fgp.should.equal(50);
+                    Object.keys(teams[4]).should.have.length(7);
+                    teams[4].hasOwnProperty("stats").should.equal(false);
 
-                pf = player.filter([p, p], {
-                    attrs: ["tid", "awards"],
-                    ratings: ["season", "ovr"],
-                    stats: ["season", "abbrev", "fg", "fgp", "per"],
-                    tid: 4,
-                    season: 2012
+                    done();
                 });
-
-                for (i = 0; i < 2; i++) {
-                    pf[i].tid.should.equal(4);
-                    pf[i].awards.should.have.length(0);
-                    pf[i].ratings.season.should.equal(2012);
-                    pf[i].ratings.ovr.should.be.a("number");
-                    Object.keys(pf[i].ratings).should.have.length(2);
-                    pf[i].stats.season.should.equal(2012);
-                    pf[i].stats.abbrev.should.equal("CHI");
-                    pf[i].stats.fg.should.be.a("number");
-                    pf[i].stats.fgp.should.be.a("number");
-                    pf[i].stats.per.should.be.a("number");
-                    Object.keys(pf[i].stats).should.have.length(5);
-
-                    pf[i].hasOwnProperty("statsPlayoffs").should.equal(false);
-                    pf[i].hasOwnProperty("careerStats").should.equal(false);
-                    pf[i].hasOwnProperty("careerStatsPlayoffs").should.equal(false);
-                }
             });
-            it("should return requested info if tid/season match, even when no attrs requested", function () {
-                var pf;
-
-                pf = player.filter(p, {
-                    ratings: ["season", "ovr"],
-                    stats: ["season", "abbrev", "fg", "fgp", "per"],
+            it("should return requested info if tid/season match, even when no attrs requested", function (done) {
+                team.filter({
+                    seasonAttrs: ["season", "won"],
+                    stats: ["gp", "fg", "fgp"],
                     tid: 4,
-                    season: 2012
+                    season: g.season
+                }, function (t) {
+                    t.season.should.equal(g.season);
+                    t.won.should.equal(0);
+                    t.gp.should.equal(10);
+                    t.fg.should.equal(5);
+                    t.fgp.should.equal(50);
+                    Object.keys(t).should.have.length(5);
+
+                    done();
                 });
-
-                t.ratings.season.should.equal(2012);
-                t.ratings.ovr.should.be.a("number");
-                Object.keys(t.ratings).should.have.length(2);
-                t.stats.season.should.equal(2012);
-                t.stats.abbrev.should.equal("CHI");
-                t.stats.fg.should.be.a("number");
-                t.stats.fgp.should.be.a("number");
-                t.stats.per.should.be.a("number");
-                Object.keys(t.stats).should.have.length(5);
-
-                t.hasOwnProperty("statsPlayoffs").should.equal(false);
-                t.hasOwnProperty("careerStats").should.equal(false);
-                t.hasOwnProperty("careerStatsPlayoffs").should.equal(false);
             });
-            it("should return requested info if tid/season match, even when no seasonAttrs requested", function () {
-                var pf;
-
-                pf = player.filter(p, {
-                    attrs: ["tid", "awards"],
-                    stats: ["season", "abbrev", "fg", "fgp", "per"],
+            it("should return requested info if tid/season match, even when no seasonAttrs requested", function (done) {
+                team.filter({
+                    attrs: ["tid", "abbrev"],
+                    stats: ["gp", "fg", "fgp"],
                     tid: 4,
-                    season: 2012
+                    season: g.season
+                }, function (t) {
+                    t.tid.should.equal(4);
+                    t.abbrev.should.equal("CHI");
+                    t.gp.should.equal(10);
+                    t.fg.should.equal(5);
+                    t.fgp.should.equal(50);
+                    Object.keys(t).should.have.length(5);
+
+                    done();
                 });
-
-                t.tid.should.equal(4);
-                t.awards.should.have.length(0);
-                t.hasOwnProperty("ratings").should.equal(false);
-                t.stats.season.should.equal(2012);
-                t.stats.abbrev.should.equal("CHI");
-                t.stats.fg.should.be.a("number");
-                t.stats.fgp.should.be.a("number");
-                t.stats.per.should.be.a("number");
-                Object.keys(t.stats).should.have.length(5);
-
-                t.hasOwnProperty("statsPlayoffs").should.equal(false);
-                t.hasOwnProperty("careerStats").should.equal(false);
-                t.hasOwnProperty("careerStatsPlayoffs").should.equal(false);
             });
-            it("should return requested info if tid/season match, even when no stats requested", function () {
-                var pf;
-
-                pf = player.filter(p, {
-                    attrs: ["tid", "awards"],
-                    ratings: ["season", "ovr"],
+            it("should return requested info if tid/season match, even when no stats requested", function (done) {
+                team.filter({
+                    attrs: ["tid", "abbrev"],
+                    seasonAttrs: ["season", "won"],
                     tid: 4,
-                    season: 2012
+                    season: g.season
+                }, function (t) {
+                    t.tid.should.equal(4);
+                    t.abbrev.should.equal("CHI");
+                    t.season.should.equal(g.season);
+                    t.won.should.equal(0);
+                    Object.keys(t).should.have.length(4);
+
+                    done();
                 });
-console.log(pf);
-
-                t.tid.should.equal(4);
-                t.awards.should.have.length(0);
-                t.ratings.season.should.equal(2012);
-                t.ratings.ovr.should.be.a("number");
-                Object.keys(t.ratings).should.have.length(2);
-                t.hasOwnProperty("stats").should.equal(false);
-
-                t.hasOwnProperty("statsPlayoffs").should.equal(false);
-                t.hasOwnProperty("careerStats").should.equal(false);
-                t.hasOwnProperty("careerStatsPlayoffs").should.equal(false);
             });
-            it("should return season totals is options.totals is true, and per-game averages otherwise", function () {
+/*            it("should return season totals is options.totals is true, and per-game averages otherwise", function () {
                 var pf;
 
                 pf = player.filter(p, {

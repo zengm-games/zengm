da863c0d3a64d80791472daec6b7424661a5d674
Jeremy Scheff <jdscheff@gmail.com>
2013-05-07 22:33:06 -0400

    All players in team history shown in teamHistory

--- js/views/teamHistory.js
+++ js/views/teamHistory.js
@@ -2,13 +2,18 @@
  * @name views.teamHistory
  * @namespace Team history.
  */
-define(["db", "globals", "ui", "lib/jquery", "util/bbgmView", "util/viewHelpers"], function (db, g, ui, $, bbgmView, viewHelpers) {
+define(["db", "globals", "ui", "core/player", "lib/jquery", "lib/knockout", "lib/underscore", "util/bbgmView", "util/helpers", "util/viewHelpers"], function (db, g, ui, player, $, ko, _, bbgmView, helpers, viewHelpers) {
     "use strict";
 
     var mapping;
 
     mapping = {
         history: {
+            create: function (options) {
+                return options.data;
+            }
+        },
+        players: {
             create: function (options) {
                 return options.data;
             }
@@ -52,18 +57,41 @@ define(["db", "globals", "ui", "lib/jquery", "util/bbgmView", "util/viewHelpers"
                 }
                 history.reverse(); // Show most recent season first
 
-                deferred.resolve({
-                    abbrev: abbrev,
-                    history: history
-                });
+                g.dbl.transaction("players").objectStore("players").index("statsTids").getAll(g.userTid).onsuccess = function (event) {
+                    var i, players;
+
+                    players = player.filter(event.target.result, {
+                        attrs: ["pid", "name", "pos", "injury"],
+                        stats: ["gp", "min", "pts", "trb", "ast", "per"],
+                        tid: g.userTid
+                    });
+
+                    for (i = 0; i < players.length; i++) {
+                        delete players[i].ratings;
+                        delete players[i].stats;
+                    }
+console.log(players);
+
+                    deferred.resolve({
+                        abbrev: abbrev,
+                        history: history,
+                        players: players
+                    });
+                };
             };
 
             return deferred.promise();
         }
     }
 
-    function uiFirst() {
+    function uiFirst(vm) {
         ui.title("Team History");
+
+        ko.computed(function () {
+            ui.datatable($("#team-history-players"), 2, _.map(vm.players(), function (p) {
+                return [helpers.playerNameLabels(p.pid, p.name, p.injury, []), p.pos, String(p.careerStats.gp), helpers.round(p.careerStats.min, 1), helpers.round(p.careerStats.pts, 1), helpers.round(p.careerStats.trb, 1), helpers.round(p.careerStats.ast, 1), helpers.round(p.careerStats.per, 1)];
+            }));
+        }).extend({throttle: 1});
     }
 
     return bbgmView.init({

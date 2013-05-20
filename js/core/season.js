ee537e65f51bc3cd3a5f2fd92e9516bbde6ced7c
Jeremy Scheff <jdscheff@gmail.com>
2013-05-20 12:42:42 -0400

    If a player refuses to resign, this error message is displayed to the user now.

--- js/core/season.js
+++ js/core/season.js
@@ -2,7 +2,7 @@
  * @name core.season
  * @namespace Somewhat of a hodgepodge. Basically, this is for anything related to a single season that doesn't deserve to be broken out into its own file. Currently, this includes things that happen when moving between phases of the season (i.e. regular season to playoffs) and scheduling. As I write this, I realize that it might make more sense to break up those two classes of functions into two separate modules, but oh well.
  */
-define(["db", "globals", "ui", "core/contractNegotiation", "core/draft", "core/finances", "core/freeAgents", "core/player", "core/team", "lib/underscore", "util/helpers", "util/message", "util/random"], function (db, g, ui, contractNegotiation, draft, finances, freeAgents, player, team, _, helpers, message, random) {
+define(["db", "globals", "ui", "core/contractNegotiation", "core/draft", "core/finances", "core/freeAgents", "core/player", "core/team", "lib/jquery", "lib/underscore", "util/helpers", "util/message", "util/random"], function (db, g, ui, contractNegotiation, draft, finances, freeAgents, player, team, $, _, helpers, message, random) {
     "use strict";
 
     /**
@@ -840,7 +840,7 @@ define(["db", "globals", "ui", "core/contractNegotiation", "core/draft", "core/f
                                 // Open negotiations with player
                                 contractNegotiation.create(transaction, p.pid, true, function (error) {
                                     if (error !== undefined && error) {
-                                        return helpers.error(error);
+                                        $("#league_content").before('<div class="alert alert-error league-alert"><button type="button" class="close" data-dismiss="alert">&times;</button><p>' + error + '</p></div>');
                                     }
                                 });
                             });

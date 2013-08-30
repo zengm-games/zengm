04a656676938eed6724b2aaa97146038890458b0
badshar <badsharulez@hotmail.com>
2013-08-29 21:12:16 -0400

    Enabled support to change basic team information.
    Players can now change team regions, names and population sizes for each
    team.

--- js/views.js
+++ js/views.js
@@ -2,7 +2,7 @@
  * @name views
  * @namespace Contains all the view modules.
  */
-define(["views/dashboard", "views/deleteLeague", "views/draft", "views/draftSummary", "views/exportRosters", "views/freeAgents", "views/gameLog", "views/hallOfFame", "views/history", "views/inbox", "views/leaders", "views/leagueDashboard", "views/leagueFinances", "views/manual", "views/message", "views/negotiation", "views/negotiationList", "views/newLeague", "views/newTeam", "views/player", "views/playerRatingDists", "views/playerRatings", "views/playerShotLocations", "views/playerStatDists", "views/playerStats", "views/playoffs", "views/roster", "views/schedule", "views/standings", "views/teamFinances", "views/teamHistory", "views/teamShotLocations", "views/teamStatDists", "views/teamStats", "views/trade"], function (dashboard, deleteLeague, draft, draftSummary, exportRosters, freeAgents, gameLog, hallOfFame, history, inbox, leaders, leagueDashboard, leagueFinances, manual, message, negotiation, negotiationList, newLeague, newTeam, player, playerRatingDists, playerRatings, playerShotLocations, playerStatDists, playerStats, playoffs, roster, schedule, standings, teamFinances, teamHistory, teamShotLocations, teamStatDists, teamStats, trade) {
+define(["views/dashboard", "views/deleteLeague", "views/draft", "views/draftSummary", "views/exportRosters", "views/freeAgents", "views/gameLog", "views/hallOfFame", "views/editTeamNames", "views/history", "views/inbox", "views/leaders", "views/leagueDashboard", "views/leagueFinances", "views/manual", "views/message", "views/negotiation", "views/negotiationList", "views/newLeague", "views/newTeam", "views/player", "views/playerRatingDists", "views/playerRatings", "views/playerShotLocations", "views/playerStatDists", "views/playerStats", "views/playoffs", "views/roster", "views/schedule", "views/standings", "views/teamFinances", "views/teamHistory", "views/teamShotLocations", "views/teamStatDists", "views/teamStats", "views/trade"], function (dashboard, deleteLeague, draft, draftSummary, exportRosters, freeAgents, gameLog, hallOfFame, editTeamNames, history, inbox, leaders, leagueDashboard, leagueFinances, manual, message, negotiation, negotiationList, newLeague, newTeam, player, playerRatingDists, playerRatings, playerShotLocations, playerStatDists, playerStats, playoffs, roster, schedule, standings, teamFinances, teamHistory, teamShotLocations, teamStatDists, teamStats, trade) {
     "use strict";
 
     return {
@@ -19,6 +19,7 @@ define(["views/dashboard", "views/deleteLeague", "views/draft", "views/draftSumm
         leagueFinances: leagueFinances,
         history: history,
         hallOfFame: hallOfFame,
+        editTeamNames: editTeamNames,
         roster: roster,
         schedule: schedule,
         teamFinances: teamFinances,
---
+++ js/views/editTeamNames.js
@@ -0,0 +1,57 @@
+/**
+ * @name views.editTeamNames
+ * @namespace Edit Team Names.
+ */
+define(["db", "globals", "ui", "core/team", "lib/jquery", "util/bbgmView", "util/helpers"], function (db, g, ui, team, $, bbgmView, helpers) {
+    "use strict";
+    
+    function post(req) {
+        g.dbl.transaction("teams", "readwrite").objectStore("teams").openCursor().onsuccess = function (event) {
+            var cursor, i, t;
+            cursor = event.target.result;
+            if (cursor) {
+                t = cursor.value;
+                t.region = req.params.region[t.tid];
+                t.name = req.params.name[t.tid];
+                t.seasons[0].pop = req.params.population[t.tid];
+                cursor.update(t);
+                cursor.continue();
+            } else {
+                ui.realtimeUpdate([], helpers.leagueUrl([]));
+            }
+        }
+        db.setGameAttributes({teamRegionsCache: req.params.region});
+        db.setGameAttributes({teamNamesCache: req.params.name});
+    }
+    
+    function updateTeamInfo() {
+        var deferred;
+        deferred = $.Deferred();
+
+        team.filter({
+            attrs: ["tid", "region", "name", "seasons"],
+            seasonAttrs: ["pop"],
+            season: g.season
+        }, function (teams) {
+            deferred.resolve({
+                teams: teams,
+                region: teams.region,
+                name: teams.name,
+                pop: _.last(teams).seasons[0].pop
+            });
+        });
+        
+        return deferred.promise();
+    }
+
+    function uiFirst() {
+        ui.title("Edit Team Names");
+    }
+
+    return bbgmView.init({
+        id: "editTeamNames",
+        post: post,
+        runBefore: [updateTeamInfo],
+        uiFirst: uiFirst
+    });
+});

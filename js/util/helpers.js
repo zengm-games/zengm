d5751b5c6121a0bc42ad36c32b96993ef788a164
Jeremy Scheff <jdscheff@gmail.com>
2013-09-26 23:44:59 -0400

    Fixed injury icons

--- js/util/helpers.js
+++ js/util/helpers.js
@@ -423,10 +423,10 @@ define(["globals", "lib/jquery", "lib/knockout"], function (g, $, ko) {
 
         html = '<a href="' + leagueUrl(["player", pid]) + '">' + name + '</a>';
         if (injury.gamesRemaining > 0) {
-            html += '<span class="label label-important label-injury" title="' + injury.type + ' (out ' + injury.gamesRemaining + ' more games)">' + injury.gamesRemaining + '</span>';
+            html += '<span class="label label-danger label-injury" title="' + injury.type + ' (out ' + injury.gamesRemaining + ' more games)">' + injury.gamesRemaining + '</span>';
         } else if (injury.gamesRemaining === -1) {
             // This is used in box scores, where it would be confusing to display "out X more games" in old box scores
-            html += '<span class="label label-important label-injury" title="' + injury.type + '">&nbsp;</span>';
+            html += '<span class="label label-danger label-injury" title="' + injury.type + '">&nbsp;</span>';
         }
         html += skillsBlock(skills);
 

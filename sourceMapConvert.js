8a2ebb7947287707cfb4e3a4ea266d9a536e660c
Jeremy Scheff <jdscheff@gmail.com>
2013-10-05 18:41:28 -0400

    Script for using source maps to convert logged error messages, but logged columns are all 68 for some reason so this is not useful.

---
+++ sourceMapConvert.js
@@ -0,0 +1,20 @@
+// Problem is, my data says the column is always 68. But it's not. Why???
+
+var fs = require('fs');
+var sourceMap = require('source-map');
+
+fs.readFile("./gen/app.js.map", 'utf8', function (err, data) {
+    if (err) {
+        console.log('Error: ' + err);
+        return;
+    }
+
+    data = JSON.parse(data);
+
+    var smc = new sourceMap.SourceMapConsumer(data);
+
+    console.log(smc.originalPositionFor({
+        line: 14,
+        column: 68
+    }));
+});

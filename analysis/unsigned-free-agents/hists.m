ee3e962892ae07c81d87e5d77ff298135db2badb
Jeremy Scheff <jdscheff@gmail.com>
2013-12-07 01:05:15 -0500

    Tweak display

--- analysis/unsigned-free-agents/hists.m
+++ analysis/unsigned-free-agents/hists.m
@@ -1,14 +1,18 @@
+clear all; close all;
+
 x1 = load('0yrs.csv');
 x2 = load('10yrs.csv');
 
 figure;
+
+subplot(1,2,1);
 [n1, y1] = hist(x1(:,1));
 [n2, y2] = hist(x2(:,1));
 plot(y1, n1, y2, n2);
 title('Player Value');
 legend('0yrs', '10yrs');
 
-figure;
+subplot(1,2,2);
 [n1, y1] = hist(x1(:,2));
 [n2, y2] = hist(x2(:,2));
 plot(y1, n1, y2, n2);

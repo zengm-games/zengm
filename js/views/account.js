8ab9f5dd65a77273a122360cd8ca9ee71e3e856b
Jeremy Scheff <jdscheff@gmail.com>
2014-03-01 16:46:37 -0500

    Decent looking achievements list

--- js/views/account.js
+++ js/views/account.js
@@ -5,6 +5,58 @@
 define(["globals", "ui", "core/league", "lib/jquery", "util/account", "util/bbgmView", "util/helpers", "util/random", "util/viewHelpers"], function (g, ui, league, $, account, bbgmView, helpers, random, viewHelpers) {
     "use strict";
 
+    var allAchievements;
+
+    allAchievements = [{
+        aid: "participation",
+        name: "Participation",
+        desc: "You get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation",
+        name: "Participation",
+        desc: "You get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation",
+        name: "Participation",
+        desc: "You get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation",
+        name: "Participation",
+        desc: "You get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation",
+        name: "Participation",
+        desc: "You get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }, {
+        aid: "participation2",
+        name: "Participation2",
+        desc: "You2 get an achievement just for signing up, you special snowflake!"
+    }];
+
     function updateAccount(inputs, updateEvents, vm) {
         var deferred;
 
@@ -33,8 +85,14 @@ define(["globals", "ui", "core/league", "lib/jquery", "util/account", "util/bbgm
         if (updateEvents.indexOf("firstRun") >= 0) {
             deferred = $.Deferred();
 
-            account.getAchievements(function (achievements) {
-console.log(achievements);
+            account.getAchievements(function (userAchievements) {
+                var achievement, achievements, i;
+
+                achievements = allAchievements.slice();
+                for (i = 0; i < achievements.length; i++) {
+                    achievements[i].count = userAchievements[achievements[i].aid] !== undefined ? userAchievements[achievements[i].aid] : 0;
+                }
+
                 deferred.resolve({
                     achievements: achievements
                 });

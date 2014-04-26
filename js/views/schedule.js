9cf158e079cdfcea5357a5f7180b21fde9a53884
Jeremy Scheff <jdscheff@gmail.com>
2014-04-26 14:51:16 -0400

    Core of new Schedule UI

--- js/views/schedule.js
+++ js/views/schedule.js
@@ -8,14 +8,19 @@ define(["db", "globals", "ui", "core/season", "lib/jquery", "util/bbgmView", "ut
     var mapping;
 
     mapping = {
-        games: {
+        upcoming: {
+            create: function (options) {
+                return options.data;
+            }
+        },
+        completed: {
             create: function (options) {
                 return options.data;
             }
         }
     };
 
-    function updateSchedule(inputs, updateEvents, vm) {
+    function updateUpcoming(inputs, updateEvents, vm) {
         var deferred, vars;
 
         if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || updateEvents.indexOf("gameSim") >= 0) {
@@ -23,7 +28,7 @@ define(["db", "globals", "ui", "core/season", "lib/jquery", "util/bbgmView", "ut
             vars = {};
 
             season.getSchedule(null, 0, function (schedule_) {
-                var data, game, games, i, row, team0, team1;
+                var game, games, i, row, team0, team1;
 
                 games = [];
                 for (i = 0; i < schedule_.length; i++) {
@@ -34,12 +39,44 @@ define(["db", "globals", "ui", "core/season", "lib/jquery", "util/bbgmView", "ut
                         if (g.userTid === game.homeTid) {
                             row = {teams: [team1, team0], vsat: "at"};
                         } else {
-                            row = {teams: [team0, team1], vsat: "vs"};
+                            row = {teams: [team1, team0], vsat: "at"};
                         }
                         games.push(row);
                     }
                 }
-                vars = {games: games};
+                vars = {upcoming: games};
+                deferred.resolve(vars);
+            });
+
+            return deferred.promise();
+        }
+    }
+
+    function updateCompleted(inputs, updateEvents, vm) {
+        var deferred, vars;
+
+        if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || updateEvents.indexOf("gameSim") >= 0) {
+            deferred = $.Deferred();
+            vars = {};
+
+            season.getSchedule(null, 0, function (schedule_) {
+                var game, games, i, row, team0, team1;
+
+                games = [];
+                for (i = 0; i < schedule_.length; i++) {
+                    game = schedule_[i];
+                    if (g.userTid === game.homeTid || g.userTid === game.awayTid) {
+                        team0 = {tid: game.homeTid, abbrev: g.teamAbbrevsCache[game.homeTid], region: g.teamRegionsCache[game.homeTid], name: g.teamNamesCache[game.homeTid]};
+                        team1 = {tid: game.awayTid, abbrev: g.teamAbbrevsCache[game.awayTid], region: g.teamRegionsCache[game.awayTid], name: g.teamNamesCache[game.awayTid]};
+                        if (g.userTid === game.homeTid) {
+                            row = {teams: [team1, team0], vsat: "at"};
+                        } else {
+                            row = {teams: [team1, team0], vsat: "at"};
+                        }
+                        games.push(row);
+                    }
+                }
+                vars = {completed: games};
                 deferred.resolve(vars);
             });
 
@@ -54,7 +91,7 @@ define(["db", "globals", "ui", "core/season", "lib/jquery", "util/bbgmView", "ut
     return bbgmView.init({
         id: "schedule",
         mapping: mapping,
-        runBefore: [updateSchedule],
+        runBefore: [updateUpcoming, updateCompleted],
         uiFirst: uiFirst
     });
 });

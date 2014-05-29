b672416f45a5e6730bb273522fb7f65a96988743
Jeremy Scheff <jdscheff@gmail.com>
2014-05-28 20:31:53 -0400

    Fix historyAll for leagues with some data deleted (Improve Performance)

--- js/views/historyAll.js
+++ js/views/historyAll.js
@@ -40,29 +40,41 @@ define(["globals", "ui", "core/player", "core/team", "lib/jquery", "lib/knockout
                 }
 
                 tx.objectStore("teams").openCursor().onsuccess = function (event) {
-                    var cursor, i, t;
+                    var cursor, found, i, j, t;
 
                     cursor = event.target.result;
                     if (cursor) {
                         t = cursor.value;
 
                         // t.seasons has same season entries as the "seasons" array built from awards
-                        for (i = 0; i < t.seasons.length; i++) {
-                            if (t.seasons[i].playoffRoundsWon === 4) {
+                        for (i = 0; i < seasons.length; i++) {
+                            // Find corresponding entries in seasons and t.seasons. Can't assume they are the same because they aren't if some data has been deleted (Improve Performance)
+                            found = false;
+                            for (j = 0; j < t.seasons.length; j++) {
+                                if (t.seasons[j].season === seasons[i].season) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (!found) {
+                                continue;
+                            }
+
+                            if (t.seasons[j].playoffRoundsWon === 4) {
                                 seasons[i].champ = {
                                     abbrev: t.abbrev,
                                     region: t.region,
                                     name: t.name,
-                                    won: t.seasons[i].won,
-                                    lost: t.seasons[i].lost
+                                    won: t.seasons[j].won,
+                                    lost: t.seasons[j].lost
                                 };
-                            } else if (t.seasons[i].playoffRoundsWon === 3) {
+                            } else if (t.seasons[j].playoffRoundsWon === 3) {
                                 seasons[i].runnerUp = {
                                     abbrev: t.abbrev,
                                     region: t.region,
                                     name: t.name,
-                                    won: t.seasons[i].won,
-                                    lost: t.seasons[i].lost
+                                    won: t.seasons[j].won,
+                                    lost: t.seasons[j].lost
                                 };
                             }
                         }
@@ -94,12 +106,26 @@ define(["globals", "ui", "core/player", "core/team", "lib/jquery", "lib/knockout
             return helpers.playerNameLabels(award.pid, award.name) + ' (<a href="' + helpers.leagueUrl(["roster", g.teamAbbrevsCache[award.tid], season]) + '">' + g.teamAbbrevsCache[award.tid] + '</a>)';
         };
         teamName = function (t, season) {
-            return '<a href="' + helpers.leagueUrl(["roster", t.abbrev, season]) + '">' + t.region + '</a> (' + t.won + '-' + t.lost + ')';
+            if (t) {
+                return '<a href="' + helpers.leagueUrl(["roster", t.abbrev, season]) + '">' + t.region + '</a> (' + t.won + '-' + t.lost + ')';
+            } else {
+                // This happens if there is missing data, such as from Improve Performance
+                return 'N/A';
+            }
         };
 
         ko.computed(function () {
             ui.datatable($("#history-all"), 0, _.map(vm.seasons(), function (s) {
-                return ['<a href="' + helpers.leagueUrl(["history", s.season]) + '">' + s.season + '</a>', teamName(s.champ, s.season), teamName(s.runnerUp, s.season), awardName(s.finalsMvp, s.season), awardName(s.mvp, s.season), awardName(s.dpoy, s.season), awardName(s.roy, s.season)];
+                var seasonLink;
+
+                if (s.champ) {
+                    seasonLink = '<a href="' + helpers.leagueUrl(["history", s.season]) + '">' + s.season + '</a>';
+                } else {
+                    // This happens if there is missing data, such as from Improve Performance
+                    seasonLink = String(s.season);
+                }
+
+                return [seasonLink, teamName(s.champ, s.season), teamName(s.runnerUp, s.season), awardName(s.finalsMvp, s.season), awardName(s.mvp, s.season), awardName(s.dpoy, s.season), awardName(s.roy, s.season)];
             }));
         }).extend({throttle: 1});
     }

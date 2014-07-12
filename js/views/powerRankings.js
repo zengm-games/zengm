e0a856a950c7c781aeb5406464651a278f8a3dce
Jeremy Scheff <jdscheff@gmail.com>
2014-07-12 11:48:25 -0400

    Don't include pot in talent calculation for power rankings

--- js/views/powerRankings.js
+++ js/views/powerRankings.js
@@ -32,7 +32,7 @@ define(["globals", "ui", "core/team", "core/player", "lib/jquery", "lib/undersco
             }, function (teams) {
 
                 tx.objectStore("players").index("tid").getAll(IDBKeyRange.lowerBound(0)).onsuccess = function (event) {
-                    var i, j, overallRanks, overallRankMetric, performanceRanks, players, playerValuesByTid, talentRanks, weights;
+                    var i, j, overallRankMetric, players, playerValuesByTid, weights;
 
                     players = event.target.result;
 
@@ -47,7 +47,7 @@ define(["globals", "ui", "core/team", "core/player", "lib/jquery", "lib/undersco
                     // TALENT
                     // Get player values and sort by tid
                     for (i = 0; i < players.length; i++) {
-                        playerValuesByTid[players[i].tid].push(player.value(players[i]));
+                        playerValuesByTid[players[i].tid].push(player.value(players[i], {noPot: true}));
                     }
                     // Sort and weight the values - doesn't matter how good your 12th man is
                     weights = [2, 1.5, 1.25, 1.1, 1, 0.9, 0.8, 0.7, 0.6, 0.4, 0.2, 0.1];

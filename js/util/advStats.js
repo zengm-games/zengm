e6723cc7adcf3e8a8168f42e04b2132f4779bdaa
Jeremy Scheff <jdscheff@gmail.com>
2014-12-14 22:21:35 -0500

    Fix flow in advStats

--- js/util/advStats.js
+++ js/util/advStats.js
@@ -28,7 +28,7 @@ define(["dao", "globals", "core/player", "core/team", "lib/bluebird", "lib/under
 
             // Total league stats (not per game averages) - gp, ft, pf, ast, fg, pts, fga, orb, tov, fta, trb
             leagueStats = ["gp", "ft", "pf", "ast", "fg", "pts", "fga", "orb", "tov", "fta", "trb"];
-            league = _.reduce(teams, function (memo, team) {
+            league = teams.reduce(function (memo, team) {
                 var i;
                 for (i = 0; i < leagueStats.length; i++) {
                     if (memo.hasOwnProperty(leagueStats[i])) {
@@ -40,9 +40,8 @@ define(["dao", "globals", "core/player", "core/team", "lib/bluebird", "lib/under
                 return memo;
             }, {});
 
-            // If no games have been played, somehow, don't continue. But why would no games be played?
+            // If no games have been played, somehow, don't continue. But why would no games be played? I don't know, but it happens some times.
             if (league.gp === 0) {
-// Should cancel whole promise chain!
                 return;
             }
 
@@ -59,137 +58,138 @@ define(["dao", "globals", "core/player", "core/team", "lib/bluebird", "lib/under
 
             // Total player stats (not per game averages) - min, tp, ast, fg, ft, tov, fga, fta, trb, orb, stl, blk, pf
             // Active players have tid >= 0
-            return [league, teams, dao.players.getAll({
+            return dao.players.getAll({
                 index: "tid",
                 key: IDBKeyRange.lowerBound(0),
                 statsSeasons: [g.season],
                 statsPlayoffs: g.PHASE.PLAYOFFS === g.phase
-            })];
-        }).spread(function (league, teams, players) {
-            var aPER, drbp, EWA, factor, i, mins, PER, tid, uPER, vop, tx;
+            // Can't drop this then to another level because of the short circuit return above
+            }).then(function (players) {
+                var aPER, drbp, EWA, factor, i, mins, PER, tid, uPER, vop, tx;
 
-            players = player.filter(players, {
-                attrs: ["pid", "tid", "pos"],
-                stats: ["min", "tp", "ast", "fg", "ft", "tov", "fga", "fta", "trb", "orb", "stl", "blk", "pf"],
-                season: g.season,
-                totals: true,
-                playoffs: g.PHASE.PLAYOFFS === g.phase
-            });
+                players = player.filter(players, {
+                    attrs: ["pid", "tid", "pos"],
+                    stats: ["min", "tp", "ast", "fg", "ft", "tov", "fga", "fta", "trb", "orb", "stl", "blk", "pf"],
+                    season: g.season,
+                    totals: true,
+                    playoffs: g.PHASE.PLAYOFFS === g.phase
+                });
 
-            aPER = [];
-            mins = [];
-            league.aPER = 0;
-            for (i = 0; i < players.length; i++) {
-                tid = players[i].tid;
-
-                // Is the player active?
-                players[i].active = true;  // Assume all players are active, since the IndexedDB query above only takes tid >= 0
-                if (g.PHASE.PLAYOFFS === g.phase) {
-                    players[i].active = false;
-                    if (!_.isEmpty(players[i].statsPlayoffs)) {
-                        players[i].active = true;
-                        players[i].stats = players[i].statsPlayoffs;
-                    }
-                }
-
-                if (players[i].active) {  // No need to calculate for non-active players
-                    factor = (2 / 3) - (0.5 * (league.ast / league.fg)) / (2 * (league.fg / league.ft));
-                    vop = league.pts / (league.fga - league.orb + league.tov + 0.44 * league.fta);
-                    drbp = (league.trb - league.orb) / league.trb;  // DRB%
-
-                    if (players[i].stats.min > 0) {
-                        uPER = (1 / players[i].stats.min) *
-                               (players[i].stats.tp
-                               + (2 / 3) * players[i].stats.ast
-                               + (2 - factor * (teams[tid].ast / teams[tid].fg)) * players[i].stats.fg
-                               + (players[i].stats.ft * 0.5 * (1 + (1 - (teams[tid].ast / teams[tid].fg)) + (2 / 3) * (teams[tid].ast / teams[tid].fg)))
-                               - vop * players[i].stats.tov
-                               - vop * drbp * (players[i].stats.fga - players[i].stats.fg)
-                               - vop * 0.44 * (0.44 + (0.56 * drbp)) * (players[i].stats.fta - players[i].stats.ft)
-                               + vop * (1 - drbp) * (players[i].stats.trb - players[i].stats.orb)
-                               + vop * drbp * players[i].stats.orb
-                               + vop * players[i].stats.stl
-                               + vop * drbp * players[i].stats.blk
-                               - players[i].stats.pf * ((league.ft / league.pf) - 0.44 * (league.fta / league.pf) * vop));
-                    } else {
-                        uPER = 0;
-                    }
-
-                    aPER[i] = teams[tid].pace * uPER;
-                    league.aPER = league.aPER + aPER[i] * players[i].stats.min;
-
-                    mins[i] = players[i].stats.min; // Save for EWA calculation
-                }
-            }
-
-            league.aPER = league.aPER / (league.gp * 5 * 48);
-
-            PER = _.map(aPER, function (num) { return num * (15 / league.aPER); });
-
-            // Estimated Wins Added http://insider.espn.go.com/nba/hollinger/statistics
-            EWA = [];
-            (function () {
-                var i, prl, prls, va;
-
-                // Position Replacement Levels
-                prls = {
-                    PG: 11,
-                    G: 10.75,
-                    SG: 10.5,
-                    GF: 10.5,
-                    SF: 10.5,
-                    F: 11,
-                    PF: 11.5,
-                    FC: 11.05,
-                    C: 10.6
-                };
-
+                aPER = [];
+                mins = [];
+                league.aPER = 0;
                 for (i = 0; i < players.length; i++) {
-                    if (players[i].active) {
-                        if (prls.hasOwnProperty(players[i].pos)) {
-                            prl = prls[players[i].pos];
+                    tid = players[i].tid;
+
+                    // Is the player active?
+                    players[i].active = true;  // Assume all players are active, since the IndexedDB query above only takes tid >= 0
+                    if (g.PHASE.PLAYOFFS === g.phase) {
+                        players[i].active = false;
+                        if (!_.isEmpty(players[i].statsPlayoffs)) {
+                            players[i].active = true;
+                            players[i].stats = players[i].statsPlayoffs;
+                        }
+                    }
+
+                    if (players[i].active) {  // No need to calculate for non-active players
+                        factor = (2 / 3) - (0.5 * (league.ast / league.fg)) / (2 * (league.fg / league.ft));
+                        vop = league.pts / (league.fga - league.orb + league.tov + 0.44 * league.fta);
+                        drbp = (league.trb - league.orb) / league.trb;  // DRB%
+
+                        if (players[i].stats.min > 0) {
+                            uPER = (1 / players[i].stats.min) *
+                                   (players[i].stats.tp
+                                   + (2 / 3) * players[i].stats.ast
+                                   + (2 - factor * (teams[tid].ast / teams[tid].fg)) * players[i].stats.fg
+                                   + (players[i].stats.ft * 0.5 * (1 + (1 - (teams[tid].ast / teams[tid].fg)) + (2 / 3) * (teams[tid].ast / teams[tid].fg)))
+                                   - vop * players[i].stats.tov
+                                   - vop * drbp * (players[i].stats.fga - players[i].stats.fg)
+                                   - vop * 0.44 * (0.44 + (0.56 * drbp)) * (players[i].stats.fta - players[i].stats.ft)
+                                   + vop * (1 - drbp) * (players[i].stats.trb - players[i].stats.orb)
+                                   + vop * drbp * players[i].stats.orb
+                                   + vop * players[i].stats.stl
+                                   + vop * drbp * players[i].stats.blk
+                                   - players[i].stats.pf * ((league.ft / league.pf) - 0.44 * (league.fta / league.pf) * vop));
                         } else {
-                            // This should never happen unless someone manually enters the wrong position, which can happen in custom roster files
-                            prl = 10.75;
+                            uPER = 0;
                         }
 
-                        va = players[i].stats.min * (PER[i] - prl) / 67;
+                        aPER[i] = teams[tid].pace * uPER;
+                        league.aPER = league.aPER + aPER[i] * players[i].stats.min;
 
-                        EWA[i] = va / 30 * 0.8; // 0.8 is a fudge factor to approximate the difference between (BBGM) EWA and (real) win shares
+                        mins[i] = players[i].stats.min; // Save for EWA calculation
                     }
                 }
-            }());
 
-            return new Promise(function (resolve, reject) {
-                // Save to database
-                tx = g.dbl.transaction("playerStats", "readwrite");
-                for (i = 0; i < players.length; i++) {
-                    if (players[i].active) {
-                        (function (i) {
-                            var key;
-                            key = [players[i].pid, g.season, players[i].tid];
-                            tx.objectStore("playerStats").index("pid, season, tid").openCursor(key, "prev").onsuccess = function (event) {
-                                var cursor, playerStats;
+                league.aPER = league.aPER / (league.gp * 5 * 48);
 
-                                cursor = event.target.result;
-                                playerStats = cursor.value;
+                PER = _.map(aPER, function (num) { return num * (15 / league.aPER); });
 
-                                // Since index is not on playoffs, manually check
-                                if (playerStats.playoffs !== (g.phase === g.PHASE.PLAYOFFS)) {
-                                    return cursor.continue();
-                                }
+                // Estimated Wins Added http://insider.espn.go.com/nba/hollinger/statistics
+                EWA = [];
+                (function () {
+                    var i, prl, prls, va;
 
-                                playerStats.per = PER[i];
-                                playerStats.ewa = EWA[i];
+                    // Position Replacement Levels
+                    prls = {
+                        PG: 11,
+                        G: 10.75,
+                        SG: 10.5,
+                        GF: 10.5,
+                        SF: 10.5,
+                        F: 11,
+                        PF: 11.5,
+                        FC: 11.05,
+                        C: 10.6
+                    };
 
-                                cursor.update(playerStats);
-                            };
-                        }(i));
+                    for (i = 0; i < players.length; i++) {
+                        if (players[i].active) {
+                            if (prls.hasOwnProperty(players[i].pos)) {
+                                prl = prls[players[i].pos];
+                            } else {
+                                // This should never happen unless someone manually enters the wrong position, which can happen in custom roster files
+                                prl = 10.75;
+                            }
+
+                            va = players[i].stats.min * (PER[i] - prl) / 67;
+
+                            EWA[i] = va / 30 * 0.8; // 0.8 is a fudge factor to approximate the difference between (BBGM) EWA and (real) win shares
+                        }
                     }
-                }
-                tx.oncomplete = function () {
-                    resolve();
-                };
+                }());
+
+                return new Promise(function (resolve, reject) {
+                    // Save to database
+                    tx = g.dbl.transaction("playerStats", "readwrite");
+                    for (i = 0; i < players.length; i++) {
+                        if (players[i].active) {
+                            (function (i) {
+                                var key;
+                                key = [players[i].pid, g.season, players[i].tid];
+                                tx.objectStore("playerStats").index("pid, season, tid").openCursor(key, "prev").onsuccess = function (event) {
+                                    var cursor, playerStats;
+
+                                    cursor = event.target.result;
+                                    playerStats = cursor.value;
+
+                                    // Since index is not on playoffs, manually check
+                                    if (playerStats.playoffs !== (g.phase === g.PHASE.PLAYOFFS)) {
+                                        return cursor.continue();
+                                    }
+
+                                    playerStats.per = PER[i];
+                                    playerStats.ewa = EWA[i];
+
+                                    cursor.update(playerStats);
+                                };
+                            }(i));
+                        }
+                    }
+                    tx.oncomplete = function () {
+                        resolve();
+                    };
+                });
             });
         });
     }

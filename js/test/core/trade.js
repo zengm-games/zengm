75747357dc3408f736d06592837020fb53998093
Jeremy Scheff <jdscheff@gmail.com>
2014-12-21 12:16:02 -0500

    core.trade tests mostly promisified, but one fails

--- js/test/core/trade.js
+++ js/test/core/trade.js
@@ -8,83 +8,75 @@ define(["db", "globals", "core/league", "core/trade"], function (db, g, league, 
     describe("core/trade", function () {
         var testCreateTrade;
 
-        before(function (done) {
-            db.connectMeta(function () {
-                league.create("Test", 0, undefined, 2013, false, function () {
-                    done();
-                });
+        before(function () {
+            return db.connectMeta().then(function () {
+                return league.create("Test", 0, undefined, 2013, false);
             });
         });
-        after(function (done) {
-            league.remove(g.lid, done);
+        after(function () {
+            return league.remove(g.lid);
         });
-        afterEach(function (done) {
+        afterEach(function () {
             // Set to a trade with team 1 and no players;
-            trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 1, pids: [], dpids: []}], function () {
-                trade.clear(done);
-            });
+            return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 1, pids: [], dpids: []}]).then(trade.clear);
         });
 
-        testCreateTrade = function (otherTidTest, userPidsTest, otherPidsTest, cb) {
-            trade.get(function (teams) {
+        testCreateTrade = function (otherTidTest, userPidsTest, otherPidsTest) {
+            return trade.get().then(function (teams) {
                 teams[1].tid.should.equal(otherTidTest);
                 JSON.stringify(teams[0].pids).should.equal(JSON.stringify(userPidsTest));
                 JSON.stringify(teams[1].pids).should.equal(JSON.stringify(otherPidsTest));
-                cb();
             });
         };
 
         describe("#create()", function () {
-            it("should create trade with team ID", function (done) {
-                trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 22, pids: [], dpids: []}], function () {
-                    testCreateTrade(22, [], [], done);
+            it("should create trade with team ID", function () {
+                return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 22, pids: [], dpids: []}]).then(function () {
+                    return testCreateTrade(22, [], []);
                 });
             });
-            it("should create trade with player ID", function (done) {
-                trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: null, pids: [81], dpids: []}], function () {
-                    testCreateTrade(2, [], [81], done);
+            it("should create trade with player ID", function () {
+                return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: null, pids: [81], dpids: []}]).then(function () {
+                    return testCreateTrade(2, [], [81]);
                 });
             });
         });
 
         describe("#updatePlayers()", function () {
-            it("should allow players from both teams to be set", function (done) {
-                trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}], function () {
+            it("should allow players from both teams to be set", function () {
+                return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}]).then(function () {
                     var userPidsTest, otherPidsTest;
 
                     userPidsTest = [48, 50];
                     otherPidsTest = [87, 97];
-                    trade.updatePlayers([{tid: g.userTid, pids: userPidsTest, dpids: []}, {tid: 3, pids: otherPidsTest, dpids: []}], function (teams) {
+                    return trade.updatePlayers([{tid: g.userTid, pids: userPidsTest, dpids: []}, {tid: 3, pids: otherPidsTest, dpids: []}]).then(function (teams) {
                         JSON.stringify(teams[0].pids).should.equal(JSON.stringify(userPidsTest));
                         JSON.stringify(teams[1].pids).should.equal(JSON.stringify(otherPidsTest));
-                        done();
                     });
                 });
             });
-            it("should filter out invalid players", function (done) {
-                trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}], function () {
-                    trade.updatePlayers([{tid: g.userTid, pids: [1, 16, 20, 48, 50, 90], dpids: []}, {tid: 3, pids: [12, 63, 70, 87, 97, 524], dpids: []}], function (teams) {
-                        JSON.stringify(teams[0].pids).should.equal(JSON.stringify([48, 50]));
-                        JSON.stringify(teams[1].pids).should.equal(JSON.stringify([87, 97]));
-                        done();
-                    });
+            it("should filter out invalid players", function () {
+                return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}]).then(function () {
+                    return trade.updatePlayers([{tid: g.userTid, pids: [1, 16, 20, 48, 50, 90], dpids: []}, {tid: 3, pids: [12, 63, 70, 87, 97, 524], dpids: []}]);
+                }).then(function (teams) {
+                    JSON.stringify(teams[0].pids).should.equal(JSON.stringify([48, 50]));
+                    JSON.stringify(teams[1].pids).should.equal(JSON.stringify([87, 97]));
                 });
             });
-            it("should delete the other team's players, but not the user's players, from the trade when a new team is selected", function (done) {
-                trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}], function () {
+            it("should delete the other team's players, but not the user's players, from the trade when a new team is selected", function () {
+                return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 3, pids: [], dpids: []}]).then(function () {
                     var userPidsTest, otherPidsTest;
 
                     userPidsTest = [48, 50];
                     otherPidsTest = [87, 97];
-                    trade.updatePlayers([{tid: g.userTid, pids: userPidsTest, dpids: []}, {tid: 3, pids: otherPidsTest, dpids: []}], function (teams) {
+                    return trade.updatePlayers([{tid: g.userTid, pids: userPidsTest, dpids: []}, {tid: 3, pids: otherPidsTest, dpids: []}]).then(function (teams) {
                         JSON.stringify(teams[0].pids).should.equal(JSON.stringify(userPidsTest));
                         JSON.stringify(teams[1].pids).should.equal(JSON.stringify(otherPidsTest));
-                        trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 4, pids: [], dpids: []}], function () {
-                            trade.get(function (teams) {
-                                JSON.stringify(teams[0].pids).should.equal(JSON.stringify(userPidsTest));
-                                JSON.stringify(teams[1].pids).should.equal(JSON.stringify([]));
-                                done();
-                            })
+                        return trade.create([{tid: g.userTid, pids: [], dpids: []}, {tid: 4, pids: [], dpids: []}]);
+                    }).then(function () {
+                        trade.get(function (teams) {
+                            JSON.stringify(teams[0].pids).should.equal(JSON.stringify(userPidsTest));
+                            JSON.stringify(teams[1].pids).should.equal(JSON.stringify([]));
                         });
                     });
                 });

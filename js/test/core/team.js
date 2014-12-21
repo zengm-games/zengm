8e30cbd54a596e7b31e3dce8910d9341645906af
Jeremy Scheff <jdscheff@gmail.com>
2014-12-21 11:46:38 -0500

    Promisify team.filter tests

--- js/test/core/team.js
+++ js/test/core/team.js
@@ -2,46 +2,40 @@
  * @name test.core.team
  * @namespace Tests for core.team.
  */
-define(["db", "globals", "core/league", "core/player", "core/team"], function (db, g, league, player, team) {
+define(["dao", "db", "globals", "core/league", "core/player", "core/team"], function (dao, db, g, league, player, team) {
     "use strict";
 
     describe("core/team", function () {
         describe("#filter()", function () {
-            before(function (done) {
-                db.connectMeta(function () {
-                    league.create("Test", 0, undefined, 2013, false, function () {
-                        g.dbl.transaction("teams", "readwrite").objectStore("teams").openCursor(4).onsuccess = function (event) {
-                            var cursor, t;
+            before(function () {
+                return db.connectMeta().then(function () {
+                    return league.create("Test", 0, undefined, 2013, false);
+                }).then(function () {
+                    return dao.teams.get({key: 4}).then(function (t) {
+                        t.stats[0].gp = 10;
+                        t.stats[0].fg = 50;
+                        t.stats[0].fga = 100;
+                        t = team.addStatsRow(t, true);
+                        t.stats[1].gp = 4;
+                        t.stats[1].fg = 12;
+                        t.stats[1].fga = 120;
 
-                            cursor = event.target.result;
-                            t = cursor.value;
-                            t.stats[0].gp = 10;
-                            t.stats[0].fg = 50;
-                            t.stats[0].fga = 100;
-                            t = team.addStatsRow(t, true);
-                            t.stats[1].gp = 4;
-                            t.stats[1].fg = 12;
-                            t.stats[1].fga = 120;
-
-                            cursor.update(t);
-
-                            done();
-                        };
+                        return dao.teams.put({value: t});
                     });
                 });
             });
-            after(function (done) {
-                league.remove(g.lid, done);
+            after(function () {
+                return league.remove(g.lid);
             });
 
-            it("should return requested info if tid/season match", function (done) {
-                team.filter({
+            it("should return requested info if tid/season match", function () {
+                return team.filter({
                     attrs: ["tid", "abbrev"],
                     seasonAttrs: ["season", "won", "payroll"],
                     stats: ["gp", "fg", "fgp"],
                     tid: 4,
                     season: g.season
-                }, function (t) {
+                }).then(function (t) {
                     t.tid.should.equal(4);
                     t.abbrev.should.equal("CIN");
                     t.season.should.equal(g.season);
@@ -52,17 +46,15 @@ define(["db", "globals", "core/league", "core/player", "core/team"], function (d
                     t.fgp.should.equal(50);
                     Object.keys(t).should.have.length(8);
                     t.hasOwnProperty("stats").should.equal(false);
-
-                    done();
                 });
             });
-            it("should return an array if no team ID is specified", function (done) {
-                team.filter({
+            it("should return an array if no team ID is specified", function () {
+                return team.filter({
                     attrs: ["tid", "abbrev"],
                     seasonAttrs: ["season", "won"],
                     stats: ["gp", "fg", "fgp"],
                     season: g.season
-                }, function (teams) {
+                }).then(function (teams) {
                     teams.should.have.length(g.numTeams);
                     teams[4].tid.should.equal(4);
                     teams[4].abbrev.should.equal("CIN");
@@ -73,99 +65,87 @@ define(["db", "globals", "core/league", "core/player", "core/team"], function (d
                     teams[4].fgp.should.equal(50);
                     Object.keys(teams[4]).should.have.length(7);
                     teams[4].hasOwnProperty("stats").should.equal(false);
-
-                    done();
                 });
             });
-            it("should return requested info if tid/season match, even when no attrs requested", function (done) {
-                team.filter({
+            it("should return requested info if tid/season match, even when no attrs requested", function () {
+                return team.filter({
                     seasonAttrs: ["season", "won"],
                     stats: ["gp", "fg", "fgp"],
                     tid: 4,
                     season: g.season
-                }, function (t) {
+                }).then(function (t) {
                     t.season.should.equal(g.season);
                     t.won.should.equal(0);
                     t.gp.should.equal(10);
                     t.fg.should.equal(5);
                     t.fgp.should.equal(50);
                     Object.keys(t).should.have.length(5);
-
-                    done();
                 });
             });
-            it("should return requested info if tid/season match, even when no seasonAttrs requested", function (done) {
-                team.filter({
+            it("should return requested info if tid/season match, even when no seasonAttrs requested", function () {
+                return team.filter({
                     attrs: ["tid", "abbrev"],
                     stats: ["gp", "fg", "fgp"],
                     tid: 4,
                     season: g.season
-                }, function (t) {
+                }).then(function (t) {
                     t.tid.should.equal(4);
                     t.abbrev.should.equal("CIN");
                     t.gp.should.equal(10);
                     t.fg.should.equal(5);
                     t.fgp.should.equal(50);
                     Object.keys(t).should.have.length(5);
-
-                    done();
                 });
             });
-            it("should return requested info if tid/season match, even when no stats requested", function (done) {
-                team.filter({
+            it("should return requested info if tid/season match, even when no stats requested", function () {
+                return team.filter({
                     attrs: ["tid", "abbrev"],
                     seasonAttrs: ["season", "won"],
                     tid: 4,
                     season: g.season
-                }, function (t) {
+                }).then(function (t) {
                     t.tid.should.equal(4);
                     t.abbrev.should.equal("CIN");
                     t.season.should.equal(g.season);
                     t.won.should.equal(0);
                     Object.keys(t).should.have.length(4);
-
-                    done();
                 });
             });
-            it("should return season totals is options.totals is true", function (done) {
-                team.filter({
+            it("should return season totals is options.totals is true", function () {
+                return team.filter({
                     stats: ["gp", "fg", "fga", "fgp"],
                     tid: 4,
                     season: g.season,
                     totals: true
-                }, function (t) {
+                }).then(function (t) {
                     t.gp.should.equal(10);
                     t.fg.should.equal(50);
                     t.fga.should.equal(100);
                     t.fgp.should.equal(50);
-
-                    done();
                 });
             });
-            it("should return playoff stats if options.playoffs is true", function (done) {
-                team.filter({
+            it("should return playoff stats if options.playoffs is true", function () {
+                return team.filter({
                     stats: ["gp", "fg", "fga", "fgp"],
                     tid: 4,
                     season: g.season,
                     playoffs: true
-                }, function (t) {
+                }).then(function (t) {
                     t.gp.should.equal(4);
                     t.fg.should.equal(3);
                     t.fga.should.equal(30);
                     t.fgp.should.equal(10);
-
-                    done();
                 });
             });
-            it("should use supplied IndexedDB transaction", function (done) {
+            it("should use supplied IndexedDB transaction", function () {
                 var tx = dao.tx(["players", "releasedPlayers", "teams"]);
-                team.filter({
+                return team.filter({
                     attrs: ["tid", "abbrev"],
                     seasonAttrs: ["season", "won"],
                     tid: 4,
                     season: g.season,
                     ot: tx
-                }, function (t) {
+                }).then(function (t) {
                     t.tid.should.equal(4);
                     t.abbrev.should.equal("CIN");
                     t.season.should.equal(g.season);
@@ -173,37 +153,31 @@ define(["db", "globals", "core/league", "core/player", "core/team"], function (d
                     Object.keys(t).should.have.length(4);
 
                     // If another transaction was used inside team.filter besides tx, this will cause an error because the transaction will no longer be active
-                    console.log(tx.objectStore("players").get(0));
-
-                    done();
+                    return dao.players.get({ot: tx, key: 0});
                 });
             });
-            it("should return stats in an array if no season is specified", function (done) {
-                team.filter({
+            it("should return stats in an array if no season is specified", function () {
+                return team.filter({
                     stats: ["gp", "fg", "fga", "fgp"],
                     tid: 4,
                     playoffs: true
-                }, function (t) {
+                }).then(function (t) {
                     t.stats[0].gp.should.equal(4);
                     t.stats[0].fg.should.equal(3);
                     t.stats[0].fga.should.equal(30);
                     t.stats[0].fgp.should.equal(10);
-
-                    done();
                 });
             });
         });
 
-        describe("#checkRosterSizes()", function () {
-            before(function (done) {
-                db.connectMeta(function () {
-                    league.create("Test", 0, undefined, 2013, false, function () {
-                        done();
-                    });
+/*        describe("#checkRosterSizes()", function () {
+            before(function () {
+                return db.connectMeta().then(function () {
+                    return league.create("Test", 0, undefined, 2013, false);
                 });
             });
-            after(function (done) {
-                league.remove(g.lid, done);
+            after(function () {
+                return league.remove(g.lid);
             });
 
             function addTen(tid, cb) {
@@ -330,6 +304,6 @@ define(["db", "globals", "core/league", "core/player", "core/team"], function (d
                     });
                 });
             });
-        });
+        });*/
     });
 });

2b2e4478d6ff7facd4d2bb49134c7c86d0b2e601
Jeremy Scheff <jdscheff@gmail.com>
2015-01-06 23:49:39 -0500

    Set timestamps with Node, not sed

--- tools/build.js
+++ tools/build.js
@@ -2,11 +2,10 @@ var Promise = require("bluebird");
 var execAsync = Promise.promisify(require("child_process").exec);
 var fs = require("fs");
 var CleanCSS = require('clean-css');
+var moment = require("moment");
+var replace = require("replace");
 
-execAsync("rm -f gen/*").then(function () {
-    console.log("Minifying JS...");
-    return execAsync("node_modules/.bin/r.js -o baseUrl=js paths.requireLib=lib/require optimize=uglify2 preserveLicenseComments=false generateSourceMaps=true name=app include=requireLib mainConfigFile=js/app.js out=gen/app.js");
-}).then(function () {
+function minifyCss() {
     console.log("Minifying CSS...");
 
     var source = fs.readFileSync("css/bootstrap.css") +
@@ -14,18 +13,42 @@ execAsync("rm -f gen/*").then(function () {
                  fs.readFileSync("css/bbgm-notifications.css") +
                  fs.readFileSync("css/DT_bootstrap.css");
     fs.writeFileSync("gen/bbgm.css", (new CleanCSS()).minify(source));
+}
 
+function setTimestamps() {
     console.log("Setting timestamps...");
 
-    var d = new Date();
-    var mins = d.getMinutes() + 60 * d.getHours();
+    var d = moment();
+    var mins = d.minutes() + 60 * d.hours();
+    var rev = d.format("YYYY-MM-DD") + "." + mins;
 
-    return Promise.all([
-        execAsync('sed -i "s/LAST UPDATED:.*/LAST UPDATED: `date`/" bbgm.appcache'),
-        execAsync('sed -i "s/<!--rev-->.*<\\/p>/<!--rev-->`date +"%Y.%m.%d"`.' + mins + '<\\/p>/" index.html'),
-        execAsync('sed -i "s/Bugsnag\\.appVersion = \\".*\\"/Bugsnag.appVersion = \\"`date +"%Y.%m.%d"`.' + mins + '\\"/" index.html')
-    ]);
+    replace({
+        regex: "LAST UPDATED:.*",
+        replacement: "LAST UPDATED: " + d.toString(),
+        paths: ["bbgm.appcache"],
+        silent: true
+    });
+    replace({
+        regex: "<!--rev-->.*</p>",
+        replacement: "<!--rev-->" + rev + "</p>",
+        paths: ["index.html"],
+        silent: true
+    });
+    replace({
+        regex: 'Bugsnag\\.appVersion = ".*"',
+        replacement: 'Bugsnag.appVersion = "' + rev + '"',
+        paths: ["index.html"],
+        silent: true
+    });
+}
+
+execAsync("rm -f gen/*").then(function () {
+    console.log("Minifying JS...");
+    return execAsync("node_modules/.bin/r.js -o baseUrl=js paths.requireLib=lib/require optimize=uglify2 preserveLicenseComments=false generateSourceMaps=true name=app include=requireLib mainConfigFile=js/app.js out=gen/app.js");
 }).then(function () {
+    minifyCss();
+    setTimestamps();
+
     if (process.argv.length > 2 && process.argv[2] === "cordova") {
         console.log("Copying and processing files for Cordova...");
 

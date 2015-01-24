41587d0f9c4bd31533821d8e9d5fcde0564b93f0
Jeremy Scheff <jdscheff@gmail.com>
2015-01-24 10:15:13 -0500

    Fix ESLint warning

--- js/util/templateHelpers.js
+++ js/util/templateHelpers.js
@@ -253,5 +253,5 @@ define(["globals", "lib/faces", "lib/knockout", "util/helpers"], function (g, fa
                 return helpers.round(arg.pts() + 0.4 * arg.fg() - 0.7 * arg.fga() - 0.4 * (arg.fta() - arg.ft()) + 0.7 * arg.orb() + 0.3 * (arg.trb() - arg.orb()) + arg.stl() + 0.7 * arg.ast() + 0.7 * arg.blk() - 0.4 * arg.pf() - arg.tov(), 1);
             });
         }
-    }
+    };
 });

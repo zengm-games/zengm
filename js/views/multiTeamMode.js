f8cf16fbb69e8bc4ce7b1ae1852b0a4c70cc129f
Jeremy Scheff <jdscheff@gmail.com>
2015-03-21 18:12:50 -0400

    Functional multi team mode switcher

--- js/views/multiTeamMode.js
+++ js/views/multiTeamMode.js
@@ -2,36 +2,53 @@
  * @name views.multiTeamMode
  * @namespace Enable or disable Multi Team Mode.
  */
-define(["globals", "ui", "core/league", "util/bbgmView", "util/helpers"], function (g, ui, league, bbgmView, helpers) {
+define(["globals", "ui", "core/league", "lib/knockout", "util/bbgmView", "util/helpers"], function (g, ui, league, ko, bbgmView, helpers) {
     "use strict";
 
     function updateMultiTeamMode(inputs, updateEvents) {
         if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0) {
             // Make sure it's current
             return league.loadGameAttribute(null, "godMode").then(function () {
+                var i, teams;
+
+                teams = [];
+                for (i = 0; i < g.numTeams; i++) {
+                    teams.push({
+                        tid: i,
+                        name: g.teamRegionsCache[i] + " " + g.teamNamesCache[i]
+                    });
+                }
+
                 return {
-                    userTids: g.userTids
+                    userTids: g.userTids,
+                    teams: teams
                 };
             });
         }
     }
 
-    function uiFirst() {
+    function uiFirst(vm) {
         ui.title("Multi Team Mode");
 
-/*        document.getElementById("enable-god-mode").addEventListener("click", function () {
-            league.setGameAttributesComplete({godMode: true, godModeInPast: true}).then(function () {
-                league.updateLastDbChange();
-                ui.realtimeUpdate(["toggleGodMode"], helpers.leagueUrl(["god_mode"]));
-            });
-        });
+        ko.computed(function () {
+            var gameAttributes, newUserTids;
 
-        document.getElementById("disable-god-mode").addEventListener("click", function () {
-            league.setGameAttributesComplete({godMode: false}).then(function () {
+            newUserTids = vm.userTids();
+
+            if (newUserTids.length < 1) {
+                return;
+            }
+
+            if (JSON.stringify(newUserTids) !== JSON.stringify(g.userTids)) {
+                gameAttributes = {userTids: newUserTids};
+                if (newUserTids.indexOf(g.userTid) < 0) {
+                    gameAttributes.userTid = newUserTids[0];
+                }
+
+                league.setGameAttributes(null, gameAttributes);
                 league.updateLastDbChange();
-                ui.realtimeUpdate(["toggleGodMode"], helpers.leagueUrl(["god_mode"]));
-            });
-        });*/
+            }
+        });
     }
 
     return bbgmView.init({

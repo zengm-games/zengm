7a10042f285757067bed0efad2362caae6c14ab0
Jeremy Scheff <jdscheff@gmail.com>
2015-03-21 23:39:57 -0400

    Never mind

--- js/util/templateHelpers.js
+++ js/util/templateHelpers.js
@@ -270,7 +270,7 @@ define(["globals", "lib/faces", "lib/knockout", "util/helpers"], function (g, fa
             userTids = ko.unwrap(arg[1]);
 
             // Hide if not multi team or not loaded yet
-            if (userTids === undefined || userTids.length <= 1 || g.teamRegionsCache === undefined) {
+            if (userTids.length <= 1 || g.teamRegionsCache === undefined) {
                 return ko.bindingHandlers.visible.update(element, function () {
                     return false;
                 });

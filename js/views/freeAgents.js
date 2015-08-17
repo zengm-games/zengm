77b82c4cfbf56437c79bb1ab7836e7fbd11c6dec
briansd9 <briansd9@binkmail.com>
2015-08-17 23:02:45 +0800

    Changed table sort order to descending on header click

--- js/views/freeAgents.js
+++ js/views/freeAgents.js
@@ -102,7 +102,11 @@ define(["dao", "globals", "ui", "core/freeAgents", "core/player", "core/team", "
                 }
                 // The display: none for mood allows sorting, somehow
                 return [helpers.playerNameLabels(p.pid, p.name, p.injury, p.ratings.skills, p.watch), p.ratings.pos, String(p.age), String(p.ratings.ovr), String(p.ratings.pot), helpers.round(p.stats.min, 1), helpers.round(p.stats.pts, 1), helpers.round(p.stats.trb, 1), helpers.round(p.stats.ast, 1), helpers.round(p.stats.per, 1), helpers.formatCurrency(p.contract.amount, "M") + ' thru ' + p.contract.exp, '<div title="' + p.mood.text + '" style="width: 100%; height: 21px; background-color: ' + p.mood.color + '"><span style="display: none">' + p.freeAgentMood[g.userTid] + '</span></div>', negotiateButton];
-            }));
+            }), {
+                columnDefs: [{
+                    orderSequence: ["desc", "asc"], "targets": "_all"
+                }]
+            });
         }).extend({throttle: 1});
 
         ui.tableClickableRows($("#free-agents"));

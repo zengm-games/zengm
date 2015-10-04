42bc20a961443658cd31bbd523a17d6139ead1e2
Jeremy Scheff <jdscheff@gmail.com>
2015-10-03 21:08:11 -0400

    Dummy test running in karma

--- js/test/app.js
+++ js/test/app.js
@@ -1,44 +1,15 @@
-/*eslint no-unused-vars: 0, no-underscore-dangle: 0*/
 'use strict';
 
-var chai = require('lib/chai');
-require('lib/IndexedDB-getAll-shim');
-require('../util/templateHelpers');
-
-var baseUrl, inKarma, should, startTests;
-
-inKarma = window.__karma__ !== undefined;
-baseUrl = inKarma ? "/base/js" : "/js";
-startTests = inKarma ? window.__karma__.start : mocha.run;
-
-require.config({
-    baseUrl: baseUrl,
-    shim: {
-        "lib/davis": {
-            deps: ["lib/jquery"],
-            exports: "Davis"
-        },
-        "lib/faces": {
-            exports: "faces"
-        },
-        "lib/IndexedDB-getAll-shim": {},
-        "lib/jquery": {
-            exports: "$"
-        },
-        "underscore": {
-            exports: "_"
-        }
-    }
-});
+var assert = require('assert');
 
 mocha.setup({
     ui: "bdd",
     globals: ["console"],
     timeout: 2000000000
 });
-should = chai.should();
 
-require(["test/core/contractNegotiation", "test/core/draft", "test/core/finances", "test/core/league", "test/core/player", "test/core/season", "test/core/team", "test/core/trade", "test/util/account", "test/util/helpers", "test/views/components", "test/views/gameLog"], function () {
-    startTests();
+describe('b', function() {
+    it('should equal fixture contents', function() {
+        assert.equal([1,2].length, 2);
+    });
 });
-
--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,39 @@
-// Karma configuration
-// Generated on Sat Jan 10 2015 12:16:49 GMT-0500 (EST)
+module.exports = function (config) {
+    config.set({
+        basePath: '',
 
-module.exports = function(config) {
-  config.set({
+        frameworks: ['mocha', 'browserify'],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        files: [
+            'js/test/app.js',
+//            {pattern: 'js/**/*.js', included: false},
+//            {pattern: 'js/**/*.json', included: false},
+//            {pattern: 'templates/*.html', included: false}
+        ],
 
+        exclude: [
+        ],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
+        preprocessors: {
+            'js/**/*.js': ['browserify']
+        },
 
+        reporters: ['progress'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'js/test/app.js',
-      {pattern: 'js/**/*.js', included: false},
-      {pattern: 'js/**/*.json', included: false},
-      {pattern: 'templates/*.html', included: false}
-    ],
+        port: 9876,
 
+        colors: true,
 
-    // list of files to exclude
-    exclude: [
-    ],
+        logLevel: config.LOG_INFO,
 
+        autoWatch: false,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        browsers: ['Chrome'],// 'Firefox'],
 
+        singleRun: true,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+        browserify: {
+          transform: ['browserify-shim', 'brfs']
+        }
+    });
 };

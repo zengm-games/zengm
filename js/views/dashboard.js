cc1edb75501fae3d461773f324ee538898797bb9
Jeremy Scheff <jdscheff@gmail.com>
2016-05-26 22:12:21 -0400

    Get rid of https -> http link, until http usage is completely gone

--- js/views/dashboard.js
+++ js/views/dashboard.js
@@ -24,7 +24,7 @@ async function updateDashboard() {
         }
         otherUrl = `https://${window.location.hostname}/`;
     } else {
-        otherUrl = `http://${window.location.hostname}/`;
+        otherUrl = null;
     }
 
     return {

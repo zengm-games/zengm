82f8c913ce561365787716d8091c8c3fcd6e7c26
Kevin Gilbert <kevinmgilbert06@gmail.com>
2016-06-09 16:41:11 -0700

    use triple equal instead of double

--- src/js/core/player.js
+++ src/js/core/player.js
@@ -1115,7 +1115,7 @@ function filter(p, options) {
                         });
                     }
                 }
-            } else if (options.attrs[i] == "name") {
+            } else if (options.attrs[i] === "name") {
                 fp.name = `${p.firstName} ${p.lastName}`;
             } else {
                 fp[options.attrs[i]] = p[options.attrs[i]];

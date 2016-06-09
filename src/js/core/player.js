4eebe2525512812f44ab136733e03222262d396b
Kevin Gilbert <kevinmgilbert06@gmail.com>
2016-06-09 15:25:27 -0700

    add name attribute to player.filter

--- src/js/core/player.js
+++ src/js/core/player.js
@@ -1115,6 +1115,8 @@ function filter(p, options) {
                         });
                     }
                 }
+            } else if (options.attrs[i] == "name") {
+                fp.name = `${p.firstName} ${p.lastName}`;
             } else {
                 fp[options.attrs[i]] = p[options.attrs[i]];
             }

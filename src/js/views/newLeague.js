d35117bf1040e175883e50026245149346280938
Jeremy Scheff <jdscheff@gmail.com>
2016-06-18 18:52:31 -0400

    Log error, why not

--- src/js/views/newLeague.js
+++ src/js/views/newLeague.js
@@ -200,6 +200,7 @@ function uiFirst(vm) {
                 try {
                     leagueFile = JSON.parse(event.target.result);
                 } catch (e) {
+                    console.log(e);
                     vm.invalidLeagueFile(true);
                     return;
                 }

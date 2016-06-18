df362d95d69857fa6def550cb201fe2093624211
Jeremy Scheff <jdscheff@gmail.com>
2016-06-18 18:37:56 -0400

    Fix ordering for non-conference playoff matchups

--- src/js/core/season.js
+++ src/js/core/season.js
@@ -733,9 +733,10 @@ function genPlayoffSeries(teams) {
             }
         }
         for (let i = 0; i < numPlayoffTeams / 2; i++) {
-            series[0][i] = {home: teamsConf[i], away: teamsConf[numPlayoffTeams - 1 - i]};
-            series[0][i].home.seed = i + 1;
-            series[0][i].away.seed = numPlayoffTeams - i;
+            const j = i < numPlayoffTeams / 4 ? i * 2 : (i - numPlayoffTeams / 4) * 2 + 1;
+            series[0][j] = {home: teamsConf[i], away: teamsConf[numPlayoffTeams - 1 - i]};
+            series[0][j].home.seed = i + 1;
+            series[0][j].away.seed = numPlayoffTeams - i;
         }
     }
 

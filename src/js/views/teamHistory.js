05817913ae733443bcb8a9b8cf6f1935ba120445
Jeremy Scheff <jdscheff@gmail.com>
2016-08-13 09:02:21 -0400

    s/vm/state/

--- src/js/views/teamHistory.js
+++ src/js/views/teamHistory.js
@@ -13,8 +13,8 @@ function get(req) {
     return inputs;
 }
 
-async function updateTeamHistory(inputs, updateEvents, vm) {
-    if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || updateEvents.indexOf("gameSim") >= 0 || inputs.abbrev !== vm.abbrev()) {
+async function updateTeamHistory(inputs, updateEvents, state) {
+    if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || updateEvents.indexOf("gameSim") >= 0 || inputs.abbrev !== state.abbrev) {
         let [teamSeasons, players] = await Promise.all([
             g.dbl.teamSeasons.index("tid, season").getAll(backboard.bound([inputs.tid], [inputs.tid, ''])),
             g.dbl.players.index('statsTids').getAll(inputs.tid).then(players => {

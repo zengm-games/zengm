7fd3249710946a6292e140b1490d68ac83844551
Gary King <garyking@gmail.com>
2016-08-13 17:09:26 -0400

    Finish highlighting our team's values on teamStats.

--- src/js/views/views/TeamStats.js
+++ src/js/views/views/TeamStats.js
@@ -38,31 +38,56 @@ const TeamStats = ({season, stats, teams = []}) => {
 
     const cols = getCols('Team', 'GP', 'W', 'L', 'M', 'A', '%', 'M', 'A', '%', 'M', 'A', '%', 'Off', 'Def', 'Tot', 'Ast', 'TO', 'Stl', 'Blk', 'BA', 'PF', 'Pts', 'OPts', 'Diff');
 
+    const teamCount = teams.length;
     const rows = teams.map(t => {
-        const statTypeColumns = ['fg', 'fga', 'fgp', 'tp', 'tpa', 'tpp', 'ft', 'fta', 'ftp', 'orb', 'drb', 'trb', 'ast', 'tov', 'stl', 'blk', 'ba', 'pf', 'pts', 'oppPts'];
+        const statTypeColumns = ['fg', 'fga', 'fgp', 'tp', 'tpa', 'tpp', 'ft', 'fta', 'ftp', 'orb', 'drb', 'trb', 'ast', 'tov', 'stl', 'blk', 'ba', 'pf', 'pts', 'oppPts', 'diff'];
+        const otherStatColumns = ['won', 'lost'];
 
         // Create the cells for this row.
-        const data = [
-            <a href={helpers.leagueUrl(["roster", t.abbrev, season])}>{t.abbrev}</a>,
-            t.gp, t.won, t.lost
-        ];
+        const data = {
+            abbrev: <a href={helpers.leagueUrl(["roster", t.abbrev, season])}>{t.abbrev}</a>,
+            gp: t.gp,
+            won: t.won,
+            lost: t.lost,
+        };
 
         for (const column of statTypeColumns) {
-            data.push(helpers.round(t[column], 1));
+            const value = helpers.round(t[column], 1);
+            data[column] = value;
         }
 
-        data.push(<span className={t.diff > 0 ? 'text-success' : 'text-danger'}>{helpers.round(t.diff, 1)}</span>);
+        data['diff'] = <span className={t.diff > 0 ? 'text-success' : 'text-danger'}>{helpers.round(t.diff, 1)}</span>;
 
         // This is our team.
         if (g.userTid === t.tid) {
+            // Color stat values accordingly.
+            for (const [key, value] of Object.entries(data)) {
+                if (statTypeColumns.indexOf(key) === -1 && otherStatColumns.indexOf(key) === -1) {
+                    continue;
+                }
+
+                // Determine our team's percentile for this stat type. Closer to the start is better.
+                const percentile = 1 - (stats[key].indexOf(t[key]) / (teamCount - 1));
+
+                let className;
+                if (percentile >= 2 / 3) className = 'success';
+                else if (percentile >= 1 / 3) className = 'info';
+                else className = 'danger';
+
+                data[key] = {
+                    classNames: className,
+                    value,
+                }
+            }
+
             return {
                 key: t.tid,
-                data,
+                data: Object.values(data),
             };
         } else {
             return {
                 key: t.tid,
-                data,
+                data: Object.values(data),
             };
         }
     });

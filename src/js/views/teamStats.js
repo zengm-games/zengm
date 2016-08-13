a7c42599aa4ea74cba0d971934ce94df0f49dafb
Gary King <garyking@gmail.com>
2016-08-13 16:40:24 -0400

    teamStats: Return sorted stats.

--- src/js/views/teamStats.js
+++ src/js/views/teamStats.js
@@ -19,8 +19,44 @@ async function updateTeams(inputs, updateEvents, state) {
             season: inputs.season,
         });
 
+        /*
+         * Sort stats so we can determine what percentile our team is in.
+         */
+        const stats = {};
+        const statTypes = ['won', 'lost', 'fg', 'fga', 'fgp', 'tp', 'tpa', 'tpp', 'ft', 'fta', 'ftp', 'orb', 'drb', 'trb', 'ast', 'tov', 'stl', 'blk', 'ba', 'pf', 'pts', 'oppPts', 'diff'];
+        const lowerIsBetter = ['lost', 'tov', 'ba', 'pf', 'oppPts'];
+
+        // Loop teams and stat types.
+        for (const team of teams) {
+            for (const statType of statTypes) {
+                if (!stats[statType]) {
+                    stats[statType] = [];
+                }
+
+                stats[statType].push(team[statType]);
+            }
+        }
+
+        // Sort stat types. "Better" values are at the start of the arrays.
+        for (const [statType, statValues] of Object.entries(stats)) {
+            stats[statType].sort((a, b) => {
+                // Sort lowest first.
+                if (lowerIsBetter.indexOf(statType) > -1) {
+                    if (a < b) return -1;
+                    else if (a > b) return 1;
+                    else return 0;
+                // Sort highest first.
+                } else {
+                    if (a < b) return 1;
+                    else if (a > b) return -1;
+                    else return 0;
+                }
+            });
+        }
+
         return {
             season: inputs.season,
+            stats,
             teams,
         };
     }

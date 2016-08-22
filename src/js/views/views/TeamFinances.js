518bce40c03f5ab15743871276520a1858daed67
Gary King <garyking@gmail.com>
2016-08-22 00:57:52 -0400

    TeamFinances: Simplify looping salaries for each player.

--- src/js/views/views/TeamFinances.js
+++ src/js/views/views/TeamFinances.js
@@ -191,14 +191,19 @@ const TeamFinances = ({abbrev, barData = {expenses: {salary: [], minTax: [], lux
                 watch={p.watch}
             >{p.firstName} {p.lastName}</PlayerNameLabels>,
         ];
+
+        // Loop through the salaries for the next five years for this player.
         for (let i = 0; i < 5; i++) {
             if (p.amounts[i]) {
-                data.push(helpers.formatCurrency(p.amounts[i], "M"));
+                const formattedAmount = helpers.formatCurrency(p.amounts[i], "M");
+
+                if (p.released) {
+                    data.push(<i>{formattedAmount}</i>);
+                } else {
+                    data.push(formattedAmount);
+                }
             } else {
                 data.push(null);
-            }
-            if (p.released) {
-                data[i + 1] = <i>{data[i + 1]}</i>;
             }
         }
 

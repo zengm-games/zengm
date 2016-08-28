50afa23fb90c00bdc856d25f82376aad3add9675
Jeremy Scheff <jdscheff@gmail.com>
2016-08-28 11:21:09 -0400

    Reactify delete old data

--- src/js/templates.js
+++ src/js/templates.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 module.exports = {
     account: fs.readFileSync(__dirname + '/../templates/account.html', 'utf8'),
     accountUpdateCard: fs.readFileSync(__dirname + '/../templates/accountUpdateCard.html', 'utf8'),
-    deleteOldData: fs.readFileSync(__dirname + '/../templates/deleteOldData.html', 'utf8'),
     error: fs.readFileSync(__dirname + '/../templates/error.html', 'utf8'),
     loginOrRegister: fs.readFileSync(__dirname + '/../templates/loginOrRegister.html', 'utf8'),
     lostPassword: fs.readFileSync(__dirname + '/../templates/lostPassword.html', 'utf8'),

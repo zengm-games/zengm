61f0d842374e3388a1ce82e07e9927a8bcd67f22
Jeremy Scheff <jdscheff@gmail.com>
2016-08-28 17:36:10 -0400

    Reactify account page

--- src/js/templates.js
+++ src/js/templates.js
@@ -4,7 +4,6 @@
 const fs = require('fs');
 
 module.exports = {
-    account: fs.readFileSync(__dirname + '/../templates/account.html', 'utf8'),
     accountUpdateCard: fs.readFileSync(__dirname + '/../templates/accountUpdateCard.html', 'utf8'),
     error: fs.readFileSync(__dirname + '/../templates/error.html', 'utf8'),
     resetPassword: fs.readFileSync(__dirname + '/../templates/resetPassword.html', 'utf8'),
--- src/js/views/account.js
+++ src/js/views/account.js
@@ -1,12 +1,7 @@
 const g = require('../globals');
-const ui = require('../ui');
-const Promise = require('bluebird');
-const $ = require('jquery');
 const account = require('../util/account');
-const bbgmView = require('../util/bbgmView');
-const viewHelpers = require('../util/viewHelpers');
-
-const ajaxErrorMsg = "Error connecting to server. Check your Internet connection or try again later.";
+const bbgmViewReact = require('../util/bbgmViewReact');
+const Account = require('./views/Account');
 
 function get(req) {
     return {
@@ -28,7 +23,7 @@ async function updateAccount(inputs, updateEvents) {
         const showGoldPitch = !showGoldActive;
 
         return {
-            username: g.vm.topMenu.username,
+            username: g.vm.topMenu.username(),
             goldUntilDateString,
             showGoldActive,
             showGoldCancelled,
@@ -49,125 +44,10 @@ async function updateAchievements(inputs, updateEvents) {
     }
 }
 
-
-function handleStripeButton() {
-    const buttonEl = document.getElementById("stripe-button");
-
-    if (!buttonEl) { return; }
-
-    $.getScript('https://checkout.stripe.com/checkout.js', () => {
-        const email = g.vm.topMenu.email();
-
-        const handler = window.StripeCheckout.configure({
-            key: g.stripePublishableKey,
-            image: '/ico/icon128.png',
-            token: async token => {
-                try {
-                    const data = await Promise.resolve($.ajax({
-                        type: "POST",
-                        url: `//account.basketball-gm.${g.tld}/gold_start.php`,
-                        data: {
-                            sport: "basketball",
-                            token: token.id,
-                        },
-                        dataType: "json",
-                        xhrFields: {
-                            withCredentials: true,
-                        },
-                    }));
-                    ui.realtimeUpdate(["account"], "/account", undefined, {goldResult: data});
-                } catch (err) {
-                    console.log(err);
-                    ui.realtimeUpdate(["account"], "/account", undefined, {goldResult: {
-                        success: false,
-                        message: ajaxErrorMsg,
-                    }});
-                }
-            },
-        });
-
-        buttonEl.addEventListener("click", e => {
-            handler.open({
-                name: 'Basketball GM Gold',
-                description: '',
-                amount: 500,
-                email,
-                allowRememberMe: false,
-                panelLabel: "Subscribe for $5/month",
-            });
-            e.preventDefault();
-        });
-    });
-}
-
-function handleCancelLink() {
-    document.getElementById("gold-cancel").addEventListener("click", async e => {
-        e.preventDefault();
-
-        const result = window.confirm("Are you sure you want to cancel your Basketball GM Gold subscription?");
-
-        if (result) {
-            try {
-                const data = await Promise.resolve($.ajax({
-                    type: "POST",
-                    url: `//account.basketball-gm.${g.tld}/gold_cancel.php`,
-                    data: {
-                        sport: "basketball",
-                    },
-                    dataType: "json",
-                    xhrFields: {
-                        withCredentials: true,
-                    },
-                }));
-                ui.realtimeUpdate(["account"], "/account", undefined, {goldResult: data});
-            } catch (err) {
-                console.log(err);
-                ui.realtimeUpdate(["account"], "/account", undefined, {goldResult: {
-                    success: false,
-                    message: ajaxErrorMsg,
-                }});
-            }
-        }
-    });
-}
-
-function uiFirst() {
-    ui.title("Account");
-
-    document.getElementById("logout").addEventListener("click", e => {
-        e.preventDefault();
-
-        // Reset error display
-        document.getElementById("logout-error").innerHTML = "";
-
-        $.ajax({
-            type: "POST",
-            url: `//account.basketball-gm.${g.tld}/logout.php`,
-            data: `sport=${g.sport}`,
-            xhrFields: {
-                withCredentials: true,
-            },
-            success: () => {
-                // Reset error display
-                document.getElementById("logout-error").innerHTML = "";
-
-                g.vm.topMenu.username("");
-                ui.realtimeUpdate(["account"], "/");
-            },
-            error: () => {
-                document.getElementById("logout-error").innerHTML = "Error connecting to server. Check your Internet connection or try again later.";
-            },
-        });
-    });
-
-    handleStripeButton();
-    handleCancelLink();
-}
-
-module.exports = bbgmView.init({
+module.exports = bbgmViewReact.init({
     id: "account",
     get,
-    beforeReq: viewHelpers.beforeNonLeague,
+    inLeague: false,
     runBefore: [updateAccount, updateAchievements],
-    uiFirst,
+    Component: Account,
 });

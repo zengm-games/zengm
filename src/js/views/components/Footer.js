d593015ad9480dec476fefbd188aaab523a4ab8f
Jeremy Scheff <jdscheff@gmail.com>
2016-08-30 23:24:11 -0400

    Prettier (maybe) loading screen

--- src/js/views/components/Footer.js
+++ src/js/views/components/Footer.js
@@ -15,11 +15,11 @@ class Footer extends React.Component {
 
             <footer>
                 <p>
-                    <a href="http://basketball-gm.com/about/" target="_blank">About</a> ·{' '}
-                    <a href="http://basketball-gm.com/advertise/" target="_blank">Advertise</a> ·{' '}
-                    <a href="http://basketball-gm.com/blog/" target="_blank">Blog</a> ·{' '}
-                    <a href="http://basketball-gm.com/contact/" target="_blank">Contact</a> ·{' '}
-                    <a href="http://basketball-gm.com/share/" target="_blank">Share</a><br />
+                    <a href="https://basketball-gm.com/about/" target="_blank">About</a> ·{' '}
+                    <a href="https://basketball-gm.com/advertise/" target="_blank">Advertise</a> ·{' '}
+                    <a href="https://basketball-gm.com/blog/" target="_blank">Blog</a> ·{' '}
+                    <a href="https://basketball-gm.com/contact/" target="_blank">Contact</a> ·{' '}
+                    <a href="https://basketball-gm.com/share/" target="_blank">Share</a><br />
                 </p>
                 <p className="rev">v3.6 · {window.bbgmVersion}</p>
             </footer>

787cbc7d71aa0fe87f6a2f204861e72b294ef00f
Jeremy Scheff <jdscheff@gmail.com>
2016-09-11 21:42:34 -0400

    Slightly smarter Jump To menu

--- src/js/views/components/JumpTo.js
+++ src/js/views/components/JumpTo.js
@@ -1,25 +1,45 @@
 const React = require('react');
 const DropdownButton = require('react-bootstrap/lib/DropdownButton');
 const MenuItem = require('react-bootstrap/lib/MenuItem');
+const g = require('../../globals');
 const helpers = require('../../util/helpers');
 
-const JumpTo = ({season}) => <div className="pull-right">
-    <DropdownButton id="jump-to-dropdown" title="Jump To">
-        <MenuItem href={helpers.leagueUrl(['standings', season])}>Standings</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['playoffs', season])}>Playoffs</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['history', season])}>Season Summary</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['league_finances', season])}>Finances</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['transactions', 'all', season])}>Transactions</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['draft_summary', season])}>Draft</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['leaders', season])}>Leaders</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['team_stats', season])}>Team Stats</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['player_stats', 'all', season])}>Player Stats</MenuItem>
-        <MenuItem href={helpers.leagueUrl(['player_ratings', 'all', season])}>Player Ratings</MenuItem>
-    </DropdownButton>
-</div>;
+const genUrl = (parts, season) => {
+    if (season !== undefined) {
+        parts.push(season);
+    }
+
+    return helpers.leagueUrl(parts);
+};
+
+const JumpTo = ({season}) => {
+    // Sometimes the season will be some nonsense like "all", in which case we can't generally use
+    // it (although maybe it would be good to in some cases). And if the season is g.season, there's
+    // no need to pollute the URL with that, since it's the default on all pages.
+    const s = typeof season === 'number' && season !== g.season ? season : undefined;
+
+    return <div className="pull-right">
+        <DropdownButton id="jump-to-dropdown" title="Jump To">
+            <MenuItem href={genUrl(['standings'], s)}>Standings</MenuItem>
+            <MenuItem href={genUrl(['playoffs'], s)}>Playoffs</MenuItem>
+            <MenuItem href={genUrl(['history'], s)}>Season Summary</MenuItem>
+            <MenuItem href={genUrl(['league_finances'], s)}>Finances</MenuItem>
+            <MenuItem href={genUrl(['transactions', 'all'], s)}>Transactions</MenuItem>
+            <MenuItem href={genUrl(['draft_summary'], s)}>Draft</MenuItem>
+            <MenuItem href={genUrl(['leaders'], s)}>Leaders</MenuItem>
+            <MenuItem href={genUrl(['team_stats'], s)}>Team Stats</MenuItem>
+            <MenuItem href={genUrl(['player_stats', 'all'], s)}>Player Stats</MenuItem>
+            <MenuItem href={genUrl(['player_ratings', 'all'], s)}>Player Ratings</MenuItem>
+        </DropdownButton>
+    </div>;
+};
 
 JumpTo.propTypes = {
-    season: React.PropTypes.number.isRequired,
+    // Not just a number, because sometimes the season might be something like "all"
+    season: React.PropTypes.oneOfType([
+        React.PropTypes.number,
+        React.PropTypes.string,
+    ]),
 };
 
 module.exports = JumpTo;

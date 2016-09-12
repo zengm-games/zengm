021f979669a42ea8c59476a7c4dcf492053fc6da
Jeremy Scheff <jdscheff@gmail.com>
2016-09-11 22:55:09 -0400

    Uglify harmony branch seems to work... this is probably a bad idea...

--- tools/build-js.js
+++ tools/build-js.js
@@ -1,6 +1,6 @@
 // Used to be:
 // browserify -d -p [minifyify --map app.js.map --output gen/app.js.map] js/app.js -o gen/app.js
-// ...but then https://github.com/ben-ng/minifyify/issues/116 made it too complicated.
+// ...but then it got too complicated, and this seemed easier
 
 const browserify = require('browserify');
 const envify = require('envify/custom');
@@ -8,27 +8,6 @@ const exorcist = require('exorcist');
 const fs = require('fs');
 
 const bundler = browserify('src/js/app.js', {debug: true});
-
-// Disabled until it supports ES2015 out of the box (waiting on new UglifyJS relase, mostly)
-/*bundler.plugin('minifyify', {
-    map: '/gen/app.js.map',
-    uglify: {
-        // https://github.com/ben-ng/minifyify/issues/116
-        mangle: {
-            except: ['require']
-        },
-        sourceRoot: '/' // This has no affect on anything, I'm not sure why. Instead all my paths contain /gen, sigh.
-    }
-});
-
-bundler.bundle((err, src, map) => {
-    if (err) {
-        throw err;
-    }
-
-    fs.writeFileSync('gen/app.js', src);
-    fs.writeFileSync('gen/app.js.map', map);
-});*/
 
 console.log('Bundling JavaScript files...');
 
--- tools/minify-js.js
+++ tools/minify-js.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const UglifyJS = require("uglify-js");
+const UglifyJS = require('uglify-js');
 
-const result = UglifyJS.minify("build/gen/app.js", {
-    inSourceMap: "build/gen/app.js.map",
-    outSourceMap: "build/gen/app.js.map",
-    mangle: {
-        except: ['require'],
-    },
+console.log('Minifying JS bundle...\nWARNING: This is likely to cause bugs');
+
+const result = UglifyJS.minify('build/gen/app.js', {
+    inSourceMap: 'build/gen/app.js.map',
+    outSourceMap: 'build/gen/app.js.map',
+    sourceMapUrl: 'app.js.map',
 });
 
 fs.writeFileSync('build/gen/app.js', result.code);

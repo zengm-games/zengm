afb2ef913931e2a06e450afa081c1a1035860c8c
Jeremy Scheff <jdscheff@gmail.com>
2016-09-24 15:22:15 -0400

    More flow

--- src/js/util/helpers.js
+++ src/js/util/helpers.js
@@ -1,4 +1,5 @@
 // @flow
+
 import React from 'react';
 import g from '../globals';
 import logEvent from './logEvent';
--- src/js/util/lock.js
+++ src/js/util/lock.js
@@ -1,6 +1,9 @@
+// @flow
+
 import g from '../globals';
 import * as league from '../core/league';
 import * as helpers from './helpers';
+import type {BackboardTx} from './types';
 
 /**
  * Is game simulation in progress?
@@ -8,11 +11,11 @@ import * as helpers from './helpers';
  * Calls the callback function with either true or false depending on whether there is a game simulation currently in progress.
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on gameAttributes; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on gameAttributes; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-async function gamesInProgress(ot) {
-    await league.loadGameAttribute(ot, "gamesInProgress");
+async function gamesInProgress(tx: ?BackboardTx): Promise<boolean> {
+    await league.loadGameAttribute(tx, "gamesInProgress");
     return g.gamesInProgress;
 }
 
@@ -22,11 +25,11 @@ async function gamesInProgress(ot) {
  * Calls the callback function with either true or false depending on whether there is an ongoing negoation.
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on negotiations; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on negotiations; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-async function negotiationInProgress(ot) {
-    const dbOrTx = ot !== null ? ot : g.dbl;
+async function negotiationInProgress(tx: ?BackboardTx): Promise<boolean> {
+    const dbOrTx = tx !== undefined && tx !== null ? tx : g.dbl;
     const negotiations = await dbOrTx.negotiations.getAll();
     return negotiations.length > 0;
 }
@@ -35,11 +38,11 @@ async function negotiationInProgress(ot) {
  * Is a phase change in progress?
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on gameAttributes; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on gameAttributes; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-async function phaseChangeInProgress(ot) {
-    await league.loadGameAttribute(ot, "phaseChangeInProgress");
+async function phaseChangeInProgress(tx: ?BackboardTx): Promise<boolean> {
+    await league.loadGameAttribute(tx, "phaseChangeInProgress");
     return g.phaseChangeInProgress;
 }
 
@@ -49,22 +52,22 @@ async function phaseChangeInProgress(ot) {
  * Calls the callback function with either true or false. If games are in progress or any contract negotiation is in progress, false.
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on gameAttributes and negotiations; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on gameAttributes and negotiations; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-function canStartGames(ot) {
-    return helpers.maybeReuseTx(["gameAttributes", "negotiations"], "readonly", ot, async tx => {
-        const gamesInProgressBool = await gamesInProgress(tx);
+function canStartGames(tx: ?BackboardTx): Promise<boolean> {
+    return helpers.maybeReuseTx(["gameAttributes", "negotiations"], "readonly", tx, async tx2 => {
+        const gamesInProgressBool = await gamesInProgress(tx2);
         if (gamesInProgressBool) {
             return false;
         }
 
-        const negotiationInProgressBool = await negotiationInProgress(tx);
+        const negotiationInProgressBool = await negotiationInProgress(tx2);
         if (negotiationInProgressBool) {
             return false;
         }
 
-        const phaseChangeInProgressBool = await phaseChangeInProgress(tx);
+        const phaseChangeInProgressBool = await phaseChangeInProgress(tx2);
         if (phaseChangeInProgressBool) {
             return false;
         }
@@ -79,18 +82,18 @@ function canStartGames(ot) {
  * Calls the callback function with either true or false. If games are in progress or a free agent (not re-signing!) is being negotiated with, false.
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on gameAttributes and negotiations; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on gameAttributes and negotiations; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-function canStartNegotiation(ot) {
-    return helpers.maybeReuseTx(["gameAttributes", "negotiations"], "readonly", ot, async tx => {
-        const gamesInProgressBool = await gamesInProgress(tx);
+function canStartNegotiation(tx: ?BackboardTx): Promise<boolean> {
+    return helpers.maybeReuseTx(["gameAttributes", "negotiations"], "readonly", tx, async tx2 => {
+        const gamesInProgressBool = await gamesInProgress(tx2);
         if (gamesInProgressBool) {
             return false;
         }
 
         // Allow multiple parallel negotiations only for re-signing players
-        const negotiations = await tx.negotiations.getAll();
+        const negotiations = await tx2.negotiations.getAll();
 
         for (let i = 0; i < negotiations.length; i++) {
             if (!negotiations[i].resigning) {
@@ -110,11 +113,11 @@ function canStartNegotiation(ot) {
  * Calls the callback function with either true or false.
  *
  * @memberOf util.lock
- * @param {IDBTransaction|null} ot An IndexedDB transaction on messages; if null is passed, then a new transaction will be used.
+ * @param {IDBTransaction|null} tx An IndexedDB transaction on messages; if null is passed, then a new transaction will be used.
  * @return {Promise.boolean}
  */
-async function unreadMessage(ot) {
-    const dbOrTx = ot !== null ? ot : g.dbl;
+async function unreadMessage(tx: ?BackboardTx): Promise<boolean> {
+    const dbOrTx = tx !== undefined && tx !== null ? tx : g.dbl;
     const messages = await dbOrTx.messages.getAll();
     for (let i = 0; i < messages.length; i++) {
         if (!messages[i].read) {

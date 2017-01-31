4449546c486fba14b62bc9ef6cc148563223840b
Jeremy Scheff <jdscheff@gmail.com>
2017-01-30 22:25:10 -0500

    Whoops

---
+++ src/js/db/index.js
@@ -0,0 +1,27 @@
+// @flow
+
+import Backboard from 'backboard';
+import Promise from 'bluebird';
+import connectMeta from './connectMeta';
+import connectLeague from './connectLeague';
+import reset from './reset';
+import logEvent from '../util/logEvent';
+
+Backboard.setPromiseConstructor(Promise);
+Backboard.on('quotaexceeded', () => {
+    logEvent(null, {
+        type: "error",
+        text: 'Your browser isn\'t letting Basketball GM store any more data!<br><br>Try <a href="/">deleting some old leagues</a> or deleting old data (Tools > Improve Performance within a league). Clearing space elsewhere on your hard drive might help too. <a href="https://basketball-gm.com/manual/debugging/quota-errors/"><b>Read this for more info.</b></a>',
+        saveToDb: false,
+        persistent: true,
+    });
+});
+Backboard.on('blocked', () => {
+    window.alert("Please close any other tabs with this league open!");
+});
+
+export {
+    connectMeta,
+    connectLeague,
+    reset,
+};

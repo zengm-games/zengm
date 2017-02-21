cb8726bc324a1c87d0c0bcc8fc523149f5cea998
Jeremy Scheff <jdscheff@gmail.com>
2017-02-21 13:29:35 -0500

    Port team.filter tests to getCopy.teams

---
+++ src/js/test/db/getCopy/teams.js
@@ -0,0 +1,215 @@
+import assert from 'assert';
+import {Cache, connectMeta, getCopy} from '../../../db';
+import g from '../../../globals';
+import * as league from '../../../core/league';
+import * as team from '../../../core/team';
+
+describe("db/getCopy", () => {
+    describe("#teams()", () => {
+        before(async () => {
+            await connectMeta();
+            await league.create("Test", 0, undefined, 2013, false);
+            g.cache = new Cache();
+            await g.cache.fill();
+
+            let teamStats = await g.cache.indexGet('teamStatsByPlayoffsTid', '0,4');
+            teamStats.gp = 10;
+            teamStats.fg = 50;
+            teamStats.fga = 100;
+
+            teamStats = team.genStatsRow(4, true);
+            teamStats.gp = 4;
+            teamStats.fg = 12;
+            teamStats.fga = 120;
+            await g.cache.add('teamStats', teamStats);
+        });
+        after(() => league.remove(g.lid));
+
+        it("should return requested info if tid/season match", async () => {
+            const t = await getCopy.teams({
+                attrs: ["tid", "abbrev"],
+                seasonAttrs: ["season", "won", "payroll"],
+                stats: ["gp", "fg", "fgp"],
+                tid: 4,
+                season: g.season,
+            });
+
+            assert(t.seasonAttrs.payroll > 0);
+            assert.deepEqual(t, {
+                tid: 4,
+                abbrev: 'CIN',
+                seasonAttrs: {
+                    season: g.season,
+                    won: 0,
+                    payroll: t.seasonAttrs.payroll,
+                },
+                stats: {
+                    gp: 10,
+                    fg: 5,
+                    fgp: 50,
+                    playoffs: false,
+                },
+            });
+        });
+        it("should return an array if no team ID is specified", async () => {
+            const teams = await getCopy.teams({
+                attrs: ["tid", "abbrev"],
+                seasonAttrs: ["season", "won"],
+                stats: ["gp", "fg", "fgp"],
+                season: g.season,
+            });
+
+            assert.equal(teams.length, g.numTeams);
+            const t = teams[4];
+            assert.deepEqual(t, {
+                tid: 4,
+                abbrev: 'CIN',
+                seasonAttrs: {
+                    season: g.season,
+                    won: 0,
+                },
+                stats: {
+                    gp: 10,
+                    fg: 5,
+                    fgp: 50,
+                    playoffs: false,
+                },
+            });
+        });
+        it("should return requested info if tid/season match, even when no attrs requested", async () => {
+            const t = await getCopy.teams({
+                seasonAttrs: ["season", "won"],
+                stats: ["gp", "fg", "fgp"],
+                tid: 4,
+                season: g.season,
+            });
+
+            assert.deepEqual(t, {
+                seasonAttrs: {
+                    season: g.season,
+                    won: 0,
+                },
+                stats: {
+                    gp: 10,
+                    fg: 5,
+                    fgp: 50,
+                    playoffs: false,
+                },
+            });
+        });
+        it("should return requested info if tid/season match, even when no seasonAttrs requested", async () => {
+            const t = await getCopy.teams({
+                attrs: ["tid", "abbrev"],
+                stats: ["gp", "fg", "fgp"],
+                tid: 4,
+                season: g.season,
+            });
+
+            assert.deepEqual(t, {
+                tid: 4,
+                abbrev: 'CIN',
+                stats: {
+                    gp: 10,
+                    fg: 5,
+                    fgp: 50,
+                    playoffs: false,
+                },
+            });
+        });
+        it("should return requested info if tid/season match, even when no stats requested", async () => {
+            const t = await getCopy.teams({
+                attrs: ["tid", "abbrev"],
+                seasonAttrs: ["season", "won"],
+                tid: 4,
+                season: g.season,
+            });
+
+            assert.deepEqual(t, {
+                tid: 4,
+                abbrev: 'CIN',
+                seasonAttrs: {
+                    season: g.season,
+                    won: 0,
+                },
+            });
+        });
+        it("should return season totals if statType is 'totals'", async () => {
+            const t = await getCopy.teams({
+                stats: ["gp", "fg", "fga", "fgp"],
+                tid: 4,
+                season: g.season,
+                statType: 'totals',
+            });
+
+            assert.deepEqual(t, {
+                stats: {
+                    gp: 10,
+                    fg: 50,
+                    fga: 100,
+                    fgp: 50,
+                    playoffs: false,
+                },
+            });
+        });
+        it("should return playoff stats if playoffs is true", async () => {
+            const t = await getCopy.teams({
+                stats: ["gp", "fg", "fga", "fgp"],
+                tid: 4,
+                season: g.season,
+                playoffs: true,
+                regularSeason: false,
+            });
+
+            assert.deepEqual(t, {
+                stats: {
+                    gp: 4,
+                    fg: 3,
+                    fga: 30,
+                    fgp: 10,
+                    playoffs: true,
+                },
+            });
+        });
+        it("should return stats in an array if no season is specified", async () => {
+            const t = await getCopy.teams({
+                stats: ["gp", "fg", "fga", "fgp"],
+                tid: 4,
+                playoffs: true,
+                regularSeason: false,
+            });
+
+            assert.deepEqual(t, {
+                stats: [{
+                    gp: 4,
+                    fg: 3,
+                    fga: 30,
+                    fgp: 10,
+                    playoffs: true,
+                }],
+            });
+        });
+        it("should return stats in an array if regular season and playoffs are specified", async () => {
+            const t = await getCopy.teams({
+                stats: ["gp", "fg", "fga", "fgp"],
+                tid: 4,
+                playoffs: true,
+            });
+
+            assert.deepEqual(t, {
+                stats: [{
+                    gp: 10,
+                    fg: 5,
+                    fga: 10,
+                    fgp: 50,
+                    playoffs: false,
+                }, {
+                    gp: 4,
+                    fg: 3,
+                    fga: 30,
+                    fgp: 10,
+                    playoffs: true,
+                }],
+            });
+        });
+    });
+});

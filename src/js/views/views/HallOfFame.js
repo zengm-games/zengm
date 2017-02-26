41407ffd7a8fa842a7e0859b12bcb8740e995045
Kevin Gilbert <kgilbert.cmu@gmail.com>
2017-02-26 02:26:53 -0800

    add explanation of new colors to HoF page

--- src/js/views/views/HallOfFame.js
+++ src/js/views/views/HallOfFame.js
@@ -58,7 +58,7 @@ const HallOfFame = ({players}) => {
     return <div>
         <h1>Hall of Fame <NewWindowLink /></h1>
 
-        <p>Players are eligible to be inducted into the Hall of Fame after they retire. The formula for inclusion is very similar to <a href="http://espn.go.com/nba/story/_/id/8736873/nba-experts-rebuild-springfield-hall-fame-espn-magazine">the method described in this article</a>. Hall of famers who played for your team are <span className="text-info">highlighted in blue</span>.</p>
+        <p>Players are eligible to be inducted into the Hall of Fame after they retire. The formula for inclusion is very similar to <a href="http://espn.go.com/nba/story/_/id/8736873/nba-experts-rebuild-springfield-hall-fame-espn-magazine">the method described in this article</a>. Hall of Famers who played for your team are <span className="text-info">highlighted in blue</span>. Hall of Famers who retired with your team are <span className="text-success">highlighted in green</span>. Hall of Famers who played most of their career with your team are <span className="text-danger">highlighted in red</span>.</p>
 
         <DataTable
             cols={cols}

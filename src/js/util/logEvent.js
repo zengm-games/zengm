0605471eeeded1077894b620747019d872bb6fe9
Jeremy Scheff <jdscheff@gmail.com>
2017-02-26 10:06:15 -0500

    Reorganize logEvent a bit

--- src/js/util/logEvent.js
+++ src/js/util/logEvent.js
@@ -4,38 +4,95 @@ import g from '../globals';
 import * as league from '../worker/core/league';
 import notify from '../lib/bbgm-notifications';
 
+type EventType = (
+    'achievement' |
+    'award' |
+    'changes' |
+    'draft' |
+    'error' |
+    'freeAgent' |
+    'gameLost' |
+    'gameWon' |
+    'hallOfFame' |
+    'healed' |
+    'injured' |
+    'playerFeat' |
+    'playoffs' |
+    'reSigned' |
+    'refuseToSign' |
+    'release' |
+    'retired' |
+    'screenshot' |
+    'trade' |
+    'tragedy'
+);
+
 // Really, pids, tids, and type should not be optional if saveToDb is true
 type LogEventOptions = {
     extraClass?: string,
-    persistent?: boolean,
+    persistent: boolean,
     pids?: number[],
-    saveToDb?: boolean,
-    showNotification?: boolean,
+    saveToDb: boolean,
+    showNotification: boolean,
     text: string,
     tids?: number[],
-    type: ?(
-        'achievement' |
-        'award' |
-        'changes' |
-        'draft' |
-        'error' |
-        'freeAgent' |
-        'gameLost' |
-        'gameWon' |
-        'hallOfFame' |
-        'healed' |
-        'injured' |
-        'playerFeat' |
-        'playoffs' |
-        'reSigned' |
-        'refuseToSign' |
-        'release' |
-        'retired' |
-        'screenshot' |
-        'trade' |
-        'tragedy'
-    ),
-}
+    type: EventType,
+};
+
+const saveEvent = (event: {
+    type: EventType,
+    text: string,
+    pids: number[],
+    tids: number[],
+}) => {
+    if (g.cache) {
+        g.cache.add('events', Object.assign({}, event, {season: g.season}));
+    }
+};
+
+const showEvent = ({
+    extraClass,
+    persistent,
+    text,
+    type,
+}: {
+    extraClass?: string,
+    persistent: boolean,
+    text: string,
+    type: string,
+}) => {
+    let title;
+    if (type === "error") {
+        title = "Error!";
+    } else if (type === "changes") {
+        title = "Changes since your last visit";
+    }
+
+    if (persistent && extraClass === undefined) {
+        extraClass = 'notification-danger';
+    }
+
+    // Don't show non-critical notification if we're viewing a live game now
+    if (!location.pathname.includes('/live') || persistent) {
+        notify(text, title, {
+            extraClass,
+            persistent,
+        });
+
+        // Persistent notifications are very rare and should stop game sim when displayed
+        if (persistent && g.autoPlaySeasons <= 0) {
+            league.setGameAttributes({stopGames: true});
+        }
+    }
+
+    // Hacky way to make sure there is room for the multi team mode menu
+    const notificationContainer = document.getElementById("notification-container");
+    if (g.userTids !== undefined && g.userTids.length > 1 && notificationContainer && !notificationContainer.classList.contains("notification-container-extra-margin-bottom")) {
+        notificationContainer.classList.add("notification-container-extra-margin-bottom");
+    } else if (g.userTids !== undefined && g.userTids.length === 1 && notificationContainer && notificationContainer.classList.contains("notification-container-extra-margin-bottom")) {
+        notificationContainer.classList.remove("notification-container-extra-margin-bottom");
+    }
+};
 
 const logEvent = ({
     extraClass,
@@ -47,9 +104,11 @@ const logEvent = ({
     tids,
     type,
 }: LogEventOptions) => {
-    if (saveToDb && g.lid) { // Only save to league event log if within a league
-        g.cache.add('events', {
-            season: g.season,
+    if (saveToDb) {
+        if (pids === undefined || tids === undefined || type === undefined) {
+            throw new Error('Saved event must include pids, tids, and type');
+        }
+        saveEvent({
             type,
             text,
             pids,
@@ -58,37 +117,12 @@ const logEvent = ({
     }
 
     if (showNotification) {
-        let title;
-        if (type === "error") {
-            title = "Error!";
-        } else if (type === "changes") {
-            title = "Changes since your last visit";
-        }
-
-        if (persistent && extraClass === undefined) {
-            extraClass = 'notification-danger';
-        }
-
-        // Don't show non-critical notification if we're viewing a live game now
-        if (!location.pathname.includes('/live') || persistent) {
-            notify(text, title, {
-                extraClass,
-                persistent,
-            });
-
-            // Persistent notifications are very rare and should stop game sim when displayed
-            if (persistent && g.autoPlaySeasons <= 0) {
-                league.setGameAttributes({stopGames: true});
-            }
-        }
-    }
-
-    // Hacky way to make sure there is room for the multi team mode menu
-    const notificationContainer = document.getElementById("notification-container");
-    if (g.userTids !== undefined && g.userTids.length > 1 && notificationContainer && !notificationContainer.classList.contains("notification-container-extra-margin-bottom")) {
-        notificationContainer.classList.add("notification-container-extra-margin-bottom");
-    } else if (g.userTids !== undefined && g.userTids.length === 1 && notificationContainer && notificationContainer.classList.contains("notification-container-extra-margin-bottom")) {
-        notificationContainer.classList.remove("notification-container-extra-margin-bottom");
+        showEvent({
+            extraClass,
+            persistent,
+            text,
+            type,
+        });
     }
 };
 

ce8d9360acf18855ad7e2399ee39ba8378f19795
Jeremy Scheff <jdscheff@gmail.com>
2017-02-28 22:50:45 -0500

    Separate UI and worker

--- src/js/worker/api.js
+++ src/js/worker/api.js
@@ -1,11 +1,44 @@
 // @flow
 
-import {emitter, realtimeUpdate} from '../ui/util';
+import {ads, emitter, realtimeUpdate} from '../ui/util';
 import {showEvent} from '../ui/util/logEvent';
 import type {GameAttributes, LogEventShowOptions, UpdateEvents} from '../common/types';
 
 const emit = (name: string, content: any) => {
     emitter.emit(name, content);
+};
+
+const initAds = (goldUntil: number | void) => {
+    // No ads for Gold members
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    if (currentTimestamp > goldUntil) {
+        let el;
+        el = document.getElementById('banner-ad-top-wrapper');
+        if (el) {
+            el.innerHTML = '<div id="div-gpt-ad-1473268147477-1" style="text-align: center; min-height: 95px; margin-top: 1em"></div>';
+        }
+        el = document.getElementById('banner-ad-bottom-wrapper-1');
+        if (el) {
+            el.innerHTML = '<div id="div-gpt-ad-1479941549483-2" style="text-align: center; height: 250px; position: absolute; top: 5px; left: 0"></div>';
+        }
+        el = document.getElementById('banner-ad-bottom-wrapper-2');
+        if (el) {
+            el.innerHTML = '<div id="div-gpt-ad-1479941549483-1" style="text-align: center; height: 250px; position: absolute; top: 5px; right: 0"></div>';
+        }
+        el = document.getElementById('banner-ad-bottom-wrapper-logo');
+        if (el) {
+            el.innerHTML = '<div style="height: 250px; margin: 5px 310px 0 310px; display:flex; align-items: center; justify-content: center;"><img src="https://basketball-gm.com/files/logo.png" style="max-height: 100%; max-width: 100%"></div>';
+        }
+        ads.showBanner();
+    } else {
+        const wrappers = ['banner-ad-top-wrapper', 'banner-ad-bottom-wrapper-1', 'banner-ad-bottom-wrapper-logo', 'banner-ad-bottom-wrapper-2'];
+        for (const wrapper of wrappers) {
+            const el = document.getElementById(wrapper);
+            if (el) {
+                el.innerHTML = '';
+            }
+        }
+    }
 };
 
 async function realtimeUpdate2(updateEvents: UpdateEvents = [], url?: string, raw?: Object = {}) {
@@ -22,6 +55,7 @@ const showEvent2 = (options: LogEventShowOptions) => {
 
 export {
     emit,
+    initAds,
     realtimeUpdate2 as realtimeUpdate,
     setGameAttributes,
     showEvent2 as showEvent,
--- src/js/worker/util/account.js
+++ src/js/worker/util/account.js
@@ -6,7 +6,6 @@ import $ from 'jquery';
 import {SPORT, g} from '../../common';
 import * as api from '../api';
 import {getCopy, idb} from '../db';
-import {ads} from '../../ui/util';
 import {logEvent} from '../util';
 import type {AchievementKey} from '../../common/types';
 
@@ -149,36 +148,7 @@ async function check() {
             username: data.username,
         });
 
-        // No ads for Gold members
-        const currentTimestamp = Math.floor(Date.now() / 1000);
-        if (currentTimestamp > data.gold_until) {
-            let el;
-            el = document.getElementById('banner-ad-top-wrapper');
-            if (el) {
-                el.innerHTML = '<div id="div-gpt-ad-1473268147477-1" style="text-align: center; min-height: 95px; margin-top: 1em"></div>';
-            }
-            el = document.getElementById('banner-ad-bottom-wrapper-1');
-            if (el) {
-                el.innerHTML = '<div id="div-gpt-ad-1479941549483-2" style="text-align: center; height: 250px; position: absolute; top: 5px; left: 0"></div>';
-            }
-            el = document.getElementById('banner-ad-bottom-wrapper-2');
-            if (el) {
-                el.innerHTML = '<div id="div-gpt-ad-1479941549483-1" style="text-align: center; height: 250px; position: absolute; top: 5px; right: 0"></div>';
-            }
-            el = document.getElementById('banner-ad-bottom-wrapper-logo');
-            if (el) {
-                el.innerHTML = '<div style="height: 250px; margin: 5px 310px 0 310px; display:flex; align-items: center; justify-content: center;"><img src="https://basketball-gm.com/files/logo.png" style="max-height: 100%; max-width: 100%"></div>';
-            }
-            ads.showBanner();
-        } else {
-            const wrappers = ['banner-ad-top-wrapper', 'banner-ad-bottom-wrapper-1', 'banner-ad-bottom-wrapper-logo', 'banner-ad-bottom-wrapper-2'];
-            for (const wrapper of wrappers) {
-                const el = document.getElementById(wrapper);
-                if (el) {
-                    el.innerHTML = '';
-                }
-            }
-        }
+        api.initAds(data.gold_until);
 
         // If user is logged in, upload any locally saved achievements
         if (data.username !== "") {

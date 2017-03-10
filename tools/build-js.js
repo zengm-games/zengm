f1e924f69fd8ba40b168aeb41081636772403d71
Jeremy Scheff <jdscheff@gmail.com>
2017-03-09 21:32:48 -0500

    Use blacklistify to prevent ui/worker bundle contamination

--- tools/build-js.js
+++ tools/build-js.js
@@ -5,14 +5,21 @@
 // ...but then it got too complicated, and this seemed easier
 
 const browserify = require('browserify');
+const blacklistify = require('blacklistify/custom');
 const envify = require('envify/custom');
 const exorcist = require('exorcist');
 const fs = require('fs');
 
 console.log('Bundling JavaScript files...');
 
+const BLACKLIST = {
+    ui: [/.*\/worker.*/],
+    worker: [/.*\/ui.*/, /.*react.*/],
+};
+
 for (const name of ['ui', 'worker']) {
     browserify(`src/js/${name}/index.js`, {debug: true})
+        .transform(blacklistify(BLACKLIST[name]))
         .transform({global: true}, envify({NODE_ENV: 'production'}))
         .bundle()
         .pipe(exorcist(`build/gen/${name}.js.map`))

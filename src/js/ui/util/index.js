2526c47700f2422c2975b2b0e1c4147a0dc7b0b0
Jeremy Scheff <jdscheff@gmail.com>
2017-03-28 20:44:49 -0400

    Send Worker errors to Bugsnag

--- src/js/ui/util/index.js
+++ src/js/ui/util/index.js
@@ -3,6 +3,14 @@
 import PromiseWorker from 'promise-worker-bi';
 const worker = window.useSharedWorker ? new SharedWorker('/gen/worker.js') : new Worker('/gen/worker.js');
 export const promiseWorker = new PromiseWorker(worker);
+promiseWorker.registerError((e) => {
+    if (window.Bugsnag) {
+        window.Bugsnag.notifyException(new Error(e.message), 'ErrorInWorker', {
+            colno: e.colno,
+            lineno: e.lineno,
+        });
+    }
+});
 
 export {default as ads} from './ads';
 export {default as emitter} from './emitter';

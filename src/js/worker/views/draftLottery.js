1833fd08ffd4cf87f5553e47b112a62951cca047
Jeremy Scheff <jdscheff@gmail.com>
2017-07-07 21:10:24 -0400

    Don't always update draft lottery screen

--- src/js/worker/views/draftLottery.js
+++ src/js/worker/views/draftLottery.js
@@ -3,40 +3,44 @@
 import {PHASE, g} from '../../common';
 import {draft} from '../core';
 import {idb} from '../db';
-import type {DraftLotteryResultArray} from '../../common/types';
+import type {DraftLotteryResultArray, UpdateEvents} from '../../common/types';
 
 async function updateDraftLottery(
     {season}: {season: number},
+    updateEvents: UpdateEvents,
+    state: any,
 ): Promise<{
     result: DraftLotteryResultArray | void,
     season: number,
     type: 'completed' | 'projected',
 }> {
-    // View completed draft lottery
-    if (season < g.season || season === g.season && g.phase >= PHASE.DRAFT) {
-        const draftLotteryResult = await idb.getCopy.draftLotteryResults({season});
+    if (updateEvents.includes('firstRun') || season !== state.season || (season === g.season && updateEvents.includes('gameSim'))) {
+        // View completed draft lottery
+        if (season < g.season || season === g.season && g.phase >= PHASE.DRAFT) {
+            const draftLotteryResult = await idb.getCopy.draftLotteryResults({season});
 
-        const result = draftLotteryResult !== undefined ? draftLotteryResult.result : undefined;
+            const result = draftLotteryResult !== undefined ? draftLotteryResult.result : undefined;
+
+            return {
+                result,
+                season,
+                type: 'completed',
+            };
+        }
+
+        // View projected draft lottery for this season
+        const draftLotteryResult = await draft.genOrder(true);
+
+        for (const pick of draftLotteryResult.result) {
+            pick.pick = undefined;
+        }
 
         return {
-            result,
-            season,
-            type: 'completed',
+            result: draftLotteryResult.result,
+            season: draftLotteryResult.season,
+            type: 'projected',
         };
     }
-
-    // View projected draft lottery for this season
-    const draftLotteryResult = await draft.genOrder(true);
-
-    for (const pick of draftLotteryResult.result) {
-        pick.pick = undefined;
-    }
-
-    return {
-        result: draftLotteryResult.result,
-        season: draftLotteryResult.season,
-        type: 'projected',
-    };
 }
 
 export default {

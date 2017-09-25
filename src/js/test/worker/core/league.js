67e193be3eea0738bff46882ab42e9d2c2267566
Jeremy Scheff <jdscheff@gmail.com>
2017-09-25 19:04:35 -0400

    Fix test

--- src/js/test/worker/core/league.js
+++ src/js/test/worker/core/league.js
@@ -100,7 +100,7 @@ describe("core/league", () => {
             assert.equal(gTest.daysLeft, 0);
             assert.equal(gTest.showFirstOwnerMessage, true);
 
-            assert.equal(Object.keys(gTest).length, 35);
+            assert.equal(Object.keys(gTest).length, 36);
         });
         it("should initialize draftOrder object store", async () => {
             const draftOrder = await idb.league.draftOrder.getAll();

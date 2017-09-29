1ad77beee09369ea988126c524c484013566dd2a
Jeremy Scheff <jdscheff@gmail.com>
2017-09-29 13:46:04 -0400

    Minify in parallel to shave a few seconds of build time

--- tools/minify-js.js
+++ tools/minify-js.js
@@ -6,14 +6,28 @@ const UglifyJS = require('uglify-es');
 console.log('Minifying JS bundle...\nWARNING: This is likely to cause bugs');
 
 for (const name of ['ui', 'worker']) {
-    const result = UglifyJS.minify(fs.readFileSync(`build/gen/${name}.js`, 'utf8'), {
-        sourceMap: {
-            content: 'inline',
-            filename: `build/gen/${name}.js`,
-            url: `${name}.js.map`,
-        },
-    });
+    fs.readFile(`build/gen/${name}.js`, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
 
-    fs.writeFileSync(`build/gen/${name}.js`, result.code);
-    fs.writeFileSync(`build/gen/${name}.js.map`, result.map);
+        const result = UglifyJS.minify(data, {
+            sourceMap: {
+                content: 'inline',
+                filename: `build/gen/${name}.js`,
+                url: `${name}.js.map`,
+            },
+        });
+
+        fs.writeFile(`build/gen/${name}.js`, result.code, (err2) => {
+            if (err2) {
+                throw err2;
+            }
+        });
+        fs.writeFile(`build/gen/${name}.js.map`, result.map, (err2) => {
+            if (err2) {
+                throw err2;
+            }
+        });
+    });
 }

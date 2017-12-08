9de1a0ba3f5f894984d00ee6ef5208d23afd5296
Jeremy Scheff <jdscheff@gmail.com>
2017-12-08 09:18:34 -0500

    Don't allow free agents to win awards based on last season

--- src/js/worker/core/season.js
+++ src/js/worker/core/season.js
@@ -162,8 +162,7 @@ async function doAwards(conditions: Conditions) {
             "season",
         ],
     });
-
-    players = players.filter(p => p.stats.length > 0);
+    players = players.filter(p => p.stats.length > 0 && p.stats[p.stats.length - 1].season === g.season);
     for (const p of players) {
         p.currentStats = p.stats[p.stats.length - 1];
     }

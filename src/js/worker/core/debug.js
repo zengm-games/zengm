f29d7592fb43d95b2aec98f16839eda963a11a6d
Jeremy Scheff <jdscheff@gmail.com>
2018-01-11 21:49:32 -0500

    debug.avgRatingDists

--- src/js/worker/core/debug.js
+++ src/js/worker/core/debug.js
@@ -3,6 +3,7 @@
 // Functions only used for debugging the game, particularly balance issues. This should not be included or loaded in the compiled version.
 
 import backboard from "backboard";
+import range from "lodash/range";
 import { PLAYER, g, helpers } from "../../common";
 import { player } from "../core";
 import { idb } from "../db";
@@ -301,7 +302,7 @@ function averageCareerArc(ratingToSave: RatingKey) {
     }
 }
 
-const ratingDists = (numPlayers: number = 100) => {
+const maxRatingDists = (numPlayers: number = 100) => {
     // Each player gets one entry per array: their career max in a rating
     const ratings = {
         ovr: [],
@@ -390,9 +391,76 @@ const ratingDists = (numPlayers: number = 100) => {
     }
 };
 
+const avgRatingDists = (numPlayers: number = 100) => {
+    const NUM_SEASONS = 20;
+
+    const ratings = range(NUM_SEASONS).map(() => {
+        return {
+            ovr: [],
+            stre: [],
+            spd: [],
+            jmp: [],
+            endu: [],
+            ins: [],
+            dnk: [],
+            ft: [],
+            fg: [],
+            tp: [],
+            oiq: [],
+            diq: [],
+            drb: [],
+            pss: [],
+            reb: [],
+        };
+    })
+
+    for (let i = 0; i < numPlayers; i++) {
+        // Log every 5%
+        if (i % Math.round(numPlayers / 20) === 0) {
+            console.log(`${Math.round(100 * i / numPlayers)}%`);
+        }
+
+        const p = player.generate(PLAYER.FREE_AGENT, 19, g.season, false, 15.5);
+
+        for (let j = 0; j < NUM_SEASONS; j++) {
+            player.develop(p, 1, false, 15.5, true);
+            p.born.year -= 1; // Aging after develop
+
+
+            for (const key of Object.keys(ratings[j])) {
+                ratings[j][key].push(p.ratings[0][key]);
+            }
+        }
+    }
+
+    const q1 = Math.floor(0.25 * numPlayers);
+    const q2 = Math.floor(0.5 * numPlayers);
+    const q3 = Math.floor(0.75 * numPlayers);
+
+    console.log("Career arc for the q1/median/q3 player");
+
+    for (const key of Object.keys(ratings[0])) {
+        const ratingsForKey = ratings.map(r => {
+            // $FlowFixMe
+            return r[key].sort((a, b) => a - b);
+        });
+
+        const q1s = ratingsForKey.map(row => row[q1]);
+        const q2s = ratingsForKey.map(row => row[q2]);
+        const q3s = ratingsForKey.map(row => row[q3]);
+
+        console.log(`${key}:`);
+        console.log(`q1: ${JSON.stringify(q1s)}`);
+        console.log(`q2: ${JSON.stringify(q2s)}`);
+        console.log(`q3: ${JSON.stringify(q3s)}`);
+        console.log("");
+    }
+};
+
 export default {
     regressRatingsPer,
     leagueAverageContract,
     averageCareerArc,
-    ratingDists,
+    maxRatingDists,
+    avgRatingDists,
 };

a581bf355aaba92abe80e9323221911e3f28e01b
Jeremy Scheff <jdscheff@gmail.com>
2018-01-27 19:42:03 -0500

    Restore ovr

--- src/js/worker/core/player/index.js
+++ src/js/worker/core/player/index.js
@@ -55,23 +55,22 @@ function ovr(ratings: PlayerRatings): number {
     // This formula is loosely based on linear regression:
     return helpers.bound(
         Math.round(
-            1.2 *
-                (4 * ratings.hgt +
-                    2 * ratings.stre +
-                    4 * ratings.spd +
-                    2 * ratings.jmp +
-                    3 * ratings.endu +
-                    3 * ratings.ins +
-                    4 * ratings.dnk +
-                    ratings.ft +
-                    ratings.fg +
-                    2 * ratings.tp +
-                    5 * ratings.oiq +
-                    5 * ratings.diq +
-                    ratings.drb +
-                    5 * ratings.pss +
-                    ratings.reb) /
-                43,
+            (4 * ratings.hgt +
+                2 * ratings.stre +
+                4 * ratings.spd +
+                2 * ratings.jmp +
+                3 * ratings.endu +
+                3 * ratings.ins +
+                4 * ratings.dnk +
+                ratings.ft +
+                ratings.fg +
+                2 * ratings.tp +
+                5 * ratings.oiq +
+                5 * ratings.diq +
+                ratings.drb +
+                5 * ratings.pss +
+                ratings.reb) /
+            43,
         ),
         0,
         100,

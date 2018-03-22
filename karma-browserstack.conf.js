5d128e10935fd99c39f369a01af1e255fda49900
Jeremy Scheff <jdscheff@gmail.com>
2018-03-22 17:00:59 -0400

    Comments about supported versions

--- karma-browserstack.conf.js
+++ karma-browserstack.conf.js
@@ -5,19 +5,19 @@ const browserStack = require('../../.browserstack.json');
 const customLaunchers = [{
     base: 'BrowserStack',
     browser: 'firefox',
-    browser_version: '52.0',
+    browser_version: '52.0', // 52 is an LTS and the last release on XP, but it works back to 47 currently
     os: 'Windows',
     os_version: '10',
 }, {
     base: 'BrowserStack',
     browser: 'chrome',
-    browser_version: '56.0',
+    browser_version: '49.0', // Last release on XP and some old Mac versions
     os: 'Windows',
     os_version: '10',
 }, {
     base: 'BrowserStack',
     browser: 'safari',
-    browser_version: '10.1',
+    browser_version: '10.1', // Works in 10 too, but 10.1 has buggy getAll
     os: 'OS X',
     os_version: 'Sierra',
 }].reduce((acc, browser, i) => {

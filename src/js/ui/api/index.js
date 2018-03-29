c11a23cdedf59f9084bbe415310a1b9cba7cad95
Jeremy Scheff <jdscheff@gmail.com>
2018-03-29 19:54:20 -0400

    bbgm-ads-oop

--- src/js/ui/api/index.js
+++ src/js/ui/api/index.js
@@ -73,7 +73,7 @@ const initAds = (goldUntil: number | void) => {
             // This initializes the ads and displays the initial banners. It returns a promise
             // which resolves when it's done.
             window.bbgmAds
-                .init(["bbgm-ads-top", "bbgm-ads-bottom1", "bbgm-ads-bottom2"])
+                .init(["bbgm-ads-top", "bbgm-ads-bottom1", "bbgm-ads-bottom2", "bbgm-ads-oop"])
                 .then(() => {
                     // Show the logo too (it's not an ad so it's not managed by bbgmAds)
                     const logo = document.getElementById("bbgm-ads-logo");
--- src/js/ui/components/Footer.js
+++ src/js/ui/components/Footer.js
@@ -58,6 +58,7 @@ class Footer extends React.Component<{}> {
                         }}
                     />
                 </div>
+                <div id="bbgm-ads-oop" style={{height: "1px", width: "1px"}} />
 
                 <div className="clearfix" />
                 <hr />

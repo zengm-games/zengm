c3207b4cf289cead6f9dce92c8ebd57ab1cc5d64
Jeremy Scheff <jdscheff@gmail.com>
2018-04-14 15:35:40 -0400

    Move debug functions to individual files

---
+++ src/js/worker/core/debug/compositeRatingDists.js
@@ -0,0 +1,28 @@
+// @flow
+
+import backboard from "backboard";
+import { COMPOSITE_WEIGHTS, PLAYER } from "../../../common";
+import { player } from "../../core";
+import { idb } from "../../db";
+
+const compositeRatingDists = async () => {
+    // All non-retired players
+    const players = await idb.league.players
+        .index("tid")
+        .getAll(backboard.lowerBound(PLAYER.FREE_AGENT));
+
+    const compositeRatings = players
+        .map(p => {
+            return player.compositeRating(
+                p.ratings[p.ratings.length - 1],
+                COMPOSITE_WEIGHTS.shootingThreePointer.ratings,
+                COMPOSITE_WEIGHTS.shootingThreePointer.weights,
+                false,
+            );
+        })
+        .sort((a, b) => b - a);
+
+    console.log(compositeRatings);
+};
+
+export default compositeRatingDists;
---
+++ src/js/worker/core/debug/index.js
@@ -0,0 +1,23 @@
+// @flow
+
+// Functions only used for debugging the game, particularly balance issues. This should not be included or loaded in the compiled version.
+
+import averageCareerArc from "./averageCareerArc";
+import avgRatingDists from "./avgRatingDists";
+import compositeRatingDists from "./compositeRatingDists";
+import countPositions from "./countPositions";
+import countSkills from "./countSkills";
+import leagueAverageContract from "./leagueAverageContract";
+import maxRatingDists from "./maxRatingDists";
+import valueDiff from "./valueDiff";
+
+export default {
+    averageCareerArc,
+    avgRatingDists,
+    compositeRatingDists,
+    countPositions,
+    countSkills,
+    leagueAverageContract,
+    maxRatingDists,
+    valueDiff,
+};

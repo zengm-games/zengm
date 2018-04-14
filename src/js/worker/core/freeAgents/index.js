0b9849c9a519a3056b24bd144d055fd0d98cc3a1
Jeremy Scheff <jdscheff@gmail.com>
2018-04-14 15:12:31 -0400

    Move freeAgents functions to individual files

---
+++ src/js/worker/core/freeAgents/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import amountWithMood from "./amountWithMood";
+import autoSign from "./autoSign";
+import decreaseDemands from "./decreaseDemands";
+import play from "./play";
+
+export default {
+    amountWithMood,
+    autoSign,
+    decreaseDemands,
+    play,
+};

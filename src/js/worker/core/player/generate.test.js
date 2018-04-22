b2a6c9fc59a8bc3c7af406e3cd097866efff6478
Jeremy Scheff <jdscheff@gmail.com>
2018-04-21 22:53:36 -0400

    Convert more tests

---
+++ src/js/worker/core/player/generate.test.js
@@ -0,0 +1,12 @@
+// @flow
+
+import assert from "assert";
+import { describe, it } from "mocha";
+import player from "./index";
+
+describe("worker/core/player/generate", () => {
+    it("create player with no stats", () => {
+        const p = player.generate(-2, 19, 2012, false, 15.5);
+        assert.equal(p.stats.length, 0);
+    });
+});

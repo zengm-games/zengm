50383f0d3ac905fd0dbf1ccaa3fac8ca05b5d9ae
Jeremy Scheff <jdscheff@gmail.com>
2018-04-23 21:29:14 -0400

    Convert more tests

--- src/js/worker/db/Cache.test.js
+++ src/js/worker/db/Cache.test.js
@@ -1,14 +1,14 @@
 import assert from "assert";
-import { g } from "../../../common";
-import helpers from "../../helpers";
-import { player } from "../../../worker/core";
-import { idb } from "../../../worker/db";
+import { g } from "../../common";
+import testHelpers from "../../test/helpers";
+import { player } from "../core";
+import { idb } from ".";
 
-describe("db/Cache", () => {
+describe("worker/db/Cache", () => {
     before(async () => {
-        helpers.resetG();
+        testHelpers.resetG();
 
-        await helpers.resetCache({
+        await testHelpers.resetCache({
             players: [player.generate(g.userTid, 30, 2017, true, 15.5)],
         });
     });
@@ -16,19 +16,19 @@ describe("db/Cache", () => {
         idb.cache._status = "full";
     });
 
-    describe("#get()", () => {
-        it("should retrieve an object", async () => {
+    describe("get", () => {
+        it("retrieve an object", async () => {
             const p = (await idb.cache.players.getAll())[0];
             const p2 = await idb.cache.players.get(p.pid);
             assert.equal(p.pid, p2.pid);
         });
-        it("should return undefined for invalid ID", async () => {
+        it("return undefined for invalid ID", async () => {
             const p = await idb.cache.players.get(-1);
             assert.equal(typeof p, "undefined");
         });
 
         for (const status of ["filling", "flushing"]) {
-            it(`should wait until ${status} complete before resolving query`, async () => {
+            it(`wait until ${status} complete before resolving query`, async () => {
                 const p = (await idb.cache.players.getAll())[0];
 
                 idb.cache._status = status;

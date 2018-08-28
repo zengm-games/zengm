3b3ccc3372232e06b4a1ff6800dbb22f58e5e534
Jeremy Scheff <jdscheff@gmail.com>
2018-08-28 19:54:06 -0400

    Correct obsolete comment

--- src/js/worker/util/g.js
+++ src/js/worker/util/g.js
@@ -2,7 +2,7 @@
 
 import type { GameAttributes } from "../../common/types";
 
-// This will get filled by values from IndexedDB. It is initialized by different mechanisms in the ui and worker, but the end result is the same.
+// This will get filled by values from IndexedDB
 const g: GameAttributes = {
     lid: undefined,
 };

d45e2191de23c65f37619ed98cd5fddc5a32534c
Jeremy Scheff <jdscheff@gmail.com>
2018-09-17 00:27:05 -0400

    Restore empty appcache

--- src/sw.js
+++ src/sw.js
@@ -10,12 +10,14 @@ workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerNavigationRoute("/index.html", {
     blacklist: [
+        new RegExp("^/bbgm.appcache"),
         new RegExp("^/files"),
         new RegExp("^/fonts"),
         new RegExp("^/gen"),
         new RegExp("^/ico"),
         new RegExp("^/img"),
         new RegExp("^/manifest.webapp"),
+        new RegExp("^/manifest_hack"),
         new RegExp("^/robots.txt"),
         new RegExp("^/sw.js"),
     ],

6ffc7a4b02f8a92d6857b2de9f30eafd6e8dc986
Jeremy Scheff <jdscheff@gmail.com>
2018-10-05 23:14:55 -0400

    Scrolling in a responsive table will not be mistaken for opening/closing the sidebar

--- src/js/ui/components/ResponsiveTableWrapper.js
+++ src/js/ui/components/ResponsiveTableWrapper.js
@@ -10,7 +10,42 @@ type Props = {
     nonfluid?: boolean,
 };
 
+const handleTouchStart = (event: TouchEvent) => {
+    // This is to prevent scrolling within the responsive table from opening/closing SideBar
+    event.stopPropagation();
+};
+
 class ResponsiveTableWrapper extends React.Component<Props> {
+    ref: { current: null | React.ElementRef<"div"> };
+
+    constructor(props: Props) {
+        super(props);
+
+        this.ref = React.createRef();
+    }
+
+    componentDidMount() {
+        if (this.ref && this.ref.current) {
+            // The true here (ensures that ResponsiveTableWrapper's touchstart runs before SideBar's, the true means it
+            // will listen during the capturing phase and thus get triggered before the bubbling phase which terminates
+            // when SideBar is triggered, since SideBar listens on `document`.
+            this.ref.current.addEventListener(
+                "touchstart",
+                handleTouchStart,
+                true,
+            );
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.ref && this.ref.current) {
+            this.ref.current.removeEventListener(
+                "touchstart",
+                handleTouchStart,
+            );
+        }
+    }
+
     render() {
         const { children, className, nonfluid } = this.props;
 
@@ -19,6 +54,7 @@ class ResponsiveTableWrapper extends React.Component<Props> {
                 className={classNames("table-responsive", className, {
                     "table-nonfluid": nonfluid,
                 })}
+                ref={this.ref}
             >
                 {children}
             </div>

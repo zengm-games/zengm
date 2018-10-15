a06bbeecc81327a781ad4f75230c117c623a3ffc
Jeremy Scheff <jdscheff@gmail.com>
2018-10-14 21:32:04 -0400

    Refactor more

--- src/js/ui/views/Trade/index.js
+++ src/js/ui/views/Trade/index.js
@@ -1,10 +1,10 @@
-import classNames from "classnames";
 import PropTypes from "prop-types";
 import React from "react";
 import { PHASE } from "../../../common";
 import { NewWindowLink } from "../../components";
-import { helpers, realtimeUpdate, setTitle, toWorker } from "../../util";
+import { realtimeUpdate, setTitle, toWorker } from "../../util";
 import AssetList from "./AssetList";
+import Summary from "./Summary";
 
 class Trade extends React.Component {
     constructor(props) {
@@ -224,108 +224,12 @@ class Trade extends React.Component {
                         />
                     </div>
                     <div className="col-md-3 trade-summary">
-                        <div className="row">
-                            {summary.teams.map((t, i) => (
-                                <div key={i} className="col-md-12 col-6 mb-3">
-                                    <h4>{t.name}</h4>
-                                    <h5>Trade Away:</h5>
-                                    <ul className="list-unstyled">
-                                        {t.trade.map(p => (
-                                            <li key={`p${p.pid}`}>
-                                                <a
-                                                    href={helpers.leagueUrl([
-                                                        "player",
-                                                        p.pid,
-                                                    ])}
-                                                >
-                                                    {p.name}
-                                                </a>{" "}
-                                                (
-                                                {helpers.formatCurrency(
-                                                    p.contract.amount,
-                                                    "M",
-                                                )}
-                                                )
-                                            </li>
-                                        ))}
-                                        {t.picks.map(pick => (
-                                            <li key={pick.dpid}>{pick.desc}</li>
-                                        ))}
-                                        <li>
-                                            {helpers.formatCurrency(
-                                                t.total,
-                                                "M",
-                                            )}{" "}
-                                            Total
-                                        </li>
-                                    </ul>
-                                    <h5>Receive:</h5>
-                                    <ul className="list-unstyled">
-                                        {summary.teams[t.other].trade.map(p => (
-                                            <li key={`p${p.pid}`}>
-                                                <a
-                                                    href={helpers.leagueUrl([
-                                                        "player",
-                                                        p.pid,
-                                                    ])}
-                                                >
-                                                    {p.name}
-                                                </a>{" "}
-                                                (
-                                                {helpers.formatCurrency(
-                                                    p.contract.amount,
-                                                    "M",
-                                                )}
-                                                )
-                                            </li>
-                                        ))}
-                                        {summary.teams[t.other].picks.map(
-                                            pick => (
-                                                <li key={pick.dpid}>
-                                                    {pick.desc}
-                                                </li>
-                                            ),
-                                        )}
-                                        <li>
-                                            {helpers.formatCurrency(
-                                                summary.teams[t.other].total,
-                                                "M",
-                                            )}{" "}
-                                            Total
-                                        </li>
-                                    </ul>
-                                    <h5>
-                                        Payroll after trade:{" "}
-                                        {helpers.formatCurrency(
-                                            t.payrollAfterTrade,
-                                            "M",
-                                        )}
-                                    </h5>
-                                    <h5>
-                                        Salary cap:{" "}
-                                        {helpers.formatCurrency(salaryCap, "M")}
-                                    </h5>
-                                </div>
-                            ))}
-                        </div>
-
-                        {summary.warning ? (
-                            <p className="alert alert-danger">
-                                <strong>Warning!</strong> {summary.warning}
-                            </p>
-                        ) : null}
-                        {this.state.message ? (
-                            <p
-                                className={classNames(
-                                    "alert",
-                                    this.state.accepted
-                                        ? "alert-success"
-                                        : "alert-info",
-                                )}
-                            >
-                                {this.state.message}
-                            </p>
-                        ) : null}
+                        <Summary
+                            accepted={this.state.accepted}
+                            message={this.state.message}
+                            salaryCap={salaryCap}
+                            summary={summary}
+                        />
 
                         <center>
                             {godMode ? (

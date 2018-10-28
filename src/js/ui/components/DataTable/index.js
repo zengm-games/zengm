8a8788db3bc71af7871c4eb11bf8b8b44e38715c
Jeremy Scheff <jdscheff@gmail.com>
2018-10-28 14:52:51 -0400

    More refactoring

--- src/js/ui/components/DataTable/index.js
+++ src/js/ui/components/DataTable/index.js
@@ -13,6 +13,7 @@ import Pagination from "./Pagination";
 import PerPage from "./PerPage";
 import getSearchVal from "./getSearchVal";
 import getSortVal from "./getSortVal";
+import loadStateFromCache from "./loadStateFromCache";
 import ResponsiveTableWrapper from "../ResponsiveTableWrapper";
 import { helpers } from "../../util";
 import type { SortOrder, SortType } from "../../../common/types";
@@ -33,7 +34,7 @@ export type SuperCol = {
     title: string,
 };
 
-type Props = {
+export type Props = {
     className?: string,
     cols: Col[],
     defaultSort: SortBy,
@@ -54,76 +55,6 @@ type State = {
     perPage: number,
     searchText: string,
     sortBys: SortBy[],
-};
-
-const loadStateFromCache = (props: Props) => {
-    let perPage = parseInt(localStorage.getItem("perPage"), 10);
-    if (Number.isNaN(perPage)) {
-        perPage = 10;
-    }
-
-    const sortBysJSON = localStorage.getItem(`DataTableSort:${props.name}`);
-    let sortBys: SortBy[];
-    if (sortBysJSON === null || sortBysJSON === undefined) {
-        sortBys = [props.defaultSort];
-    } else {
-        try {
-            sortBys = JSON.parse(sortBysJSON);
-        } catch (err) {
-            sortBys = [props.defaultSort];
-        }
-    }
-
-    // Don't let sortBy reference invalid col
-    sortBys = sortBys.filter((sortBy: SortBy) => sortBy[0] < props.cols.length);
-    if (sortBys.length === 0) {
-        sortBys = [props.defaultSort];
-    }
-
-    const defaultFilters: string[] = props.cols.map(() => "");
-    let filters = localStorage.getItem(`DataTableFilters:${props.name}`);
-    if (filters === null || filters === undefined) {
-        filters = defaultFilters;
-    } else {
-        try {
-            filters = JSON.parse(filters);
-
-            // Confirm valid filters
-            if (
-                !Array.isArray(filters) ||
-                filters.length !== props.cols.length
-            ) {
-                filters = defaultFilters;
-            } else {
-                for (const filter of filters) {
-                    if (typeof filter !== "string") {
-                        filters = defaultFilters;
-                        break;
-                    }
-                }
-            }
-        } catch (err) {
-            filters = defaultFilters;
-        }
-    }
-    // Repeat for Flow
-    if (
-        filters === null ||
-        filters === undefined ||
-        typeof filters === "string"
-    ) {
-        filters = defaultFilters;
-    }
-
-    return {
-        currentPage: 1,
-        enableFilters: filters !== defaultFilters,
-        filters,
-        perPage,
-        prevName: props.name,
-        searchText: "",
-        sortBys,
-    };
 };
 
 class DataTable extends React.Component<Props, State> {

6a7d0bc7d4b0b073554ad89c9e88e78fe7e52fd0
Jeremy Scheff <jdscheff@gmail.com>
2018-11-15 14:32:35 -0500

    First attempt at Venatus

--- src/js/ui/api/index.js
+++ src/js/ui/api/index.js
@@ -80,36 +80,60 @@ const initAds = (goldUntil: number | void) => {
             container.style.maxWidth = "100%";
         }
     } else {
-        window.bbgmAds.cmd.push(() => {
-            // Show hidden divs. skyscraper has its own code elsewhere to manage display.
-            const showDivs = [
-                "bbgm-ads-top",
-                "bbgm-ads-bottom1",
-                "bbgm-ads-bottom2",
-                "skyscraper-wrapper",
-            ];
-            for (const id of showDivs) {
-                const div = document.getElementById(id);
-                if (div) {
-                    div.style.removeProperty("display");
-                }
+        // Show hidden divs. skyscraper has its own code elsewhere to manage display.
+        const showDivs = [
+            "bbgm-ads-top",
+            "bbgm-ads-bottom1",
+            "bbgm-ads-bottom2",
+            "skyscraper-wrapper",
+        ];
+        for (const id of showDivs) {
+            const div = document.getElementById(id);
+            if (div) {
+                div.style.removeProperty("display");
             }
+        }
 
-            window.bbgmAds
-                .init([
-                    "bbgm-ads-top",
-                    "bbgm-ads-bottom1",
-                    "bbgm-ads-bottom2",
-                    "bbgm-ads-skyscraper",
-                ])
-                .then(() => {
-                    // Show the logo too
-                    const logo = document.getElementById("bbgm-ads-logo");
-                    if (logo) {
-                        logo.style.display = "flex";
-                    }
-                });
-        });
+        window.top.__vm_add = window.top.__vm_add || [];
+        const venatusAds = [
+            {
+                divID: "bbgm-ads-top",
+                adID: "5bebecac46e0fb0001ad2a75",
+            },
+            {
+                divID: "bbgm-ads-bottom1",
+                adID: "5bebecc546e0fb000177eb10",
+            },
+            {
+                divID: "bbgm-ads-bottom2",
+                adID: "5bebecc546e0fb000177eb10",
+            },
+            {
+                divID: "bbgm-ads-skyscraper",
+                adID: "5bebecd346e0fb0001c54769",
+            },
+        ];
+        for (const { divID, adID } of venatusAds) {
+            const div = document.getElementById(divID);
+            if (div && div.style.display !== "none") {
+                const elm = document.createElement("div");
+                elm.classList.add("vm-placement");
+                elm.dataset.id = adID;
+
+                while (div.firstChild) {
+                    div.removeChild(div.firstChild);
+                }
+                div.appendChild(elm);
+
+                window.top.__vm_add.push(elm);
+            }
+        }
+
+        // Show the logo too
+        const logo = document.getElementById("bbgm-ads-logo");
+        if (logo) {
+            logo.style.display = "flex";
+        }
     }
 };
 
--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -231,9 +231,41 @@ api.bbgmPing("version");
             }
 
             if (!initialLoad) {
-                window.bbgmAds.cmd.push(() => {
-                    window.bbgmAds.refresh();
-                });
+                console.log("REFRESH ADS");
+                window.top.__vm_add = window.top.__vm_add || [];
+                const venatusAds = [
+                    {
+                        divID: "bbgm-ads-top",
+                        adID: "5bebecac46e0fb0001ad2a75",
+                    },
+                    {
+                        divID: "bbgm-ads-bottom1",
+                        adID: "5bebecc546e0fb000177eb10",
+                    },
+                    {
+                        divID: "bbgm-ads-bottom2",
+                        adID: "5bebecc546e0fb000177eb10",
+                    },
+                    {
+                        divID: "bbgm-ads-skyscraper",
+                        adID: "5bebecd346e0fb0001c54769",
+                    },
+                ];
+                for (const { divID, adID } of venatusAds) {
+                    const div = document.getElementById(divID);
+                    if (div && div.style.display !== "none") {
+                        const elm = document.createElement("div");
+                        elm.classList.add("vm-placement");
+                        elm.dataset.id = adID;
+
+                        while (div.firstChild) {
+                            div.removeChild(div.firstChild);
+                        }
+                        div.appendChild(elm);
+
+                        window.top.__vm_add.push(elm);
+                    }
+                }
             } else {
                 initialLoad = false;
             }

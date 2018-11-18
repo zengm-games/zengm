fe8e34f8896c646d1244f51b23f2005ebe1fdfc6
Jeremy Scheff <jdscheff@gmail.com>
2018-11-18 11:56:09 -0500

    RatingsOverview will stay in basketball

--- src/basketball/ui/components/index.js
+++ src/basketball/ui/components/index.js
@@ -2,10 +2,12 @@
 
 import BoxScore from "./BoxScore";
 import BoxScoreRow from "./BoxScoreRow";
+import RatingsOverview from "./RatingsOverview";
 import RatingsStatsPopover from "./RatingsStatsPopover";
 
 export default {
     BoxScore,
     BoxScoreRow,
+    RatingsOverview,
     RatingsStatsPopover,
 };
--- src/basketball/ui/views/Player.js
+++ src/basketball/ui/views/Player.js
@@ -9,6 +9,8 @@ import {
     WatchBlock,
 } from "../../../deion/ui/components";
 import { getCols, helpers, setTitle, toWorker } from "../../../deion/ui/util";
+import components from "../components";
+const { RatingsOverview } = components;
 
 const Relatives = ({ relatives }) => {
     if (relatives.length === 0) {
@@ -28,96 +30,6 @@ const Relatives = ({ relatives }) => {
 
 Relatives.propTypes = {
     relatives: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
-
-const RatingsOverview = ({ ratings }) => {
-    const r = ratings.length - 1;
-
-    return (
-        <>
-            <div className="d-none d-lg-flex row">
-                <div className="col-lg-8">
-                    <h3>Overall: {ratings[r].ovr}</h3>
-                </div>
-                <div className="col-lg-4">
-                    <h3>Potential: {ratings[r].pot}</h3>
-                </div>
-            </div>
-            <div className="d-lg-none row">
-                <div className="col-6">
-                    <h3>Overall: {ratings[r].ovr}</h3>
-                </div>
-                <div className="col-6">
-                    <h3 className="float-right">Potential: {ratings[r].pot}</h3>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-4">
-                    <b>Physical</b>
-                    <br />
-                    Height: {ratings[r].hgt}
-                    <br />
-                    Strength: {ratings[r].stre}
-                    <br />
-                    Speed: {ratings[r].spd}
-                    <br />
-                    Jumping: {ratings[r].jmp}
-                    <br />
-                    Endurance: {ratings[r].endu}
-                </div>
-                <div className="col-4">
-                    <b>Shooting</b>
-                    <br />
-                    Inside: {ratings[r].ins}
-                    <br />
-                    Dunks/Layups: {ratings[r].dnk}
-                    <br />
-                    Free Throws: {ratings[r].ft}
-                    <br />
-                    Two Pointers: {ratings[r].fg}
-                    <br />
-                    Three Pointers: {ratings[r].tp}
-                </div>
-                <div className="col-4">
-                    <b>Skill</b>
-                    <br />
-                    Offensive IQ: {ratings[r].oiq}
-                    <br />
-                    Defensive IQ: {ratings[r].diq}
-                    <br />
-                    Dribbling: {ratings[r].drb}
-                    <br />
-                    Passing: {ratings[r].pss}
-                    <br />
-                    Rebounding: {ratings[r].reb}
-                </div>
-            </div>
-        </>
-    );
-};
-
-RatingsOverview.propTypes = {
-    ratings: PropTypes.arrayOf(
-        PropTypes.shape({
-            diq: PropTypes.number.isRequired,
-            dnk: PropTypes.number.isRequired,
-            drb: PropTypes.number.isRequired,
-            endu: PropTypes.number.isRequired,
-            fg: PropTypes.number.isRequired,
-            ft: PropTypes.number.isRequired,
-            hgt: PropTypes.number.isRequired,
-            ins: PropTypes.number.isRequired,
-            jmp: PropTypes.number.isRequired,
-            oiq: PropTypes.number.isRequired,
-            ovr: PropTypes.number.isRequired,
-            pot: PropTypes.number.isRequired,
-            pss: PropTypes.number.isRequired,
-            reb: PropTypes.number.isRequired,
-            spd: PropTypes.number.isRequired,
-            stre: PropTypes.number.isRequired,
-            tp: PropTypes.number.isRequired,
-        }),
-    ).isRequired,
 };
 
 const StatsTable = ({ careerStats = {}, name, stats = [] }) => {
@@ -639,12 +551,16 @@ const Player = ({
                 stats={statsRegularSeason}
             />
 
-            <h3>Shot Locations</h3>
-            <ShotLocationsTable
-                careerStats={player.careerStats}
-                name="Player:ShotLocations"
-                stats={statsRegularSeason}
-            />
+            {process.env.SPORT === "basketball" ? (
+                <>
+                    <h3>Shot Locations</h3>
+                    <ShotLocationsTable
+                        careerStats={player.careerStats}
+                        name="Player:ShotLocations"
+                        stats={statsRegularSeason}
+                    />
+                </>
+            ) : null}
 
             <h2>Playoffs</h2>
             <h3>Stats</h3>
@@ -661,12 +577,16 @@ const Player = ({
                 stats={statsPlayoffs}
             />
 
-            <h3>Shot Locations</h3>
-            <ShotLocationsTable
-                careerStats={player.careerStatsPlayoffs}
-                name="Player:PlayoffShotLocations"
-                stats={statsPlayoffs}
-            />
+            {process.env.SPORT === "basketball" ? (
+                <>
+                    <h3>Shot Locations</h3>
+                    <ShotLocationsTable
+                        careerStats={player.careerStatsPlayoffs}
+                        name="Player:PlayoffShotLocations"
+                        stats={statsPlayoffs}
+                    />
+                </>
+            ) : null}
 
             <h2>Ratings</h2>
             <DataTable

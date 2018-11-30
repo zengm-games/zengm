f5838e0dafb95b097c755bb212ead5128d89d1cb
Jeremy Scheff <jdscheff@gmail.com>
2018-11-29 19:06:57 -0500

    DRY

--- src/basketball/worker/util/processPlayerStats.js
+++ src/basketball/worker/util/processPlayerStats.js
@@ -4,6 +4,33 @@ import { PLAYER } from "../../../deion/common";
 import { helpers } from "../../../deion/worker/util";
 import type { PlayerStats, PlayerStatType } from "../../../deion/common/types";
 
+const straightThrough = [
+    "gp",
+    "gs",
+    "per",
+    "ewa",
+    "yearsWithTeam",
+    "astp",
+    "blkp",
+    "drbp",
+    "orbp",
+    "stlp",
+    "trbp",
+    "usgp",
+    "drtg",
+    "ortg",
+    "dws",
+    "ows",
+];
+
+const percentage = (numerator, denominator) => {
+    if (denominator > 0) {
+        return (100 * numerator) / denominator;
+    }
+
+    return 0;
+};
+
 const processPlayerStats = (
     ps: PlayerStats,
     stats: string[],
@@ -12,137 +39,67 @@ const processPlayerStats = (
 ) => {
     const row = {};
 
-    for (const attr of stats) {
-        if (attr === "gp") {
-            row.gp = ps.gp;
-        } else if (attr === "gs") {
-            row.gs = ps.gs;
-        } else if (attr === "fgp") {
-            if (ps.fga > 0) {
-                row.fgp = (100 * ps.fg) / ps.fga;
-            } else {
-                row.fgp = 0;
-            }
-        } else if (attr === "fgpAtRim") {
-            if (ps.fgaAtRim > 0) {
-                row.fgpAtRim = (100 * ps.fgAtRim) / ps.fgaAtRim;
-            } else {
-                row.fgpAtRim = 0;
-            }
-        } else if (attr === "fgpLowPost") {
-            if (ps.fgaLowPost > 0) {
-                row.fgpLowPost = (100 * ps.fgLowPost) / ps.fgaLowPost;
-            } else {
-                row.fgpLowPost = 0;
-            }
-        } else if (attr === "fgpMidRange") {
-            if (ps.fgaMidRange > 0) {
-                row.fgpMidRange = (100 * ps.fgMidRange) / ps.fgaMidRange;
-            } else {
-                row.fgpMidRange = 0;
-            }
-        } else if (attr === "tpp") {
-            if (ps.tpa > 0) {
-                row.tpp = (100 * ps.tp) / ps.tpa;
-            } else {
-                row.tpp = 0;
-            }
-        } else if (attr === "ftp") {
-            if (ps.fta > 0) {
-                row.ftp = (100 * ps.ft) / ps.fta;
-            } else {
-                row.ftp = 0;
-            }
-        } else if (attr === "tsp") {
-            if (ps.fga > 0 || ps.fta > 0) {
-                row.tsp = (100 * ps.pts) / (2 * (ps.fga + 0.44 * ps.fta));
-            } else {
-                row.tsp = 0;
-            }
-        } else if (attr === "tpar") {
-            if (ps.fga > 0) {
-                row.tpar = (100 * ps.tpa) / ps.fga;
-            } else {
-                row.tpar = 0;
-            }
-        } else if (attr === "ftr") {
-            if (ps.fga > 0) {
-                row.ftr = (100 * ps.fta) / ps.fga;
-            } else {
-                row.ftr = 0;
-            }
-        } else if (attr === "tovp") {
-            if (ps.fga > 0 || ps.fta > 0) {
-                row.tovp = (100 * ps.tov) / (2 * (ps.fga + 0.44 * ps.fta));
-            } else {
-                row.tovp = 0;
-            }
-        } else if (attr === "season") {
+    for (const stat of stats) {
+        if (straightThrough.includes(stat)) {
+            row[stat] = ps[stat];
+        } else if (stat === "fgp") {
+            row[stat] = percentage(ps.fg, ps.fga);
+        } else if (stat === "fgpAtRim") {
+            row[stat] = percentage(ps.fgAtRim, ps.fgaAtRim);
+        } else if (stat === "fgpLowPost") {
+            row[stat] = percentage(ps.fgLowPost, ps.fgaLowPost);
+        } else if (stat === "fgpMidRange") {
+            row[stat] = percentage(ps.fgMidRange, ps.fgaMidRange);
+        } else if (stat === "tpp") {
+            row[stat] = percentage(ps.tp, ps.tpa);
+        } else if (stat === "ftp") {
+            row[stat] = percentage(ps.ft, ps.fta);
+        } else if (stat === "tsp") {
+            row[stat] = percentage(ps.pts, 2 * (ps.fga + 0.44 * ps.fta));
+        } else if (stat === "tpar") {
+            row[stat] = percentage(ps.tpa, ps.fga);
+        } else if (stat === "ftr") {
+            row[stat] = percentage(ps.fta, ps.fga);
+        } else if (stat === "tovp") {
+            row[stat] = percentage(ps.tov, 2 * (ps.fga + 0.44 * ps.fta));
+        } else if (stat === "season") {
             row.season = ps.season;
-        } else if (attr === "age") {
+        } else if (stat === "age") {
             row.age = ps.season - bornYear;
-        } else if (attr === "abbrev") {
+        } else if (stat === "abbrev") {
             if (ps.tid === undefined) {
                 row.abbrev = helpers.getAbbrev(PLAYER.FREE_AGENT);
             } else {
                 row.abbrev = helpers.getAbbrev(ps.tid);
             }
-        } else if (attr === "tid") {
+        } else if (stat === "tid") {
             if (ps.tid === undefined) {
                 row.tid = PLAYER.FREE_AGENT;
             } else {
                 row.tid = ps.tid;
             }
-        } else if (attr === "per") {
-            row.per = ps.per;
-        } else if (attr === "ewa") {
-            row.ewa = ps.ewa;
-        } else if (attr === "yearsWithTeam") {
-            row.yearsWithTeam = ps.yearsWithTeam;
-        } else if (attr === "astp") {
-            row.astp = ps.astp;
-        } else if (attr === "blkp") {
-            row.blkp = ps.blkp;
-        } else if (attr === "drbp") {
-            row.drbp = ps.drbp;
-        } else if (attr === "orbp") {
-            row.orbp = ps.orbp;
-        } else if (attr === "stlp") {
-            row.stlp = ps.stlp;
-        } else if (attr === "trbp") {
-            row.trbp = ps.trbp;
-        } else if (attr === "usgp") {
-            row.usgp = ps.usgp;
-        } else if (attr === "drtg") {
-            row.drtg = ps.drtg;
-        } else if (attr === "ortg") {
-            row.ortg = ps.ortg;
-        } else if (attr === "dws") {
-            row.dws = ps.dws;
-        } else if (attr === "ows") {
-            row.ows = ps.ows;
-        } else if (attr === "ws") {
+        } else if (stat === "ws") {
             row.ws = ps.dws + ps.ows;
-        } else if (attr === "ws48") {
+        } else if (stat === "ws48") {
             row.ws48 = ((ps.dws + ps.ows) * 48) / ps.min;
         } else if (statType === "totals") {
-            if (attr === "trb") {
+            if (stat === "trb") {
                 row.trb = ps.drb + ps.orb;
             } else {
-                row[attr] = ps[attr];
+                row[stat] = ps[stat];
             }
-        } else if (statType === "per36" && attr !== "min") {
+        } else if (statType === "per36" && stat !== "min") {
             // Don't scale min by 36 minutes
-            const val = attr === "trb" ? ps.drb + ps.orb : ps[attr];
-            row[attr] = ps.min > 0 ? (val * 36) / ps.min : 0;
+            const val = stat === "trb" ? ps.drb + ps.orb : ps[stat];
+            row[stat] = ps.min > 0 ? (val * 36) / ps.min : 0;
         } else {
-            const val = attr === "trb" ? ps.drb + ps.orb : ps[attr];
-            row[attr] = ps.gp > 0 ? val / ps.gp : 0;
+            const val = stat === "trb" ? ps.drb + ps.orb : ps[stat];
+            row[stat] = ps.gp > 0 ? val / ps.gp : 0;
         }
 
         // For keepWithNoStats
-        if (row[attr] === undefined || Number.isNaN(row[attr])) {
-            row[attr] = 0;
+        if (row[stat] === undefined || Number.isNaN(row[stat])) {
+            row[stat] = 0;
         }
     }
 
--- src/basketball/worker/util/processTeamStats.js
+++ src/basketball/worker/util/processTeamStats.js
@@ -27,6 +27,14 @@ const poss = ts => {
     return 0;
 };
 
+const percentage = (numerator, denominator) => {
+    if (denominator > 0) {
+        return (100 * numerator) / denominator;
+    }
+
+    return 0;
+};
+
 const processTeamStats = (
     ts: TeamStats,
     stats: TeamStatAttr[],
@@ -40,79 +48,29 @@ const processTeamStats = (
             if (stat === "gp") {
                 row.gp = ts.gp;
             } else if (stat === "fgp") {
-                if (ts.fga > 0) {
-                    row.fgp = (100 * ts.fg) / ts.fga;
-                } else {
-                    row.fgp = 0;
-                }
+                row[stat] = percentage(ts.fg, ts.fga);
             } else if (stat === "oppFgp") {
-                if (ts.oppFga > 0) {
-                    row.oppFgp = (100 * ts.oppFg) / ts.oppFga;
-                } else {
-                    row.oppFgp = 0;
-                }
+                row[stat] = percentage(ts.oppFg, ts.oppFga);
             } else if (stat === "fgpAtRim") {
-                if (ts.fgaAtRim > 0) {
-                    row.fgpAtRim = (100 * ts.fgAtRim) / ts.fgaAtRim;
-                } else {
-                    row.fgpAtRim = 0;
-                }
+                row[stat] = percentage(ts.fgAtRim, ts.fgaAtRim);
             } else if (stat === "oppFgpAtRim") {
-                if (ts.oppFgaAtRim > 0) {
-                    row.oppFgpAtRim = (100 * ts.oppFgAtRim) / ts.oppFgaAtRim;
-                } else {
-                    row.oppFgpAtRim = 0;
-                }
+                row[stat] = percentage(ts.oppFgAtRim, ts.oppFgaAtRim);
             } else if (stat === "fgpLowPost") {
-                if (ts.fgaLowPost > 0) {
-                    row.fgpLowPost = (100 * ts.fgLowPost) / ts.fgaLowPost;
-                } else {
-                    row.fgpLowPost = 0;
-                }
+                row[stat] = percentage(ts.fgLowPost, ts.fgaLowPost);
             } else if (stat === "oppFgpLowPost") {
-                if (ts.oppFgaLowPost > 0) {
-                    row.oppFgpLowPost =
-                        (100 * ts.oppFgLowPost) / ts.oppFgaLowPost;
-                } else {
-                    row.oppFgpLowPost = 0;
-                }
+                row[stat] = percentage(ts.oppFgLowPost, ts.oppFgaLowPost);
             } else if (stat === "fgpMidRange") {
-                if (ts.fgaMidRange > 0) {
-                    row.fgpMidRange = (100 * ts.fgMidRange) / ts.fgaMidRange;
-                } else {
-                    row.fgpMidRange = 0;
-                }
+                row[stat] = percentage(ts.fgMidRange, ts.fgaMidRange);
             } else if (stat === "oppFgpMidRange") {
-                if (ts.oppFgaMidRange > 0) {
-                    row.oppFgpMidRange =
-                        (100 * ts.oppFgMidRange) / ts.oppFgaMidRange;
-                } else {
-                    row.oppFgpMidRange = 0;
-                }
+                row[stat] = percentage(ts.oppFgMidRange, ts.oppFgaMidRange);
             } else if (stat === "tpp") {
-                if (ts.tpa > 0) {
-                    row.tpp = (100 * ts.tp) / ts.tpa;
-                } else {
-                    row.tpp = 0;
-                }
+                row[stat] = percentage(ts.tp, ts.tpa);
             } else if (stat === "oppTpp") {
-                if (ts.oppTpa > 0) {
-                    row.oppTpp = (100 * ts.oppTp) / ts.oppTpa;
-                } else {
-                    row.oppTpp = 0;
-                }
+                row[stat] = percentage(ts.oppTp, ts.oppTpa);
             } else if (stat === "ftp") {
-                if (ts.fta > 0) {
-                    row.ftp = (100 * ts.ft) / ts.fta;
-                } else {
-                    row.ftp = 0;
-                }
+                row[stat] = percentage(ts.ft, ts.fta);
             } else if (stat === "oppFtp") {
-                if (ts.oppFta > 0) {
-                    row.oppFtp = (100 * ts.oppFt) / ts.oppFta;
-                } else {
-                    row.oppFtp = 0;
-                }
+                row[stat] = percentage(ts.oppFt, ts.oppFta);
             } else if (stat === "mov") {
                 if (statType === "totals") {
                     row.mov = ts.pts - ts.oppPts;
@@ -148,25 +106,13 @@ const processTeamStats = (
                 }
             } else if (stat === "ortg") {
                 const possessions = poss(ts);
-                if (possessions > 0) {
-                    row.ortg = (100 * ts.pts) / possessions;
-                } else {
-                    row.ortg = 0;
-                }
+                row[stat] = percentage(ts.pts, possessions);
             } else if (stat === "drtg") {
                 const possessions = poss(ts);
-                if (possessions > 0) {
-                    row.drtg = (100 * ts.oppPts) / possessions;
-                } else {
-                    row.drtg = 0;
-                }
+                row[stat] = percentage(ts.oppPts, possessions);
             } else if (stat === "nrtg") {
                 const possessions = poss(ts);
-                if (possessions > 0) {
-                    row.nrtg = (100 * (ts.pts - ts.oppPts)) / possessions;
-                } else {
-                    row.nrtg = 0;
-                }
+                row[stat] = percentage(ts.pts - ts.oppPts, possessions);
             } else if (stat === "pace") {
                 if (ts.min > 0) {
                     row.pace = (g.quarterLength * 4 * poss(ts)) / (ts.min / 5);
@@ -176,17 +122,9 @@ const processTeamStats = (
             } else if (stat === "poss") {
                 row.poss = poss(ts);
             } else if (stat === "tpar") {
-                if (ts.fga > 0) {
-                    row.tpar = (100 * ts.tpa) / ts.fga;
-                } else {
-                    row.tpar = 0;
-                }
+                row[stat] = percentage(ts.tpa, ts.fga);
             } else if (stat === "ftr") {
-                if (ts.fga > 0) {
-                    row.ftr = (100 * ts.fta) / ts.fga;
-                } else {
-                    row.ftr = 0;
-                }
+                row[stat] = percentage(ts.fta, ts.fga);
             } else if (stat === "season" || stat === "playoffs") {
                 row[stat] = ts[stat];
             } else if (statType === "totals") {

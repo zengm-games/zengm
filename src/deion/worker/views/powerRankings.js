21fb6741b67973a949e83894fd6a84f8f126fef7
Jeremy Scheff <jdscheff@gmail.com>
2019-02-24 17:12:12 -0500

    Football power rankings

--- src/deion/worker/views/powerRankings.js
+++ src/deion/worker/views/powerRankings.js
@@ -11,7 +11,7 @@ async function updatePowerRankings(
     if (updateEvents.includes("firstRun") || updateEvents.includes("gameSim")) {
         const [teams, players] = await Promise.all([
             idb.getCopies.teamsPlus({
-                attrs: ["tid", "abbrev", "region", "name"],
+                attrs: ["tid", "abbrev", "region", "name", "depth"],
                 seasonAttrs: ["won", "lost", "lastTen"],
                 stats: ["gp", "mov"],
                 season: g.season,
@@ -28,44 +28,86 @@ async function updatePowerRankings(
         }
 
         // TALENT
-        // Get player values and sort by tid
-        for (let i = 0; i < players.length; i++) {
-            playerValuesByTid[players[i].tid].push(players[i].valueNoPot);
-        }
-        // Sort and weight the values - doesn't matter how good your 12th man is
-        const weights = [
-            2,
-            1.5,
-            1.25,
-            1.1,
-            1,
-            0.9,
-            0.8,
-            0.7,
-            0.6,
-            0.4,
-            0.2,
-            0.1,
-        ];
-        for (let i = 0; i < playerValuesByTid.length; i++) {
-            playerValuesByTid[i].sort((a, b) => b - a);
+        if (process.env.SPORT === "basketball") {
+            // Get player values and sort by tid
+            for (const p of players) {
+                playerValuesByTid[p.tid].push(p.valueNoPot);
+            }
 
-            for (let j = 0; j < playerValuesByTid[i].length; j++) {
-                if (j < weights.length) {
-                    teams[i].talent += weights[j] * playerValuesByTid[i][j];
+            // Sort and weight the values - doesn't matter how good your 12th man is
+            const weights = [
+                2,
+                1.5,
+                1.25,
+                1.1,
+                1,
+                0.9,
+                0.8,
+                0.7,
+                0.6,
+                0.4,
+                0.2,
+                0.1,
+            ];
+            for (let i = 0; i < playerValuesByTid.length; i++) {
+                playerValuesByTid[i].sort((a, b) => b - a);
+
+                for (let j = 0; j < playerValuesByTid[i].length; j++) {
+                    if (j < weights.length) {
+                        teams[i].talent += weights[j] * playerValuesByTid[i][j];
+                    }
                 }
+            }
+        } else {
+            for (const t of teams) {
+                // Equal weights to: QB, rest of offense, defense
+
+                const qb = players.find(p => p.pid === t.depth.QB[0]);
+                const qbVal = qb ? qb.valueNoPot : 0;
+
+                const offVal =
+                    [
+                        ...t.depth.RB.slice(0, 1),
+                        ...t.depth.WR.slice(0, 3),
+                        ...t.depth.TE.slice(0, 1),
+                        ...t.depth.OL.slice(0, 5),
+                    ]
+                        .map(pid => players.find(p => p.pid === pid))
+                        .reduce((sum, p) => {
+                            if (p === null) {
+                                return sum;
+                            }
+
+                            return sum + p.valueNoPot;
+                        }, 0) / 10;
+
+                const defVal =
+                    [
+                        ...t.depth.DL.slice(0, 4),
+                        ...t.depth.LB.slice(0, 3),
+                        ...t.depth.S.slice(0, 3),
+                        ...t.depth.CB.slice(0, 2),
+                    ]
+                        .map(pid => players.find(p => p.pid === pid))
+                        .reduce((sum, p) => {
+                            if (p === null) {
+                                return sum;
+                            }
+
+                            return sum + p.valueNoPot;
+                        }, 0) / 12;
+
+                t.talent = (qbVal + offVal + defVal) / 3;
             }
         }
 
         // PERFORMANCE
-        for (let i = 0; i < g.numTeams; i++) {
-            playerValuesByTid[i] = [];
+        for (const t of teams) {
             // Modulate point differential by recent record: +5 for 10-0 in last 10 and -5 for 0-10
-            teams[i].performance =
-                teams[i].stats.mov -
+            t.performance =
+                t.stats.mov -
                 5 +
-                (5 * parseInt(teams[i].seasonAttrs.lastTen.split("-")[0], 10)) /
-                    10;
+                (5 * parseInt(t.seasonAttrs.lastTen.split("-")[0], 10)) / 10;
         }
 
         // RANKS

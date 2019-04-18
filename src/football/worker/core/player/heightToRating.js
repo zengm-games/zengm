82aa7ea37f2b91de9e502daaf0cb8eef0eeb5168
Jeremy Scheff <jdscheff@gmail.com>
2019-04-18 15:03:54 -0400

    Football heightToRating, although I think it is not used currently

--- src/football/worker/core/player/heightToRating.js
+++ src/football/worker/core/player/heightToRating.js
@@ -3,9 +3,8 @@
 import limitRating from "../../../../deion/worker/core/player/limitRating";
 
 const heightToRating = (heightInInches: number) => {
-    // Min/max for hgt rating.  Displayed height ranges from 4'6" to 9'0", though we might never see the true extremes
-    const minHgt = 66; // 5'6"
-    const maxHgt = 93; // 7'9"
+    const minHgt = 64; // 5'4"
+    const maxHgt = 82; // 6'10"
     return limitRating((100 * (heightInInches - minHgt)) / (maxHgt - minHgt));
 };
 

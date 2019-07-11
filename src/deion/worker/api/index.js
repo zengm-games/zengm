673481b9decaa01d68d946341b450d09825f98b4
Jeremy Scheff <jdscheff@gmail.com>
2019-07-11 10:02:45 -0400

    Flow

--- src/deion/worker/api/index.js
+++ src/deion/worker/api/index.js
@@ -1035,6 +1035,7 @@ const removeLastTeam = async (): Promise<void> => {
         }
     }
 
+    // $FlowFixMe
     const teamSeasons = await idb.cache.teamSeasons.indexGetAll(
         "teamSeasonsByTidSeason",
         [[tid], [tid, "Z"]],

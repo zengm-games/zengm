6949801940ad9a6a3a5f92a52ade72ca9d213d2b
nicidob <nicholasidobson@gmail.com>
2019-07-27 21:55:16 -0400

    add in some EWA and WS/48 into regression until oIQ and dIQ are roughly equal

--- src/basketball/worker/core/player/ovr.js
+++ src/basketball/worker/core/player/ovr.js
@@ -13,23 +13,23 @@ import type { PlayerRatings } from "../../../common/types";
 const ovr = (ratings: PlayerRatings): number => {
     // This formula is loosely based on linear regression of ratings to zscore(ws48)+zscore(per):
     const r =
-        ((0.19 * (ratings.hgt - 47)) / 14 +
-            (0.07 * (ratings.stre - 50)) / 11 +
-            (0.18 * (ratings.spd - 53)) / 16 +
-            (0.15 * (ratings.jmp - 51)) / 17 +
-            (0.02 * (ratings.endu - 42)) / 12 +
-            (0.04 * (ratings.ins - 43)) / 13 +
-            (0.02 * (ratings.dnk - 50)) / 13 +
-            (0.01 * (ratings.ft - 48)) / 13 +
-            (0.01 * (ratings.fg - 48)) / 13 +
-            (0.1 * (ratings.tp - 48)) / 13 +
-            (0.06 * (ratings.oiq - 47)) / 10 +
-            (0.11 * (ratings.diq - 47)) / 11 +
-            (0.06 * (ratings.drb - 56)) / 11 +
-            (0.02 * (ratings.pss - 52)) / 12 +
+        ((0.18 * (ratings.hgt - 47)) / 14 +
+            (0.05 * (ratings.stre - 50)) / 11 +
+            (0.15 * (ratings.spd - 53)) / 16 +
+            (0.13 * (ratings.jmp - 51)) / 17 +
+            (0.02 * (ratings.endu - 41)) / 13 +
+            (0.02 * (ratings.ins - 42)) / 13 +
+            (0.02 * (ratings.dnk - 49)) / 13 +
+            (0.01 * (ratings.ft - 47)) / 13 +
+            (0.01 * (ratings.fg - 47)) / 13 +
+            (0.09 * (ratings.tp - 47)) / 13 +
+            (0.09 * (ratings.oiq - 46)) / 11 +
+            (0.09 * (ratings.diq - 46)) / 11 +
+            (0.06 * (ratings.drb - 55)) / 11 +
+            (0.03 * (ratings.pss - 51)) / 12 +
             (0.05 * (ratings.reb - 51)) / 12) *
-            12.36 +
-        48.1;
+            13.55 +
+        47.47;
 
     // Fudge factor to keep ovr ratings the same as they used to be (back before 2018 ratings rescaling)
     // +8 at 68

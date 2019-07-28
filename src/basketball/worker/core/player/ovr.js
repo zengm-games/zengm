8417cd5c8bc3c1a11fa4aad5a19f0faf0604b798
nicidob <nicholasidobson@gmail.com>
2019-07-28 02:19:49 -0400

    only using +/- on larger dataset

--- src/basketball/worker/core/player/ovr.js
+++ src/basketball/worker/core/player/ovr.js
@@ -13,23 +13,23 @@ import type { PlayerRatings } from "../../../common/types";
 const ovr = (ratings: PlayerRatings): number => {
     // This formula is loosely based on linear regression of ratings to zscore(ws48)+zscore(per):
     const r =
-        ((0.18 * (ratings.hgt - 47)) / 14 +
-            (0.05 * (ratings.stre - 50)) / 11 +
-            (0.15 * (ratings.spd - 53)) / 16 +
-            (0.13 * (ratings.jmp - 51)) / 17 +
-            (0.02 * (ratings.endu - 41)) / 13 +
-            (0.02 * (ratings.ins - 42)) / 13 +
-            (0.02 * (ratings.dnk - 49)) / 13 +
-            (0.01 * (ratings.ft - 47)) / 13 +
-            (0.01 * (ratings.fg - 47)) / 13 +
-            (0.09 * (ratings.tp - 47)) / 13 +
-            (0.09 * (ratings.oiq - 46)) / 11 +
-            (0.09 * (ratings.diq - 46)) / 11 +
-            (0.06 * (ratings.drb - 55)) / 11 +
-            (0.03 * (ratings.pss - 51)) / 12 +
-            (0.05 * (ratings.reb - 51)) / 12) *
-            13.55 +
-        47.47;
+        ((0.23 * (ratings.hgt - 47)) / 14 +
+            (0.1 * (ratings.stre - 49)) / 11 +
+            (0.22 * (ratings.spd - 51)) / 17 +
+            (0.12 * (ratings.jmp - 48)) / 18 +
+            (0.05 * (ratings.endu - 41)) / 13 +
+            (0.03 * (ratings.ins - 42)) / 14 +
+            (0.03 * (ratings.dnk - 49)) / 13 +
+            (0.01 * (ratings.ft - 47)) / 12 +
+            (0.01 * (ratings.fg - 47)) / 12 +
+            (0.09 * (ratings.tp - 47)) / 12 +
+            (0.09 * (ratings.oiq - 47)) / 10 +
+            (0.13 * (ratings.diq - 47)) / 10 +
+            (0.07 * (ratings.drb - 55)) / 11 +
+            (0.07 * (ratings.pss - 51)) / 12 +
+            (0.02 * (ratings.reb - 51)) / 12) *
+            10.18 +
+        48.36;
 
     // Fudge factor to keep ovr ratings the same as they used to be (back before 2018 ratings rescaling)
     // +8 at 68

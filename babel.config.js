5a3d5c2567c02573881ee233a3dd8e55ed46fae1
Jeremy Scheff <jdscheff@gmail.com>
2019-09-08 01:56:11 -0400

    Ugh need to transpile d3 for old browsers

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+
+// Need this rather than .babelrc to run on d3 inside node_modules (see also babelify config)
+
+module.exports = api => {
+    api.cache.using(() => process.env.NODE_ENV);
+
+    return {
+        presets: ["@babel/preset-react", "@babel/preset-flow"],
+        plugins: [
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-transform-for-of",
+            "@babel/plugin-transform-parameters",
+            "@babel/plugin-transform-destructuring",
+            "@babel/plugin-transform-modules-commonjs",
+            "@babel/plugin-transform-exponentiation-operator",
+            "@babel/plugin-transform-async-to-generator",
+            "@babel/plugin-proposal-object-rest-spread",
+        ],
+    };
+};

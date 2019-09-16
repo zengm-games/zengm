73cf8b35241a87a0dcce705927fef38ecb790ae2
Jeremy Scheff <jdscheff@gmail.com>
2019-09-15 21:13:30 -0400

    Tests are still an issue..

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,15 @@
 /* eslint-env node */
 
+const React = require("react");
+const ReactDOM = require("react-dom");
+const alias = require("rollup-plugin-alias");
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+const globals = require("rollup-plugin-node-globals");
+const json = require("rollup-plugin-json");
+const builtins = require("rollup-plugin-node-builtins");
+const resolve = require("rollup-plugin-node-resolve");
+const replace = require("rollup-plugin-replace");
 const build = require("./tools/buildFuncs");
 
 const sport = build.getSport();
@@ -7,8 +17,8 @@ const sport = build.getSport();
 const files = [
     "src/deion/test/index.js",
     `src/${sport}/worker/index.js`, // For overrides
-    `src/${sport}/**/*.test.js`,
-    "src/deion/test/**/*.js",
+    //    `src/${sport}/**/*.test.js`,
+    //    "src/deion/test/**/*.js",
 ];
 
 if (sport === "basketball") {
@@ -18,12 +28,17 @@ if (sport === "basketball") {
 
 module.exports = function(config) {
     config.set({
-        frameworks: ["mocha", "browserify", "source-map-support"],
+        frameworks: ["mocha", "source-map-support"],
 
-        files,
+        files: files.map(pattern => {
+            return {
+                pattern,
+                watched: false,
+            };
+        }),
 
         preprocessors: {
-            "src/**/*.js": ["browserify"],
+            "src/**/*.js": ["rollup"],
         },
 
         // http://stackoverflow.com/a/42379383/786644
@@ -36,20 +51,53 @@ module.exports = function(config) {
 
         singleRun: true,
 
-        browserify: {
-            debug: true,
-            transform: [
-                "babelify",
-                ["envify", { SPORT: sport }],
-                [
-                    "aliasify",
-                    {
-                        aliases: {
-                            "league-schema.json": `./public/football/files/league-schema.json`,
+        rollupPreprocessor: {
+            plugins: [
+                alias({
+                    entries: [
+                        {
+                            find: "league-schema",
+                            replacement: `./../../../${sport}/ui/util/leagueSchema.js`,
                         },
+                        // This is so Karma doesn't crash when using the big names file.
+                        {
+                            find: "player-names",
+                            replacement:
+                                process.env.NODE_ENV === "test"
+                                    ? "./util/namesTest.js"
+                                    : "./util/names.js",
+                        },
+                    ],
+                }),
+                replace({
+                    "process.env.NODE_ENV": JSON.stringify("production"),
+                    "process.env.SPORT": JSON.stringify(sport),
+                }),
+                babel({
+                    exclude: "node_modules/!(d3)**",
+                    runtimeHelpers: true,
+                }),
+                json({
+                    compact: true,
+                    namedExports: false,
+                }),
+                commonjs({
+                    namedExports: {
+                        react: Object.keys(React),
+                        "react-dom": Object.keys(ReactDOM),
                     },
-                ],
+                }),
+                resolve({
+                    preferBuiltins: true,
+                }),
+                globals(),
+                builtins(),
             ],
+            output: {
+                format: "iife",
+                name: "bbgm",
+                sourcemap: "inline",
+            },
         },
 
         browserNoActivityTimeout: 15 * 60 * 1000, // 15 minutes

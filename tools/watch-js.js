3b7484ace873e326f4501b1b55d2368cf18359fe
Jeremy Scheff <jdscheff@gmail.com>
2019-09-16 12:59:57 -0400

    Watch mostly works

--- tools/watch-js.js
+++ tools/watch-js.js
@@ -1,82 +1,38 @@
-// @flow
-
-const aliasify = require("aliasify");
-const babelify = require("babelify");
-const browserify = require("browserify");
-const envify = require("envify/custom");
-const fs = require("fs");
-const watchify = require("watchify");
+const rollup = require("rollup");
+const build = require("./buildFuncs");
+const rollupConfig = require("./rollupConfig");
 
 console.log("Watching JavaScript files...");
 
-let sport = process.env.SPORT;
-if (typeof sport !== "string") {
-    sport = "basketball";
-}
+const sport = build.getSport();
 
+// Could pass an array to rollup.watch, but that seems to make it bundle in series rather than parallel
 for (const name of ["ui", "worker"]) {
-    // Mostly copied from cmd.js in watchify
+    const file = `build/gen/${name}.js`;
 
-    const b = browserify(`src/${sport}/${name}/index.js`, {
-        debug: true,
-        cache: {},
-        packageCache: {},
-    })
-        .transform(babelify)
-        .transform(envify({ NODE_ENV: "development", SPORT: sport }), {
-            global: true,
-        })
-        .transform(aliasify, {
-            aliases: {
-                "league-schema.json": `./public/${sport}/files/league-schema.json`,
-            },
-        })
-        .plugin(watchify);
-
-    let bytes = 0;
-    let time = 0;
-    b.on("bytes", bytesLocal => {
-        bytes = bytesLocal;
-    });
-    b.on("time", timeLocal => {
-        time = timeLocal;
+    const watcher = rollup.watch({
+        ...rollupConfig,
+        input: `src/${sport}/${name}/index.js`,
+        output: {
+            name,
+            file,
+            format: "iife",
+        },
     });
 
-    const outFilename = `build/gen/${name}.js`;
-
-    const bundle = () => {
-        let didError = false;
-
-        const writeStream = fs.createWriteStream(outFilename);
-        writeStream.on("error", err => {
-            console.error(err);
-        });
-
-        b.bundle()
-            .on("error", err => {
-                console.error(String(err));
-                if (!didError) {
-                    didError = true;
-                    writeStream.end(
-                        `console.error(${JSON.stringify(String(err))});`,
-                    );
-                }
-            })
-            .pipe(writeStream);
-
-        writeStream.on("finish", async () => {
-            if (!didError) {
-                console.log(
-                    `${(bytes / 1024 / 1024).toFixed(
-                        2,
-                    )} MB written to ${outFilename} (${(time / 1000).toFixed(
-                        2,
-                    )} seconds) at ${new Date().toLocaleTimeString()}`,
-                );
-            }
-        });
-    };
-
-    b.on("update", bundle);
-    bundle();
+    let startTime;
+    watcher.on("event", event => {
+        if (event.code === "START") {
+            startTime = new Date().getTime();
+        } else if (event.code === "END") {
+            const delta = new Date().getTime() - startTime;
+            console.log(
+                `Bundle written to ${file} (${(delta / 1000).toFixed(
+                    2,
+                )} seconds) at ${new Date().toLocaleTimeString()}`,
+            );
+        } else if (event.code === "ERROR" || event.code === "FATAL") {
+            console.log(name, event);
+        }
+    });
 }

b1010edea4668f846e349a3c86504219d88acfc6
Jeremy Scheff <jdscheff@gmail.com>
2019-09-16 01:25:42 -0400

    Planning

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
 /* eslint-env node */
-const build = require("./tools/buildFuncs");
-
-const sport = build.getSport();
-
 module.exports = {
     setupFilesAfterEnv: [
         "./src/deion/test/jest.js",
-        `./src/${sport}/worker/index.js`,
+        "./src/basketball/worker/index.js",
     ],
 };

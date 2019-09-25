ec3a6adaaeb4806cfaf5e28012f974470fd83c50
Jeremy Scheff <jdscheff@gmail.com>
2019-09-24 23:25:51 -0400

    Fix Play -> Until playoffs

--- src/deion/worker/core/season/getDaysLeftSchedule.js
+++ src/deion/worker/core/season/getDaysLeftSchedule.js
@@ -9,22 +9,26 @@ import getSchedule from "./getSchedule";
  * @return {Promise} The number of days left in the schedule.
  */
 const getDaysLeftSchedule = async (untilAllStarGame: boolean) => {
-    let schedule = await getSchedule();
+    const schedule = await getSchedule();
 
     let numDays = 0;
+    let iPrev = 0;
 
-    while (schedule.length > 0) {
+    while (iPrev < schedule.length) {
         // Only take the games up until right before a team plays for the second time that day
         const tids = [];
         let i;
-        for (i = 0; i < schedule.length; i++) {
+        for (i = iPrev; i < schedule.length; i++) {
             const { awayTid, homeTid } = schedule[i];
 
             // All-Star Game
             if (awayTid === -2 && homeTid === -1) {
+                // Add one to represent that the "current day" of games before the ASG ended. Then the normal +1 at the end of the while loop will account for the ASG itself
+                numDays += 1;
                 if (untilAllStarGame) {
-                    return numDays + 1;
+                    return numDays;
                 }
+                i += 1;
                 break;
             }
 
@@ -35,8 +39,8 @@ const getDaysLeftSchedule = async (untilAllStarGame: boolean) => {
                 break;
             }
         }
+        iPrev = i;
         numDays += 1;
-        schedule = schedule.slice(i);
     }
 
     return numDays;

1a97ed7907cc68bda0860397216486e1a64bc5b4
Jeremy Scheff <jdscheff@gmail.com>
2019-10-05 22:51:00 -0400

    Convert a couple components to hooks

--- src/deion/ui/components/Header.js
+++ src/deion/ui/components/Header.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from "react";
+import React, { useEffect } from "react";
 
 // Ensure there is enough room to display 160px wide ad with 15px margins next to 1200px wide container
 const widthCutoff = 1200 + 190;
@@ -31,64 +31,59 @@ const resizeListener = () => {
     });
 };
 
-class Header extends React.Component<{}> {
-    // eslint-disable-next-line class-methods-use-this
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    componentDidMount() {
+const Header = React.memo(() => {
+    useEffect(() => {
         updateSkyscraperDisplay();
 
         window.addEventListener("resize", resizeListener);
         window.addEventListener("optimizedResize", updateSkyscraperDisplay);
-    }
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", resizeListener);
-        window.removeEventListener("optimizedResize", updateSkyscraperDisplay);
-    }
+        return () => {
+            window.removeEventListener("resize", resizeListener);
+            window.removeEventListener(
+                "optimizedResize",
+                updateSkyscraperDisplay,
+            );
+        };
+    }, []);
 
-    // eslint-disable-next-line class-methods-use-this
-    render() {
-        return (
-            // banner-ad class is so ad blockers remove it cleanly. I'm so nice!
-            <div>
+    return (
+        // banner-ad class is so ad blockers remove it cleanly. I'm so nice!
+        <>
+            <div
+                className="banner-ad"
+                id="bbgm-ads-top"
+                style={{
+                    display: "none",
+                    textAlign: "center",
+                    minHeight: "95px",
+                }}
+            />
+            <div
+                className="banner-ad"
+                id="bbgm-ads-mobile"
+                style={{
+                    display: "none",
+                    textAlign: "center",
+                    minHeight: "55px",
+                }}
+            />
+            <div
+                className="banner-ad skyscraper-wrapper"
+                id="skyscraper-wrapper"
+                style={{
+                    display: "none",
+                }}
+            >
                 <div
-                    className="banner-ad"
-                    id="bbgm-ads-top"
+                    id="bbgm-ads-skyscraper"
                     style={{
                         display: "none",
-                        textAlign: "center",
-                        minHeight: "95px",
                     }}
                 />
-                <div
-                    className="banner-ad"
-                    id="bbgm-ads-mobile"
-                    style={{
-                        display: "none",
-                        textAlign: "center",
-                        minHeight: "55px",
-                    }}
-                />
-                <div
-                    className="banner-ad skyscraper-wrapper"
-                    id="skyscraper-wrapper"
-                    style={{
-                        display: "none",
-                    }}
-                >
-                    <div
-                        id="bbgm-ads-skyscraper"
-                        style={{
-                            display: "none",
-                        }}
-                    />
-                </div>
             </div>
-        );
-    }
-}
+        </>
+    );
+});
 
 export default Header;

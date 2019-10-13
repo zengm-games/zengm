a25116885661b2d858f098ac7c4886819e55dc5f
Jeremy Scheff <jdscheff@gmail.com>
2019-10-13 13:47:56 -0400

    Hooks

--- src/deion/ui/views/DeleteLeague.js
+++ src/deion/ui/views/DeleteLeague.js
@@ -1,71 +1,64 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { helpers, realtimeUpdate, setTitle, toWorker } from "../util";
 
-class DeleteLeague extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { deleting: false };
-    }
+const DeleteLeague = ({ lid, name, numPlayers, numSeasons }) => {
+    const [deleting, setDeleting] = useState(false);
 
-    async handleDeleteLeague(lid) {
-        this.setState({ deleting: true });
-        await toWorker("removeLeague", lid);
-        realtimeUpdate([], "/");
-    }
+    const handleDeleteLeague = async lid2 => {
+        setDeleting(true);
+        await toWorker("removeLeague", lid2);
+        await realtimeUpdate([], "/");
+    };
 
-    render() {
-        const { lid, name, numPlayers, numSeasons } = this.props;
+    setTitle(`Delete League ${lid}?`);
 
-        setTitle(`Delete League ${lid}?`);
-
-        let message;
-        if (name !== undefined) {
-            message = (
-                <p>
-                    Are you <i>absolutely</i> sure you want to delete {name}{" "}
-                    (League {lid}
-                    )? You will <i>permanently</i> lose any record of all{" "}
-                    {helpers.numberWithCommas(numSeasons)} seasons and{" "}
-                    {helpers.numberWithCommas(numPlayers)} players from this
-                    league (well... unless you have a backup somewhere).
-                </p>
-            );
-        } else {
-            message = (
-                <p>
-                    Are you <i>absolutely</i> sure you want to delete League{" "}
-                    {lid}? You will <i>permanently</i> lose any record of all
-                    seasons, players, and games from this league (well... unless
-                    you have a backup somewhere).
-                </p>
-            );
-        }
-
-        return (
-            <>
-                <h1>Delete League {lid}?</h1>
-
-                {message}
-
-                <button
-                    className="btn btn-danger float-left mr-3"
-                    disabled={this.state.deleting}
-                    onClick={() => this.handleDeleteLeague(lid)}
-                >
-                    Yes, I am sure. Delete it!
-                </button>
-                <a
-                    disabled={this.state.deleting}
-                    className="btn btn-light-bordered"
-                    href="/"
-                >
-                    Cancel
-                </a>
-            </>
+    let message;
+    if (name !== undefined) {
+        message = (
+            <p>
+                Are you <i>absolutely</i> sure you want to delete {name} (League{" "}
+                {lid}
+                )? You will <i>permanently</i> lose any record of all{" "}
+                {helpers.numberWithCommas(numSeasons)} seasons and{" "}
+                {helpers.numberWithCommas(numPlayers)} players from this league
+                (well... unless you have a backup somewhere).
+            </p>
+        );
+    } else {
+        message = (
+            <p>
+                Are you <i>absolutely</i> sure you want to delete League {lid}?
+                You will <i>permanently</i> lose any record of all seasons,
+                players, and games from this league (well... unless you have a
+                backup somewhere).
+            </p>
         );
     }
-}
+
+    return (
+        <>
+            <h1>Delete League {lid}?</h1>
+
+            {message}
+
+            <button
+                className="btn btn-danger float-left mr-3"
+                disabled={deleting}
+                onClick={() => handleDeleteLeague(lid)}
+            >
+                Yes, I am sure. Delete it!
+            </button>
+            <button
+                className="btn btn-light-bordered"
+                disabled={deleting}
+                onClick={() => realtimeUpdate([], "/")}
+            >
+                Cancel
+            </button>
+        </>
+    );
+};
 
 DeleteLeague.propTypes = {
     lid: PropTypes.number.isRequired,

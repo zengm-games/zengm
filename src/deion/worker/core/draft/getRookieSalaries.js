c446960f4ea80faba2023ebfb8f79d3fc9c4441d
3cbb <55162221+3cbb@users.noreply.github.com>
2019-11-14 23:44:26 -0500

    Betterdraftsalaries (#261)
    * Update getRookieSalaries.js

--- src/deion/worker/core/draft/getRookieSalaries.js
+++ src/deion/worker/core/draft/getRookieSalaries.js
@@ -11,8 +11,8 @@ import { g } from "../../util";
  * @return {Array.<number>} Array of salaries, in thousands of dollars/year.
  */
 const getRookieSalaries = (): number[] => {
-	// Default for 60 picks
-	const rookieSalaries = [
+	// Default for first round
+	const firstRoundRookieSalaries = [
 		5000,
 		4500,
 		4000,
@@ -42,7 +42,10 @@ const getRookieSalaries = (): number[] => {
 		1000,
 		1000,
 		1000,
-		1000,
+		1000]
+	  
+	  //default for all subsequent rounds
+	const otherRoundRookieSalaries = [
 		500,
 		500,
 		500,
@@ -75,14 +78,27 @@ const getRookieSalaries = (): number[] => {
 		500,
 	];
 
-	while (g.numTeams * g.numDraftRounds > rookieSalaries.length) {
+	while (g.numTeams > firstRoundRookieSalaries.length){
+		//add first round contracts on to end of first round
+		firstRoundRookieSalaries.push(1000);
+	}
+	
+	while (g.numTeams < firstRoundRookieSalaries.length){
+		//remove smallest first round salaries
+		firstRoundRookieSalaries.pop();
+	}
+	
+	while (g.numTeams * (g.numDraftRounds-1) > otherRoundRookieSalaries.length) {
 		// Add min contracts on to end
-		rookieSalaries.push(500);
+		otherRoundRookieSalaries.push(500);
 	}
-	while (g.numTeams * g.numDraftRounds < rookieSalaries.length) {
+	while (g.numTeams * (g.numDraftRounds-1) < otherRoundRookieSalaries.length) {
 		// Remove smallest salaries
-		rookieSalaries.pop();
+		otherRoundRookieSalaries.pop();
 	}
+	
+	//combine first round and other rounds
+	const rookieSalaries = firstRoundRookieSalaries.concat(otherRoundRookieSalaries);
 
 	if (g.minContract !== 500 || g.maxContract !== 20000) {
 		for (let i = 0; i < rookieSalaries.length; i++) {

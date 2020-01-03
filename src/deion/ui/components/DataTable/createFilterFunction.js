6af403e399aefd0021a444eb1666702c997237a9
Jeremy Scheff <jdscheff@gmail.com>
2020-01-03 11:38:24 -0500

    Simpler

--- src/deion/ui/components/DataTable/createFilterFunction.js
+++ src/deion/ui/components/DataTable/createFilterFunction.js
@@ -1,77 +1,64 @@
 // @flow
 
-import booleanParser from "boolean-parser";
 import getSearchVal from "./getSearchVal";
 import getSortVal from "./getSortVal";
+import type { SortType } from "../../../common/types";
 
-// false - doesn't match. true - does match
-const createFilterFunction = (originalFilterText: string, sortType: string) => {
-	const matches = booleanParser.parseBooleanQuery(originalFilterText);
+const createFilterFunction = (
+	originalFilterText: string,
+	sortType?: SortType,
+) => {
+	const filterTexts = originalFilterText
+		.split("|")
+		.map(text => text.trim())
+		.filter(text => text !== "" && text !== "|");
 
-	return value => {
-		if (matches.length === 0) {
+	// false - doesn't match. true - does match
+	return (value: any) => {
+		if (filterTexts.length === 0) {
 			return true;
 		}
 
-		let foundMatch = false;
-
-		for (const match of matches) {
-			let noMatch = false;
-			for (const filterText of match) {
-				if (sortType === "number" || sortType === "currency") {
-					let number = filterText.replace(/[^0-9.<>]/g, "");
-					let direction;
-					if (number[0] === ">" || number[0] === "<" || number[0] === "=") {
-						direction = number[0];
-						number = number.slice(1); // Remove first char
-					}
-					number = parseFloat(number);
-
-					if (Number.isNaN(number)) {
-						continue;
-					}
-
-					const numericVal = parseFloat(getSortVal(value, sortType));
-					if (Number.isNaN(numericVal)) {
-						continue;
-					}
-
-					if (direction === ">" && numericVal < number) {
-						noMatch = true;
-						break;
-					}
-					if (direction === "<" && numericVal > number) {
-						noMatch = true;
-						break;
-					}
-					if (direction === "=" && numericVal !== number) {
-						noMatch = true;
-						break;
-					}
-					if (
-						direction === undefined &&
-						!getSearchVal(value).includes(filterText)
-					) {
-						noMatch = true;
-						break;
-					}
-				} else {
-					if (filterText === "") {
-						continue;
-					}
-
-					if (!getSearchVal(value).includes(filterText)) {
-						noMatch = true;
-						break;
-					}
+		for (const filterText of filterTexts) {
+			if (sortType === "number" || sortType === "currency") {
+				let number = filterText.replace(/[^0-9.<>]/g, "");
+				let direction;
+				if (number[0] === ">" || number[0] === "<" || number[0] === "=") {
+					direction = number[0];
+					number = number.slice(1); // Remove first char
 				}
-			}
-			if (!noMatch) {
-				foundMatch = true;
+				number = parseFloat(number);
+
+				if (Number.isNaN(number)) {
+					continue;
+				}
+
+				const numericVal = parseFloat(getSortVal(value, sortType));
+				if (Number.isNaN(numericVal)) {
+					continue;
+				}
+
+				if (direction === ">" && numericVal >= number) {
+					return true;
+				}
+				if (direction === "<" && numericVal <= number) {
+					return true;
+				}
+				if (direction === "=" && numericVal === number) {
+					return true;
+				}
+				if (
+					direction === undefined &&
+					getSearchVal(value).includes(filterText)
+				) {
+					return true;
+				}
+			} else if (getSearchVal(value).includes(filterText)) {
+				return true;
 			}
 		}
 
-		return foundMatch;
+		return false;
 	};
 };
 

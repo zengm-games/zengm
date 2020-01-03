11ce9e3c1a9bbfefc01b7212ec221d4c9c5f74e9
Jeremy Scheff <jdscheff@gmail.com>
2020-01-03 11:05:23 -0500

    Boolean filtering works

--- src/deion/ui/components/DataTable/createFilterFunction.js
+++ src/deion/ui/components/DataTable/createFilterFunction.js
@@ -1,57 +1,77 @@
 // @flow
 
+import booleanParser from "boolean-parser";
 import getSearchVal from "./getSearchVal";
 import getSortVal from "./getSortVal";
 
 // false - doesn't match. true - does match
-const createFilterFunction = (filterText: string, sortType: string) => {
-	if (sortType === "number" || sortType === "currency") {
-		let number = filterText.replace(/[^0-9.<>]/g, "");
-		let direction;
-		if (number[0] === ">" || number[0] === "<" || number[0] === "=") {
-			direction = number[0];
-			number = number.slice(1); // Remove first char
-		}
-		number = parseFloat(number);
-
-		return value => {
-			if (Number.isNaN(number)) {
-				return true;
-			}
-
-			const numericVal = parseFloat(getSortVal(value, sortType));
-			if (Number.isNaN(numericVal)) {
-				return true;
-			}
-
-			if (direction === ">" && numericVal < number) {
-				return false;
-			}
-			if (direction === "<" && numericVal > number) {
-				return false;
-			}
-			if (direction === "=" && numericVal !== number) {
-				return false;
-			}
-			if (
-				direction === undefined &&
-				!getSearchVal(value).includes(filterText)
-			) {
-				return false;
-			}
-		};
-	}
+const createFilterFunction = (originalFilterText: string, sortType: string) => {
+	const matches = booleanParser.parseBooleanQuery(originalFilterText);
 
 	return value => {
-		if (filterText === "") {
+		if (matches.length === 0) {
 			return true;
 		}
 
-		if (!getSearchVal(value).includes(filterText)) {
-			return false;
+		let foundMatch = false;
+
+		for (const match of matches) {
+			let noMatch = false;
+			for (const filterText of match) {
+				if (sortType === "number" || sortType === "currency") {
+					let number = filterText.replace(/[^0-9.<>]/g, "");
+					let direction;
+					if (number[0] === ">" || number[0] === "<" || number[0] === "=") {
+						direction = number[0];
+						number = number.slice(1); // Remove first char
+					}
+					number = parseFloat(number);
+
+					if (Number.isNaN(number)) {
+						continue;
+					}
+
+					const numericVal = parseFloat(getSortVal(value, sortType));
+					if (Number.isNaN(numericVal)) {
+						continue;
+					}
+
+					if (direction === ">" && numericVal < number) {
+						noMatch = true;
+						break;
+					}
+					if (direction === "<" && numericVal > number) {
+						noMatch = true;
+						break;
+					}
+					if (direction === "=" && numericVal !== number) {
+						noMatch = true;
+						break;
+					}
+					if (
+						direction === undefined &&
+						!getSearchVal(value).includes(filterText)
+					) {
+						noMatch = true;
+						break;
+					}
+				} else {
+					if (filterText === "") {
+						continue;
+					}
+
+					if (!getSearchVal(value).includes(filterText)) {
+						noMatch = true;
+						break;
+					}
+				}
+			}
+			if (!noMatch) {
+				foundMatch = true;
+			}
 		}
 
-		return true;
+		return foundMatch;
 	};
 };
 

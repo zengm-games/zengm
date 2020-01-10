2de81ed8ee699b33b760e82e2bd0fe0465769c12
Jeremy Scheff <jdscheff@gmail.com>
2020-01-10 15:09:26 -0500

    Refactor football team ovr

--- src/football/worker/core/team/ovr.ts
+++ src/football/worker/core/team/ovr.ts
@@ -1,4 +1,3 @@
-import groupBy from "lodash/groupBy";
 import orderBy from "lodash/orderBy";
 import { helpers } from "../../../../deion/worker/util";
 
@@ -10,78 +9,108 @@ const ovr = (
 		};
 	}[],
 ) => {
+	const info = {
+		QB: {
+			ovrs: [],
+			minLength: 1,
+		},
+		RB: {
+			ovrs: [],
+			minLength: 2,
+		},
+		TE: {
+			ovrs: [],
+			minLength: 2,
+		},
+		WR: {
+			ovrs: [],
+			minLength: 5,
+		},
+		OL: {
+			ovrs: [],
+			minLength: 5,
+		},
+		CB: {
+			ovrs: [],
+			minLength: 3,
+		},
+		S: {
+			ovrs: [],
+			minLength: 3,
+		},
+		LB: {
+			ovrs: [],
+			minLength: 4,
+		},
+		DL: {
+			ovrs: [],
+			minLength: 4,
+		},
+		K: {
+			ovrs: [],
+			minLength: 1,
+		},
+		P: {
+			ovrs: [],
+			minLength: 1,
+		},
+	};
+
 	const ratings = orderBy(
 		players.map(p => p.ratings),
 		"ovr",
 		"desc",
 	);
-	const ratingsByPos = groupBy(ratings, "pos"); // Pad to minimum lengths
 
-	const minLengths = {
-		QB: 1,
-		RB: 2,
-		TE: 2,
-		WR: 5,
-		OL: 5,
-		CB: 3,
-		S: 3,
-		LB: 4,
-		DL: 4,
-		K: 1,
-		P: 1,
-	};
-
-	const ovrsByPos: Record<keyof typeof minLengths, number[]> = {};
-	for (const pos of Object.keys(minLengths)) {
-		if (!ratingsByPos[pos]) {
-			ratingsByPos[pos] = [];
+	for (const { ovr, pos } of ratings) {
+		if (info[pos]) {
+			info[pos].ovrs.push(ovr);
 		}
+	}
 
-		const ovrs = ratingsByPos[pos].map(r => r.ovr);
-		const minLength = minLengths[pos];
-
+	// Pad to minimum lengths=
+	for (const { minLength, ovrs } of Object.values(info)) {
 		while (ovrs.length < minLength) {
 			ovrs.push(20);
 		}
-
-		ovrsByPos[pos] = ovrs;
 	}
 
 	// See analysis/team-ovr-football
 	const predictedMOV =
 		-141.16249800360956 +
-		0.27475987 * ovrsByPos.QB[0] +
-		0.01937121 * ovrsByPos.RB[0] +
-		0.00436415 * ovrsByPos.RB[1] +
-		0.03105994 * ovrsByPos.TE[0] +
-		0.00674677 * ovrsByPos.TE[1] +
-		0.06098162 * ovrsByPos.WR[0] +
-		0.03331908 * ovrsByPos.WR[1] +
-		0.02548362 * ovrsByPos.WR[2] +
-		0.01550788 * ovrsByPos.WR[3] +
-		0.00100514 * ovrsByPos.WR[4] +
-		0.19319358 * ovrsByPos.OL[0] +
-		0.13220752 * ovrsByPos.OL[1] +
-		0.10476351 * ovrsByPos.OL[2] +
-		0.10256055 * ovrsByPos.OL[3] +
-		0.08791494 * ovrsByPos.OL[4] +
-		0.13368333 * ovrsByPos.CB[0] +
-		0.08610441 * ovrsByPos.CB[1] +
-		0.03589333 * ovrsByPos.CB[2] +
-		0.10272653 * ovrsByPos.S[0] +
-		0.05600678 * ovrsByPos.S[1] +
-		0.04070437 * ovrsByPos.S[2] +
-		0.07226837 * ovrsByPos.LB[0] +
-		0.00066672 * ovrsByPos.LB[1] +
-		0.00281938 * ovrsByPos.LB[2] +
-		0.00002897 * ovrsByPos.LB[3] +
-		0.23767406 * ovrsByPos.DL[0] +
-		0.16659192 * ovrsByPos.DL[1] +
-		0.13864289 * ovrsByPos.DL[2] +
-		0.09932564 * ovrsByPos.DL[3] +
-		0.08030877 * ovrsByPos.K[0] +
-		0.05119918 * ovrsByPos.P[0]; // Translate from -15/15 to 0/100 scale
+		0.27475987 * info.QB.ovrs[0] +
+		0.01937121 * info.RB.ovrs[0] +
+		0.00436415 * info.RB.ovrs[1] +
+		0.03105994 * info.TE.ovrs[0] +
+		0.00674677 * info.TE.ovrs[1] +
+		0.06098162 * info.WR.ovrs[0] +
+		0.03331908 * info.WR.ovrs[1] +
+		0.02548362 * info.WR.ovrs[2] +
+		0.01550788 * info.WR.ovrs[3] +
+		0.00100514 * info.WR.ovrs[4] +
+		0.19319358 * info.OL.ovrs[0] +
+		0.13220752 * info.OL.ovrs[1] +
+		0.10476351 * info.OL.ovrs[2] +
+		0.10256055 * info.OL.ovrs[3] +
+		0.08791494 * info.OL.ovrs[4] +
+		0.13368333 * info.CB.ovrs[0] +
+		0.08610441 * info.CB.ovrs[1] +
+		0.03589333 * info.CB.ovrs[2] +
+		0.10272653 * info.S.ovrs[0] +
+		0.05600678 * info.S.ovrs[1] +
+		0.04070437 * info.S.ovrs[2] +
+		0.07226837 * info.LB.ovrs[0] +
+		0.00066672 * info.LB.ovrs[1] +
+		0.00281938 * info.LB.ovrs[2] +
+		0.00002897 * info.LB.ovrs[3] +
+		0.23767406 * info.DL.ovrs[0] +
+		0.16659192 * info.DL.ovrs[1] +
+		0.13864289 * info.DL.ovrs[2] +
+		0.09932564 * info.DL.ovrs[3] +
+		0.08030877 * info.K.ovrs[0] +
+		0.05119918 * info.P.ovrs[0];
 
+	// Translate from -15/15 to 0/100 scale
 	const rawOVR = (predictedMOV * 100) / 30 + 50;
 	return helpers.bound(Math.round(rawOVR), 0, Infinity);
 };

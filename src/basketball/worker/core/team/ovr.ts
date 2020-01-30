a067dbac05fbb1ad8fb3c4c49de8ab468de1bb32
Jeremy Scheff <jdscheff@gmail.com>
2020-01-29 19:57:23 -0500

    Slightly better basketball team ovr formula from @nicidob

--- src/basketball/worker/core/team/ovr.ts
+++ src/basketball/worker/core/team/ovr.ts
@@ -15,18 +15,26 @@ const ovr = (
 	}
 
 	// See analysis/team-ovr-basketball
+	// But after that, @nicidob fit the coefficients to an exponential:
+	// > The code is just.. get a best-fit exponential model from the OLS data
+	// >     a,b = np.polyfit(np.arange(10),np.log(np.array(res.params[1:])),1)
+	// > Then use that as an initial guess for an optimizer
+	// >     def best_fit_func_exp(x):
+	// >         return np.linalg.norm((dataX @ np.exp(x[0]* np.arange(10))x[1]-x[2] - dataset['mov']))
+	// >     res2 = opt.minimize(best_fit_func_exp,[a,np.exp(b),-125],method='Nelder-Mead')
+	// >     plt.plot(np.exp(res2.x[0] np.arange(10))*res2.x[1],label='best-fit exp model',alpha=0.6)
 	const predictedMOV =
-		-124.508969997621 +
-		0.4218802 * ovrs[0] +
-		0.34070417 * ovrs[1] +
-		0.3458834 * ovrs[2] +
-		0.28931358 * ovrs[3] +
-		0.21194375 * ovrs[4] +
-		0.14966658 * ovrs[5] +
-		0.14314723 * ovrs[6] +
-		0.10012792 * ovrs[7] +
-		0.10397797 * ovrs[8] +
-		0.06609493 * ovrs[9];
+		-124.13 +
+		0.4417 * Math.exp(-0.1905 * 0) * ovrs[0] +
+		0.4417 * Math.exp(-0.1905 * 1) * ovrs[1] +
+		0.4417 * Math.exp(-0.1905 * 2) * ovrs[2] +
+		0.4417 * Math.exp(-0.1905 * 3) * ovrs[3] +
+		0.4417 * Math.exp(-0.1905 * 4) * ovrs[4] +
+		0.4417 * Math.exp(-0.1905 * 5) * ovrs[5] +
+		0.4417 * Math.exp(-0.1905 * 6) * ovrs[6] +
+		0.4417 * Math.exp(-0.1905 * 7) * ovrs[7] +
+		0.4417 * Math.exp(-0.1905 * 8) * ovrs[8] +
+		0.4417 * Math.exp(-0.1905 * 9) * ovrs[9];
 
 	// Translate from -20/20 to 0/100 scale
 	const rawOVR = (predictedMOV * 50) / 20 + 50;

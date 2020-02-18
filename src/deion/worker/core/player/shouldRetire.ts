a3d3cc454ecd21d8e2cd78db4ba58c1db3525189
Jeremy Scheff <jdscheff@gmail.com>
2020-02-17 21:39:06 -0500

    New retirement formula from @nicidob will allow rare old and good players to have longer careers

--- src/deion/worker/core/player/shouldRetire.ts
+++ src/deion/worker/core/player/shouldRetire.ts
@@ -6,33 +6,57 @@ import {
 	PlayerWithoutKey,
 } from "../../../common/types"; // Players meeting one of these cutoffs might retire
 
-let maxAge = 34;
-const minPot = process.env.SPORT === "basketball" ? 40 : 50;
-
 const shouldRetire = (
 	p: Player<MinimalPlayerRatings> | PlayerWithoutKey<MinimalPlayerRatings>,
 ): boolean => {
 	const age = g.get("season") - p.born.year;
 	const { pos, pot } = p.ratings[p.ratings.length - 1];
 
-	if (process.env.SPORT === "football") {
-		maxAge = pos === "QB" || pos === "P" || pos === "K" ? 32 : 28;
-	}
+	if (process.env.SPORT === "basketball") {
+		const maxAge = 33;
+		const minPot = 40;
 
-	// Only players older than maxAge or without a contract will retire
-	if (age > maxAge || (pot < minPot && p.tid === PLAYER.FREE_AGENT)) {
 		// Only players older than maxAge or without a contract will retire
-		let excessAge = 0;
+		if (age > maxAge || (pot < minPot && p.tid === PLAYER.FREE_AGENT)) {
+			let ws = 0;
+			for (const stats of p.stats) {
+				if (stats.season === g.get("season") && !stats.playoffs) {
+					ws += stats.dws + stats.ows;
+				}
+			}
 
-		if (age > maxAge) {
-			excessAge =
-				(age - maxAge) / (process.env.SPORT === "basketball" ? 20 : 40); // 0.05 or 0.025 for each year beyond maxAge
+			// Formulas from @nicidob
+			const estWS = (1 / 85.017) * (Math.max(pot, 37) - 37) ** 2;
+			const logit =
+				-1.4134 +
+				(1 / 175.19) * (Math.max(age, 18) - 18) ** 2 -
+				0.971 * Math.max(ws, estWS);
+
+			const odds = Math.exp(logit);
+			const prob = odds / (1 + odds);
+
+			if (prob > Math.random()) {
+				return true;
+			}
 		}
+	} else {
+		const maxAge = pos === "QB" || pos === "P" || pos === "K" ? 32 : 28;
+		const minPot = 50;
 
-		const excessPot = (minPot - pot) / 50; // 0.02 for each potential rating below minPot (this can be negative)
+		// Only players older than maxAge or without a contract will retire
+		if (age > maxAge || (pot < minPot && p.tid === PLAYER.FREE_AGENT)) {
+			// Only players older than maxAge or without a contract will retire
+			let excessAge = 0;
 
-		if (excessAge + excessPot + random.gauss(0, 1) > 0) {
-			return true;
+			if (age > maxAge) {
+				excessAge = (age - maxAge) / 40; // 0.025 for each year beyond maxAge
+			}
+
+			const excessPot = (minPot - pot) / 50; // 0.02 for each potential rating below minPot (this can be negative)
+
+			if (excessAge + excessPot + random.gauss(0, 1) > 0) {
+				return true;
+			}
 		}
 	}
 

7a6bb0e3bfa143d5a70275fea72533fc6b76f7ac
Jeremy Scheff <jdscheff@gmail.com>
2020-04-19 15:37:23 -0400

    Optimize getPayroll

--- src/deion/worker/core/team/getPayroll.ts
+++ src/deion/worker/core/team/getPayroll.ts
@@ -1,5 +1,5 @@
+import { idb } from "../../db";
 import type { ContractInfo } from "../../../common/types";
-import getContracts from "./getContracts";
 
 /**
  * Get the total current payroll for a team.
@@ -10,16 +10,31 @@ import getContracts from "./getContracts";
  * @param {number | ContractInfo[]} tid Team ID, or a list of contracts from getContracts.
  * @return {Promise.<number>} Resolves to payroll in thousands of dollars.
  */
-async function getPayroll(input: number | ContractInfo[]): Promise<number> {
-	const contracts =
-		typeof input === "number" ? await getContracts(input) : input;
+const getPayroll = async (input: number | ContractInfo[]): Promise<number> => {
 	let payroll = 0;
 
-	for (let i = 0; i < contracts.length; i++) {
-		payroll += contracts[i].amount; // No need to check exp, since anyone without a contract for the current season will not have an entry
+	if (typeof input === "number") {
+		const tid = input;
+		const players = await idb.cache.players.indexGetAll("playersByTid", tid);
+		for (const p of players) {
+			payroll += p.contract.amount;
+		}
+
+		const releasedPlayers = await idb.cache.releasedPlayers.indexGetAll(
+			"releasedPlayersByTid",
+			tid,
+		);
+		for (const p of releasedPlayers) {
+			payroll += p.contract.amount;
+		}
+	} else {
+		const contracts = input;
+		for (let i = 0; i < contracts.length; i++) {
+			payroll += contracts[i].amount;
+		}
 	}
 
 	return payroll;
-}
+};
 
 export default getPayroll;

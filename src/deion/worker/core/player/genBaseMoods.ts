cfb29676627808814173ef48f77275de4659a221
Jeremy Scheff <jdscheff@gmail.com>
2020-05-06 20:05:52 -0400

    Fix free agent moods towards expansion teams with no TeamSeason

--- src/deion/worker/core/player/genBaseMoods.ts
+++ src/deion/worker/core/player/genBaseMoods.ts
@@ -10,11 +10,20 @@ import { g } from "../../util";
  * @return {Promise} Array of base moods, one for each team.
  */
 const genBaseMoods = async (reSigning: boolean = false): Promise<number[]> => {
+	const teams = await idb.cache.teams.getAll();
 	const teamSeasons = await idb.cache.teamSeasons.indexGetAll(
 		"teamSeasonsBySeasonTid",
 		[[g.get("season")], [g.get("season"), "Z"]],
 	);
-	return teamSeasons.map(ts => player.genBaseMood(ts, reSigning));
+	return teams.map(t => {
+		const ts = teamSeasons.find(ts2 => ts2.tid === t.tid);
+		if (ts) {
+			return player.genBaseMood(ts, reSigning);
+		}
+
+		// Must be an expansion team - not very desirable!
+		return 1;
+	});
 };
 
 export default genBaseMoods;

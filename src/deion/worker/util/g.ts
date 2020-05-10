e720b189d5fe977e2c4754dccab8e01c061610fa
Jeremy Scheff <jdscheff@gmail.com>
2020-05-10 18:26:48 -0400

    More thorough checking, in case first element of array is undefined, which can happen in teamImgURLsCache

--- src/deion/worker/util/g.ts
+++ src/deion/worker/util/g.ts
@@ -9,6 +9,7 @@ export const gameAttributeHasHistory = (gameAttribute: any) => {
 	return (
 		Array.isArray(gameAttribute) &&
 		gameAttribute.length > 0 &&
+		gameAttribute[0] &&
 		typeof gameAttribute[0].start === "number"
 	);
 };

37c762e32b51597a9873a278f652c21afd3104a0
Jeremy Scheff <jdscheff@gmail.com>
2020-05-20 20:54:16 -0400

    In live box score, update quarter right when it starts, not when first points are scored

--- src/basketball/ui/util/processLiveGameEvents.ts
+++ src/basketball/ui/util/processLiveGameEvents.ts
@@ -14,11 +14,33 @@ const processLiveGameEvents = ({
 }) => {
 	let stop = false;
 	let text;
+	let prevQtr;
 	while (!stop && events.length > 0) {
 		const e = events.shift();
 
 		// Swap teams order, so home team is at bottom in box score
 		const actualT = e.t === 0 ? 1 : 0;
+
+		// Hacky quarter stuff, ugh
+		if (e.qtr !== undefined) {
+			prevQtr = e.qtr;
+		}
+		if (e.text && e.text.startsWith("Start of")) {
+			boxScore.teams[0].ptsQtrs.push(0);
+			boxScore.teams[1].ptsQtrs.push(0);
+			const quarter = prevQtr + 2; // 1 because it's a new quarter, 1 for starting qtr at 0
+			if (quarter > 4) {
+				overtimes = quarter - 4;
+				if (overtimes === 1) {
+					boxScore.overtime = " (OT)";
+				} else if (overtimes > 1) {
+					boxScore.overtime = ` (${overtimes}OT)`;
+				}
+				boxScore.quarter = `${helpers.ordinal(overtimes)} overtime`;
+			} else {
+				boxScore.quarter = `${helpers.ordinal(quarter)} quarter`;
+			}
+		}
 
 		if (e.type === "text") {
 			if (e.text === "End of game" || e.text.startsWith("Start of")) {
@@ -49,23 +71,6 @@ const processLiveGameEvents = ({
 			// Quarter-by-quarter score
 			if (e.s === "pts") {
 				const ptsQtrs = boxScore.teams[actualT].ptsQtrs;
-				if (ptsQtrs.length <= e.qtr) {
-					// Must be overtime! This updates ptsQtrs too.
-					boxScore.teams[0].ptsQtrs.push(0);
-					boxScore.teams[1].ptsQtrs.push(0);
-
-					if (ptsQtrs.length > 4) {
-						overtimes += 1;
-						if (overtimes === 1) {
-							boxScore.overtime = " (OT)";
-						} else if (overtimes > 1) {
-							boxScore.overtime = ` (${overtimes}OT)`;
-						}
-						boxScore.quarter = `${helpers.ordinal(overtimes)} overtime`;
-					} else {
-						boxScore.quarter = `${helpers.ordinal(ptsQtrs.length)} quarter`;
-					}
-				}
 				ptsQtrs[e.qtr] += e.amt;
 				boxScore.teams[actualT].ptsQtrs = ptsQtrs;
 			}

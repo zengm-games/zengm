8254fcad1c4aeb798e5fc8fd41df4b1d8be428d6
Jeremy Scheff <jdscheff@gmail.com>
2020-05-28 22:50:27 -0400

    Auto play will pick a new team if the user's team is disabled

--- src/deion/worker/core/league/autoPlay.ts
+++ src/deion/worker/core/league/autoPlay.ts
@@ -1,7 +1,17 @@
 import { PHASE } from "../../../common";
-import { draft, freeAgents, game, phase, season, expansionDraft } from "..";
-import { g } from "../../util";
+import {
+	draft,
+	freeAgents,
+	game,
+	phase,
+	season,
+	expansionDraft,
+	league,
+	team,
+} from "..";
+import { g, random } from "../../util";
 import type { Conditions } from "../../../common/types"; // Depending on phase, initiate action that will lead to the next phase
+import { idb } from "../../db";
 
 const autoPlay = async (conditions: Conditions = {}) => {
 	// No newPhase call is triggered after expansion draft, so this check comes first
@@ -11,6 +21,19 @@ const autoPlay = async (conditions: Conditions = {}) => {
 		}
 		if (g.get("expansionDraft").phase === "draft") {
 			await draft.runPicks(false, conditions);
+		}
+	}
+
+	// If game over and user's team is disabled, need to pick a new team or there will be errors
+	if (g.get("gameOver")) {
+		const t = await idb.cache.teams.get(g.get("userTid"));
+		if (t.disabled) {
+			// If multi team mode was enabled, a new team would have already been picked in team.disable(). So go with a random team here.
+			const teams = (await idb.cache.teams.getAll()).filter(t => !t.disabled);
+			if (teams.length === 0) {
+				throw new Error("No active teams");
+			}
+			await team.switchTo(random.choice(teams).tid);
 		}
 	}
 

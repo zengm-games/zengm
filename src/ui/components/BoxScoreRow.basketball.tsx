882a3b5b8e6299809fa8f16a6d57542517540674
Jeremy Scheff <jdscheff@gmail.com>
2020-08-10 11:40:18 -0400

    Show injury type

--- src/ui/components/BoxScoreRow.basketball.tsx
+++ src/ui/components/BoxScoreRow.basketball.tsx
@@ -22,7 +22,7 @@ const BoxScoreRow = ({
 
 	const statCols = showDNP ? (
 		<td colSpan={15} className="text-center">
-			DNP - {p.injury.gamesRemaining === 0 ? "Coach's decision" : "Injury"}
+			DNP - {p.injury.gamesRemaining === 0 ? "Coach's decision" : p.injury.type}
 		</td>
 	) : (
 		<>

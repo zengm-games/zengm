0db628545bb4a0f0d2616f75faa03c9e5a652cc9
Jeremy Scheff <jdscheff@gmail.com>
2020-08-19 21:43:14 -0400

    Filter player seasons

--- src/worker/views/gmHistory.ts
+++ src/worker/views/gmHistory.ts
@@ -1,6 +1,6 @@
-import { idb } from "../db";
+import { idb, iterate } from "../db";
 import { g, helpers } from "../util";
-import type { UpdateEvents, TeamSeason } from "../../common/types";
+import type { UpdateEvents, TeamSeason, Player } from "../../common/types";
 import { getHistory } from "./teamHistory";
 
 const updateGmHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
@@ -15,6 +15,7 @@ const updateGmHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
 			throw new Error("Invalid team ID number");
 		}
 
+		const seasonsByTid: Record<number, Set<number>> = {};
 		const teamSeasons: TeamSeason[] = [];
 		const teamSeasonsIndex = idb.league
 			.transaction("teamSeasons")
@@ -27,17 +28,40 @@ const updateGmHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
 			const ts = await teamSeasonsIndex.get([season, g.get("userTid", season)]);
 			if (ts) {
 				teamSeasons.push(ts);
+
+				if (!seasonsByTid[ts.tid]) {
+					seasonsByTid[ts.tid] = new Set();
+				}
+				seasonsByTid[ts.tid].add(season);
 			}
 		}
 
 		const tids = new Set(teamSeasons.map(ts => ts.tid));
 
-		const players = await idb.getCopies.players({
-			statsTid: tid,
-		});
-		for (const p of players) {
-			p.stats = p.stats.filter(row => row.tid === tid);
-		}
+		const players: Player[] = [];
+		await iterate(
+			idb.league.transaction("players").store,
+			undefined,
+			undefined,
+			p => {
+				let hasTid = false;
+				for (const tid of p.statsTids) {
+					if (tids.has(tid)) {
+						hasTid = true;
+						break;
+					}
+				}
+				if (!hasTid) {
+					return;
+				}
+
+				p.stats = p.stats.filter(
+					row => seasonsByTid[row.tid] && seasonsByTid[row.tid].has(row.season),
+				);
+
+				players.push(p);
+			},
+		);
 
 		return getHistory(t, teamSeasons, players);
 	}

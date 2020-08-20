67634205fb198753052d25beef2e1f1720e07750
Jeremy Scheff <jdscheff@gmail.com>
2020-08-19 21:35:44 -0400

    Get correct team seasons

--- src/worker/views/gmHistory.ts
+++ src/worker/views/gmHistory.ts
@@ -1,6 +1,6 @@
 import { idb } from "../db";
 import { g, helpers } from "../util";
-import type { UpdateEvents } from "../../common/types";
+import type { UpdateEvents, TeamSeason } from "../../common/types";
 import { getHistory } from "./teamHistory";
 
 const updateGmHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
@@ -15,9 +15,22 @@ const updateGmHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
 			throw new Error("Invalid team ID number");
 		}
 
-		const teamSeasons = await idb.getCopies.teamSeasons({
-			tid: tid,
-		});
+		const teamSeasons: TeamSeason[] = [];
+		const teamSeasonsIndex = idb.league
+			.transaction("teamSeasons")
+			.store.index("season, tid");
+		for (
+			let season = g.get("startingSeason");
+			season <= g.get("season");
+			season++
+		) {
+			const ts = await teamSeasonsIndex.get([season, g.get("userTid", season)]);
+			if (ts) {
+				teamSeasons.push(ts);
+			}
+		}
+
+		const tids = new Set(teamSeasons.map(ts => ts.tid));
 
 		const players = await idb.getCopies.players({
 			statsTid: tid,

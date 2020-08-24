2ae6fc56e6c29063a90fc9b300c49582869aba11
Jeremy Scheff <jdscheff@gmail.com>
2020-08-23 20:27:59 -0400

    Adjust for salary cap better

--- src/worker/core/season/updateOwnerMood.ts
+++ src/worker/core/season/updateOwnerMood.ts
@@ -36,7 +36,9 @@ const updateOwnerMood = async (): Promise<
 		return;
 	}
 
-	const expectedProfit = (15 * g.get("salaryCap")) / 90000;
+	const salaryCapFactor = g.get("salaryCap") / 90000;
+
+	const expectedProfit = 15 * salaryCapFactor;
 
 	const numPlayoffRounds = g.get("numGamesPlayoffSeries", "current").length;
 	const deltas = {
@@ -44,7 +46,10 @@ const updateOwnerMood = async (): Promise<
 			(0.25 * (t.seasonAttrs.won - g.get("numGames") / 2)) /
 			(g.get("numGames") / 2),
 		playoffs: 0,
-		money: g.get("budget") ? (t.seasonAttrs.profit - expectedProfit) / 100 : 0,
+		money: g.get("budget")
+			? (t.seasonAttrs.profit - expectedProfit) /
+			  (100 * Math.sqrt(salaryCapFactor))
+			: 0,
 	};
 
 	if (t.seasonAttrs.playoffRoundsWon < 0) {

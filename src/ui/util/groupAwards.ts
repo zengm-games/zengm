6e38162575e5c2a54ede86e3fe70ceef8720cc35
Jeremy Scheff <jdscheff@gmail.com>
2020-11-02 16:54:47 -0500

    Better awards grouping

--- src/ui/util/groupAwards.ts
+++ src/ui/util/groupAwards.ts
@@ -29,32 +29,58 @@ const awardsOrder = [
 	"League Blocks Leader",
 ];
 
-const groupAwards = (awards: Player["awards"]) => {
-	const awardsGrouped = [];
-	const awardsGroupedTemp = groupBy(awards, award => award.type);
+const groupAwards = (awards: Player["awards"], shortNames?: boolean) => {
+	const getType = (originalType: string) => {
+		if (!shortNames) {
+			return originalType;
+		}
 
-	for (const award of awardsOrder) {
-		if (awardsGroupedTemp.hasOwnProperty(award)) {
+		let type = originalType;
+
+		if (type.endsWith("Leader")) {
+			type = type.replace("League ", "");
+		}
+
+		type = type
+			.replace("First Team ", "")
+			.replace("Second Team ", "")
+			.replace("Third Team ", "");
+
+		if (type.endsWith(" Team")) {
+			type = type.replace(" Team ", "");
+		}
+
+		return type;
+	};
+
+	const seen = new Set();
+	const awardsGrouped = [];
+	const awardsGroupedTemp = groupBy(awards, award => getType(award.type));
+	console.log(awardsGroupedTemp);
+
+	for (const originalType of awardsOrder) {
+		const type = getType(originalType);
+
+		if (awardsGroupedTemp[type] && !seen.has(type)) {
 			awardsGrouped.push({
-				type: award,
-				count: awardsGroupedTemp[award].length,
-				seasons: helpers.yearRanges(
-					awardsGroupedTemp[award].map(a => a.season),
-				),
+				type,
+				count: awardsGroupedTemp[type].length,
+				seasons: helpers.yearRanges(awardsGroupedTemp[type].map(a => a.season)),
 			});
+			seen.add(type);
 		}
 	}
 
 	// Handle non-default awards, just for fun if someone wants to add more
-	for (const award of Object.keys(awardsGroupedTemp).sort()) {
-		if (!awardsOrder.includes(award)) {
+	for (const originalType of Object.keys(awardsGroupedTemp).sort()) {
+		const type = getType(originalType);
+		if (!seen.has(type)) {
 			awardsGrouped.push({
-				type: award,
-				count: awardsGroupedTemp[award].length,
-				seasons: helpers.yearRanges(
-					awardsGroupedTemp[award].map(a => a.season),
-				),
+				type,
+				count: awardsGroupedTemp[type].length,
+				seasons: helpers.yearRanges(awardsGroupedTemp[type].map(a => a.season)),
 			});
+			seen.add(type);
 		}
 	}
 
--- src/ui/views/Player/AwardsSummary.tsx
+++ src/ui/views/Player/AwardsSummary.tsx
@@ -2,28 +2,20 @@ import React from "react";
 import type { Player } from "../../../common/types";
 import { groupAwards } from "../../util";
 
-const style = { fontSize: "90%", width: 160 };
+const style = { fontSize: "90%", width: 150 };
 
 const AwardsSummary = ({ awards }: { awards: Player["awards"] }) => {
 	if (awards.length === 0) {
 		return null;
 	}
 
-	const awardsGrouped = groupAwards(awards);
+	const awardsGrouped = groupAwards(awards, true);
 
 	// "First Team All-League", "Second Team All-League", "Third Team All-League", "First Team All-Defensive", "Second Team All-Defensive", "Third Team All-Defensive",
 
 	return (
 		<div className="flex-grow-1 clearfix" style={{ maxWidth: 500 }}>
 			{awardsGrouped.map((a, i) => {
-				let type = a.type.endsWith("Leader")
-					? a.type.replace("League ", "")
-					: a.type;
-
-				type = type.replace("First", "1st");
-				type = type.replace("Second", "2nd");
-				type = type.replace("Third", "3rd");
-
 				return (
 					<div
 						key={i}
@@ -32,7 +24,7 @@ const AwardsSummary = ({ awards }: { awards: Player["awards"] }) => {
 						style={style}
 					>
 						{a.count > 1 ? `${a.count}x ` : null}
-						{type}
+						{a.type}
 					</div>
 				);
 			})}

5d09797ad63e8fc691ec66c63c808456cd831bc1
Bugs Bunny <57536929+mamamia5x@users.noreply.github.com>
2020-12-04 09:21:55 -0600

    fixed onerror code

--- src/ui/components/PlayerPicture.tsx
+++ src/ui/components/PlayerPicture.tsx
@@ -27,7 +27,7 @@ const PlayerPicture = ({
 	}, [face, imgURL, teamColors, wrapper]);
 
 	if (imgURL) {
-		return <img alt="Player" src={imgURL} style={imgStyle} onError="this.onerror=null;this.src='img/blank-face.png'"/>;
+		return <img alt="Player" src={imgURL} style={imgStyle} onerror="this.src='img/blank-face.png';"/>;
 	}
 
 	if (face) {

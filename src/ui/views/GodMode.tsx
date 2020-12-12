c1b4d16b1732fc1668c0c14448bf95e7caaf067a
Jeremy Scheff <jdscheff@gmail.com>
2020-12-12 02:17:38 -0500

    Better promo of God Mode features

--- src/ui/views/GodMode.tsx
+++ src/ui/views/GodMode.tsx
@@ -22,60 +22,24 @@ const GodMode = (props: View<"godMode">) => {
 		await toWorker("main", "updateGameAttributes", attrs);
 		localActions.update({ godMode: attrs.godMode });
 	};
-
 	return (
-		<>
+		<div style={{ maxWidth: 1100 }}>
 			<p>
-				God Mode is a collection of customization features that allow you to
-				kind of do whatever you want. If you enable God Mode, you get access to
-				the following features (which show up in the UI as{" "}
-				<span className="god-mode god-mode-text">purple</span>
-				):
+				God Mode is a collection of powerful customization features that give
+				you more control over your league. These features show up in the UI in{" "}
+				<span className="god-mode god-mode-text">purple</span>.
 			</p>
 
-			<ul>
-				<li>
-					Customize tons of options at{" "}
-					<a href={helpers.leagueUrl(["settings"])}>
-						Tools &gt; League Settings
-					</a>
-				</li>
-				<li>
-					Create custom players by going to{" "}
-					<a href={helpers.leagueUrl(["customize_player"])}>
-						Tools &gt; Create A Player
-					</a>
-				</li>
-				<li>
-					Edit any player by going to their player page and clicking Edit Player
-				</li>
-				<li>
-					Force any trade to be accepted by checking the Force Trade checkbox
-					before proposing a trade
-				</li>
-				<li>
-					You can become the GM of another team at any time with{" "}
-					<a href={helpers.leagueUrl(["new_team"])}>Tools &gt; Switch Team</a>
-				</li>
-				<li>
-					Add, remove and edit teams with{" "}
-					<a href={helpers.leagueUrl(["manage_teams"])}>
-						Tools &gt; Manage Teams
-					</a>
-				</li>
-				<li>You will never be fired!</li>
-			</ul>
-
-			<p>
-				However, if you enable God Mode within a league, you will not get credit
-				for any <a href="/account">Achievements</a>. This persists even if you
-				disable God Mode. You can only get Achievements in a league where God
-				Mode has never been enabled.
+			<p className="text-danger">
+				If you enable God Mode within a league, you will not get credit for any{" "}
+				<a href="/account">Achievements</a>. This persists even if you disable
+				God Mode. You can only get Achievements in a league where God Mode has
+				never been enabled.
 			</p>
 
 			<button
 				className={classNames(
-					"btn border-0",
+					"btn btn-lg border-0",
 					godMode ? "btn-success" : "btn-god-mode",
 				)}
 				onClick={handleGodModeToggle}
@@ -83,16 +47,104 @@ const GodMode = (props: View<"godMode">) => {
 				{godMode ? "Disable God Mode" : "Enable God Mode"}
 			</button>
 
-			<div>
-				<p className="alert alert-info mt-3 d-inline-block">
-					The additional settings that used to be here are now available at{" "}
-					<a href={helpers.leagueUrl(["settings"])}>
-						Tools &gt; League Settings
-					</a>
-					.
-				</p>
+			<div className="card-group mt-3">
+				<div className="card border-radius-bottom-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Advanced Settings</h3>
+						<p className="card-text">
+							Customize tons of options at{" "}
+							<a href={helpers.leagueUrl(["settings"])}>
+								Tools &gt; League Settings
+							</a>
+							.
+						</p>
+					</div>
+				</div>
+				<div className="card border-radius-bottom-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Customize Players</h3>
+						<p className="card-text">
+							Create custom players by going to{" "}
+							<a href={helpers.leagueUrl(["customize_player"])}>
+								Tools &gt; Create A Player
+							</a>
+							. You can also edit, delete, and clone existing players from their
+							profile pages. Edit draft classes at{" "}
+							<a href={helpers.leagueUrl(["draft_scouting"])}>
+								Tools &gt; Draft &gt; Draft Scouting
+							</a>
+							. Import/export players between leagues at{" "}
+							<a href={helpers.leagueUrl(["export_players"])}>
+								Tools &gt; Import/Export Players
+							</a>
+							.
+						</p>
+					</div>
+				</div>
+				<div className="card border-radius-bottom-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Customize Teams</h3>
+						<p className="card-text">
+							Create, disable, and edit teams in your league at{" "}
+							<a href={helpers.leagueUrl(["manage_teams"])}>
+								Tools &gt; Manage Teams
+							</a>
+							. Do the same for divisions and conferenes at{" "}
+							<a href={helpers.leagueUrl(["manage_confs"])}>
+								Tools &gt; Manage Confs
+							</a>
+							.
+						</p>
+					</div>
+				</div>
 			</div>
-		</>
+
+			<div className="card-group">
+				<div className="card border-top-sm-0 border-radius-top-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Control Other Teams</h3>
+						<p className="card-text">
+							You can become the GM of another team at any time by going to{" "}
+							<a href={helpers.leagueUrl(["new_team"])}>
+								Tools &gt; Switch Team
+							</a>
+							. You can control multiple teams at the same time at{" "}
+							<a href={helpers.leagueUrl(["multi_team_mode"])}>
+								Tools &gt; Multi Team Mode
+							</a>
+							.
+						</p>
+					</div>
+				</div>
+				<div className="card border-top-sm-0 border-radius-top-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Force Results</h3>
+						<p className="card-text">
+							Force any trade to go through by clicking the "Force Trade"
+							checkbox on the <a href={helpers.leagueUrl(["trade"])}>Trade</a>{" "}
+							page. Or really play God and pick which team will win a game
+							before it happens, from either{" "}
+							<a href={helpers.leagueUrl(["schedule"])}>Team &gt; Schedule</a>{" "}
+							or the{" "}
+							<a href={helpers.leagueUrl(["live"])}>Live Game Simulation</a>{" "}
+							page.
+						</p>
+					</div>
+				</div>
+				<div className="card border-top-sm-0 border-radius-top-sm-0">
+					<div className="card-body">
+						<h3 className="card-title">Edit Awards</h3>
+						<p className="card-text">
+							Decide who should win any end-of-season awards at{" "}
+							<a href={helpers.leagueUrl(["edit_awards"])}>
+								Tools &gt; Edit Awards
+							</a>
+							.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 

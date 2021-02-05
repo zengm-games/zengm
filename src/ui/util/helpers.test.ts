6faa92a3811a6a7aaf5cfa3ec98ffc6fb870764b
Jeremy Scheff <jdscheff@gmail.com>
2021-02-05 02:39:10 -0500

    Lint

--- src/ui/util/helpers.test.ts
+++ src/ui/util/helpers.test.ts
@@ -20,8 +20,9 @@ describe("ui/util/helpers", () => {
 
 	describe("roundStat", () => {
 		test("work", () => {
-			assert.strictEqual(helpers.roundStat(49.99, "fgp"), "50.0");
-			assert.strictEqual(helpers.roundStat(100, "fgp"), "100.0");
+			assert.strictEqual(helpers.roundStat(49.99, "fgp"), "50");
+			assert.strictEqual(helpers.roundStat(49.9, "fgp"), "49.9");
+			assert.strictEqual(helpers.roundStat(100, "fgp"), "100");
 			assert.strictEqual(helpers.roundStat(15.7, "trb"), "15.7");
 			assert.strictEqual(helpers.roundStat(15.7, "trb", true), "16");
 		});

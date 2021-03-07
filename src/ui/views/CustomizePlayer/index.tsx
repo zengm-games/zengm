8262d9dd36b026bb32bd7bb88a8f3c88ef54efa4
Jeremy Scheff <jdscheff@gmail.com>
2021-03-06 21:19:43 -0500

    Hockey randomize ratings by position

--- src/ui/views/CustomizePlayer/index.tsx
+++ src/ui/views/CustomizePlayer/index.tsx
@@ -482,7 +482,7 @@ const CustomizePlayer = (props: View<"customizePlayer">) => {
 			const pos = p.ratings[r].pos;
 
 			const keys = posRatings(pos);
-			if (isSport("football")) {
+			if (isSport("football") || isSport("hockey")) {
 				keys.push("stre", "spd", "endu");
 			}
 
@@ -901,7 +901,9 @@ const CustomizePlayer = (props: View<"customizePlayer">) => {
 								type="button"
 								className="btn btn-secondary btn-sm mb-1"
 								title={`Ratings will be taken from a randomly generated player with the same age${
-									isSport("football") ? " and position" : ""
+									isSport("football") || isSport("hockey")
+										? " and position"
+										: ""
 								} as this player`}
 								onClick={async event => {
 									event.preventDefault();
@@ -909,7 +911,9 @@ const CustomizePlayer = (props: View<"customizePlayer">) => {
 										"main",
 										"getRandomRatings",
 										(p as any).age,
-										isSport("football") ? p.ratings[r].pos : undefined,
+										isSport("football") || isSport("hockey")
+											? p.ratings[r].pos
+											: undefined,
 									);
 
 									setState(prevState => {

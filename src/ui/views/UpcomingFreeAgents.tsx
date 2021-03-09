3f11a21f9cd6bbb7d85354bad9ee9d72315cbde8
domini7 <67447432+domini7@users.noreply.github.com>
2021-03-08 16:03:03 -0800

    Cleanup TODO

--- src/ui/views/UpcomingFreeAgents.tsx
+++ src/ui/views/UpcomingFreeAgents.tsx
@@ -106,7 +106,7 @@ const UpcomingFreeAgents = ({
 			{phase !== PHASE.RESIGN_PLAYERS ? (
 				<p>
 					Keep in mind that many of these players will choose to re-sign with
-					their current team rather than become free agents. Also even if a
+					their current team rather than become free agents. Also, even if a
 					player is &gt;99% willing to re-sign with his team, he still may
 					become a free agent if his team does not want him.
 				</p>

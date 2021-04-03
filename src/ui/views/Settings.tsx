4a309fa568853e79681a4dad0fe54cc413648e08
Jeremy Scheff <jdscheff@gmail.com>
2021-04-03 15:21:17 -0400

    Get rid of all the lines, it was confusing to have the columns "connected" when that meant nothing

--- src/ui/views/Settings.tsx
+++ src/ui/views/Settings.tsx
@@ -773,7 +773,7 @@ export const options: {
 		name: "Player Mood Traits",
 		godModeRequired: "existingLeagueOnly",
 		type: "bool",
-		description: (
+		descriptionLong: (
 			<>
 				This controls the existence of player mood traits (fame, loyalty, money,
 				winning). Even if you disable it, the player mood system will still
@@ -2831,30 +2831,22 @@ const Settings = (props: View<"settings">) => {
 											}
 										}
 
-										// Similar logic could extend this to 3 columns or more, if necessary
-										const lastInCol = i === catOptions.length - 1;
-										const lastInCol2 = lastInCol || i === catOptions.length - 2;
-										const lastInCol3 =
-											lastInCol2 || i === catOptions.length - 3;
-
-										const firstInCol = i === 0;
-										const firstInCol2 = firstInCol || i === 1;
-										const firstInCol3 = firstInCol2 || i === 2;
+										// Similar logic could extend this to 4 columns or more, if necessary
+										const striped1 = i % 2 === 0;
+										const striped2 = Math.floor(i / 2) % 2 === 0;
+										const striped3 = Math.floor(i / 3) % 2 === 0;
 
 										return (
-											<div key={key} className="col-md-6 col-xxl-4 d-flex">
+											<div key={key} className="col-md-6 col-xxl-4 ">
 												<div
-													className={classNames("fake-list-group-item border", {
-														"rounded-bottom": lastInCol,
-														"rounded-top": firstInCol,
-														"border-top-0": !firstInCol,
-														"rounded-md-bottom": lastInCol2,
-														"rounded-md-top": firstInCol2,
-														"border-md-top": firstInCol2,
-														"rounded-xxl-bottom": lastInCol3,
-														"rounded-xxl-top": firstInCol3,
-														"border-xxl-top": firstInCol3,
-													})}
+													className={classNames(
+														"fake-list-group-item rounded",
+														{
+															"settings-striped-bg-1": striped1,
+															"settings-striped-bg-2": striped2,
+															"settings-striped-bg-3": striped3,
+														},
+													)}
 												>
 													<Option
 														type={type}

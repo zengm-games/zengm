e94f2e2bd6db352ecbce1484e33cfebc83ac8d35
Jeremy Scheff <jdscheff@gmail.com>
2021-04-03 01:46:50 -0400

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
 		// This is assumed to be generated prior to rollup being started
 		"^league-schema$": "<rootDir>/build/files/league-schema.json",
 	},
+
 	setupFilesAfterEnv: ["./src/test/jest.ts", "./src/worker/index.ts"],
+
+	// https://stackoverflow.com/a/43197503/786644
+	transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es)"],
 };

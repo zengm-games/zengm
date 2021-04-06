9bb33ba7e5d665e9c2f09a7f959a8a60fe7d50bc
Jeremy Scheff <jdscheff@gmail.com>
2021-04-06 11:10:03 -0400

    Eastern time zone for version number, so it will be aligned with changelog dates

--- tools/lib/buildFuncs.js
+++ tools/lib/buildFuncs.js
@@ -204,15 +204,16 @@ const copyFiles = () => {
 };
 
 const genRev = () => {
-	const d = new Date();
-	const date = d.toISOString().split("T")[0].replace(/-/g, ".");
-	const minutes = String(d.getUTCMinutes() + 60 * d.getUTCHours()).padStart(
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const day = String(date.getDate()).padStart(2, "0");
+	const minutes = String(date.getMinutes() + 60 * date.getHours()).padStart(
 		4,
 		"0",
 	);
-	const rev = `${date}.${minutes}`;
 
-	return rev;
+	return `${year}.${month}.${day}.${minutes}`;
 };
 
 const reset = () => {

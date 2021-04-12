2f68381790e6786f80a97da5f532a9edeb080e1c
Jeremy Scheff <jdscheff@gmail.com>
2021-04-12 19:07:19 -0400

    babel-preset-env

--- babel.config.js
+++ babel.config.js
@@ -6,34 +6,6 @@ module.exports = api => {
 		JSON.stringify([process.env.LEGACY, process.env.NODE_ENV]),
 	);
 
-	const plugins = [
-		require("./tools/babel-plugin-sport-functions"),
-		[
-			"@babel/plugin-proposal-optional-chaining",
-			{
-				loose: true,
-			},
-		], // Chrome 80, Firefox 74, Safari 13.1
-		[
-			"@babel/plugin-proposal-nullish-coalescing-operator",
-			{
-				loose: true,
-			},
-		], // Chrome 80, Firefox 72, Safari 13.1
-	];
-
-	// For module/nomodule switch, cutoffs are Chrome 60, Firefox 54, and Safari 10.1 https://philipwalton.com/articles/deploying-es2015-code-in-production-today/ so technically this is broken for Firefox 55 and Safari 11. For Safari 10.1, a bug https://gist.github.com/samthor/64b114e4a4f539915a95b91ffd340acc results in both bundles being loaded, and the legacy one takes precedence. Maybe that helps in Firefox 55, which seems to work.
-	if (process.env.LEGACY) {
-		plugins.push(
-			"@babel/plugin-proposal-object-rest-spread", // Chrome 60, Firefox 55, Safari 11.1
-			"@babel/plugin-transform-for-of", // Chrome 51, Firefox 53
-			"@babel/plugin-transform-parameters", // Firefox 53
-			"@babel/plugin-transform-destructuring", // Chrome 51, Firefox 53
-			"@babel/plugin-transform-exponentiation-operator", // Chrome 52, Firefox 52, Safari 10.1
-			"@babel/plugin-transform-async-to-generator", // Chrome 55, Firefox 52, Safari 11
-		);
-	}
-
 	return {
 		presets: [
 			[
@@ -46,8 +18,25 @@ module.exports = api => {
 				},
 			],
 			"@babel/preset-typescript",
+			[
+				"@babel/preset-env",
+				{
+					bugfixes: true,
+					loose: true,
+					targets: process.env.LEGACY
+						? {
+								chrome: "49",
+								firefox: "47",
+								safari: "10",
+						  }
+						: {
+								esmodules: true,
+						  },
+					exclude: ["@babel/plugin-transform-regenerator"],
+				},
+			],
 		],
-		plugins,
+		plugins: [require("./tools/babel-plugin-sport-functions")],
 		env: {
 			test: {
 				plugins: ["@babel/plugin-transform-modules-commonjs"],

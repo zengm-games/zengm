084189df5ceee89066ef9b22f28107dc132af9ec
Jeremy Scheff <jdscheff@gmail.com>
2021-05-02 10:39:32 -0400

    New FBGM team ovr formula

--- src/worker/core/team/ovr.football.ts
+++ src/worker/core/team/ovr.football.ts
@@ -79,38 +79,38 @@ const ovr = (
 
 	// See analysis/team-ovr-football
 	const predictedMOV =
-		-141.16249800360956 +
-		0.27475987 * info.QB.ovrs[0] +
-		0.01937121 * info.RB.ovrs[0] +
-		0.00436415 * info.RB.ovrs[1] +
-		0.03105994 * info.TE.ovrs[0] +
-		0.00674677 * info.TE.ovrs[1] +
-		0.06098162 * info.WR.ovrs[0] +
-		0.03331908 * info.WR.ovrs[1] +
-		0.02548362 * info.WR.ovrs[2] +
-		0.01550788 * info.WR.ovrs[3] +
-		0.00100514 * info.WR.ovrs[4] +
-		0.19319358 * info.OL.ovrs[0] +
-		0.13220752 * info.OL.ovrs[1] +
-		0.10476351 * info.OL.ovrs[2] +
-		0.10256055 * info.OL.ovrs[3] +
-		0.08791494 * info.OL.ovrs[4] +
-		0.13368333 * info.CB.ovrs[0] +
-		0.08610441 * info.CB.ovrs[1] +
-		0.03589333 * info.CB.ovrs[2] +
-		0.10272653 * info.S.ovrs[0] +
-		0.05600678 * info.S.ovrs[1] +
-		0.04070437 * info.S.ovrs[2] +
-		0.07226837 * info.LB.ovrs[0] +
-		0.00066672 * info.LB.ovrs[1] +
-		0.00281938 * info.LB.ovrs[2] +
-		0.00002897 * info.LB.ovrs[3] +
-		0.23767406 * info.DL.ovrs[0] +
-		0.16659192 * info.DL.ovrs[1] +
-		0.13864289 * info.DL.ovrs[2] +
-		0.09932564 * info.DL.ovrs[3] +
-		0.08030877 * info.K.ovrs[0] +
-		0.05119918 * info.P.ovrs[0];
+		-97.2246364425006 +
+		0.14020132 * info.QB.ovrs[0] +
+		0.04154452 * info.RB.ovrs[0] +
+		0.00650348 * info.RB.ovrs[1] +
+		0.02776459 * info.TE.ovrs[0] +
+		0.005 * info.TE.ovrs[1] +
+		0.02381475 * info.WR.ovrs[0] +
+		0.01436188 * info.WR.ovrs[1] +
+		0.01380022 * info.WR.ovrs[2] +
+		0.005 * info.WR.ovrs[3] +
+		0.005 * info.WR.ovrs[4] +
+		0.1362113 * info.OL.ovrs[0] +
+		0.10290326 * info.OL.ovrs[1] +
+		0.07238786 * info.OL.ovrs[2] +
+		0.07662868 * info.OL.ovrs[3] +
+		0.08502353 * info.OL.ovrs[4] +
+		0.07704007 * info.CB.ovrs[0] +
+		0.06184627 * info.CB.ovrs[1] +
+		0.03215704 * info.CB.ovrs[2] +
+		0.04717957 * info.S.ovrs[0] +
+		0.03800769 * info.S.ovrs[1] +
+		0.00527162 * info.S.ovrs[2] +
+		0.05825392 * info.LB.ovrs[0] +
+		0.0242329 * info.LB.ovrs[1] +
+		0.00794022 * info.LB.ovrs[2] +
+		0.005 * info.LB.ovrs[3] +
+		0.17763777 * info.DL.ovrs[0] +
+		0.12435656 * info.DL.ovrs[1] +
+		0.09421874 * info.DL.ovrs[2] +
+		0.07085633 * info.DL.ovrs[3] +
+		0.04497254 * info.K.ovrs[0] +
+		0.0408595 * info.P.ovrs[0];
 
 	if (onlyPos) {
 		// In this case, we're ultimately using the value to compute a rank, so we don't care about the scale. And bounding the scale to be positive below makes it always 0.

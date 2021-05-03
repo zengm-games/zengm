0175610809c749e215078bea2065629048a0e21e
Jeremy Scheff <jdscheff@gmail.com>
2021-05-03 11:36:56 -0400

    Improve performance

--- src/worker/core/realRosters/addRelatives.ts
+++ src/worker/core/realRosters/addRelatives.ts
@@ -1,5 +1,8 @@
+import groupBy from "lodash-es/groupBy";
 import type { Relative } from "../../../common/types";
 import type { Basketball } from "./loadData.basketball";
+
+let allRelativesBySlug: Record<string, Basketball["relatives"]> | undefined;
 
 const addRelatives = (
 	players: {
@@ -10,12 +13,25 @@ const addRelatives = (
 	}[],
 	allRelatives: Basketball["relatives"],
 ) => {
+	if (!allRelativesBySlug) {
+		allRelativesBySlug = groupBy(allRelatives, "slug");
+	}
+
+	const playersBySlug: Record<string, typeof players[number] | undefined> = {};
 	for (const p of players) {
-		const relatives = allRelatives.filter(row => row.slug === p.srID);
+		playersBySlug[p.srID] = p;
+	}
+
+	console.time("foo");
+	for (const p of players) {
+		const relatives = allRelativesBySlug[p.srID];
+		if (!relatives) {
+			continue;
+		}
 
 		const relatives2 = [];
 		for (const relative of relatives) {
-			const p2 = players.find(p2 => relative.slug2 === p2.srID);
+			const p2 = playersBySlug[relative.slug2];
 			if (p2) {
 				relatives2.push({
 					type: relative.type,
@@ -29,6 +45,7 @@ const addRelatives = (
 			p.relatives = relatives2;
 		}
 	}
+	console.timeEnd("foo");
 };
 
 export default addRelatives;

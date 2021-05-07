63a5cfd250bc0e4a5b257253c50066d51c7e10e2
Jeremy Scheff <jdscheff@gmail.com>
2021-05-07 17:48:41 -0400

    Rescale football team ovr

--- src/worker/core/team/ovr.football.ts
+++ src/worker/core/team/ovr.football.ts
@@ -4,26 +4,26 @@ import ovrByPosFactory from "./ovrByPosFactory";
 
 // See analysis/team-ovr-football
 
-const intercept = -97.2246364425006;
+const intercept = -88.6122439420989;
 
 // minLength - number of players at this position who typically play in a game, barring injuries. These are the only players used when wholeRoster is false (normal power rankings).
 const weights: Record<PrimaryPosition, number[]> = {
-	QB: [0.14020132],
-	RB: [0.04154452],
-	TE: [0.02776459],
-	WR: [0.02381475, 0.01436188, 0.01380022],
-	OL: [0.1362113, 0.10290326, 0.07238786, 0.07662868, 0.08502353],
-	CB: [0.07704007, 0.06184627],
-	S: [0.04717957, 0.03800769],
-	LB: [0.05825392, 0.0242329],
-	DL: [0.17763777, 0.12435656, 0.09421874, 0.07085633],
-	K: [0.04497254],
-	P: [0.0408595],
+	QB: [0.14477738],
+	RB: [0.02352997],
+	TE: [0.02434117],
+	WR: [0.01847262, 0.0170727, 0.01084306],
+	OL: [0.12422164, 0.09414169, 0.06244824, 0.07886718, 0.08150448],
+	CB: [0.06431334, 0.06365693],
+	S: [0.04009449, 0.03528115],
+	LB: [0.05269825, 0.01306937],
+	DL: [0.16511873, 0.11970458, 0.089116, 0.07061544],
+	K: [0.03984891],
+	P: [0.02809455],
 };
 
 const scale = (predictedMOV: number) => {
-	// Translate from -15/15 to 0/100 scale
-	const rawOVR = (predictedMOV * 100) / 30 + 50;
+	// Translate from -10/10 to 0/100 scale
+	const rawOVR = (predictedMOV * 100) / 20 + 50;
 	return helpers.bound(Math.round(rawOVR), 0, Infinity);
 };
 

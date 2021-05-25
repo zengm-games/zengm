105ac25e4ae9a4a7dc1f04ac25eb5bd7b7a90a9e
Jeremy Scheff <jdscheff@gmail.com>
2021-05-25 15:08:36 -0400

    Changes

--- src/worker/util/changes.hockey.ts
+++ src/worker/util/changes.hockey.ts
@@ -1,6 +1,11 @@
 const changes: {
 	date: string;
 	msg: string;
-}[] = [];
+}[] = [
+	{
+		date: "2021-05-25",
+		msg: 'New "Auto ticket price" setting at Team > Finances, enabled by default in all leagues because it\'s very useful. <a href="https://zengm.com/blog/2021/05/auto-ticket-prices/">More details</a>',
+	},
+];
 
 export default changes;

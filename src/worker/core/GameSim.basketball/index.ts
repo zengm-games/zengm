411635f74a0acee49273f7ccca30e76d2512f187
nicidob <nicholasidobson@gmail.com>
2021-05-30 12:11:52 -0400

    just changing lets to consts in the matrix mul

--- src/worker/core/GameSim.basketball/index.ts
+++ src/worker/core/GameSim.basketball/index.ts
@@ -170,40 +170,40 @@ const linearTransform4D = (
 	point: number[],
 ): number[] => {
 	// Give a simple variable name to each part of the matrix, a column and row number
-	let c0r0 = matrix[0],
+	const c0r0 = matrix[0],
 		c1r0 = matrix[1],
 		c2r0 = matrix[2],
 		c3r0 = matrix[3];
-	let c0r1 = matrix[4],
+	const c0r1 = matrix[4],
 		c1r1 = matrix[5],
 		c2r1 = matrix[6],
 		c3r1 = matrix[7];
-	let c0r2 = matrix[8],
+	const c0r2 = matrix[8],
 		c1r2 = matrix[9],
 		c2r2 = matrix[10],
 		c3r2 = matrix[11];
-	let c0r3 = matrix[12],
+	const c0r3 = matrix[12],
 		c1r3 = matrix[13],
 		c2r3 = matrix[14],
 		c3r3 = matrix[15];
 
 	// Now set some simple names for the point
-	let x = point[0];
-	let y = point[1];
-	let z = point[2];
-	let w = point[3];
+	const x = point[0];
+	const y = point[1];
+	const z = point[2];
+	const w = point[3];
 
 	// Multiply the point against each part of the 1st column, then add together
-	let resultX = x * c0r0 + y * c0r1 + z * c0r2 + w * c0r3;
+	const resultX = x * c0r0 + y * c0r1 + z * c0r2 + w * c0r3;
 
 	// Multiply the point against each part of the 2nd column, then add together
-	let resultY = x * c1r0 + y * c1r1 + z * c1r2 + w * c1r3;
+	const resultY = x * c1r0 + y * c1r1 + z * c1r2 + w * c1r3;
 
 	// Multiply the point against each part of the 3rd column, then add together
-	let resultZ = x * c2r0 + y * c2r1 + z * c2r2 + w * c2r3;
+	const resultZ = x * c2r0 + y * c2r1 + z * c2r2 + w * c2r3;
 
 	// Multiply the point against each part of the 4th column, then add together
-	let resultW = x * c3r0 + y * c3r1 + z * c3r2 + w * c3r3;
+	const resultW = x * c3r0 + y * c3r1 + z * c3r2 + w * c3r3;
 
 	return [
 		resultX + bias[0],

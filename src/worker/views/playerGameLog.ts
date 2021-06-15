958d4126bf23152f5120d8fc16bb41f404107dea
Jeremy Scheff <jdscheff@gmail.com>
2021-06-15 02:13:31 -0400

    Start gathering game log info

--- src/worker/views/playerGameLog.ts
+++ src/worker/views/playerGameLog.ts
@@ -23,13 +23,55 @@ const updatePlayerGameLog = async (
 			return returnValue;
 		}
 
-		const games = await idb.getCopies.games({ season });
-
 		const seasons = Array.from(
-			new Set(topStuff.player.stats.filter(row => row.gp > 0)),
+			new Set(
+				topStuff.player.stats.filter(row => row.gp > 0).map(row => row.season),
+			),
 		);
 
-		console.log(games, seasons);
+		const games = await idb.getCopies.games({ season });
+
+		const gameLog = [];
+		for (const game of games) {
+			let row = game.teams[0].players.find(p => p.pid === pid);
+			let t0 = 0;
+			if (!row) {
+				row = game.teams[1].players.find(p => p.pid === pid);
+				t0 = 1;
+			}
+			if (!row) {
+				continue;
+			}
+
+			const t1 = t0 === 0 ? 1 : 0;
+
+			let result;
+			if (game.teams[t0].pts > game.teams[t1].pts) {
+				result = "W";
+			} else if (game.teams[t0].pts < game.teams[t1].pts) {
+				result = "L";
+			} else {
+				result = "T";
+			}
+
+			let overtimes = "";
+			if (game.overtimes !== undefined && game.overtimes > 0) {
+				if (game.overtimes === 1) {
+					overtimes = " OT";
+				} else if (game.overtimes > 1) {
+					overtimes = ` ${game.overtimes}OT`;
+				}
+			}
+
+			gameLog.push({
+				tid: game.teams[t0].tid,
+				oppTid: game.teams[t1].tid,
+				result,
+				score: `${game.teams[t0].pts}-${game.teams[t1].pts}${overtimes}`,
+			});
+		}
+
+		console.log(seasons, games, gameLog);
 
 		return {
 			...topStuff,

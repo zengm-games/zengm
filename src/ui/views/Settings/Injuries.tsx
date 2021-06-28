bfc15d673772c0bd4048b1686431b0c5302a73d6
Jeremy Scheff <jdscheff@gmail.com>
2021-06-27 22:00:02 -0400

    Modal

--- src/ui/views/Settings/Injuries.tsx
+++ src/ui/views/Settings/Injuries.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
 import { godModeRequiredMessage } from "./SettingsForm";
 
 const Injuries = ({
@@ -7,16 +9,39 @@ const Injuries = ({
 	disabled: boolean;
 	godModeRequired?: "always" | "existingLeagueOnly";
 }) => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	const title = disabled ? godModeRequiredMessage(godModeRequired) : undefined;
 	return (
-		<button
-			className="btn btn-secondary"
-			type="button"
-			disabled={disabled}
-			title={title}
-		>
-			Customize
-		</button>
+		<>
+			<button
+				className="btn btn-secondary"
+				type="button"
+				disabled={disabled}
+				title={title}
+				onClick={handleShow}
+			>
+				Customize
+			</button>
+
+			<Modal show={show} onHide={handleClose} size="lg" backdrop="static">
+				<Modal.Header closeButton>
+					<Modal.Title>Injury Types</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+				<Modal.Footer>
+					<button className="btn btn-secondary" onClick={handleClose}>
+						Cancel
+					</button>
+					<button className="btn btn-primary" onClick={handleClose}>
+						Save
+					</button>
+				</Modal.Footer>
+			</Modal>
+		</>
 	);
 };
 

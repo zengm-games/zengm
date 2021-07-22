9ca0541b4507b3b95db24068e18508a9d7625bbd
Jeremy Scheff <jdscheff@gmail.com>
2021-07-21 20:22:27 -0400

    Update rosters after 2021 playoffs

--- src/ui/views/NewLeague/index.tsx
+++ src/ui/views/NewLeague/index.tsx
@@ -917,9 +917,9 @@ const NewLeague = (props: View<"newLeague">) => {
 		invalidSeasonPhaseMessage =
 			"Starting after the playoffs is not yet supported for seasons where league mergers occurred.";
 	}
-	if (state.season === 2021 && state.phase > PHASE.PLAYOFFS) {
+	if (state.season === 2021 && state.phase > PHASE.DRAFT) {
 		invalidSeasonPhaseMessage =
-			"Sorry, I'm not allowed to share the results of the 2021 playoffs yet.";
+			"Sorry, I'm not allowed to share the results of the 2021 draft yet.";
 	}
 
 	const sortedDisplayedTeams = orderBy(displayedTeams, ["region", "name"]);

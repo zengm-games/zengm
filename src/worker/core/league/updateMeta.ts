083a45f1957578fa795e249d15779fa3b96d23e6
Jeremy Scheff <jdscheff@gmail.com>
2021-08-02 12:12:38 -0400

    More careful

--- src/worker/core/league/updateMeta.ts
+++ src/worker/core/league/updateMeta.ts
@@ -21,25 +21,30 @@ const updateMeta = async (
 			}
 		}
 
-		if (g.get("lid") !== undefined) {
-			// Upgrade
-			if (l.startingSeason === undefined) {
-				l.startingSeason = g.get("startingSeason");
-			}
+		// Hacky try/catch, but basically want to ignore any errors from a g variable not being set
+		try {
+			if (g.get("lid") !== undefined) {
+				// Upgrade
+				if (l.startingSeason === undefined) {
+					l.startingSeason = g.get("startingSeason");
+				}
 
-			// Just do this here, rather than figuring out when it should be updated exactly
-			const teamInfo = g.get("teamInfoCache")[g.get("userTid")];
-			if (teamInfo) {
-				l.imgURL = teamInfo.imgURLSmall ?? teamInfo.imgURL;
+				// Just do this here, rather than figuring out when it should be updated exactly
+				const teamInfo = g.get("teamInfoCache")[g.get("userTid")];
+				if (teamInfo) {
+					l.imgURL = teamInfo.imgURLSmall ?? teamInfo.imgURL;
 
-				if (g.get("userTids").length > 1) {
-					l.teamName = "Multi Team Mode";
-					l.teamRegion = "";
-				} else {
-					l.teamName = teamInfo.name;
-					l.teamRegion = teamInfo.region;
+					if (g.get("userTids").length > 1) {
+						l.teamName = "Multi Team Mode";
+						l.teamRegion = "";
+					} else {
+						l.teamName = teamInfo.name;
+						l.teamRegion = teamInfo.region;
+					}
 				}
 			}
+		} catch (error) {
+			console.error(error);
 		}
 
 		await transation.store.put(l);

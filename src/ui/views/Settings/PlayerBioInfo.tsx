ba56ff1113c3accfe788161b3c9289393ded7057
Jeremy Scheff <jdscheff@gmail.com>
2021-08-19 21:32:03 -0400

    Switch away from nested modal

--- src/ui/views/Settings/PlayerBioInfo.tsx
+++ src/ui/views/Settings/PlayerBioInfo.tsx
@@ -1,11 +1,10 @@
-import { csvFormat, csvParse } from "d3-dsv";
-import { m, AnimatePresence } from "framer-motion";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Dropdown, Modal } from "react-bootstrap";
 import type { PlayerBioInfo, Race, ThenArg } from "../../../common/types";
 import {
 	confirm,
 	downloadFile,
+	helpers,
 	logEvent,
 	resetFileInput,
 	toWorker,
@@ -18,10 +17,11 @@ import { getFrequencies, mergeCountries } from "../../../common/names";
 import isEqual from "lodash-es/isEqual";
 import orderBy from "lodash-es/orderBy";
 import { RacesEditor } from "./PlayerBioInfoEditors";
+import { CountriesEditor } from "./PlayerBioInfoCountries";
 
-type Defaults = ThenArg<ReturnType<typeof initDefaults>>;
+export type Defaults = ThenArg<ReturnType<typeof initDefaults>>;
 
-const objectToArray = <T extends string>(
+export const objectToArray = <T extends string>(
 	object: Record<string, number>,
 	key: T,
 	sortKey: string,
@@ -39,7 +39,7 @@ const objectToArray = <T extends string>(
 		order,
 	);
 
-const formatInfoState = (
+export const formatInfoState = (
 	playerBioInfo: PlayerBioInfo | undefined,
 	defaults: Defaults | undefined,
 ) => {
@@ -143,192 +143,7 @@ const formatInfoState = (
 	return orderBy(countries, "country");
 };
 
-type PlayerBioInfoState = ReturnType<typeof formatInfoState>;
-
-// https://stackoverflow.com/a/35200633/786644
-const ImportButton = ({
-	defaults,
-	setErrorMessage,
-	setInfoState,
-}: {
-	defaults: Defaults | undefined;
-	setErrorMessage: (errorMessage?: string) => void;
-	setInfoState: (injuries: PlayerBioInfoState) => void;
-}) => (
-	<button
-		className="btn btn-light-bordered"
-		style={{ position: "relative", overflow: "hidden" }}
-		onClick={() => {}}
-	>
-		Import
-		<input
-			className="cursor-pointer"
-			type="file"
-			style={{
-				position: "absolute",
-				top: 0,
-				right: 0,
-				minWidth: "100%",
-				minHeight: "100%",
-				fontSize: 100,
-				display: "block",
-				filter: "alpha(opacity=0)",
-				opacity: 0,
-				outline: "none",
-			}}
-			onClick={resetFileInput}
-			onChange={event => {
-				if (!event.target.files) {
-					return;
-				}
-				const file = event.target.files[0];
-				if (!file) {
-					return;
-				}
-
-				setErrorMessage();
-
-				const reader = new window.FileReader();
-				reader.readAsText(file);
-
-				reader.onload = async event2 => {
-					try {
-						// @ts-ignore
-						const rows = csvParse(event2.currentTarget.result);
-
-						if (
-							!rows.columns.includes("name") ||
-							!rows.columns.includes("frequency") ||
-							!rows.columns.includes("games")
-						) {
-							setErrorMessage(
-								"File should be a CSV file with columns: name, frequency, games",
-							);
-							return;
-						}
-
-						setInfoState(formatInfoState(rows as any), defaults);
-					} catch (error) {
-						setErrorMessage(error.message);
-						return;
-					}
-				};
-			}}
-		/>
-	</button>
-);
-
-const ExportButton = ({ infoState }: { infoState: PlayerBioInfoState }) => (
-	<button
-		className="btn btn-light-bordered"
-		onClick={() => {
-			const output = csvFormat(infoState, ["name", "frequency", "games"]);
-
-			downloadFile("injuries.csv", output, "text/csv");
-		}}
-	>
-		Export
-	</button>
-);
-
-const Controls = ({
-	defaults,
-	infoState,
-	position,
-	setInfoState,
-}: {
-	defaults: Defaults;
-	infoState: PlayerBioInfoState;
-	position: "top" | "bottom";
-	setInfoState: (
-		infoState:
-			| PlayerBioInfoState
-			| ((infoState: PlayerBioInfoState) => PlayerBioInfoState),
-	) => void;
-}) => {
-	const [importErrorMessage, setImportErrorMessage] = useState<
-		string | undefined
-	>();
-
-	return (
-		<>
-			<div className="d-flex justify-content-between">
-				<div className="btn-group">
-					<button
-						className="btn btn-light-bordered"
-						onClick={() => {
-							const newCountry: PlayerBioInfoState[number] = {
-								id: Math.random(),
-								country: "Country",
-								frequency: "1",
-
-								defaultRaces: false,
-								races: objectToArray(defaults.races.USA, "race", "race"),
-
-								defaultNames: false,
-								defaultNamesAllowed: false,
-								names: {
-									first: [],
-									last: [],
-								},
-							};
-							if (position === "top") {
-								setInfoState(rows => [newCountry, ...rows]);
-							} else {
-								setInfoState(rows => [...rows, newCountry]);
-							}
-						}}
-					>
-						Add
-					</button>
-					<Dropdown>
-						<Dropdown.Toggle
-							className="btn-light-bordered btn-light-bordered-group-right"
-							variant="foo"
-							id="dropdown-injuries-reset"
-						>
-							Reset
-						</Dropdown.Toggle>
-
-						<Dropdown.Menu>
-							<Dropdown.Item
-								onClick={async () => {
-									setInfoState(
-										formatInfoState(
-											await toWorker("main", "getDefaultInjuries"),
-											defaults,
-										),
-									);
-								}}
-							>
-								Default
-							</Dropdown.Item>
-							<Dropdown.Item
-								onClick={() => {
-									setInfoState([]);
-								}}
-							>
-								Clear
-							</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
-				</div>
-				<div className="btn-group">
-					<ImportButton
-						defaults={defaults}
-						setErrorMessage={setImportErrorMessage}
-						setInfoState={setInfoState}
-					/>
-					<ExportButton infoState={infoState} />
-				</div>
-			</div>
-
-			{importErrorMessage ? (
-				<div className="text-danger mt-3">{importErrorMessage}</div>
-			) : null}
-		</>
-	);
-};
+export type PlayerBioInfoState = ReturnType<typeof formatInfoState>;
 
 export const isInvalidNumber = (number: number) =>
 	Number.isNaN(number) || number <= 0;
@@ -363,6 +178,15 @@ const parseAndValidate = (
 	return injuries;
 };
 
+export type PageInfo =
+	| {
+			name: "countries";
+	  }
+	| {
+			name: "races";
+			index: number;
+	  };
+
 const PlayerBioInfo2 = ({
 	defaultValue,
 	disabled,
@@ -381,6 +205,9 @@ const PlayerBioInfo2 = ({
 	const [dirty, setDirty] = useState(false);
 	const lastSavedState = useRef<PlayerBioInfoState | undefined>();
 	const [defaults, setDefaults] = useState<Defaults | undefined>();
+	const [pageInfo, setPageInfo] = useState<PageInfo>({
+		name: "countries",
+	});
 
 	const setInfoState = (
 		infoState: PlayerBioInfoState | ((infoState: PlayerBioInfoState) => void),
@@ -490,12 +317,24 @@ const PlayerBioInfo2 = ({
 					};
 				}),
 			);
+
+			setPageInfo({
+				name: "countries",
+			});
 		};
 
 	const title = disabled ? godModeRequiredMessage(godModeRequired) : undefined;
 
 	let modal = null;
 	if (infoState && defaults) {
+		let title = "Player Bio Info";
+		if (pageInfo.name !== "countries") {
+			const selectedCountry = infoState[pageInfo.index];
+			title += ` - ${selectedCountry.country} - ${helpers.upperCaseFirstLetter(
+				pageInfo.name,
+			)}`;
+		}
+
 		modal = (
 			<Modal
 				size="lg"
@@ -505,127 +344,29 @@ const PlayerBioInfo2 = ({
 				scrollable
 			>
 				<Modal.Header closeButton>
-					<Modal.Title>Player Biographical Info</Modal.Title>
+					<Modal.Title>{title}</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>
-					<p>
-						The probability of a new player being from a certain country being
-						selected is its "frequency" value divided by the sum of all
-						frequencies. Names, colleges, and races work the same way.
-					</p>
-
-					<Controls
+				{pageInfo.name === "countries" ? (
+					<CountriesEditor
 						defaults={defaults}
-						position="top"
+						handleCancel={handleCancel}
+						handleChange={handleChange}
+						handleSave={handleSave}
 						infoState={infoState}
 						setInfoState={setInfoState}
+						setPageInfo={setPageInfo}
 					/>
-
-					{infoState.length > 0 ? (
-						<form onSubmit={handleSave} className="my-3">
-							<input type="submit" className="d-none" />
-							<div
-								className="form-row font-weight-bold"
-								style={{ marginRight: 22 }}
-							>
-								<div className="col-4">Name</div>
-								<div className="col-2">Frequency</div>
-								<div className="col-2">Names</div>
-								<div className="col-2">Colleges</div>
-								<div className="col-2">Races</div>
-							</div>
-							<AnimatePresence initial={false}>
-								{infoState.map((country, i) => (
-									<m.div
-										key={country.id}
-										initial={{ opacity: 0, y: -38 }}
-										animate={{ opacity: 1, y: 0 }}
-										exit={{}}
-										layout
-										transition={{ duration: 0.2, type: "tween" }}
-									>
-										<div className="d-flex">
-											<div className="form-row mt-2 flex-grow-1" key={i}>
-												<div className="col-4">
-													<input
-														type="text"
-														className="form-control"
-														value={country.country}
-														onChange={handleChange("name", i)}
-													/>
-												</div>
-												<div className="col-2">
-													<input
-														type="text"
-														className={classNames("form-control", {
-															"is-invalid": isInvalidNumber(
-																parseFloat(country.frequency),
-															),
-														})}
-														value={country.frequency}
-														onChange={handleChange("frequency", i)}
-													/>
-												</div>
-												<div className="col-2">AAA</div>
-												<div className="col-2">AAA</div>
-												<div className="col-2">
-													<RacesEditor
-														id={country.id}
-														country={country.country}
-														defaultSelected={country.defaultRaces}
-														races={country.races}
-														onSetDefault={() => {
-															console.log("setDefault");
-														}}
-														onSave={handleChange2("races", i)}
-													/>
-												</div>
-											</div>
-											<button
-												className="text-danger btn btn-link pl-2 pr-0 border-0"
-												onClick={() => {
-													setInfoState(rows =>
-														rows.filter(row => row !== country),
-													);
-												}}
-												style={{ fontSize: 20 }}
-												title="Delete"
-												type="button"
-											>
-												<span className="glyphicon glyphicon-remove" />
-											</button>
-										</div>
-									</m.div>
-								))}
-							</AnimatePresence>
-						</form>
-					) : (
-						<div className="mt-3 text-danger">
-							You must define at least one country.
-						</div>
-					)}
-
-					{infoState.length > 0 ? (
-						<Controls
-							defaults={defaults}
-							position="bottom"
-							infoState={infoState}
-							setInfoState={setInfoState}
-						/>
-					) : null}
-				</Modal.Body>
-				<Modal.Footer>
-					<button className="btn btn-secondary" onClick={handleCancel}>
-						Cancel
-					</button>
-					<button
-						className="btn btn-primary"
-						onClick={handleSave}
-						disabled={infoState.length === 0}
-					>
-						Save
-					</button>
-				</Modal.Footer>
+				) : pageInfo.name === "races" ? (
+					<RacesEditor
+						country={infoState[pageInfo.index]}
+						onCancel={() => {
+							setPageInfo({
+								name: "countries",
+							});
+						}}
+						onSave={handleChange2("races", pageInfo.index)}
+					/>
+				) : null}
 			</Modal>
 		);
 	}
---
+++ src/ui/views/Settings/PlayerBioInfoCountries.tsx
@@ -0,0 +1,370 @@
+import { csvFormat, csvParse } from "d3-dsv";
+import { m, AnimatePresence } from "framer-motion";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { Dropdown, Modal } from "react-bootstrap";
+import type { PlayerBioInfo, Race, ThenArg } from "../../../common/types";
+import {
+	confirm,
+	downloadFile,
+	logEvent,
+	resetFileInput,
+	toWorker,
+} from "../../util";
+import { godModeRequiredMessage } from "./SettingsForm";
+import classNames from "classnames";
+import {
+	Defaults,
+	formatInfoState,
+	isInvalidNumber,
+	objectToArray,
+	PageInfo,
+	PlayerBioInfoState,
+} from "./PlayerBioInfo";
+
+// https://stackoverflow.com/a/35200633/786644
+const ImportButton = ({
+	defaults,
+	setErrorMessage,
+	setInfoState,
+}: {
+	defaults: Defaults | undefined;
+	setErrorMessage: (errorMessage?: string) => void;
+	setInfoState: (injuries: PlayerBioInfoState) => void;
+}) => (
+	<button
+		className="btn btn-light-bordered"
+		style={{ position: "relative", overflow: "hidden" }}
+		onClick={() => {}}
+	>
+		Import
+		<input
+			className="cursor-pointer"
+			type="file"
+			style={{
+				position: "absolute",
+				top: 0,
+				right: 0,
+				minWidth: "100%",
+				minHeight: "100%",
+				fontSize: 100,
+				display: "block",
+				filter: "alpha(opacity=0)",
+				opacity: 0,
+				outline: "none",
+			}}
+			onClick={resetFileInput}
+			onChange={event => {
+				if (!event.target.files) {
+					return;
+				}
+				const file = event.target.files[0];
+				if (!file) {
+					return;
+				}
+
+				setErrorMessage();
+
+				const reader = new window.FileReader();
+				reader.readAsText(file);
+
+				reader.onload = async event2 => {
+					try {
+						// @ts-ignore
+						const rows = csvParse(event2.currentTarget.result);
+
+						if (
+							!rows.columns.includes("name") ||
+							!rows.columns.includes("frequency") ||
+							!rows.columns.includes("games")
+						) {
+							setErrorMessage(
+								"File should be a CSV file with columns: name, frequency, games",
+							);
+							return;
+						}
+
+						setInfoState(formatInfoState(rows as any, defaults));
+					} catch (error) {
+						setErrorMessage(error.message);
+						return;
+					}
+				};
+			}}
+		/>
+	</button>
+);
+
+const ExportButton = ({ infoState }: { infoState: PlayerBioInfoState }) => (
+	<button
+		className="btn btn-light-bordered"
+		onClick={() => {
+			const output = csvFormat(infoState, ["name", "frequency", "games"]);
+
+			downloadFile("injuries.csv", output, "text/csv");
+		}}
+	>
+		Export
+	</button>
+);
+
+type SetInfoState = (
+	infoState:
+		| PlayerBioInfoState
+		| ((infoState: PlayerBioInfoState) => PlayerBioInfoState),
+) => void;
+
+const Controls = ({
+	defaults,
+	infoState,
+	position,
+	setInfoState,
+}: {
+	defaults: Defaults;
+	infoState: PlayerBioInfoState;
+	position: "top" | "bottom";
+	setInfoState: SetInfoState;
+}) => {
+	const [importErrorMessage, setImportErrorMessage] = useState<
+		string | undefined
+	>();
+
+	return (
+		<>
+			<div className="d-flex justify-content-between">
+				<div className="btn-group">
+					<button
+						className="btn btn-light-bordered"
+						onClick={() => {
+							const newCountry: PlayerBioInfoState[number] = {
+								id: Math.random(),
+								country: "Country",
+								frequency: "1",
+
+								defaultRaces: false,
+								races: objectToArray(defaults.races.USA, "race", "race"),
+
+								defaultColleges: false,
+								colleges: [],
+
+								defaultNames: false,
+								defaultNamesAllowed: false,
+								names: {
+									first: [],
+									last: [],
+								},
+							};
+							if (position === "top") {
+								setInfoState(rows => [newCountry, ...rows]);
+							} else {
+								setInfoState(rows => [...rows, newCountry]);
+							}
+						}}
+					>
+						Add
+					</button>
+					<Dropdown>
+						<Dropdown.Toggle
+							className="btn-light-bordered btn-light-bordered-group-right"
+							variant="foo"
+							id="dropdown-injuries-reset"
+						>
+							Reset
+						</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							<Dropdown.Item
+								onClick={async () => {
+									setInfoState(
+										formatInfoState(
+											await toWorker("main", "getDefaultInjuries"),
+											defaults,
+										),
+									);
+								}}
+							>
+								Default
+							</Dropdown.Item>
+							<Dropdown.Item
+								onClick={() => {
+									setInfoState([]);
+								}}
+							>
+								Clear
+							</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
+				<div className="btn-group">
+					<ImportButton
+						defaults={defaults}
+						setErrorMessage={setImportErrorMessage}
+						setInfoState={setInfoState}
+					/>
+					<ExportButton infoState={infoState} />
+				</div>
+			</div>
+
+			{importErrorMessage ? (
+				<div className="text-danger mt-3">{importErrorMessage}</div>
+			) : null}
+		</>
+	);
+};
+
+export const CountriesEditor = ({
+	defaults,
+	handleCancel,
+	handleChange,
+	handleSave,
+	infoState,
+	setInfoState,
+	setPageInfo,
+}: {
+	defaults: Defaults;
+	handleCancel: any;
+	handleChange: any;
+	handleSave: any;
+	infoState: PlayerBioInfoState;
+	setInfoState: SetInfoState;
+	setPageInfo: (pageInfo: PageInfo) => void;
+}) => {
+	const onSetDefault = () => {};
+
+	return (
+		<>
+			<Modal.Body>
+				<p>
+					The probability of a new player being from a certain country being
+					selected is its "frequency" value divided by the sum of all
+					frequencies. Names, colleges, and races work the same way.
+				</p>
+
+				<Controls
+					defaults={defaults}
+					position="top"
+					infoState={infoState}
+					setInfoState={setInfoState}
+				/>
+
+				{infoState.length > 0 ? (
+					<form onSubmit={handleSave} className="my-3">
+						<input type="submit" className="d-none" />
+						<div
+							className="form-row font-weight-bold"
+							style={{ marginRight: 22 }}
+						>
+							<div className="col-4">Name</div>
+							<div className="col-2">Frequency</div>
+							<div className="col-2">Names</div>
+							<div className="col-2">Colleges</div>
+							<div className="col-2">Races</div>
+						</div>
+						<AnimatePresence initial={false}>
+							{infoState.map((country, i) => (
+								<m.div
+									key={country.id}
+									initial={{ opacity: 0, y: -38 }}
+									animate={{ opacity: 1, y: 0 }}
+									exit={{}}
+									layout
+									transition={{ duration: 0.2, type: "tween" }}
+								>
+									<div className="d-flex">
+										<div className="form-row mt-2 flex-grow-1" key={i}>
+											<div className="col-4">
+												<input
+													type="text"
+													className="form-control"
+													value={country.country}
+													onChange={handleChange("name", i)}
+												/>
+											</div>
+											<div className="col-2">
+												<input
+													type="text"
+													className={classNames("form-control", {
+														"is-invalid": isInvalidNumber(
+															parseFloat(country.frequency),
+														),
+													})}
+													value={country.frequency}
+													onChange={handleChange("frequency", i)}
+												/>
+											</div>
+											<div className="col-2">AAA</div>
+											<div className="col-2">AAA</div>
+											<div className="col-2">
+												<Dropdown>
+													<Dropdown.Toggle
+														variant="secondary"
+														id={`dropdown-race-${country.id}`}
+													>
+														{country.defaultRaces ? "Default" : "Custom"}
+													</Dropdown.Toggle>
+
+													<Dropdown.Menu>
+														<Dropdown.Item onClick={onSetDefault}>
+															Default
+														</Dropdown.Item>
+														<Dropdown.Item
+															onClick={() => {
+																setPageInfo({
+																	name: "races",
+																	index: i,
+																});
+															}}
+														>
+															Custom
+														</Dropdown.Item>
+													</Dropdown.Menu>
+												</Dropdown>
+											</div>
+										</div>
+										<button
+											className="text-danger btn btn-link pl-2 pr-0 border-0"
+											onClick={() => {
+												setInfoState(rows =>
+													rows.filter(row => row !== country),
+												);
+											}}
+											style={{ fontSize: 20 }}
+											title="Delete"
+											type="button"
+										>
+											<span className="glyphicon glyphicon-remove" />
+										</button>
+									</div>
+								</m.div>
+							))}
+						</AnimatePresence>
+					</form>
+				) : (
+					<div className="mt-3 text-danger">
+						You must define at least one country.
+					</div>
+				)}
+
+				{infoState.length > 0 ? (
+					<Controls
+						defaults={defaults}
+						position="bottom"
+						infoState={infoState}
+						setInfoState={setInfoState}
+					/>
+				) : null}
+			</Modal.Body>
+			<Modal.Footer>
+				<button className="btn btn-secondary" onClick={handleCancel}>
+					Cancel
+				</button>
+				<button
+					className="btn btn-primary"
+					onClick={handleSave}
+					disabled={infoState.length === 0}
+				>
+					Save
+				</button>
+			</Modal.Footer>
+		</>
+	);
+};

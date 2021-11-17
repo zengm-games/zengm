a037f0b446de2c32f84c6c5b718187b7a968ac54
Jeremy Scheff <jdscheff@gmail.com>
2021-11-17 13:23:08 -0500

    d3 -> visx

--- src/ui/views/Message/OwnerMoodsChart.tsx
+++ src/ui/views/Message/OwnerMoodsChart.tsx
@@ -1,12 +1,4 @@
 import PropTypes from "prop-types";
-import { useCallback, useEffect, useState } from "react";
-import { axisBottom } from "d3-axis";
-import {
-	scaleLinear as scaleLinearD3,
-	scalePoint as scalePointD3,
-} from "d3-scale";
-import { curveMonotoneX as curveMonotoneXD3, line } from "d3-shape";
-import { select } from "d3-selection";
 import { AxisBottom } from "@visx/axis";
 import { curveMonotoneX } from "@visx/curve";
 import { Group } from "@visx/group";
@@ -16,6 +8,7 @@ import { scaleLinear, scalePoint } from "@visx/scale";
 import { Text } from "@visx/text";
 import { HelpPopover } from "../../components";
 import type { OwnerMood } from "../../../common/types";
+import { Fragment } from "react";
 
 const ReferenceLine = ({
 	x,
@@ -89,102 +82,6 @@ const OwnerMoodsChart = ({
 		left: 15,
 	};
 
-	const [node, setNode] = useState<HTMLDivElement | null>(null);
-	const getNode = useCallback(node2 => {
-		if (node2 !== null) {
-			setNode(node2);
-		}
-	}, []);
-	useEffect(() => {
-		if (node) {
-			const width = node.clientWidth - margin.left - margin.right;
-			const xScaleD3 = scalePointD3().domain(years).range([0, width]);
-			const yScaleD3 = scaleLinearD3().domain(yDomain).range([HEIGHT, 0]) as (
-				y: number,
-			) => number;
-			const svg = select(node)
-				.append("svg")
-				.attr("width", width + margin.left + margin.right)
-				.attr("height", HEIGHT + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", `translate(${margin.left},${margin.top})`);
-
-			const drawReferenceLine = (
-				y: number,
-				color: string,
-				text?: string,
-				position?: string,
-			) => {
-				const line2 = line<number>()
-					.x(d => d)
-					.y(() => yScaleD3(y));
-				svg
-					.append("path")
-					.datum(xScaleD3.range())
-					.attr("class", "chart-line")
-					.style("stroke", color)
-					.style("stroke-dasharray", "5 5")
-					.attr("d", line2);
-
-				if (text) {
-					svg
-						.append("text")
-						.attr("y", yScaleD3(y) + (position === "above" ? -7 : 17))
-						.attr("x", width - 4)
-						.attr("text-anchor", "end")
-						.style("fill", color)
-						.text(text);
-				}
-			};
-
-			drawReferenceLine(3, "var(--success)", "Perfect", "above");
-			drawReferenceLine(-1, "var(--danger)", "You're fired!", "below");
-			drawReferenceLine(0, "var(--secondary)");
-
-			type Data = typeof data[number];
-			const drawLine = (
-				attr: Exclude<keyof Data, "year">,
-				color: string,
-				strokeWidth = 1,
-			) => {
-				const line2 = line<Data>()
-					.x(d => xScaleD3(d.year) as number)
-					.y(d => yScaleD3(d[attr]))
-					.curve(curveMonotoneXD3);
-
-				svg
-					.append("path")
-					.datum(data)
-					.attr("class", "chart-line")
-					.style("stroke", color)
-					.style("stroke-width", strokeWidth)
-					.attr("d", line2);
-
-				svg
-					.selectAll()
-					.data(data)
-					.enter()
-					.append("circle")
-					.attr("class", "chart-point")
-					.attr("stroke", color)
-					.style("stroke-width", strokeWidth)
-					.attr("cx", d => xScaleD3(d.year) as number)
-					.attr("cy", d => yScaleD3(d[attr]))
-					.attr("r", 3 * Math.sqrt(strokeWidth));
-			};
-
-			drawLine("wins", "var(--danger)");
-			drawLine("playoffs", "var(--info)");
-			drawLine("money", "var(--success)");
-			drawLine("total", "var(--dark)", 4);
-			svg
-				.append("g")
-				.attr("class", "chart-axis")
-				.attr("transform", `translate(0,${HEIGHT})`)
-				.call(axisBottom(xScaleD3));
-		}
-	});
-
 	const lineInfos: {
 		key: "wins" | "playoffs" | "money" | "total";
 		color: string;
@@ -240,12 +137,6 @@ const OwnerMoodsChart = ({
 				</p>
 				<p>The owner only starts judging you two years after you're hired.</p>
 			</HelpPopover>
-			<div
-				ref={getNode}
-				style={{
-					maxWidth: MAX_WIDTH,
-				}}
-			/>
 			<ParentSize
 				parentSizeStyles={{
 					maxWidth: MAX_WIDTH,
@@ -281,9 +172,17 @@ const OwnerMoodsChart = ({
 									y={yScale(0)}
 									color="var(--secondary)"
 								/>
-								{lineInfos.map(({ key, color, width = 1 }, i) => {
+								{lineInfos.map(({ key, color, width = 1 }) => {
 									return (
-										<Group key={`line-${key}`}>
+										<Fragment key={`line-${key}`}>
+											<LinePath
+												curve={curveMonotoneX}
+												data={data}
+												x={d => xScale(d.year) ?? 0}
+												y={d => yScale(d[key]) ?? 0}
+												stroke={color}
+												strokeWidth={width}
+											/>
 											{data.map((d, j) => (
 												<circle
 													key={j}
@@ -295,15 +194,7 @@ const OwnerMoodsChart = ({
 													strokeWidth={width}
 												/>
 											))}
-											<LinePath<typeof data[number]>
-												curve={curveMonotoneX}
-												data={data}
-												x={d => xScale(d.year) ?? 0}
-												y={d => yScale(d[key]) ?? 0}
-												stroke={color}
-												strokeWidth={width}
-											/>
-										</Group>
+										</Fragment>
 									);
 								})}
 								<AxisBottom

cc2db825d5f71f230de1578a1a29f2ea3a7202d6
Jeremy Scheff <jdscheff@gmail.com>
2021-11-30 21:35:59 -0500

    Isolate logic for updating column sort on click

--- src/ui/components/DataTable/index.tsx
+++ src/ui/components/DataTable/index.tsx
@@ -28,6 +28,7 @@ import type { SortOrder, SortType } from "../../../common/types";
 import type { Argument } from "classnames";
 import { arrayMoveImmutable } from "array-move";
 import type SettingsCache from "./SettingsCache";
+import updateSortBys from "./updateSortBys";
 
 export type SortBy = [number, SortOrder];
 
@@ -214,61 +215,12 @@ const DataTable = ({
 	};
 
 	const handleColClick = (event: MouseEvent, i: number) => {
-		const col = cols[i];
-
-		// Ignore click on unsortable column
-		if (col.sortSequence && col.sortSequence.length === 0) {
-			return;
-		}
-
-		let found = false;
-		let sortBys = helpers.deepCopy(state.sortBys); // eslint-disable-line react/no-access-state-in-setstate
-
-		const nextOrder = (col2: Col, sortBy: SortBy) => {
-			const sortSequence = col2.sortSequence;
-
-			if (sortSequence) {
-				// Move up to next entry in sortSequence
-				let j = sortSequence.indexOf(sortBy[1]) + 1;
-
-				if (j >= sortSequence.length) {
-					j = 0;
-				}
-
-				return sortSequence[j];
-			}
-
-			// Default asc/desc toggle
-			return sortBy[1] === "asc" ? "desc" : "asc";
-		};
-
-		// If this column is already in sortBys and shift is pressed, update
-		if (event.shiftKey) {
-			for (const sortBy of sortBys) {
-				if (sortBy[0] === i) {
-					sortBy[1] = nextOrder(col, sortBy);
-					found = true;
-					break;
-				}
-			}
-
-			// If this column is not in sortBys and shift is pressed, append
-			if (!found) {
-				sortBys.push([i, col.sortSequence ? col.sortSequence[0] : "asc"]);
-				found = true;
-			}
-		}
-
-		// If this column is the only one in sortBys, update order
-		if (!found && sortBys.length === 1 && sortBys[0][0] === i) {
-			sortBys[0][1] = nextOrder(col, sortBys[0]);
-			found = true;
-		}
-
-		// Otherwise, reset to sorting only by this column, default order
-		if (!found) {
-			sortBys = [[i, col.sortSequence ? col.sortSequence[0] : "asc"]];
-		}
+		const sortBys = updateSortBys({
+			cols,
+			event,
+			i,
+			prevSortBys: state.sortBys, // eslint-disable-line react/no-access-state-in-setstate
+		});
 
 		state.settingsCache.set("DataTableSort", sortBys);
 		setStatePartial({

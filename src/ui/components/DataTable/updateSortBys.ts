9a3944e97f33fb4b38ac71a265f5445067a7f240
Jeremy Scheff <jdscheff@gmail.com>
2021-12-01 23:46:03 -0500

    Fix error

--- src/ui/components/DataTable/updateSortBys.ts
+++ src/ui/components/DataTable/updateSortBys.ts
@@ -17,7 +17,7 @@ const updateSortBys = ({
 
 	// Ignore click on unsortable column
 	if (col.sortSequence && col.sortSequence.length === 0) {
-		return;
+		return prevSortBys;
 	}
 
 	let found = false;

e94bb92334dc6726508d5f935dfb3b0bc8deea60
Vac1911 <andrew@quasars.com>
2021-12-08 13:41:38 -0500

    bug fix

--- src/ui/util/columns/templates/Rating.tsx
+++ src/ui/util/columns/templates/Rating.tsx
@@ -1,7 +1,7 @@
 import type { Player } from "../../../../common/types";
 import type { ColTemp } from "../getCols";
 
-export default (p: Player, c: ColTemp) => {
+export default (p: Player, c: ColTemp, vars: object) => {
 	if (vars["challengeNoRatings"]) return "";
 	const key = c.ratings[0] ?? false;
 	return key && key in p.ratings ? p.ratings[key].toString() : "";

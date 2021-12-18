94312eb180e0ec5332875775ae3638474aa64884
Jeremy Scheff <jdscheff@gmail.com>
2021-12-18 18:16:50 -0500

    noCopyCache for getCopy.headToHeads

--- src/worker/db/getCopies/playerFeats.ts
+++ src/worker/db/getCopies/playerFeats.ts
@@ -1,12 +1,17 @@
 import { getAll, idb } from "..";
 import { mergeByPk } from "./helpers";
-import type { PlayerFeat } from "../../../common/types";
+import type { GetCopyType, PlayerFeat } from "../../../common/types";
 
-const getCopies = async (): Promise<PlayerFeat[]> => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getCopies = async (
+	options: any = {},
+	type?: GetCopyType,
+): Promise<PlayerFeat[]> => {
 	return mergeByPk(
 		await getAll(idb.league.transaction("playerFeats").store),
 		await idb.cache.playerFeats.getAll(),
 		"playerFeats",
+		type,
 	);
 };
 

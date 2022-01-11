08afe2c95a49937d00100afc25163adfc534959e
Jeremy Scheff <jdscheff@gmail.com>
2022-01-11 15:55:36 -0500

    Select menu of settings to override

--- src/ui/views/GlobalSettings/DefaultNewLeagueSettings.tsx
+++ src/ui/views/GlobalSettings/DefaultNewLeagueSettings.tsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import Select from "react-select";
 import { SPORT_HAS_REAL_PLAYERS } from "../../../common";
+import { groupBy } from "../../../common/groupBy";
+import { settings } from "../Settings/settings";
 
 const DefaultNewLeagueSettings = () => {
+	const [settingsShown, setSettingsShown] = useState<Key[]>([]);
+
+	const allSettings = settings.filter(
+		setting => !setting.hidden && !settingsShown.includes(setting.key),
+	);
+
+	const options = Object.entries(groupBy(allSettings, "category")).map(
+		([category, catSettings]) => ({
+			label: category,
+			options: catSettings.map(setting => ({
+				label: setting.name,
+				value: setting.key,
+			})),
+		}),
+	);
+	console.log(options);
+
 	return (
 		<>
 			<h2>Default New League Settings</h2>
@@ -16,6 +37,16 @@ const DefaultNewLeagueSettings = () => {
 					? " Also, real players leagues have some non-default settings already applied, and those will also not be altered by your specified defaults."
 					: null}
 			</p>
+
+			<Select
+				classNamePrefix="dark-select"
+				onChange={(newValue: any) => {
+					setSettingsShown(shown => [...shown, newValue.value]);
+				}}
+				options={options}
+				placeholder="Select a setting to supply a new default value for..."
+				value={null}
+			/>
 		</>
 	);
 };

ab535e8936d5d58905f217f73c307af559111d58
Jeremy Scheff <jdscheff@gmail.com>
2022-02-01 00:40:57 -0500

    Not just basketball!

--- src/worker/util/defaultTragicDeaths.ts
+++ src/worker/util/defaultTragicDeaths.ts
@@ -1,3 +1,5 @@
+import { GAME_NAME, isSport } from "../../common";
+
 const reasons = [
 	"SPECIAL_CLUE",
 	"SPECIAL_GIFTS",
@@ -29,10 +31,8 @@ const reasons = [
 	"PLAYER_NAME fell off the edge of the flat earth.",
 	"PLAYER_NAME died a normal death. Move on, find a new slant.",
 	"PLAYER_NAME was shot by police while shoplifting in China.",
-	"PLAYER_NAME fell to his death after slapping the backboard of a hoop inexplicably placed in front of a flimsy window on the 3rd floor of a building.",
 	"PLAYER_NAME died from an adult onset peanut allergy while eating his pre-game PB&J sandwich.",
 	"PLAYER_NAME fell into a wood chipper.",
-	"PLAYER_NAME died from a skull fracture after hitting his head on the rim while practicing for a dunk contest.",
 	"PLAYER_NAME was killed in swatting attack during a heated game of Fortnite.",
 	"PLAYER_NAME choked to death on a pretzel.",
 	"PLAYER_NAME was murdered by a time traveler so he would not become the world's evil overlord following his playing days.",
@@ -40,10 +40,17 @@ const reasons = [
 	"PLAYER_NAME died of dysentery.",
 	"PLAYER_NAME was strangled to death by a teammate for not knowing the score.",
 	"PLAYER_NAME died in a freak gasoline fight accident.",
-	"PLAYER_NAME was intensely focused on playing Basketball GM on his cell phone. As he walked across the street, he was so distracted by his ultimately fatal obsession that he didn't notice the bus barreling towards him.",
+	`PLAYER_NAME was intensely focused on playing ${GAME_NAME} on his cell phone. As he walked across the street, he was so distracted by his ultimately fatal obsession that he didn't notice the bus barreling towards him.`,
 	"PLAYER_NAME drowned while crossing the Saleph River.",
 	"PLAYER_NAME uploaded himself to the cloud and can no longer participate in corporeal pursuits.",
 	"PLAYER_NAME committed suicide by two shots to the back of his head after handcuffing and throwing himself into ocean inside duffle bag, CIA reports.",
 ];
+
+if (isSport("basketball")) {
+	reasons.push(
+		"PLAYER_NAME died from a skull fracture after hitting his head on the rim while practicing for a dunk contest.",
+		"PLAYER_NAME fell to his death after slapping the backboard of a hoop inexplicably placed in front of a flimsy window on the 3rd floor of a building.",
+	);
+}
 
 export default reasons.map(reason => ({ reason, frequency: 1 }));

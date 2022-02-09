e01c9a57b0f32bc4d3fc5133a0c41db520daf592
Jeremy Scheff <jdscheff@gmail.com>
2022-02-09 12:32:17 -0500

    More efficient - bail out of accessing/updating the DOM when we know it's noop

--- src/ui/components/DataTable/useStickyXX.ts
+++ src/ui/components/DataTable/useStickyXX.ts
@@ -5,55 +5,78 @@ import type { StickyCols } from ".";
 // >1 sticky column requires some JS to compute the left offset of all besides the first sticky column (whose offset is always 0)
 const useStickyXX = (stickyCols: StickyCols) => {
 	const tableRef = useRef<HTMLTableElement>(null);
-	const prevStickyCols = useRef(stickyCols);
-	console.log("useStickyXX", stickyCols);
+	const prevLefts = useRef<number[] | undefined>();
 
 	const updateStickyCols = useCallback(() => {
-		const getRows = () => {
-			const table = tableRef.current;
-			if (!table) {
-				return [];
-			}
+		const table = tableRef.current;
+		if (!table) {
+			return;
+		}
 
-			return Array.from(table.querySelectorAll<HTMLTableRowElement>("tr"));
+		// Get example row to be used to compute column widths
+		const getRow = () => {
+			let row = table.querySelector<HTMLTableRowElement>("tbody tr");
+			if (!row) {
+				// Would rather not look at thead, since superCols could be weird
+				row = table.querySelector<HTMLTableRowElement>("thead tr");
+			}
+			return row;
 		};
 
+		const getRows = () => {
+			return table.querySelectorAll<HTMLTableRowElement>("tr");
+		};
+
+		const lefts = [0];
+
 		if (stickyCols >= 2) {
-			const rows = getRows();
-
-			// Header/footer might have weird colspan, so try to use something before the footer
-			const row = rows.at(-2) ?? rows.at(-1);
-
+			const row = getRow();
 			if (!row) {
 				return;
 			}
 
 			// For each stickyCol beyond the first, need to figure out how much it needs to be offset by the fixed width of prior sticky cols
-			let left = 0;
 			for (let i = 1; i < stickyCols; i++) {
-				left += row.cells[i - 1].offsetWidth;
+				lefts[i] = lefts[i - 1] + row.cells[i - 1].offsetWidth;
+			}
 
-				const width = `${left}px`;
-				for (const row of rows) {
-					row.cells[i].style.left = width;
+			if (
+				prevLefts.current &&
+				lefts.length === prevLefts.current.length &&
+				prevLefts.current.every((left, i) => left === lefts[i])
+			) {
+				// If left offsets did not change, then no point in updating the DOM
+				return;
+			}
+
+			const rows = getRows();
+
+			const widths = lefts.map(left => `${left}px`);
+
+			for (const row of rows) {
+				for (let i = 1; i < widths.length; i++) {
+					row.cells[i].style.left = widths[i];
 				}
 			}
 		}
 
 		// When removing a sticky col that had a style manually applied above, reset that style
-		if (prevStickyCols.current >= 2 && prevStickyCols.current > stickyCols) {
-			const colsToReset = range(stickyCols, prevStickyCols.current);
+		if (prevLefts.current) {
+			const prevStickyCols = prevLefts.current.length;
+			if (prevStickyCols >= 2 && prevStickyCols > stickyCols) {
+				const colsToReset = range(stickyCols, prevStickyCols);
 
-			const rows = getRows();
-			for (const row of rows) {
-				for (const i of colsToReset) {
-					const cell = row.cells[i];
-					cell.style.left = "";
+				const rows = getRows();
+				for (const row of rows) {
+					for (const i of colsToReset) {
+						const cell = row.cells[i];
+						cell.style.left = "";
+					}
 				}
 			}
 		}
 
-		prevStickyCols.current = stickyCols;
+		prevLefts.current = lefts;
 	}, [stickyCols]);
 
 	useEffect(() => {

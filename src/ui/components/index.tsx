3365073f47fea78633850b57471b6721b1882423
Jeremy Scheff <jdscheff@gmail.com>
2022-02-20 20:08:23 -0500

    Remove old version of PlayerNameLabels

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -33,7 +33,6 @@ export { default as NextPrevButtons } from "./NextPrevButtons";
 export { default as NewWindowLink } from "./NewWindowLink";
 export { default as NewsBlock } from "./NewsBlock";
 export { default as PlayPauseNext } from "./PlayPauseNext";
-export { default as PlayerNameLabels } from "./PlayerNameLabels";
 export { default as PlayerNameLabels2 } from "./PlayerNameLabels2";
 export { default as PlayoffMatchup } from "./PlayoffMatchup";
 export { default as PopText } from "./PopText";

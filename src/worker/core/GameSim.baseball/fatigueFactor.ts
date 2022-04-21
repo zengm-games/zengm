a75ad4c7b0be72ed71103cddb5e021dad7b78d1e
Jeremy Scheff <jdscheff@gmail.com>
2022-04-20 22:54:36 -0400

    Tweak

--- src/worker/core/GameSim.baseball/fatigueFactor.ts
+++ src/worker/core/GameSim.baseball/fatigueFactor.ts
@@ -2,12 +2,12 @@ export const fatigueFactor = (pFatigue: number, endu: number): number => {
 	// 30 pitches before any fatigue starts to show
 	const adjustedFatigue = Math.max(0, pFatigue - 30);
 
-	// How fast a player's fatigue factor drops is based on endu. 20 at 0 endu means that, when starting a game with no fatigue, after 30 pitches (free above) fatigueFactor starts to decline, and then after 20 more pitches it's at 50% (the max fatigue). For 100 endu, the decline would happen over 100 more pitches, so from pitch 31 to 130.
-	const maxFatigueLimit = 20 + 0.8 * endu;
+	// How fast a player's fatigue factor drops is based on endu
+	const pitchesToDropTenPercent = 10 + 50 * endu;
 
-	const ratio = Math.min(1, adjustedFatigue / maxFatigueLimit);
+	const ratio = adjustedFatigue / pitchesToDropTenPercent;
 
-	const fatigueFactor = 1 - 0.5 * ratio;
+	const fatigueFactor = Math.max(0, 1 - 0.1 * ratio);
 
 	return fatigueFactor;
 };

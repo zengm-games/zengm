969314ef7f949f684c6c9fe496f72d6d2f3e9db1
Jeremy Scheff <jdscheff@gmail.com>
2022-05-03 13:48:43 -0400

    Handle injuries and players at multiple positions when setting starting lineup

--- src/worker/core/GameSim.baseball/Team.ts
+++ src/worker/core/GameSim.baseball/Team.ts
@@ -1,3 +1,4 @@
+import orderBy from "lodash-es/orderBy";
 import type { Position } from "../../../common/types.baseball";
 import { getStartingPitcher } from "./getStartingPitcher";
 import type { PlayerGameSim, TeamGameSim } from "./types";
@@ -44,40 +45,9 @@ class Team<DH extends boolean> {
 		this.playersInGame = {};
 
 		// Starting pitcher
-		const starter = getStartingPitcher(this.t.depth.P);
+		const startingPitcher = getStartingPitcher(this.t.depth.P);
 
-		// Starting position players
-		const lineup = this.dh ? this.t.depth.L : this.t.depth.LP;
-		const numPositionPlayers = this.dh
-			? NUM_BATTERS_PER_SIDE
-			: NUM_BATTERS_PER_SIDE - 1;
-		for (let i = 0; i < numPositionPlayers; i++) {
-			const p = lineup[i];
-			if (!p) {
-				throw new Error("Not enough players");
-			}
-
-			if (p.id === -1) {
-				this.playersInGame[starter.id] = {
-					p: starter,
-					battingOrder: i,
-					pos: "P",
-				};
-			} else {
-				this.playersInGame[p.id] = {
-					p,
-					battingOrder: i,
-					pos: p.lineupPos as any,
-				};
-			}
-		}
-		if (dh) {
-			this.playersInGame[starter.id] = {
-				p: starter,
-				battingOrder: -1,
-				pos: "P",
-			};
-		}
+		this.playersInGame = this.getStartingPlayersInGame(startingPitcher);
 
 		this.atBat = -1;
 		this.subIndex = -1;
@@ -91,6 +61,75 @@ class Team<DH extends boolean> {
 		this.playersInGameByPos = {} as any;
 		this.playersInGameByBattingOrder = [] as any;
 		this.rebuildIndexes();
+	}
+
+	getStartingPlayersInGame(startingPitcher: PlayerGameSim) {
+		const playersInGame: Team<DH>["playersInGame"] = {};
+
+		const lineup = this.dh ? this.t.depth.L : this.t.depth.LP;
+		const roster = this.dh ? this.t.depth.D : this.t.depth.DP;
+
+		// -1 if not found
+		const pitcherBattingOrder = lineup.findIndex(p => p.id === -1);
+
+		playersInGame[startingPitcher.id] = {
+			p: startingPitcher,
+			battingOrder: pitcherBattingOrder,
+			pos: "P",
+		};
+
+		const numPositionPlayers = this.dh
+			? NUM_BATTERS_PER_SIDE
+			: NUM_BATTERS_PER_SIDE - 1;
+		const bench = roster.slice(numPositionPlayers);
+		for (let i = 0; i < numPositionPlayers; i++) {
+			let p = lineup[i];
+			if (!p) {
+				throw new Error("Not enough players");
+			}
+
+			if (p.id === -1) {
+				// Pitcher was already handled above
+				continue;
+			}
+
+			const pos = p.lineupPos as GamePositions<DH>;
+
+			if (pos === "P") {
+				throw new Error("Should never happen");
+			}
+
+			if (playersInGame[p.id]) {
+				// Player is already in game at another position (maybe pitcher), pick someone off the bench
+				const sortedBench = orderBy(
+					bench,
+					[p => (p.injured ? 1 : 0), p => p.ovrs[pos]],
+					["asc", "desc"],
+				);
+				const p2 = sortedBench.find(p => !playersInGame[p.id]);
+				if (!p2) {
+					throw new Error("Not enough players");
+				} else {
+					p = p2;
+				}
+			}
+
+			if (p.id === -1) {
+				playersInGame[startingPitcher.id] = {
+					p: startingPitcher,
+					battingOrder: i,
+					pos: "P",
+				};
+			} else {
+				playersInGame[p.id] = {
+					p,
+					battingOrder: i,
+					pos,
+				};
+			}
+		}
+
+		return playersInGame;
 	}
 
 	rebuildIndexes() {

3ecb4961d2567c21bf245820fcb126a07cf9b5c1
Jeremy Scheff <jdscheff@gmail.com>
2022-05-25 15:17:22 -0400

    Manual tweaking of baseball team ovr

--- src/worker/core/team/ovr.baseball.ts
+++ src/worker/core/team/ovr.baseball.ts
@@ -3,21 +3,23 @@ import ovrByPosFactory from "./ovrByPosFactory";
 
 // See analysis/team-ovr-baseball
 
-const intercept = -9.269895772377641;
+const intercept = -4.4;
+
+const scale2 = 0.005;
 
 // minLength - number of players at this position who typically play in a game, barring injuries. These are the only players used when wholeRoster is false (normal power rankings).
 const weights: Record<Position, number[]> = {
-	SP: [1, 1, 1, 0.9, 0.8],
-	RP: [0.8, 0.8, 0.7, 0.7, 0.6],
-	C: [1],
-	"1B": [1],
-	"2B": [1],
-	"3B": [1],
-	SS: [1],
-	LF: [1],
-	CF: [1],
-	RF: [1],
-	DH: [1],
+	SP: [1 * scale2, 1 * scale2, 1 * scale2, 0.9 * scale2, 0.8 * scale2],
+	RP: [0.8 * scale2, 0.8 * scale2, 0.7 * scale2, 0.7 * scale2, 0.6 * scale2],
+	C: [1 * scale2],
+	"1B": [1 * scale2],
+	"2B": [1 * scale2],
+	"3B": [1 * scale2],
+	SS: [1 * scale2],
+	LF: [1 * scale2],
+	CF: [1 * scale2],
+	RF: [1 * scale2],
+	DH: [1 * scale2],
 };
 
 const scale = (predictedMOV: number) => {

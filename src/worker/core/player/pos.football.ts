39a43ba9c593fcf37c87bb061d98e6adce430262
Jeremy Scheff <jdscheff@gmail.com>
2022-06-14 20:30:19 -0400

    DRY

--- src/worker/core/player/pos.football.ts
+++ src/worker/core/player/pos.football.ts
@@ -1,9 +1,10 @@
 import ovr from "./ovr.football";
 import type { PlayerRatings } from "../../../common/types.football";
 import { POSITIONS } from "../../../common";
+import { BANNED_POSITIONS } from "./pos.baseball";
 
 const pos = (ratings: PlayerRatings): string => {
-	const positions = POSITIONS.filter(pos2 => pos2 !== "KR" && pos2 !== "PR");
+	const positions = POSITIONS.filter(pos2 => !BANNED_POSITIONS.includes(pos2));
 	const ovrs = positions.map(position => ovr(ratings, position));
 	let ind = 0;
 	let max = 0;

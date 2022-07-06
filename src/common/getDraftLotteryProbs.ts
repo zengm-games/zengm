dec8ab916d21df08a07d1cfe4eb1e4055d91c596
Jeremy Scheff <jdscheff@gmail.com>
2022-07-06 15:35:38 -0400

    More, get rid of topNCombos

--- src/common/getDraftLotteryProbs.ts
+++ src/common/getDraftLotteryProbs.ts
@@ -4,18 +4,37 @@ class ProbsCache {
 	// Key is the index of the current team followed by the indexes of teams winning the prior picks (could be any number of picks from 0 to numPicksInLottery, all get stored here), value is probability of that happening
 	probs: Record<string, number>;
 
-	constructor() {
+	// Same as probs, but we don't care about order of key ([1,5] is same as [5,1], and probs get added up) and we only care about fully specified lotteries (keys length equal to numPicksInLottery)
+	probsMerged: Record<string, number>;
+
+	numPicksInLottery: number;
+
+	constructor(numPicksInLottery: number) {
+		this.numPicksInLottery = numPicksInLottery;
 		this.probs = {};
+		this.probsMerged = {};
 	}
 
 	set(keys: number[], value: number) {
 		const key = JSON.stringify(keys);
 		this.probs[key] = value;
+
+		if (keys.length === this.numPicksInLottery) {
+			const keyMerged = JSON.stringify(keys.sort());
+			if (this.probsMerged[keyMerged] === undefined) {
+				this.probsMerged[keyMerged] = 0;
+			}
+			this.probsMerged[keyMerged] += value;
+		}
 	}
 
 	get(keys: number[]) {
 		const key = JSON.stringify(keys);
 		return this.probs[key];
+	}
+
+	mergedEntries() {
+		return Object.entries(this.probsMerged);
 	}
 }
 
@@ -36,7 +55,6 @@ const getDraftLotteryProbs = (
 	}
 
 	const probs: number[][] = [];
-	const topNCombos = new Map();
 	const totalChances = result.reduce(
 		(total, { chances }) => total + chances,
 		0,
@@ -76,12 +94,12 @@ const getDraftLotteryProbs = (
 	if (draftType === "nba2019") {
 		numPicksInLottery = 4;
 	} else if (draftType === "mlb2022") {
-		numPicksInLottery = 6;
+		numPicksInLottery = 4;
 	} else {
 		numPicksInLottery = 3;
 	}
 
-	const probsCache = new ProbsCache();
+	const probsCache = new ProbsCache(numPicksInLottery);
 
 	// Get probabilities of top N picks for all teams
 	for (let i = 0; i < result.length; i++) {
@@ -126,12 +144,24 @@ const getDraftLotteryProbs = (
 				if (l === i || l === k) {
 					continue;
 				}
-				const combosTemp =
+				const prob =
 					(probsCache.get([l, k]) * result[i].chances) /
 					(totalChances - result[k].chances - result[l].chances);
-				probs[i][2] += combosTemp;
-				probsCache.set([i, l, k], combosTemp);
+				probs[i][2] += prob;
+				probsCache.set([i, l, k], prob);
+			}
+		}
+	}
 
+	for (let i = 0; i < result.length; i++) {
+		for (let k = 0; k < result.length; k++) {
+			if (k === i) {
+				continue;
+			}
+			for (let l = 0; l < result.length; l++) {
+				if (l === i || l === k) {
+					continue;
+				}
 				if (numPicksInLottery > 3) {
 					// Go one level deeper
 					// m: team that got 3st pick
@@ -140,37 +170,14 @@ const getDraftLotteryProbs = (
 							continue;
 						}
 
-						const combosTemp2 =
-							(probsCache.get([l, k]) *
-								(result[m].chances /
-									(totalChances - result[k].chances - result[l].chances)) *
-								result[i].chances) /
+						const prob =
+							(probsCache.get([m, l, k]) * result[i].chances) /
 							(totalChances -
 								result[k].chances -
 								result[l].chances -
 								result[m].chances);
-						probs[i][3] += combosTemp2;
-						const topFourKey = JSON.stringify([i, k, l, m].sort());
-
-						if (!topNCombos.has(topFourKey)) {
-							topNCombos.set(topFourKey, combosTemp2);
-						} else {
-							topNCombos.set(
-								topFourKey,
-								topNCombos.get(topFourKey) + combosTemp2,
-							);
-						}
-					}
-				} else {
-					const topThreeKey = JSON.stringify([i, k, l].sort());
-
-					if (!topNCombos.has(topThreeKey)) {
-						topNCombos.set(topThreeKey, combosTemp);
-					} else {
-						topNCombos.set(
-							topThreeKey,
-							topNCombos.get(topThreeKey) + combosTemp,
-						);
+						probs[i][3] += prob;
+						probsCache.set([i, m, l, k], prob);
 					}
 				}
 			}
@@ -182,7 +189,7 @@ const getDraftLotteryProbs = (
 		// Probabilities of being "skipped" (lower prob team in top N) i times. +1 is for when skipped 0 times, in addition to being skipped possibly up to numPicksInLottery times
 		const skipped = Array(numPicksInLottery + 1).fill(0);
 
-		for (const [key, prob] of topNCombos.entries()) {
+		for (const [key, prob] of probsCache.mergedEntries()) {
 			const inds = JSON.parse(key);
 			let skipCount = 0;
 

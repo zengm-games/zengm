1136e5d22f92dc6320203a44200588249ef5f446
Jeremy Scheff <jdscheff@gmail.com>
2022-07-14 14:38:45 -0400

    Blob.prototype.stream polyfill is no longer needed in modern build

--- src/common/polyfills.ts
+++ src/common/polyfills.ts
@@ -95,4 +95,36 @@ if (self.CountQueuingStrategy === undefined) {
 	self.CountQueuingStrategy = PolyfillCountQueuingStrategy;
 }
 
+// Chrome 76, Safari 14.1
+// Based on https://stackoverflow.com/a/65087341/786644
+if (!Blob.prototype.stream) {
+	(Blob as any).prototype.stream = function () {
+		let offset = 0;
+		const chunkSize = 64 * 1024;
+
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		const blob = this;
+
+		return new ReadableStream({
+			pull(controller) {
+				return new Promise(resolve => {
+					if (offset < blob.size) {
+						const blobChunk = blob.slice(offset, offset + chunkSize);
+						const reader = new FileReader();
+						reader.onload = event => {
+							controller.enqueue((event.currentTarget as any).result);
+							offset += chunkSize;
+							if (offset >= blob.size) {
+								controller.close();
+							}
+							resolve();
+						};
+						reader.readAsArrayBuffer(blobChunk);
+					}
+				});
+			},
+		});
+	};
+}
+
 import "./polyfills-modern";

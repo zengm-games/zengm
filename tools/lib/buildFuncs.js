552fb2beec59ce30e817358921310f253ff4527c
Jeremy Scheff <jdscheff@gmail.com>
2022-09-13 10:10:05 -0400

    Remove Facebook Pixel

--- tools/lib/buildFuncs.js
+++ tools/lib/buildFuncs.js
@@ -539,63 +539,6 @@ if (window.enableLogging) {
 		silent: true,
 	});
 
-	const sport = getSport();
-
-	const quantcastCode = "";
-	/*if (!watch && sport === "basketball") {
-		quantcastCode = `<script type="text/javascript">
-if (window.enableLogging) {
-var _qevents = _qevents || [];(function() {
-var elem = document.createElement('script');
-elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-elem.async = true;
-elem.type = "text/javascript";
-var scpt = document.getElementsByTagName('script')[0];
-scpt.parentNode.insertBefore(elem, scpt);
-})();_qevents.push({
-qacct:"p-Ye5RY6xC03ZWz"
-});
-}
-</script><noscript>
-<div style="display:none;">
-<img src="//pixel.quantserve.com/pixel/p-Ye5RY6xC03ZWz.gif" border="0" height="1" width="1" alt="Quantcast"/>
-</div>
-</noscript>`;
-	}*/
-	replace({
-		regex: "QUANTCAST_CODE",
-		replacement: quantcastCode,
-		paths: ["build/index.html"],
-		silent: true,
-	});
-
-	let facebookPixelCode = "";
-	if (!watch && sport === "basketball") {
-		facebookPixelCode = `<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1285618145138713');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1285618145138713&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->`;
-	}
-	replace({
-		regex: "FACEBOOK_PIXEL_CODE",
-		replacement: facebookPixelCode,
-		paths: ["build/index.html"],
-		silent: true,
-	});
-
 	return rev;
 };
 

c5ceadae43692d369c22b6d2d78275aaac03d396
Jeremy Scheff <jdscheff@gmail.com>
2022-10-27 13:54:20 -0400

    Sack counts as tackle

--- src/worker/core/GameSim.football/Play.ts
+++ src/worker/core/GameSim.football/Play.ts
@@ -438,6 +438,7 @@ class Play {
 				statChanges.push([state.o, event.qb, "pssSk"]);
 				statChanges.push([state.o, event.qb, "pssSkYds", Math.abs(event.yds)]);
 				statChanges.push([state.d, event.p, "defSk"]);
+				statChanges.push([state.d, event.p, "defTckSolo"]);
 			} else if (event.type === "pss") {
 				statChanges.push([state.o, event.qb, "pss"]);
 				statChanges.push([state.o, event.target, "tgt"]);

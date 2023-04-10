281669bdd34e9af6c689054b65ef1a7ed1539b86
Jeremy Scheff <jdscheff@gmail.com>
2023-04-10 15:12:21 -0400

    Start updating for 2023 playoffs

--- src/ui/views/NewLeague/index.tsx
+++ src/ui/views/NewLeague/index.tsx
@@ -140,7 +140,7 @@ const initKeptKeys = ({
 
 export const MIN_SEASON = 1947;
 export const MAX_SEASON = 2023;
-const MAX_PHASE = 0;
+const MAX_PHASE = PHASE.PLAYOFFS;
 
 const seasons: { key: string; value: string }[] = [];
 for (let i = MAX_SEASON; i >= MIN_SEASON; i--) {

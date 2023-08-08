cf2152f5b6d6dc183cda8796facf94f8b4202f9d
Jeremy Scheff <jdscheff@gmail.com>
2023-08-08 12:31:33 -0400

    Update Quantcast Choice CMP

--- tools/lib/buildFuncs.js
+++ tools/lib/buildFuncs.js
@@ -329,7 +329,7 @@ const setTimestamps = (rev /*: string*/, watch /*: boolean*/ = false) => {
   var element = document.createElement('script');
   var firstScript = document.getElementsByTagName('script')[0];
   var url = 'https://cmp.quantcast.com'
-    .concat('/choice/', 'M1Q1fpfqa7Vk4', '/', host, '/choice.js');
+    .concat('/choice/', 'M1Q1fpfqa7Vk4', '/', host, '/choice.js?tag_version=V2');
   var uspTries = 0;
   var uspTriesLimit = 3;
   element.async = true;
@@ -391,7 +391,7 @@ const setTimestamps = (rev /*: string*/, watch /*: boolean*/ = false) => {
         }
       } else {
         if(args[0] === 'init' && typeof args[3] === 'object') {
-          args[3] = { ...args[3], tag_version: 'V2' };
+          args[3] = Object.assign(args[3], { tag_version: 'V2' });
         }
         queue.push(args);
       }

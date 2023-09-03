98337dc65586764e65c431125096101ce711e8c7
Jeremy Scheff <jdscheff@gmail.com>
2023-09-03 16:57:42 -0400

    No redirect after vote

--- src/worker/views/autoExpand.ts
+++ src/worker/views/autoExpand.ts
@@ -3,8 +3,8 @@ import getTeamInfos from "../../common/getTeamInfos";
 import type { UpdateEvents } from "../../common/types";
 
 const updateExpand = async (inputs: void, updateEvents: UpdateEvents) => {
-	// Ignore team updateEvent from relocateVote
-	if (!updateEvents.includes("team")) {
+	// Ignore team updateEvent from relocateVote, and newPhase from starting the expansion draft
+	if (!updateEvents.includes("team") && !updateEvents.includes("newPhase")) {
 		const autoExpand = g.get("autoExpand");
 		if (!autoExpand) {
 			// https://stackoverflow.com/a/59923262/786644

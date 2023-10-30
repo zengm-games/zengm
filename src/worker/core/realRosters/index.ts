457ef5b2d9a799c539df407e867d4fafbacca44a
Jeremy Scheff <jdscheff@gmail.com>
2023-10-30 00:53:13 -0400

    Expose loadData

--- src/worker/core/realRosters/index.ts
+++ src/worker/core/realRosters/index.ts
@@ -1,9 +1,11 @@
 import getLeague from "./getLeague";
 import getLeagueInfo from "./getLeagueInfo";
+import loadData from "./loadData.basketball";
 import updateRandomDebutsForever from "./updateRandomDebutsForever";
 
 export default {
 	getLeague,
 	getLeagueInfo,
+	loadData,
 	updateRandomDebutsForever,
 };

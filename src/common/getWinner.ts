3d40c1e8365f42c304c21e68bee72abc9a9a4da5
Jeremy Scheff <jdscheff@gmail.com>
2024-04-08 10:41:09 -0400

    Notes

--- src/common/getWinner.ts
+++ src/common/getWinner.ts
@@ -3,7 +3,9 @@ type Team = {
 	sPts?: number;
 };
 
-// 0 means home (0th) team won, 1 means away (1th) team won, -1 means tie, undefined means game is not over yet
+// 0 means home (0th) team won, 1 means away (1th) team won, -1 means tie, undefined means game is not over yet.
+// Would be nice if TypeScript new that undefined will only be returned if pts is undefined, but we're passing `any` to here often, and idk how to handle that too (override would work fine for distinguishing number/undefined).
+// Would also be nice if TypeScript also that pts and sPts are either both undefined or neither.
 const getWinner = (teams: [Team, Team]): 1 | 0 | -1 | undefined => {
 	if (teams[0].pts === undefined || teams[1].pts === undefined) {
 		return;

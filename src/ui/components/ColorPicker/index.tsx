87e2c6dcbdfee1c2f6ad8fdac455bcf1cd06c2b8
Jeremy Scheff <jdscheff@gmail.com>
2024-05-10 20:05:35 -0400

    Add eye dropper in Chrome

--- src/ui/components/ColorPicker/index.tsx
+++ src/ui/components/ColorPicker/index.tsx
@@ -1,6 +1,6 @@
 import { useState, type CSSProperties } from "react";
 import { OverlayTrigger, Popover } from "react-bootstrap";
-import Sketch from "@uiw/react-color-sketch";
+import { Sketch } from "./Sketch";
 
 export const ColorPicker = ({
 	onClick,
@@ -15,26 +15,6 @@ export const ColorPicker = ({
 }) => {
 	const [hex, setHex] = useState(value);
 
-	// https://alumni.media.mit.edu/~wad/color/palette.html since the defaults in @uiw/react-color-sketch are only 15 colors, leaving a blank spot
-	const PRESET_COLORS = [
-		"#AD2323",
-		"#FF9233",
-		"#FFEE33",
-		"#E9DEBB",
-		"#814A19",
-		"#2A4BD7",
-		"#9DAFFF",
-		"#29D0D0",
-		"#1D6914",
-		"#81C57A",
-		"#8126C0",
-		"#FFCDF3",
-		"#000000",
-		"#575757",
-		"#A0A0A0",
-		"#FFFFFF",
-	];
-
 	return (
 		<OverlayTrigger
 			trigger="click"
@@ -47,8 +27,6 @@ export const ColorPicker = ({
 							setHex(color.hex);
 							onChange(color.hex);
 						}}
-						disableAlpha
-						presetColors={PRESET_COLORS}
 					/>
 				</Popover>
 			}

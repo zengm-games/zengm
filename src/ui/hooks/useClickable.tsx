92512aef84925db0394295fe63295a01199678a4
Jeremy Scheff <jdscheff@gmail.com>
2025-03-11 15:28:19 -0400

    Better check for button/link inside row - if there is a span or other nested element inside, look up the tree

--- src/ui/hooks/useClickable.tsx
+++ src/ui/hooks/useClickable.tsx
@@ -1,17 +1,15 @@
 import { useCallback, useState } from "react";
 
+const IGNORED_ELEMENTS = new Set(["A", "BUTTON", "INPUT", "SELECT"]);
+
 const useClickable = () => {
 	const [clicked, setClicked] = useState(false);
 
 	const toggleClicked = useCallback((event: any) => {
 		// Purposely using event.target instead of event.currentTarget because we do want check what internal element was clicked on, not the row itself
-		// Don't toggle the row if a link was clicked.
-		const ignoredElements = ["A", "BUTTON", "INPUT", "SELECT"];
 
-		if (
-			event.target.nodeName &&
-			ignoredElements.includes(event.target.nodeName)
-		) {
+		// Don't toggle the row if a link was clicked.
+		if (event.target.nodeName && IGNORED_ELEMENTS.has(event.target.nodeName)) {
 			return;
 		}
 
@@ -22,6 +20,23 @@ const useClickable = () => {
 		// This handles modals, where for some reason an event is triggered for any click on or outside the modal, even though the modal is not a child of the actual clickable element (event.currentTarget)
 		if (!event.currentTarget.contains(event.target)) {
 			return;
+		}
+
+		// Search up tree a bit, in case there was like a span inside a button or something
+		let currentElement = event.target as HTMLElement | null | undefined;
+		for (let i = 0; i < 5; i++) {
+			currentElement = currentElement?.parentElement;
+			if (!currentElement) {
+				break;
+			}
+
+			if (currentElement.tagName === "TD" || currentElement.tagName === "TH") {
+				break;
+			}
+
+			if (IGNORED_ELEMENTS.has(currentElement.tagName)) {
+				return;
+			}
 		}
 
 		setClicked((prevClicked) => !prevClicked);

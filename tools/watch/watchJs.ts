600e4c83858836dca11d8a272097981cc65676cb
Jeremy Scheff <jdscheff@gmail.com>
2025-04-10 21:40:15 -0400

    Temporarily switch to running JS watch build in the main thread, for debugging

--- tools/watch/watchJs.ts
+++ tools/watch/watchJs.ts
@@ -1,4 +1,46 @@
-import { Worker } from "node:worker_threads";
+import type { Spinners } from "./spinners.ts";
+import { watch } from "rolldown";
+import { rolldownConfig } from "../lib/rolldownConfig.ts";
+
+export const watchJs = async (
+	updateStart: (filename: string) => void,
+	updateEnd: (filename: string) => void,
+	updateError: (filename: string, error: Error) => void,
+	eventEmitter: Spinners["eventEmitter"],
+) => {
+	for (const name of ["ui", "worker"] as const) {
+		const filename = `build/gen/${name}.js`;
+
+		const makeWatcher = async () => {
+			const config = rolldownConfig(name, {
+				nodeEnv: "development",
+				postMessage(message: any) {
+					if (message.type === "start") {
+						updateStart(filename);
+					}
+					if (message.type === "end") {
+						updateEnd(filename);
+					}
+					if (message.type === "error") {
+						updateError(filename, message.error);
+					}
+				},
+			});
+
+			const watcher = await watch(config);
+			return watcher;
+		};
+
+		let watcher = await makeWatcher();
+
+		eventEmitter.on("newSport", async () => {
+			await watcher.close();
+			//watcher = await makeWatcher();
+		});
+	}
+};
+
+/*import { Worker } from "node:worker_threads";
 import type { Spinners } from "./spinners.ts";
 
 export const watchJs = (
@@ -32,6 +74,6 @@ export const watchJs = (
 			worker.postMessage({ type: "newSport" });
 		});
 	}
-};
+};*/
 
 // watchJs((filename) => console.log('updateStart', filename), (filename) => console.log('updateEnd', filename), (filename, error) => console.log('updateError', filename, error));

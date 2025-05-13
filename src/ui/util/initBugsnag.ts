91d6adbea57ed1745af09f67ef84462e538a6ae8
Jeremy Scheff <jdscheff@gmail.com>
2025-05-13 15:07:48 -0400

    Hopefully filter out ad errors

--- src/ui/util/initBugsnag.ts
+++ src/ui/util/initBugsnag.ts
@@ -9,6 +9,14 @@ Bugsnag.start({
 		// Normalize league URLs to all look the same
 		if (event && typeof event.context === "string") {
 			event.context = event.context.replace(/^\/l\/\d+/, "/l/0");
+
+			// All the errors in my application code include "src/" in the filename of some source code file in the stack trace. So hopefully this will filter out all the errors from ads.
+			const isInApplicationCode = event.errors.some((error) =>
+				error.stacktrace.some((row) => row.file.includes("src/")),
+			);
+			if (!isInApplicationCode) {
+				return false;
+			}
 		}
 	},
 	enabledReleaseStages: ["beta", "production"],

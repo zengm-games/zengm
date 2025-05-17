e48d51343bfceacf76ca715a5c2dbe17f77e4f0e
Jeremy Scheff <jdscheff@gmail.com>
2025-05-17 14:43:41 -0400

    Maybe fix bug tracking

--- src/ui/util/initBugsnag.ts
+++ src/ui/util/initBugsnag.ts
@@ -10,9 +10,9 @@ Bugsnag.start({
 		if (event && typeof event.context === "string") {
 			event.context = event.context.replace(/^\/l\/\d+/, "/l/0");
 
-			// All the errors in my application code include "src/" in the filename of some source code file in the stack trace. So hopefully this will filter out all the errors from ads.
+			// All the errors in my application code include "/gen/" in the filename of some source code file in the stack trace. So hopefully this will filter out all the errors from ads.
 			const isInApplicationCode = event.errors.some((error) =>
-				error.stacktrace.some((row) => row.file.includes("src/")),
+				error.stacktrace.some((row) => row.file.includes("/gen/")),
 			);
 			if (!isInApplicationCode) {
 				return false;

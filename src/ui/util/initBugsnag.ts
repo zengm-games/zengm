0845b66bfe5282d82f59502773f67069748a21ed
Jeremy Scheff <jdscheff@gmail.com>
2025-05-17 16:34:17 -0400

    Clarify

--- src/ui/util/initBugsnag.ts
+++ src/ui/util/initBugsnag.ts
@@ -10,7 +10,7 @@ Bugsnag.start({
 		if (event && typeof event.context === "string") {
 			event.context = event.context.replace(/^\/l\/\d+/, "/l/0");
 
-			// All the errors in my application code include "/gen/" in the filename of some source code file in the stack trace. So hopefully this will filter out all the errors from ads.
+			// All the errors in my application code include "/gen/" in the filename of some source code file in the stack trace. Stack traces fully on another domain are already filtered out automatically, but this still helps with some. But not all, since some will have part of the stack trace in my code (especially since Bugsnag is in my code, sometimes that gets in the stack trace).
 			const isInApplicationCode = event.errors.some((error) =>
 				error.stacktrace.some((row) => row.file.includes("/gen/")),
 			);

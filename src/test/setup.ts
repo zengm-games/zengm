ac011abbea0cd1875f06d8e886f06aa9390a4168
Jeremy Scheff <jdscheff@gmail.com>
2025-09-29 22:45:26 -0400

    globalThis

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,12 +2,12 @@ import { IDBKeyRange } from "fake-indexeddb";
 import fs from "node:fs/promises";
 
 // When mockIDBLeague is used, sometimes IDBKeyRange still gets called even though there is no actual database
-global.IDBKeyRange = IDBKeyRange;
+globalThis.IDBKeyRange = IDBKeyRange;
 
 // Hack because promise-worker-bi 2.2.1 always sends back hostID, but the worker tests don't run in an actual worker, so
 // self.postMessage causes an error because it requires a different number of arguments inside and outside of a worker.
-const originalPostMessage = global.postMessage;
-global.postMessage = (...args) => {
+const originalPostMessage = globalThis.postMessage;
+globalThis.postMessage = (...args) => {
 	if (Array.isArray(args[0]) && JSON.stringify(args[0]) === "[2,-1,0]") {
 		// Skip hostID message
 	} else {
@@ -17,7 +17,7 @@ global.postMessage = (...args) => {
 };
 
 const fetchCache: Record<string, any> = {};
-(global as any).fetch = async (url: string) => {
+(globalThis as any).fetch = async (url: string) => {
 	if (!Object.hasOwn(fetchCache, url)) {
 		let filePath = url.replace("/gen/", "data/");
 
@@ -34,6 +34,6 @@ const fetchCache: Record<string, any> = {};
 };
 
 // Removes the need for jsdom in most test files
-(global as any).self = global;
-(global as any).window = global;
-global.addEventListener = () => {};
+(globalThis as any).self = globalThis;
+(globalThis as any).window = globalThis;
+globalThis.addEventListener = () => {};

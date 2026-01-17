abe5d2821d88b7f85eb9adc11dc0b3022469da64
Jeremy Scheff <jdscheff@gmail.com>
2026-01-16 21:29:25 -0500

    Remove rollup support

--- tools/build/buildJsWorker.ts
+++ tools/build/buildJsWorker.ts
@@ -1,6 +1,4 @@
 import { build } from "rolldown";
-import { rollup } from "rollup";
-import rollupConfig from "../lib/rollupConfig.ts";
 import { parentPort, workerData } from "node:worker_threads";
 import { rolldownConfig } from "../lib/rolldownConfig.ts";
 
@@ -18,39 +16,12 @@ const BLACKLIST = {
 };
 
 const buildFile = async (name: "ui" | "worker", versionNumber: string) => {
-	if (process.env.BUNDLER === "rollup") {
-		if (name === "ui") {
-			console.log("Bundler: rollup");
-		}
-		const config = rollupConfig(name, {
-			nodeEnv: "production",
-			blacklistOptions: BLACKLIST[name],
-		});
-		const bundle = await rollup(config);
-
-		await bundle.write({
-			compact: true,
-			format: "es",
-			indent: false,
-			sourcemap: true,
-			externalLiveBindings: false,
-			entryFileNames: `[name]-${versionNumber}.js`,
-			chunkFileNames: `chunk-[hash].js`,
-			dir: "build/gen",
-		});
-
-		await bundle.close();
-	} else {
-		if (name === "ui") {
-			console.log("Bundler: rolldown");
-		}
-		const config = rolldownConfig(name, {
-			nodeEnv: "production",
-			blacklistOptions: BLACKLIST[name],
-			versionNumber,
-		});
-		await build(config);
-	}
+	const config = rolldownConfig(name, {
+		nodeEnv: "production",
+		blacklistOptions: BLACKLIST[name],
+		versionNumber,
+	});
+	await build(config);
 
 	parentPort!.postMessage("done");
 };

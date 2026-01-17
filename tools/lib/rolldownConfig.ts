32f98f01295fbd52e4de8160488bc98c222397bb
Jeremy Scheff <jdscheff@gmail.com>
2026-01-17 09:19:34 -0500

    Refactor

--- tools/lib/rolldownConfig.ts
+++ tools/lib/rolldownConfig.ts
@@ -1,80 +1,12 @@
 import fs from "node:fs/promises";
 import { stripVTControlCharacters } from "node:util";
-import babel from "@babel/core";
-import type { BuildOptions, RolldownPlugin, TransformResult } from "rolldown";
-import { and, code, include, moduleType, or } from "@rolldown/pluginutils";
-// @ts-expect-error
-import babelPluginSyntaxTypescript from "@babel/plugin-syntax-typescript";
-import { babelPluginSportFunctions } from "../babel-plugin-sport-functions/index.ts";
-import { getSport, type Sport } from "./getSport.ts";
+import type { BuildOptions } from "rolldown";
+import { getSport } from "./getSport.ts";
 // @ts-expect-error
 import blacklist from "rollup-plugin-blacklist";
 import terser from "@rollup/plugin-terser";
 import { visualizer } from "rollup-plugin-visualizer";
-
-// Use babel to run babel-plugin-sport-functions. This is needed even in dev mode because the way bySport is defined, the sport-specific code will run if it's present, which can produce errors. It's not actually needed for isSport in dev mode.
-export const pluginSportFunctions = (
-	nodeEnv: "development" | "production" | "test",
-	sport: Sport,
-): RolldownPlugin => {
-	const babelCache: Record<
-		string,
-		{
-			mtimeMs: number;
-			result: TransformResult;
-		}
-	> = {};
-
-	return {
-		name: "sport-functions",
-		transform: {
-			filter: [
-				include(
-					and(
-						or(moduleType("ts"), moduleType("tsx")),
-						or(code("bySport"), code("isSport")),
-					),
-				),
-			],
-			async handler(code, id, { moduleType }) {
-				let mtimeMs;
-				if (nodeEnv === "development") {
-					mtimeMs = (await fs.stat(id)).mtimeMs;
-					const cached = babelCache[id];
-					if (cached?.mtimeMs === mtimeMs) {
-						return cached.result;
-					}
-				}
-
-				const isTSX = moduleType === "tsx";
-
-				const babelResult = await babel.transformAsync(code, {
-					babelrc: false,
-					configFile: false,
-					sourceMaps: true,
-					plugins: [
-						[babelPluginSyntaxTypescript, { isTSX }],
-						[babelPluginSportFunctions, { sport }],
-					],
-				});
-
-				const result = {
-					code: babelResult!.code!,
-					map: babelResult!.map,
-				};
-
-				if (nodeEnv === "development") {
-					babelCache[id] = {
-						mtimeMs: mtimeMs!,
-						result,
-					};
-				}
-
-				return result;
-			},
-		},
-	};
-};
+import { sportFunctions } from "./rolldownPlugins/sportFunctions.ts";
 
 export const rolldownConfig = (
 	name: "ui" | "worker",
@@ -98,7 +30,7 @@ export const rolldownConfig = (
 	const sport = getSport();
 
 	const plugins: BuildOptions["plugins"] = [
-		pluginSportFunctions(envOptions.nodeEnv, sport),
+		sportFunctions(envOptions.nodeEnv, sport),
 	];
 
 	if (envOptions.nodeEnv === "development") {
---
+++ tools/lib/rolldownPlugins/sportFunctions.ts
@@ -0,0 +1,72 @@
+import fs from "node:fs/promises";
+import babel from "@babel/core";
+// @ts-expect-error
+import babelPluginSyntaxTypescript from "@babel/plugin-syntax-typescript";
+import { and, code, include, moduleType, or } from "@rolldown/pluginutils";
+import type { RolldownPlugin, TransformResult } from "rolldown";
+import { babelPluginSportFunctions } from "../../babel-plugin-sport-functions/index.ts";
+import type { Sport } from "../getSport.ts";
+
+// Use babel to run babel-plugin-sport-functions. This is needed even in dev mode because the way bySport is defined, the sport-specific code will run if it's present, which can produce errors. It's not actually needed for isSport in dev mode.
+export const sportFunctions = (
+	nodeEnv: "development" | "production" | "test",
+	sport: Sport,
+): RolldownPlugin => {
+	const babelCache: Record<
+		string,
+		{
+			mtimeMs: number;
+			result: TransformResult;
+		}
+	> = {};
+
+	return {
+		name: "sport-functions",
+		transform: {
+			filter: [
+				include(
+					and(
+						or(moduleType("ts"), moduleType("tsx")),
+						or(code("bySport"), code("isSport")),
+					),
+				),
+			],
+			async handler(code, id, { moduleType }) {
+				let mtimeMs;
+				if (nodeEnv === "development") {
+					mtimeMs = (await fs.stat(id)).mtimeMs;
+					const cached = babelCache[id];
+					if (cached?.mtimeMs === mtimeMs) {
+						return cached.result;
+					}
+				}
+
+				const isTSX = moduleType === "tsx";
+
+				const babelResult = await babel.transformAsync(code, {
+					babelrc: false,
+					configFile: false,
+					sourceMaps: true,
+					plugins: [
+						[babelPluginSyntaxTypescript, { isTSX }],
+						[babelPluginSportFunctions, { sport }],
+					],
+				});
+
+				const result = {
+					code: babelResult!.code!,
+					map: babelResult!.map,
+				};
+
+				if (nodeEnv === "development") {
+					babelCache[id] = {
+						mtimeMs: mtimeMs!,
+						result,
+					};
+				}
+
+				return result;
+			},
+		},
+	};
+};

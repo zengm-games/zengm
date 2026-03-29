6ab36c90a4bc75cc7c8a96706ddf671140f8e892
Jeremy Scheff <jdscheff@gmail.com>
2026-03-29 13:46:57 -0400

    As minimal as possible reproduction of error

--- tools/watch/watchJs.ts
+++ tools/watch/watchJs.ts
@@ -1,41 +1,12 @@
 import { Worker } from "node:worker_threads";
-import type { Spinners } from "./spinners.ts";
 
-export const watchJs = (
-	updateStart: (filename: string) => void,
-	updateEnd: (filename: string) => void,
-	updateError: (filename: string, error: Error) => void,
-	eventEmitter: Spinners["eventEmitter"],
-) => {
-	for (const name of ["ui", "worker"]) {
-		const filename = `build/gen/${name}.js`;
-
-		const worker = new Worker(new URL("watchJsWorker.ts", import.meta.url), {
-			workerData: {
-				name,
-			},
-		});
-
-		worker.on("message", (message) => {
-			if (message.type === "start") {
-				updateStart(filename);
-			}
-			if (message.type === "end") {
-				updateEnd(filename);
-			}
-			if (message.type === "error") {
-				updateError(filename, message.error);
-			}
-		});
-
-		eventEmitter.on("switchingSport", async () => {
-			worker.postMessage({ type: "switchingSport" });
-		});
-
-		eventEmitter.on("newSport", (sport) => {
-			worker.postMessage({ type: "newSport", sport });
-		});
-	}
+const exitHandler = () => {
+	process.exit(130);
 };
+process.once("SIGINT", exitHandler);
 
-// watchJs((filename) => console.log('updateStart', filename), (filename) => console.log('updateEnd', filename), (filename, error) => console.log('updateError', filename, error));
+const worker = new Worker(new URL("watchJsWorker.ts", import.meta.url));
+
+worker.on("message", (message) => {
+	console.log(message);
+});
--- tools/watch/watchJsWorker.ts
+++ tools/watch/watchJsWorker.ts
@@ -1,71 +1,25 @@
-import { parentPort, workerData } from "node:worker_threads";
+import fs from "node:fs/promises";
+import { parentPort } from "node:worker_threads";
 import { watch } from "rolldown";
-import { rolldownConfig } from "../lib/rolldownConfig.ts";
+import { sportFunctions } from "../lib/rolldownPlugins/sportFunctions.ts";
 
-// ?? is just so this can be run as a standalone script, for testing
-const name = workerData?.name ?? "worker";
-
-const makeWatcher = () => {
-	const abortController = new AbortController();
-	const { signal } = abortController;
-
-	const config = rolldownConfig(name, {
-		nodeEnv: "development",
-		postMessage(message) {
-			parentPort?.postMessage(message);
+await watch({
+	input: "src/ui/index.tsx",
+	output: {
+		entryFileNames: "ui.js",
+		dir: "build/gen",
+	},
+	plugins: [
+		sportFunctions("development", "basketball"),
+		{
+			name: "start-end",
+			buildStart() {
+				parentPort!.postMessage("Press ctrl+c now to see panic");
+			},
+			writeBundle() {
+				parentPort!.postMessage("Too late!");
+			},
 		},
-		signal,
-	});
-
-	// Do this async so the abortController can be returned synchronously and therefore can be aborted before the watcher is done being created
-	(async () => {
-		const watcher = await watch(config);
-
-		if (signal.aborted) {
-			await watcher.close();
-			return;
-		}
-
-		signal.addEventListener("abort", async () => {
-			await watcher.close();
-		});
-
-		watcher.on("event", (event) => {
-			if (signal.aborted) {
-				return;
-			}
-
-			if (event.code === "ERROR") {
-				// In case "start" wasn't set from rolldown plugin yet
-				parentPort?.postMessage({
-					type: "start",
-				});
-
-				parentPort?.postMessage({
-					type: "error",
-					error: event.error,
-				});
-
-				// https://rollupjs.org/javascript-api/#rollup-watch and https://rolldown.rs/reference/Function.watch say to do this
-				event.result.close();
-			} else if (event.code === "BUNDLE_END") {
-				// https://rollupjs.org/javascript-api/#rollup-watch and https://rolldown.rs/reference/Function.watch say to do this
-				event.result.close();
-			}
-		});
-	})();
-
-	return abortController;
-};
-
-let abortController = makeWatcher();
-
-parentPort?.on("message", async (message) => {
-	if (message.type === "switchingSport") {
-		abortController.abort();
-	} else if (message.type === "newSport") {
-		process.env.SPORT = message.sport;
-		abortController.abort(); // Maybe not necessary
-		abortController = makeWatcher();
-	}
+	],
+	external() {},
 });

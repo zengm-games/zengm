426fab6d7030aea2f0d6c0576845070784ae28be
Jeremy Scheff <jdscheff@gmail.com>
2026-04-01 12:58:21 -0400

    More

--- src/worker/core/phase/newPhaseFreeAgency.ts
+++ src/worker/core/phase/newPhaseFreeAgency.ts
@@ -1,7 +1,9 @@
-import { contractNegotiation, freeAgents } from "../index.ts";
+import { contractNegotiation, freeAgents, player } from "../index.ts";
 import { helpers } from "../../util/index.ts";
 import type { PhaseReturn } from "../../../common/types.ts";
 import { idb } from "../../db/index.ts";
+import { PLAYER } from "../../../common/constants.ts";
+import { getNumPlayersTradedAwayNormalizedAll } from "../player/getNumPlayersTradedAwayNormalized.ts";
 
 const newPhaseFreeAgency = async (): Promise<PhaseReturn> => {
 	// In case some weird situation results in games still in the schedule, clear them
@@ -13,6 +15,20 @@ const newPhaseFreeAgency = async (): Promise<PhaseReturn> => {
 	await freeAgents.ensureEnoughPlayers();
 
 	await freeAgents.normalizeContractDemands({ type: "freeAgentsOnly" });
+
+	// For any players who were free agents last year too, reset numPlayersTradedAwayNormalized
+	const numPlayersTradedAwayNormalized =
+		await getNumPlayersTradedAwayNormalizedAll();
+	const players = await idb.cache.players.indexGetAll(
+		"playersByTid",
+		PLAYER.FREE_AGENT,
+	);
+	for (const p of players) {
+		if (p.yearsFreeAgent > 0) {
+			player.addToFreeAgents(p, numPlayersTradedAwayNormalized);
+			await idb.cache.players.put(p);
+		}
+	}
 
 	return {
 		redirect: {

129b99a23ebdf7ea2b3f2ef1b577019aee9cc0cf
Jeremy Scheff <jdscheff@gmail.com>
2026-05-07 11:09:29 -0400

    More

--- src/common/simpleGameAttributesUpgrade.ts
+++ src/common/simpleGameAttributesUpgrade.ts
@@ -2,18 +2,38 @@ import { isSport } from "./sportFunctions.ts";
 import type {
 	GameAttributeWithHistory,
 	GameAttributesLeagueWithHistory,
+	PlayerContract,
+	PlayerInjury,
 } from "./types.ts";
+
+type ObsoleteGameAttributes = {
+	aiTrades?: boolean;
+	autoDeleteOldBoxScores?: boolean;
+	disableInjuries?: boolean;
+	hardCap?: unknown;
+	ties?: boolean | GameAttributeWithHistory<boolean>;
+	repeatSeason?: {
+		type?: "playersAndRosters";
+		startingSeason: number;
+		players: Record<
+			number,
+			{
+				tid: number;
+				contract: PlayerContract;
+				injury: PlayerInjury;
+			}
+		>;
+	};
+};
 
 // Ideally all upgrade stuff would be here, because this also gets called before showing the setting screen when importing a league... maybe some day!
 const simpleGameAttributesUpgrade = (
-	gameAttributes: GameAttributesLeagueWithHistory,
+	gameAttributes: GameAttributesLeagueWithHistory & ObsoleteGameAttributes,
 	version: number | undefined,
 ) => {
-	if ((gameAttributes as any).hardCap !== undefined) {
-		gameAttributes.salaryCapType = (gameAttributes as any).hardCap
-			? "hard"
-			: "soft";
-		delete (gameAttributes as any).hardCap;
+	if (gameAttributes.hardCap !== undefined) {
+		gameAttributes.salaryCapType = gameAttributes.hardCap ? "hard" : "soft";
+		delete gameAttributes.hardCap;
 	}
 
 	if (!isSport("basketball") && (version === undefined || version <= 51)) {
@@ -29,13 +49,10 @@ const simpleGameAttributesUpgrade = (
 	}
 
 	if (gameAttributes.repeatSeason && !gameAttributes.repeatSeason.type) {
-		(gameAttributes.repeatSeason as any).type = "playersAndRosters";
+		gameAttributes.repeatSeason.type = "playersAndRosters";
 	}
 
-	const ties = (gameAttributes as any).ties as
-		| boolean
-		| GameAttributeWithHistory<boolean>
-		| undefined;
+	const ties = gameAttributes.ties;
 	if (ties !== undefined) {
 		let maxOvertimes: GameAttributesLeagueWithHistory["maxOvertimes"];
 
@@ -47,6 +64,7 @@ const simpleGameAttributesUpgrade = (
 				},
 			];
 		} else {
+			// Type cast because it doesn't know NonEmptyArray is preserved in Map
 			maxOvertimes = ties.map((row) => {
 				return {
 					start: row.start,
@@ -57,11 +75,10 @@ const simpleGameAttributesUpgrade = (
 
 		gameAttributes.maxOvertimes = maxOvertimes;
 
-		delete (gameAttributes as any).ties;
+		delete gameAttributes.ties;
 	}
 
-	const autoDeleteOldBoxScores = (gameAttributes as any)
-		.autoDeleteOldBoxScores as boolean | undefined;
+	const autoDeleteOldBoxScores = gameAttributes.autoDeleteOldBoxScores;
 	if (autoDeleteOldBoxScores !== undefined) {
 		// If autoDeleteOldBoxScores was true, just let the new default apply. Only override if it was false
 		if (autoDeleteOldBoxScores === false) {
@@ -71,21 +88,21 @@ const simpleGameAttributesUpgrade = (
 			};
 		}
 
-		delete (gameAttributes as any).autoDeleteOldBoxScores;
+		delete gameAttributes.autoDeleteOldBoxScores;
 	}
 
-	const disableInjuries = (gameAttributes as any).disableInjuries;
+	const disableInjuries = gameAttributes.disableInjuries;
 	if (disableInjuries !== undefined) {
-		delete (gameAttributes as any).disableInjuries;
+		delete gameAttributes.disableInjuries;
 
 		if (disableInjuries) {
 			gameAttributes.injuryRate = 0;
 		}
 	}
 
-	const aiTrades = (gameAttributes as any).aiTrades;
+	const aiTrades = gameAttributes.aiTrades;
 	if (aiTrades !== undefined) {
-		delete (gameAttributes as any).aiTrades;
+		delete gameAttributes.aiTrades;
 
 		if (aiTrades === false && gameAttributes.aiTradesFactor === 1) {
 			gameAttributes.aiTradesFactor = 0;

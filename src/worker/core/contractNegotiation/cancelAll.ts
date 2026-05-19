bce288fb95a5708aaaabc939e47fee9b0544fc1d
Jeremy Scheff <jdscheff@gmail.com>
2026-05-19 11:30:51 -0400

    Contract negotiations never affect status/playMenu any more, so no need to reset on cancelling. This fixes newPhaseResigning resetting the "Processing..." status

--- src/worker/core/contractNegotiation/cancelAll.ts
+++ src/worker/core/contractNegotiation/cancelAll.ts
@@ -1,18 +1,12 @@
 import { idb } from "../../db/index.ts";
-import { updatePlayMenu, updateStatus } from "../../util/index.ts";
 
 /**
  * Cancel all ongoing contract negotiations.
  *
- * Currently, the only time there should be multiple ongoing negotiations in the first place is when a user is re-signing players at the end of the season, although that should probably change eventually.
- *
- * @memberOf core.contractNegotiation
- * @return {Promise}
+ * Currently, the only time there should be any ongoing negotiations in the first place is when a user is re-signing players at the end of the season.
  */
 const cancelAll = async () => {
 	await idb.cache.negotiations.clear();
-	await updateStatus("Idle");
-	await updatePlayMenu();
 };
 
 export default cancelAll;

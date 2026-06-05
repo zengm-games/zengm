dadbbf0e21ba43fa1bec0f841db791d9c3b47ab2
Jeremy Scheff <jdscheff@gmail.com>
2026-06-05 10:46:46 -0400

    Hack no longer needed

--- src/ui/util/promiseWorker.ts
+++ src/ui/util/promiseWorker.ts
@@ -5,17 +5,9 @@ const workerPath =
 	process.env.NODE_ENV === "production"
 		? `/gen/worker-${window.bbgmVersion}.js`
 		: "/gen/worker.js";
-let worker: SharedWorker | Worker;
-try {
-	worker = window.useSharedWorker
-		? new SharedWorker(workerPath, { type: "module" })
-		: new Worker(workerPath, { type: "module" });
-} catch {
-	// Chrome <83 has an error when using module type
-	worker = window.useSharedWorker
-		? new SharedWorker(workerPath)
-		: new Worker(workerPath);
-}
+const worker = window.useSharedWorker
+	? new SharedWorker(workerPath, { type: "module" })
+	: new Worker(workerPath, { type: "module" });
 
 export const promiseWorker = new PWBHost(worker);
 promiseWorker.registerError((error) => {

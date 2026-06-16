dea0a7fcdc477ed2966e95b807eabb1bba116a5f
Jeremy Scheff <jdscheff@gmail.com>
2026-06-16 12:25:45 -0400

    Fix

--- src/worker/core/draft/draftLottery.ts
+++ src/worker/core/draft/draftLottery.ts
@@ -374,11 +374,11 @@ export const getDraftLotteryProbs = (
 		}
 	>();
 
-	const bottom3IDs = new Set(result.slice(0, 3).map((t) => t.tid));
+	const bottom3IDs = new Set(result.slice(0, 3).map((t) => t.originalTid));
 	let bottom3Mask = 0n;
 
 	for (let i = 0; i < numTeams; i++) {
-		const isBottom3 = bottom3IDs.has(result[i]!.tid);
+		const isBottom3 = bottom3IDs.has(result[i]!.originalTid);
 		if (isBottom3) {
 			bottom3Mask |= 1n << BigInt(i);
 		}

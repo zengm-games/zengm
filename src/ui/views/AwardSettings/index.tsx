94f4b3236862518169acbf3323170363762d1cf5
Jeremy Scheff <jdscheff@gmail.com>
2026-08-23 21:46:51 -0400

    Always in edit mode

--- src/ui/views/AwardSettings/index.tsx
+++ src/ui/views/AwardSettings/index.tsx
@@ -1,17 +1,14 @@
 import useTitleBar from "../../hooks/useTitleBar.tsx";
-import { helpers } from "../../util/helpers.ts";
 import { MoreLinks } from "../../components/MoreLinks.tsx";
 import type { View } from "../../../common/types.ts";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import { StickyBottomButtons } from "../../components/StickyBottomButtons.tsx";
 import {
 	awardsToEditingState,
 	editingStateToAward,
 	EditSettings,
 	groupAwards,
-	type EditingStateRoot,
 } from "./EditSettings.tsx";
-import { realtimeUpdate } from "../../util/realtimeUpdate.ts";
 import { showNotification } from "../../util/showNotification.ts";
 import { toWorker } from "../../util/toWorker.ts";
 import clsx from "clsx";
@@ -32,41 +29,19 @@ const AwardSettings = ({
 		title: "Award Settings",
 	});
 
-	const [editSettings, setEditSettings] = useState<
-		| {
-				editing: false;
-		  }
-		| {
-				editing: true;
-				awards: EditingStateRoot[];
-		  }
-	>({
-		editing: false,
-	});
-
-	useEffect(() => {
-		if (!editSettings.editing) {
-			setEditSettings((state) => ({
-				editing: true,
-				awards: awardsToEditingState(awardCandidates),
-			}));
-		}
-	}, [awardCandidates, editSettings.editing]);
+	const [awardsState, setAwardsState] = useState(() =>
+		awardsToEditingState(awardCandidates),
+	);
 
 	const [saving, setSaving] = useState(false);
 
 	const saveSettings = async () => {
-		if (!editSettings.editing) {
-			setSaving(false);
-			return;
-		}
-
 		setSaving(true);
 		const awards = [];
 		const errorMessages: string[] = [];
 
 		const seenShortNames = new Set();
-		for (const { editing } of editSettings.awards) {
+		for (const { editing } of awardsState) {
 			if (editing === undefined) {
 				continue;
 			}
@@ -107,33 +82,20 @@ const AwardSettings = ({
 		if (errorMessages.length === 0) {
 			// Do this rather than realtimeUpdate in case the number of awards has changed, like from changing the group setting
 			const newAwards = await toWorker("main", "getAwardCandidates", season);
-			setEditSettings({
-				editing: true,
-				awards: awardsToEditingState(newAwards),
-			});
+			setAwardsState(awardsToEditingState(newAwards));
 		}
 	};
-
-	const actualAwardCandidates = editSettings.editing
-		? editSettings.awards.map((row) => row.raw)
-		: awardCandidates;
 
 	return (
 		<>
 			<MoreLinks type="awards" page="award_settings" season={season} />
 
 			<div className="row" style={{ marginTop: -MARGIN }}>
-				{actualAwardCandidates.map((award, i) => {
+				{awardsState.map(({ raw: award, editing }, i) => {
 					const key = getAwardKey(award);
 
-					const editing =
-						editSettings.editing && editSettings.awards[i]?.editing;
-
 					// When editing, always start a new "group" (team awards for multiple teams, or conf/div groups) on a new row, cause it looks weird to start it in the second column
-					const newRow =
-						editing &&
-						editSettings.awards[i]?.editing &&
-						!editSettings.awards[i - 1]?.editing;
+					const newRow = editing && !awardsState[i - 1]?.editing;
 
 					return (
 						<Fragment key={key}>
@@ -143,87 +105,68 @@ const AwardSettings = ({
 									award.mip ? "col-12 col-lg-9" : "col-12 col-lg-6",
 
 									// Align to bottom when editing if this is a trailing part of a group, looks better that way
-									editSettings.editing && !editing
-										? "d-flex align-items-end"
-										: undefined,
+									!editing ? "d-flex align-items-end" : undefined,
 								)}
 								style={{ marginTop: MARGIN }}
 							>
-								{editSettings.editing ? (
+								{editing ? (
 									<div>
-										{editSettings.awards[i]?.editing ? (
-											<EditSettings
-												canMoveDown={editSettings.awards.some((award, j) => {
-													// editing undefined check is for group conf/div where there may be other entries of the same editing award
-													return j > i && award.editing !== undefined;
-												})}
-												canMoveUp={i > 0}
-												move={(direction) => {
-													setEditSettings((oldState) => {
-														if (!oldState.editing) {
-															return oldState;
-														}
+										<EditSettings
+											canMoveDown={awardsState.some((award, j) => {
+												// editing undefined check is for group conf/div where there may be other entries of the same editing award
+												return j > i && award.editing !== undefined;
+											})}
+											canMoveUp={i > 0}
+											move={(direction) => {
+												setAwardsState((oldState) => {
+													const grouped = groupAwards(oldState);
+													const toMoveIndex = grouped.findIndex(
+														(group) => group[0]?.raw === award,
+													);
+													const toMove = grouped[toMoveIndex];
+													const otherIndex = toMoveIndex + direction;
+													if (!toMove || !grouped[otherIndex]) {
+														return oldState;
+													}
+													grouped[toMoveIndex] = grouped[otherIndex];
+													grouped[otherIndex] = toMove;
 
-														const grouped = groupAwards(oldState.awards);
-														const toMoveIndex = grouped.findIndex(
-															(group) => group[0]?.raw === award,
-														);
-														const toMove = grouped[toMoveIndex];
-														const otherIndex = toMoveIndex + direction;
-														if (!toMove || !grouped[otherIndex]) {
-															return oldState;
-														}
-														grouped[toMoveIndex] = grouped[otherIndex];
-														grouped[otherIndex] = toMove;
+													return {
+														...oldState,
+														awards: grouped.flat(),
+													};
+												});
+											}}
+											numGamesPlayoffSeries={numGamesPlayoffSeries}
+											playoffsByConf={playoffsByConf}
+											remove={() => {
+												setAwardsState((oldState) => {
+													const grouped = groupAwards(oldState).filter(
+														(group) => {
+															return group[0]?.raw !== award;
+														},
+													);
 
-														return {
-															...oldState,
-															awards: grouped.flat(),
-														};
-													});
-												}}
-												numGamesPlayoffSeries={numGamesPlayoffSeries}
-												playoffsByConf={playoffsByConf}
-												remove={() => {
-													setEditSettings((oldState) => {
-														if (!oldState.editing) {
-															return oldState;
-														}
-
-														const grouped = groupAwards(oldState.awards).filter(
-															(group) => {
-																return group[0]?.raw !== award;
-															},
-														);
-
-														return {
-															...oldState,
-															awards: grouped.flat(),
-														};
-													});
-												}}
-												setState={(state) => {
-													setEditSettings((oldState) => {
-														return oldState.editing
+													return {
+														...oldState,
+														awards: grouped.flat(),
+													};
+												});
+											}}
+											setState={(state) => {
+												setAwardsState((oldState) => {
+													return oldState.map((award, j) => {
+														return i === j
 															? {
-																	...oldState,
-																	awards: oldState.awards.map((award, j) => {
-																		return i === j
-																			? {
-																					raw: award.raw,
-																					editing: state,
-																				}
-																			: award;
-																	}),
+																	raw: award.raw,
+																	editing: state,
 																}
-															: {
-																	editing: false,
-																};
+															: award;
 													});
-												}}
-												state={editSettings.awards[i].editing}
-											/>
-										) : null}
+												});
+											}}
+											state={editing}
+										/>
 									</div>
 								) : null}
 								<AwardRaceTable
@@ -238,21 +181,6 @@ const AwardSettings = ({
 			</div>
 
 			<StickyBottomButtons>
-				<button
-					className="btn btn-secondary ms-auto me-2"
-					disabled={saving}
-					onClick={async () => {
-						await realtimeUpdate([], helpers.leagueUrl(["award_races"]));
-						setEditSettings((oldState) => {
-							return {
-								...oldState,
-								editing: false,
-							};
-						});
-					}}
-				>
-					Cancel
-				</button>
 				<button
 					className="btn btn-primary me-2"
 					disabled={saving}

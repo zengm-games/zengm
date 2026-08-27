6439da57bfdeff462eebf47d6b4f3e2d7aebd485
Jeremy Scheff <jdscheff@gmail.com>
2026-08-26 20:28:26 -0400

    Use last season's stats when 0 GP

--- src/worker/views/awardSettings.ts
+++ src/worker/views/awardSettings.ts
@@ -8,6 +8,19 @@ import { PHASE } from "../../common/constants.ts";
 import getPlayoffsByConf from "../core/season/getPlayoffsByConf.ts";
 import { defaultAwards } from "../../common/defaultGameAttributes.ts";
 
+const getTeams = async (season: number) => {
+	const teams = await idb.getCopies.teamsPlus(
+		{
+			attrs: ["tid"],
+			seasonAttrs: ["won", "lost", "tied", "otl"],
+			season,
+		},
+		"noCopyCache",
+	);
+
+	return teams;
+};
+
 const updateAwardSettings = async (
 	inputs: unknown,
 	updateEvents: UpdateEvents,
@@ -16,21 +29,37 @@ const updateAwardSettings = async (
 		// In theory could update on gameSim and playerMovement, but it's actually tricky to keep editing state in sync so save it for later
 		updateEvents.includes("firstRun")
 	) {
-		const season =
-			actualPhase() === PHASE.PRESEASON ? g.get("season") - 1 : g.get("season");
+		let season;
+		let teams;
+		const phase = actualPhase();
+		if (phase === PHASE.PRESEASON) {
+			season = g.get("season") - 1;
+		} else if (phase === PHASE.REGULAR_SEASON) {
+			// See if we have 0 GP in the regular season so far
+			const teamsTemp = await getTeams(g.get("season"));
+			if (
+				teamsTemp.every(
+					(t) =>
+						t.seasonAttrs.won === 0 &&
+						t.seasonAttrs.lost === 0 &&
+						t.seasonAttrs.tied === 0 &&
+						t.seasonAttrs.otl === 0,
+				)
+			) {
+				season = g.get("season") - 1;
+			} else {
+				season = g.get("season");
+				teams = teamsTemp;
+			}
+		} else {
+			season = g.get("season");
+		}
 		const { awardCandidates, errorMessages } = await getAwardCandidates(
 			season,
 			g.get("awards"),
 		);
 
-		const teams = await idb.getCopies.teamsPlus(
-			{
-				attrs: ["tid"],
-				seasonAttrs: ["won", "lost", "tied", "otl"],
-				season,
-			},
-			"noCopyCache",
-		);
+		teams ??= await getTeams(season);
 
 		const mvp = defaultAwards.mvp;
 		const baseNewAward: (typeof awardCandidates)[number][number] = {

a48b1116dbe6947f219bd72364714d679e294f4e
Jeremy Scheff <jdscheff@gmail.com>
2026-09-05 21:40:20 -0400

    Show Finals MVP or Playoffs MVP first in the HistoryAll table, so it's next to championship winner

--- src/worker/views/historyAll.ts
+++ src/worker/views/historyAll.ts
@@ -2,7 +2,7 @@ import { PHASE } from "../../common/constants.ts";
 import { idb } from "../db/index.ts";
 import { g } from "../util/index.ts";
 import type { UpdateEvents } from "../../common/types.ts";
-import { groupByUnique, last, range } from "../../common/utils.ts";
+import { groupByUnique, last, orderBy, range } from "../../common/utils.ts";
 import { formatAwardNamePrefix } from "../core/awards/prefixes.ts";
 import { bySport } from "../../common/sportFunctions.ts";
 import { PlayersCache } from "../db/PlayersCache.ts";
@@ -152,9 +152,13 @@ const updateHistory = async (inputs: unknown, updateEvents: UpdateEvents) => {
 					tid: number;
 				}[];
 				if (a) {
+					// Move Finals MVP or Playoffs MVP to the front, so it's next to the championship winer
+					const awardsSorted = orderBy(a.awards, (award) =>
+						award.statRange === -1 ? 0 : award.statRange === "playoffs" ? 1 : 2,
+					);
 					awards = (
 						await Promise.all(
-							a.awards
+							awardsSorted
 								.filter((award) => {
 									// Only want individual awards
 									return award.numTeams === undefined;

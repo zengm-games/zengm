d4728b582041acabbedfef5b33e618a05d6915ff
Jeremy Scheff <jdscheff@gmail.com>
2026-09-09 20:33:25 -0400

    Reload if shared worker is missing

--- src/ui/util/promiseWorker.ts
+++ src/ui/util/promiseWorker.ts
@@ -10,9 +10,23 @@ const worker = window.useSharedWorker
 	: new Worker(workerPath, { type: "module" });
 
 export const promiseWorker = new PWBHost(worker);
-promiseWorker.registerError((error) => {
+
+promiseWorker.addEventListener("error", ({ error }) => {
 	Bugsnag.notify(error);
 
 	console.error("Error from worker:");
 	console.error(error);
+});
+
+// As of Chrome 152 (and possibly somewhat earlier versions), Chrome seems to kill a shared worker processe if it's idle for like 30 minutes, while not doing anything to the main process in the tab (no freeze/discard). This leaves no visible error message, but makes the entire app non functional. This code listens for the worker being killed and schedules a reload for the next time the page becomes visible. In theory could reactivate the worker instead, but state could be in a weird place
+promiseWorker.addEventListener("close", () => {
+	if (document.visibilityState === "visible") {
+		window.location.reload();
+	} else {
+		document.addEventListener("visibilitychange", () => {
+			if (document.visibilityState === "visible") {
+				window.location.reload();
+			}
+		});
+	}
 });

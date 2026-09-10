3ca979f0734ec12ba369cf4f7a191667d1b117ba
Jeremy Scheff <jdscheff@gmail.com>
2026-09-10 11:30:00 -0400

    Make more robust

--- tools/pre-test.ts
+++ tools/pre-test.ts
@@ -1,7 +1,24 @@
 import { existsSync } from "node:fs";
+import { readFile } from "node:fs/promises";
 
-if (!existsSync("build/files/league-schema.json")) {
+const FILENAME = "build/files/league-schema.json";
+
+const makeFile = async () => {
 	const { createJsonSchemaFile } =
 		await import("./build/createJsonSchemaFile.ts");
 	await createJsonSchemaFile("test");
+};
+
+if (!existsSync(FILENAME)) {
+	console.log("[pre-test] No league-schema.json found, creating...");
+	await makeFile();
+} else {
+	const text = await readFile(FILENAME, "utf8");
+	try {
+		JSON.parse(text);
+	} catch {
+		// Invalid JSON in file somehow
+		console.log("[pre-test] Invalid league-schema.json found, replacing...");
+		await makeFile();
+	}
 }

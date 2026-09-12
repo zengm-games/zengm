e4d0825c696e41a3b9b08d1374ea25d2afcde177
Jeremy Scheff <jdscheff@gmail.com>
2026-09-11 22:33:21 -0400

    Improve performance a bit

--- src/worker/util/advStatsSave.ts
+++ src/worker/util/advStatsSave.ts
@@ -1,31 +1,31 @@
 import { idb } from "../db/index.ts";
 import type { Player } from "../../common/types.ts";
+import { groupByUnique } from "../../common/utils.ts";
 
 const advStatsSave = async (
 	players: any[],
 	playersRaw: Player[],
 	updatedStats: Record<string, number[] | number[][]>,
 ) => {
+	const playersByPid = groupByUnique(playersRaw, (p) => p.pid);
 	const keys = Object.keys(updatedStats);
-	await Promise.all(
-		players.map(async ({ pid }, i) => {
-			const p = playersRaw.find((p2) => p2.pid === pid);
+	for (const [i, { pid }] of players.entries()) {
+		const p = playersByPid[pid];
 
-			if (p) {
-				const ps = p.stats.at(-1);
+		if (p) {
+			const ps = p.stats.at(-1);
 
-				if (ps) {
-					for (const key of keys) {
-						if (!Number.isNaN(updatedStats[key]![i])) {
-							ps[key] = updatedStats[key]![i];
-						}
+			if (ps) {
+				for (const key of keys) {
+					if (!Number.isNaN(updatedStats[key]![i])) {
+						ps[key] = updatedStats[key]![i];
 					}
-
-					await idb.cache.players.put(p);
 				}
+
+				await idb.cache.players.put(p);
 			}
-		}),
-	);
+		}
+	}
 };
 
 export default advStatsSave;

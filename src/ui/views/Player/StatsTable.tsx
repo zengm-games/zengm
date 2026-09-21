69eec11bc4c92bf52226fa3748186689937486d0
Jeremy Scheff <jdscheff@gmail.com>
2026-09-21 19:58:53 -0400

    Only show awards once if TOT

--- src/ui/views/Player/StatsTable.tsx
+++ src/ui/views/Player/StatsTable.tsx
@@ -319,10 +319,12 @@ export const StatsTable = ({
 						{formatStatGameHigh(ps, stat)}
 					</MaybeBold>
 				)),
-				wrappedSeasonAwards({
-					awards: awardsBySeason.get(ps.season),
-					season: ps.season,
-				}),
+				ps.hasTot
+					? undefined
+					: wrappedSeasonAwards({
+							awards: awardsBySeason.get(ps.season),
+							season: ps.season,
+						}),
 			],
 			classNames: className,
 		});
